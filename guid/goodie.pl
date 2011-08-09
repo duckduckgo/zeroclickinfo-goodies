cee6ce531ffd69e98b56fe4e08d66518e2e62dac
Duck Duck Go <duckduckgo@gmail.com>
2011-08-09 13:40:16 -0400

    updated guid

--- guid/goodie.pl
+++ guid/goodie.pl
@@ -3,8 +3,36 @@
 use strict;
 use warnings;
 
-use Data::GUID;
+my $q_check_lc = 'guid';
+my $answer_results = '';
+my $answer_type = 'guid';
 
-if (my $guid = Data::GUID->new) {
-    print "{$guid}\n";
+my %guid = (
+    'guid' => 0,
+    'uuid' => 1,
+    'globally unique identifier' => 0,
+    'universally unique identifier' => 1,
+    'rfc 4122' => 0,
+    );
+
+if (exists $guid{$q_check_lc}) {
+
+    # For debugging.
+#    warn $q_check_lc;
+
+    use Data::GUID;
+
+    if (my $guid = Data::GUID->new) {
+	if ($guid{$q_check_lc}) {
+	    $guid = lc $guid;
+	} else {
+	    $guid = qq({$guid});
+	}
+	
+	$answer_results = $guid;
+	$answer_type = 'guid';
+    }
 }
+
+
+print qq($answer_type\t$answer_results\n);
