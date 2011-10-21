231684385562abe8b30a01292ec6019b6226c2b4
Me <mike@bazaar.bz>
2011-10-21 10:48:47 -0600

    fixed bug in abc.pl

--- abc/abc.pl
+++ abc/abc.pl
@@ -15,12 +15,15 @@ if ( $q_check =~ m/^\!?\s*[A-Za-z]+(\s+or\s+[A-Za-z]+)+\s*$/ ) {
     my @choices = split(/\s+or\s+/, $q_check);
     my $choice = int(rand(@choices));
 
-    if ( my @duck = grep { $_ eq 'duckduckgo' or $_ eq 'duck' or $_ eq 'ddg' } @choices ) { $answer_results = $duck[0].' (not random)' }
+    if ( my @duck = grep { $_ eq 'duckduckgo' or $_ eq 'duck' or $_ eq 'ddg' } @choices ) { 
+        $answer_results = $duck[0].' (not random)';
+        $answer_type = 'egg';
+    }
     else {
         $answer_results = $choices[$choice];
         $answer_results .= ' (random)';
+        $answer_type = 'rand';
     }
-    $answer_type = 'rand';
 }
 
 print qq($answer_type\t$answer_results\n);
