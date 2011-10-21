18cc1eb6eb88ddb3ee0ff3970d6d7cd540f59288
Torsten Raudssus <github@raudssus.de>
2011-10-21 20:16:01 +0200

    Added -a option to test with all goodies

--- goodie-test.pl
+++ goodie-test.pl
@@ -7,22 +7,19 @@ use Getopt::Std;
 
 use Data::Dumper;
 
+my $queries_filename = 'queries.txt';
+my $goodie_filename = 'goodie.pl';
+
 my %opts;
 
-getopts('f:t',\%opts);
+getopts('f:ta',\%opts);
 
 if ($opts{f} and $opts{t}) {
 	print "Please just use -f or -t not both at once\n";
 	exit 1;
 }
 
-my $goodie = shift @ARGV;
-
-if (!$goodie) {
-	print "Please give a name of the goodie you want to test!\n";
-	exit 1;
-}
-
+my @goodies;
 my @queries;
 
 if (@ARGV) {
@@ -35,51 +32,83 @@ if (@ARGV) {
 	}
 	close FILE;
 } elsif ($opts{t}) {
-	my $test_query_file = $Bin."/".$goodie."/queries.txt";
-	open FILE, $test_query_file or die "Couldn't open file ".$test_query_file.": $!"; 
-	for (<FILE>) {
-		chomp;
-		push @queries, $_;
+}
+
+if ($opts{a}) {
+	chdir($Bin);
+	for (<*>) {
+		if (-d $_) {
+			if (@queries) {
+				push @goodies, $_;
+			} elsif (-r $Bin."/".$_."/".$queries_filename) {
+				push @goodies, $_;
+			}
+		}
 	}
-	close FILE;
 } else {
-	print "Please give a query to test ".$goodie." goodie or a file with queries via -f or make execute the goodie testset with -t!\n";
-	exit 1;
+	my $argv_goodie = shift @ARGV;
+	if (!$argv_goodie) {
+		print "Please give a name of the goodie you want to test or use -a to test all!\n";
+		exit 1;
+	}
+	if (!@queries) {
+		print "Please give a query to test ".$argv_goodie." goodie or a file with queries via -f or make execute the goodie testset with -t!\n";
+		exit 1;
+	}
+	push @goodies, $argv_goodie;
 }
 
-my $filename = $Bin."/".$goodie."/goodie.pl";
+for my $goodie (@goodies) {
 
-if (!-r $filename) {
-	print "Can't find ".$filename."!";
-	exit 1;
-}
+	my $filename = $Bin."/".$goodie."/".$goodie_filename;
 
-open FILE, $filename or die "Couldn't open file: $!"; 
-my $code = join("", <FILE>); 
-close FILE;
+	if (!-r $filename) {
+		print "Can't find ".$filename."!";
+		exit 1;
+	}
 
-for (@queries) {
-	my $q_check = $_;
-	my $q_internal = '';
+	open FILE, $filename or die "Couldn't open file: $!"; 
+	my $code = join("", <FILE>); 
+	close FILE;
 
-	my $answer_results;
-	my $answer_type;
-
-	eval $code;
+	my @goodie_queries;
 	
+	if (@queries) {
+		@goodie_queries = @queries;
+	} elsif ($opts{t} || $opts{a}) {
+		my $test_query_file = $Bin."/".$goodie."/".$queries_filename;
+		open FILE, $test_query_file or die "Couldn't open file ".$test_query_file.": $!"; 
+		for (<FILE>) {
+			chomp;
+			push @goodie_queries, $_;
+		}
+		close FILE;
+	}
+	
+	for (@goodie_queries) {
+		my $q_check = $_;
+		my $q_internal = '';
+
+		my $answer_results;
+		my $answer_type;
+
+		eval $code;
+		
+		print "\n";
+
+		if ($answer_results and !$answer_type) {
+			print 'The goodie doesnt set $answer_type, but has $answer_results on "'.$_.'". You must give back $answer_results!'."\n";
+		} 
+
+		if ($answer_results and $answer_type) {
+			print 'The goodie '.$goodie.' gives back the following on the query "'.$q_check.'":'."\n";
+			print 'Answer Type: '.$answer_type."\n";
+			print 'Answer Type: '.$answer_results."\n";
+		} else {
+			print 'The goodie '.$goodie.' gave no result on "'.$q_check.'"!'."\n";
+		}
+	}
+
 	print "\n";
 
-	if ($answer_results and !$answer_type) {
-		print 'The goodie doesnt set $answer_type, but has $answer_results on "'.$_.'". You must give back $answer_results!'."\n";
-	} 
-
-	if ($answer_results and $answer_type) {
-		print 'The goodie '.$goodie.' gives back the following on the query "'.$q_check.'":'."\n";
-		print 'Answer Type: '.$answer_type."\n";
-		print 'Answer Type: '.$answer_results."\n";
-	} else {
-		print 'The goodie '.$goodie.' gave no result on "'.$q_check.'"!'."\n";
-	}
 }
-
-print "\n";
