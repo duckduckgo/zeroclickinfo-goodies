fa3ec5f1725808147e4fad9db4d08ffead64f831
Gabriel Weinberg <yegg@caerus.duckduckgo.com>
2011-10-21 09:40:23 -0400

    integrate dice goodie

--- dice/goodie.pl
+++ dice/goodie.pl
@@ -6,11 +6,11 @@ use warnings;
 
 my $q_check_lc = 'roll 5 dice';
 my $answer_results = '';
-my $answer_type = 'dice_side';
-my $choices = 6;  # To be replace with input string in the future
+my $answer_type = '';
 
 if ( $q_check_lc =~ m/^(?:roll|throw)(?:\sdie|(\d{0,2}\s)*dice)$/ ) {
     my $rolls = 1;  # If "die" is entered
+    my $choices = 6;  # To be replace with input string in the future
 
     if (defined($1)) {    # Not defined if number of "dice" not specified
         if ($1 eq " ") {
@@ -27,5 +27,7 @@ if ( $q_check_lc =~ m/^(?:roll|throw)(?:\sdie|(\d{0,2}\s)*dice)$/ ) {
         $answer_results .= " $roll";
     }
 
-    print qq($answer_type\t$answer_results\n);
+    $answer_type = 'dice';
 }
+
+print qq($answer_type\t$answer_results\n);
