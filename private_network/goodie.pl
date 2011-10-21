0805f0e8d476e5803b1a016fa87c8cac27f2d1c6
Gabriel Weinberg <yegg@caerus.duckduckgo.com>
2011-10-21 09:52:38 -0400

    integrate private network

---
+++ private_network/goodie.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $q_check_lc = 'private network';
+my $answer_results = '';
+my $answer_type = '';
+my $type = '';
+
+my %private_network = map { $_ => undef } (
+    'private network',
+    'private network address',
+    'private network addresses',
+    'private ip',
+    'private ips',
+    'private ip addresses',
+    'private ip address',
+    );
+
+
+if ($type ne 'E' && exists $private_network{$q_check_lc}) {
+
+    open(IN,"<privatenetwork.html");
+    while (my $line = <IN>) {
+	$answer_results .= $line;
+    }
+    close(IN);
+
+    if ($answer_results) {
+	$answer_type = 'network';
+	$type = 'E';
+    }
+}
+
+
+print qq($answer_type\t$answer_results\n);
