07aabd023e404230ce410ca86ef5f2995d539629
Eirik Sand <eiriksand@gmail.com>
2011-11-04 09:38:30 +0100

    Changed alternative 'of' regex from (?:of\s)? to (?:of\s|)

--- perimeter/goodie.pl
+++ perimeter/goodie.pl
@@ -2,10 +2,10 @@
 # regular octagon or the circumference of a circle with given specified edges/radius.
 use Math::Trig;
 
-if (!$type && $q_check_lc =~ m/^(perimeter) (?:of\s)?(?:(rectangle) (\d+(\.\d+)?)\s(\d+(\.\d+)?))$/ ||
- $q_check_lc =~ m/^(perimeter) (?:of\s)?(?:(triangle) (\d+(\.\d+)?)\s(\d+(\.\d+)?)\s(\d+(\.\d+)?))$/ ||
- $q_check_lc =~ m/^(perimeter) (?:of\s)?(?:(square|circle|pentagon|hexagon|octagon) (\d+(\.\d+)?))$/ ||
- $q_check_lc =~ m/^(circumference) (?:(?:of\s)?circle\s)?(\d+(\.\d+)?)$/) {
+if (!$type && $q_check_lc =~ m/^(perimeter) (?:of\s|)(?:(rectangle) (\d+(\.\d+)?)\s(\d+(\.\d+)?))$/ ||
+ $q_check_lc =~ m/^(perimeter) (?:of\s|)(?:(triangle) (\d+(\.\d+)?)\s(\d+(\.\d+)?)\s(\d+(\.\d+)?))$/ ||
+ $q_check_lc =~ m/^(perimeter) (?:of\s|)(?:(square|circle|pentagon|hexagon|octagon) (\d+(\.\d+)?))$/ ||
+ $q_check_lc =~ m/^(circumference) (?:(?:of\s|)circle\s)?(\d+(\.\d+)?)$/) {
 	my %polygons = ("pentagon" => 5, "hexagon" => 6, "octagon" => 8);
     my $shape = $1 eq "circumference" ? "circle" : $2;
     my $answerPrefix = "Perimeter of $shape: ";
