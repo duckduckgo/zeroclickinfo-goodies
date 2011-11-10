839c33ad962643e86d74232bd308febd18ca6052
Travis Williams <yodatravis@gmail.com>
2011-11-09 21:05:14 -0500

    fix regex and usage syntax

--- em_to_px/goodie.pl
+++ em_to_px/goodie.pl
@@ -1,20 +1,21 @@
 # Calculates em or px when given the other.
 
-if (!$type && $q =~ m/^em to px (?:(?:(\?)\s*:\s*(\d+(?:\.\d+)?))|(?:(\d+(?:\.\d+)?)\s*:\s*(\?)))$/i) {
+#if (!$type && $q =~ m/^em to px (?:(?:(\?)\s*:\s*(\d+(?:\.\d+)?))|(?:(\d+(?:\.\d+)?)\s*:\s*(\?)))$/i) {
+if (!$type && $q =~ m/^(\d+[.]\d*|\d*[.]\d+|\d+)\s*(em|px)\s+(in|to)\s+(em|px)$/i) {
   my $result = 0;
 
-  if ($1 && $1 eq "?") {
-    # ? : x
-    $result = $2 / 16;
-    $answer_results = "$result : $2";
-  } elsif ($4 && $4 eq "?") {
-    # x : ?
-    $result = $3 * 16;
-    $answer_results = "$3 : $result";
+  if ($2 && $2 eq "px") {
+    # ? px in em 
+    $result = $1 / 16;
+    $answer_results = "$result em in $1 px";
+  } elsif ($2 && $2 eq "em") {
+      # ? em in px
+    $result = $1 * 16;
+    $answer_results = "$result px in $1 em";
   }
 
   if ($answer_results) {
-      $answer_results = qq(em to px: $answer_results);
+      $answer_results = qq($answer_results);
       $answer_type = "em_to_px";
   }
 }
