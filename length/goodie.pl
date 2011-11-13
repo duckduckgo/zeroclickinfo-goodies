660c43cea92edd1e7fe15890f9dcb7c577fdbca7
Gabriel Weinberg <yegg@caerus.duckduckgo.com>
2011-11-13 13:21:44 -0500

    integrate length

--- length/goodie.pl
+++ length/goodie.pl
@@ -2,10 +2,15 @@
 # Outputs the numerical length of the query.
 
 if (!$type && $q_check =~ m/^length (.+)$/i) {
-      {   
-        use Encode;
-        $answer_results = length decode_utf8($1);
-      }
+
+    # Encode isn't necessary since the query is already unicode,
+    # and in a character setting.
+#      {   
+#        use Encode;
+#        $answer_results = length decode_utf8($1);
+#      }
+        $answer_results = length $1;
+
         if ($answer_results) {
                 $answer_results = qq(Length: $answer_results);
                 $answer_type = 'length';
