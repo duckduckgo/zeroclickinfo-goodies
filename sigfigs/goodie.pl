55c0d9ae3f659339e44d64828fbb0292daac3ec8
hunterlang <hunterlang@comcast.net>
2012-01-20 23:47:39 -0500

    simpler and faster rewrite

--- sigfigs/goodie.pl
+++ sigfigs/goodie.pl
@@ -1,31 +1,30 @@
 if ($type ne 'E' && $q_check_lc =~ m/^(?:sig(?:figs|digs)|significant (?:figures|digits)|sf|sd) (-?\d+(?:\.(?:\d+)?)?)$/i) {
     my $string = $1;
     $string =~ s/-//;
-    # if there's no decimal place
-    if (index($string, '.') eq -1) {
-	# remove trailing and leading zeros
-	$string =~ s/\.?0*$//;
-	$string =~ s/^0+//;
-	# get the length, which is the sigfig value.
-	my $len = length $string;
-	$answer_results = qq($len);
-    }
-    # there is a decimal
-    else {
-	$string =~ s/^0+//;
-	my @zer = split("\\.", $string);
-	my $len = 0;
-	# no integers to the left
-	if (length($zer[0]) eq 0) {
-	    $zer[1] =~ s/^0+//;
-	    $len = length $zer[1];
+    $string =~ s/^0+//;
+    my @arr = split('\\.', $string);
+    my $v = @arr;
+    my $len = 0;
+    # there's a decimal
+    unless ($v eq 1) {
+	# the string doesn't have integers on the left
+	# this means we can strip the leading zeros on the right
+	if ($string < 1) {
+	    $arr[1] =~ s/^0+//;
+	    $len = length $arr[1];
 	}
-	# there are integers to the left
+	#there are integers on the left
 	else {
-	    $len = length($zer[0]) + length($zer[1]);
+	    $len = length($arr[0]) + length($arr[1]);
 	}
-	$answer_results = qq(Significant figures: $len);
     }
+    # no decimal
+    else {
+	# lose the trailing zeros and count
+	$string =~ s/\.?0*$//;
+	$len = length $string;
+    }
+    $answer_results = qq(Significant figures: $len);
     if ($answer_results) {
 	$answer_type = 'sigfigs';
 	$type = 'E';
