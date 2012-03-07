dc61d36aa2d37a0db4126c5000ff4a33f7654d78
Zaahir Moolla <moollaza@gmail.com>
2012-03-06 23:32:04 -0500

    renamed SentenceCase to TitleCase

--- lib/DDG/Goodie/SentenceCase.pm
+++ lib/DDG/Goodie/SentenceCase.pm
@@ -1,10 +1,10 @@
-package DDG::Goodie::SentenceCase;
+package DDG::Goodie::TitleCase;
 
 use DDG::Goodie;
 
 zci is_cached => 1;
 
-triggers startend => 'sentence-case';
+triggers startend => 'title-case';
 
 handle remainder => sub { join(' ', map { ucfirst $_ } split(/ /, $_))};
 
