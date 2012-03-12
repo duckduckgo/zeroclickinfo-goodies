f40b21adae31462203b6e348975ce5ea459ea4f5
Torsten Raudssus <github@raudssus.de>
2012-03-12 11:10:09 +0100

    Small renaming

---
+++ lib/DDG/GoodieBundle/OpenSourceDuckDuckGo.pm
@@ -0,0 +1,6 @@
+package DDG::GoodieBundle::OpenSourceDuckDuckGo;
+# ABSTRACT: The open source Goodie Bundle of DuckDuckGo
+
+# This package is only a namespace/version holder
+
+1;
--- lib/DDG/Goodies/OpenSourceDuckDuckGo.pm
+++
@@ -1,6 +0,0 @@
-package DDG::Goodies::OpenSourceDuckDuckGo;
-# ABSTRACT: The open source Goodies of DuckDuckGo
-
-# This package is only a namespace/version holder
-
-1;
