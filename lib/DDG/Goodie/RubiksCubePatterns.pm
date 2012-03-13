3b1eae552ad4233d767e53eb9f5ce47c2c10b275
Robert Picard <rpicard@live.com>
2012-03-13 18:30:11 -0400

    make the comparison case insensitive

--- lib/DDG/Goodie/RubiksCubePatterns.pm
+++ lib/DDG/Goodie/RubiksCubePatterns.pm
@@ -9,9 +9,9 @@ triggers start => "rcube";
 zci is_cached => 1;
 
 handle remainder => sub {
-	return "F U F R L2 B D' R D2 L D' B R2 L F U F" if $_ eq "stripes";
-	return "F L F U' R U F2 L2 U' L' B D' B' L2 U" if $_ eq "cube in a cube";
-	return "U D' R L' F B' U D'" if $_ eq "swap centers";
+	return "F U F R L2 B D' R D2 L D' B R2 L F U F" if lc($_) eq "stripes";
+	return "F L F U' R U F2 L2 U' L' B D' B' L2 U" if lc($_) eq "cube in a cube";
+	return "U D' R L' F B' U D'" if lc($_) eq "swap centers";
 	return;
 };
 1;
