c5f4d660be71bd5091476215c295aca0573e923f
Robert Picard <rpicard@live.com>
2012-03-13 16:32:15 -0400

    add swap centers pattern

--- lib/DDG/Goodie/CubePatterns.pm
+++ lib/DDG/Goodie/CubePatterns.pm
@@ -9,6 +9,7 @@ zci is_cached => 1;
 handle remainder => sub {
 	return "F U F R L2 B D' R D2 L D' B R2 L F U F" if $_ eq "stripes";
 	return "F L F U' R U F2 L2 U' L' B D' B' L2 U" if $_ eq "in a cube";
+	return "U D' R L' F B' U D'" if $_ eq "swap centers";
 	return;
 };
 1;
