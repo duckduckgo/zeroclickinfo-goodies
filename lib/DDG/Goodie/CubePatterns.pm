b87896644d000236cd284a269b5045c211dbe604
Robert Picard <rpicard@live.com>
2012-03-13 16:10:27 -0400

    changing code format

--- lib/DDG/Goodie/CubePatterns.pm
+++ lib/DDG/Goodie/CubePatterns.pm
@@ -7,14 +7,8 @@ triggers start => "cube";
 zci is_cached => 1;
 
 handle remainder => sub {
-	if ($_ eq "stripes")
-	{
-		return "F U F R L2 B D' R D2 L D' B R2 L F U F";
-	}
-	elsif ($_ eq "in a cube")
-	{
-		return "F L F U' R U F2 L2 U' L' B D' B' L2 U";
-	}
+	return "F U F R L2 B D' R D2 L D' B R2 L F U F" if $_ eq "stripes";
+	return "F L F U' R U F2 L2 U' L' B D' B' L2 U" if $_ eq "in a cube";
 	return;
 };
 1;
