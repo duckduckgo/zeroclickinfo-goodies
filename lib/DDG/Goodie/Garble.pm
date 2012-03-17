5ef4a2676bfed11c9ca254b1365dae1861552ca9
Derrick Schwabe <admin@mrschwabe.com>
2012-03-17 13:04:42 -0500

    Added relatively pointless garble plugin

---
+++ lib/DDG/Goodie/Garble.pm
@@ -0,0 +1,21 @@
+package DDG::Goodie::Garble;
+# ABSTRACT: Take a query and spit it out randomly.
+
+use DDG::Goodie;
+use List::Util 'shuffle'; 
+
+triggers start => "garble";
+
+handle remainder => sub {
+  $_ =~ s/\s//g; #remove spaces
+  @chars = split(//, $_); #convert each character of the query to an array element
+  @garbledChars = shuffle(@chars); #randomly reorder the array
+  $garbledAnswer = join('',@garbledChars); #convert array to string
+  return $garbledAnswer; 
+};
+
+zci is_cached => 1;
+
+1;
+  
+  
