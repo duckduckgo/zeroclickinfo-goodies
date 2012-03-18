9970738e01b1256634f7356353f6492168b9410c
Hunter Lang <hunter@duckduckgo.com>
2012-03-18 19:16:16 -0400

    expand trigger coverage, CamelCase anwswer_type

--- lib/DDG/Goodie/PercentError.pm
+++ lib/DDG/Goodie/PercentError.pm
@@ -2,13 +2,13 @@ package DDG::Goodie::PercentError;
 
 use DDG::Goodie;
 
-triggers start => "percent", "%err", "percent-error", "% error";
+triggers start => "percent", "%", "percent-error";
 
-zci answer_type => "percent-error";
+zci answer_type => "PercentError";
 
 handle query_parts => sub {
     shift;
-    shift if @_[0] eq 'error' || @_[0] eq 'err';
+    shift if @_[0] =~ /(?: |-|)error/ || @_[0] =~ /(?: |-|)err/;
 
     my $length = @_;
     print "$length\n";
@@ -17,6 +17,8 @@ handle query_parts => sub {
     my ( $acc, $exp ) = @_;
     $acc =~ s/[{},;\s]+//g;
     $exp =~ s/[{},;\s]+//g;
+    
+    return if $acc =~ m/[^0-9\.\-]/ || $exp =~ m/[^0-9\.\-]/;
 
     my $diff = abs $acc - $exp;
     my $err = abs ($diff/$acc*100);
