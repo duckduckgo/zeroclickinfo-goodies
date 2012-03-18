9b8b94da9a003ec8de20a15f7342634b9ced0718
Torsten Raudssus <github@raudssus.de>
2012-03-19 00:20:58 +0100

    Ok why i see this crap just 5 sec. after the release?

--- lib/DDG/Goodie/PrivateNetwork.pm
+++ lib/DDG/Goodie/PrivateNetwork.pm
@@ -11,8 +11,8 @@ zci is_cached => 1;
 zci answer_type => 'network';
 
 handle sub {
-	io(dist_dir('DDG-GoodieBundle-OpenSourceDuckDuckGo').'/privatenetwork/privatenetwork.txt')->slurp,
-	scalar io(dist_dir('DDG-GoodieBundle-OpenSourceDuckDuckGo').'/privatenetwork/privatenetwork.html')->slurp
+	scalar io(dist_dir('DDG-GoodieBundle-OpenSourceDuckDuckGo').'/privatenetwork/privatenetwork.txt')->slurp,
+	html => scalar io(dist_dir('DDG-GoodieBundle-OpenSourceDuckDuckGo').'/privatenetwork/privatenetwork.html')->slurp
 };
 
 1;
