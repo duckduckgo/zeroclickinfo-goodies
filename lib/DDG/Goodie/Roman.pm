8d3680a9096fe439dd3a9114fba1c9c45e76484c
hunterlang <hunterlang@comcast.net>
2012-04-03 14:55:26 -0400

    fixed arabic

--- lib/DDG/Goodie/Roman.pm
+++ lib/DDG/Goodie/Roman.pm
@@ -9,7 +9,7 @@ zci is_cached => 1;
 zci answer_type => "roman_numeral_conversion";
 handle remainder => sub {
     return uc(roman($_)) . ' (roman numeral conversion)' if /^\d+$/ && roman($_);
-    return arabic $_ . ' (roman numeral conversion)' if lc($_) =~ /^[mdclxvi]+$/ && arabic($_);
+    return arabic($_) . ' (roman numeral conversion)' if lc($_) =~ /^[mdclxvi]+$/ && arabic($_);
     return;
 };
 
