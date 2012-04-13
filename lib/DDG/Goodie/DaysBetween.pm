d607ce288b4652235359fd36334bfac676d98e02
Jerry Reptak <jreptak@gmail.com>
2012-04-13 01:33:01 -0400

    Goodie to tell the days between two dates, or current date and another date

---
+++ lib/DDG/Goodie/DaysBetween.pm
@@ -0,0 +1,38 @@
+package DDG::Goodie::DaysBetween;
+# ABSTRACT: Give the number of characters (length) of the query.
+
+use DDG::Goodie;
+use Date::Calc qw( Date_to_Days); 
+use Time::localtime;
+
+triggers startend => "days between", "daysbetween", "days_between";
+zci is_cached => 1;
+zci answer_type => "days_between";
+
+
+handle remainder => sub {
+
+	@dates = $_ =~ m#([01]?[0-9])/([0-3]?[0-9])/([0-9]{4}(?=\s|$))#g;
+
+	if(scalar(@dates) == 3) {
+		$tm=localtime;
+		push(@dates, $tm->mon + 1, $tm->mday, $tm->year + 1900);
+	}
+
+	if(scalar(@dates) == 6) {
+
+		eval {
+			$days1 = Date_to_Days(@dates[2], @dates[0], @dates[1]);
+			$days2 = Date_to_Days(@dates[5], @dates[3], @dates[4]);
+		};
+		if ($@) {
+			return;
+		}
+		$daysBetween = abs($days2 - $days1);
+		$daysBetween = (/inclusive/) ? $daysBetween + 1 : $daysBetween;
+		return $daysBetween ." days between";
+	}
+	return;
+};
+
+1;
