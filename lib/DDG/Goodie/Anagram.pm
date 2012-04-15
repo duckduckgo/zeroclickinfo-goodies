469c2505a164323b2f61219085219212d0731d70
yegg <yegg@duckduckgo.com>
2012-04-15 16:23:55 -0400

    turn garble into anagram

--- lib/DDG/Goodie/Anagram.pm
+++ lib/DDG/Goodie/Anagram.pm
@@ -1,13 +1,12 @@
-package DDG::Goodie::Garble;
+package DDG::Goodie::Anagram;
 # ABSTRACT: Take a query and spit it out randomly.
 
 use DDG::Goodie;
 use List::Util 'shuffle'; 
 
-triggers start => "garble";
+triggers start => "anagram";
 
 handle remainder => sub {
-  $_ =~ s/\s//g; #remove spaces
   @chars = split(//, $_); #convert each character of the query to an array element
   @garbledChars = shuffle(@chars); #randomly reorder the array
   $garbledAnswer = join('',@garbledChars); #convert array to string
