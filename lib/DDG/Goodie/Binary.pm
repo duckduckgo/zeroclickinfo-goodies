2441edfab46abaea8671ea3789775ddd94050dc4
ogudacity <og.udacity@gmail.com>
2012-04-16 12:28:54 +0200

    List conversion features for the Binary plugin
    Now, lists of decimal or hexadecimal numbers are handled correctly by
    individual conversion.
    A missing final 1; result was added at the end of the file to comply to
    current standards (duckpan perl v0.023).
    The corresponding test file has been extended accordingly.

--- lib/DDG/Goodie/Binary.pm
+++ lib/DDG/Goodie/Binary.pm
@@ -15,22 +15,39 @@ sub bin {
     return $bin;
 }
 
+sub decbinjust {
+    my @dec = shift;
+    my $dig = unpack("B*", pack("N", $dec[0] ));
+    # first suppress leading zeros
+    $dig =~ s/^0+(?=\d)//;
+    # Then add some more ...    
+    $dig = ('0' x (8 - (length($dig) % 8))) . $dig if (length($dig)%8 > 0); 
+    return $dig; 
+}
+
 sub dec2bin {
     my @dec = shift;
+    @dec = split('[ ,|:;]+',$dec[0]);
     my $str;
-    for(my $x=0; $x<= $#dec; $x++) {
-	my $dig = unpack("B*", pack("N", $dec[$x] ));
-	# first suppress leading zeros
-    	$dig =~ s/^0+(?=\d)//;
-	# Then add some more ...    
-    	$dig = ('0' x (8 - (length($dig) % 8))) . $dig if (length($dig)%8 > 0); 
-	$str .= " ".$dig;
+	$str = decbinjust($dec[0]);
+    for(my $x=1; $x<= $#dec; $x++) {
+	$str .= ' '.decbinjust($dec[$x]);
     }
     return $str;
 }
 
 sub hex2bin {
-    dec2bin(hex(shift));
+    my @arg = shift;
+    my $hex = $arg[0];
+    $hex =~ s/0x/x/g;  
+    $hex =~ s/[Oo]x/x/g;  
+    @hex = split('[ ,|:;]+',$hex);
+    my $str;
+    $str = decbinjust(hex($hex[0]));
+    for(my $x=1; $x<= $#hex; $x++) {
+	$str .= ' '. decbinjust(hex($hex[$x]));
+    }
+    return $str;
 }
 
 sub bin2dec {
@@ -41,11 +58,14 @@ sub bin2dec {
 
 
 handle remainder => sub {
-    return bin2dec($1) if /^[^01]*([01]+)\s+(from)?$/;
-    return hex2bin($2) if /^(0x|Ox|x)([0-9a-fA-F]+)\s+(in|to)$/;
-    return dec2bin($1) if /^([0-9 ]+)\s+(in|to)$/;
-    return hex2bin($1) if /^([0-9a-fA-F]+)\s+(in|to)$/;
+    return bin2dec($1) if /^[^01]*([01]+)\s+(from|as)?$/;
+    return hex2bin($1) if /^(((0x|Ox|ox|x)([0-9a-fA-F]+)[ ,|:;]*)+)\s+(in|to)$/;
+    return dec2bin($1) if /^(([0-9]+[ ,|:;]*)+)\s+(in|to)$/;
+    return hex2bin($1) if /^(([0-9a-fA-F]+[ ,|:;]*)+)\s+(in|to)$/;
     return '"'.$1.'" as a string is '.bin($1).' in binary.' if /^(.*)\s+(in|to)$/;
     return;
-}
+};
+
+1;
+
 
--- t/Binary.t
+++ t/Binary.t
@@ -18,11 +18,16 @@ ddg_goodie_test(
         '128 to binary' => test_zci('10000000'),
         '255 to binary' => test_zci('11111111'),
         '256 to binary' => test_zci('0000000100000000'),
+	'0 1 2 3 to binary' => test_zci('00000000 00000001 00000010 00000011'),
+	'1,1,2,3,5,8 to binary' => test_zci('00000001 00000001 00000010 00000011 00000101 00001000'),
+	'0, 1; 2: 3| to binary' => test_zci('00000000 00000001 00000010 00000011'),
         '0x00 to binary' => test_zci('00000000'),
         'Ox1e to binary' => test_zci('00011110'),
+        'ox321 to binary' => test_zci('0000001100100001'),
         'xa1 to binary'  => test_zci('10100001'),
         'ffff to binary' => test_zci('1111111111111111'),
         'FEFE to binary' => test_zci('1111111011111110'),
+	'ff|ea;eb  ec:,, to binary' => test_zci('11111111 11101010 11101011 11101100'),
         '0 to binary' => test_zci('00000000')
 );
 
