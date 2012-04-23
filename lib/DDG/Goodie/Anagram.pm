1076b2e3df4645f555bb81bb4853cff8aaf8454b
Michael Smith <crazedpsyc@duckduckgo.com>
2012-04-23 12:25:51 -0600

    remove weird utf-8 tabs

--- lib/DDG/Goodie/Anagram.pm
+++ lib/DDG/Goodie/Anagram.pm
@@ -7,10 +7,10 @@ use List::Util 'shuffle';
 triggers start => "anagram";
 
 handle remainder => sub {
-  @chars = split(//, $_); #convert each character of the query to an array element
-  @garbledChars = shuffle(@chars); #randomly reorder the array
-  $garbledAnswer = join('',@garbledChars); #convert array to string
-  return $garbledAnswer; 
+  my @chars = split(//, $_); #convert each character of the query to an array element
+  my @garbledChars = shuffle(@chars); #randomly reorder the array
+  my $garbledAnswer = join('',@garbledChars); #convert array to string
+  return $garbledAnswer; 
 };
 
 zci is_cached => 0;
