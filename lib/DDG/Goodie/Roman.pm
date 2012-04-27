418102d6da78e2a04b6b0120ccb5ed3938002972
Michael Smith <crazedpsyc@duckduckgo.com>
2012-04-26 20:18:47 -0600

    better triggers for roman goodie

--- lib/DDG/Goodie/Roman.pm
+++ lib/DDG/Goodie/Roman.pm
@@ -3,11 +3,12 @@ package DDG::Goodie::Roman;
 use DDG::Goodie;
 use Roman;
 
-triggers start => "roman", "arabic";
+triggers any => "roman", "arabic";
 
 zci is_cached => 1;
 zci answer_type => "roman_numeral_conversion";
 handle remainder => sub {
+    s/\s*(?:numeral|number)\s*//i;
     return uc(roman($_)) . ' (roman numeral conversion)' if /^\d+$/ && roman($_);
     return arabic($_) . ' (roman numeral conversion)' if lc($_) =~ /^[mdclxvi]+$/ && arabic($_);
     return;
