a776643a44a2112c801aa834a62b28b2b1a39a79
unlisted <dev@unlisted.org>
2012-05-07 23:37:31 -0400

    added regex to prevent false positives

--- lib/DDG/Goodie/Frequency.pm
+++ lib/DDG/Goodie/Frequency.pm
@@ -6,17 +6,23 @@ use DDG::Goodie;
 triggers start => 'frequency', 'freq';
 
 handle remainder => sub {
-    if ($_)
+    if ($_ =~ /^of ([a-z]|all) in (.*)/i)
     {
-	my %freq;
+	my $collect = lc $1;
+	my $target_str = lc $2;
 	my $count = 0;
-	my @chars = split //, "\L$_";
+	my %freq;
+	my @chars = split //, $target_str;
+
+
 
 	foreach (@chars)
 	{
-	    if ($_ =~ /[a-z]/i)
+	    if ($_ =~ /[a-z]/)
 	    {
-		++$freq{$_};
+		if ($collect =~ /all/) { ++$freq{$_}; } 
+		else { ++$freq{$_} if $_ eq $collect; }
+
 		++$count;
 	    };
 	};
