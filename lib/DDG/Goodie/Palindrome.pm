31d92de3867d2d2d09eb44ef3b9e06750e422adf
yegg <yegg@duckduckgo.com>
2012-05-09 09:52:32 -0400

    allow palindrome to handle sentences

--- lib/DDG/Goodie/Palindrome.pm
+++ lib/DDG/Goodie/Palindrome.pm
@@ -6,13 +6,23 @@ use DDG::Goodie;
 
 triggers any => 'palindrome';
 
-handle query_clean => sub {
+handle query => sub {
 
 	#Remove the trigger text from the query.
-	return unless /^is \s+ (\S+) \s+ an? \s* palindrome\??$/ix;
+	return unless /^(?:is\s+|)(.*?)\s+an?\s*palindrome\??$/i;
+
+	my $palindrome = $1;
+
+	my $is_palindrome = 0;
+
+	# Clean up.
+	my $clean_palindrome = lc $palindrome;
+	$clean_palindrome =~ s/[^a-z0-9]+//g;
+
+	$is_palindrome = 1 if $clean_palindrome eq scalar reverse $clean_palindrome;
 
 	#Check to see if it is a palindrome.
-	return ($1 eq scalar reverse $1) ? "$1 is a palindrome." : "$1 is not a palindrome.";
+	return $is_palindrome ? qq("$palindrome" is a palindrome.) : qq("$palindrome" is not a palindrome.);
 };
 
 zci is_cached => 1;
