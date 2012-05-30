7c610d84c1a1c8bdc8792916231c3ed6397f4f2d
Michael Smith <crazedpsyc@duckduckgo.com>
2012-05-30 15:44:28 -0600

    chomp zappbrannigan quotes

--- lib/DDG/Goodie/ZappBrannigan.pm
+++ lib/DDG/Goodie/ZappBrannigan.pm
@@ -9,7 +9,9 @@ zci is_cached => 0;
 handle query => sub {
     my @quotes = share('quotes.txt')->slurp;
     my $rand = int(rand(scalar(@quotes)));
-    return $quotes[$rand];
+    my $quote = $quotes[$rand];
+    chomp $quote;
+    return $quote;
 };
 
 1;
