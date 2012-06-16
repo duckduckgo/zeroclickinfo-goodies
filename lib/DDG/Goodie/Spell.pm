830c78005a02e646e266adb67fbaf697c8acdcf8
Michael Smith <crazedpsyc@duckduckgo.com>
2012-06-16 13:09:31 -0600

    add English spellcheck goodie with suggestions

---
+++ lib/DDG/Goodie/Spell.pm
@@ -0,0 +1,20 @@
+package DDG::Goodie::Spell;
+
+use DDG::Goodie;
+use Text::Aspell;
+
+triggers start => "spell", "how to spell", "how do i spell", "spellcheck";
+
+my $speller = Text::Aspell->new;
+$speller->set_option('lang','en_US'); 
+$speller->set_option('sug-mode','fast');
+
+handle remainder => sub {
+    return unless /^[\w']+$/; # only accept letters and ' (aspell handles contractions)
+    my $correct = $speller->check($_) ? "'\u$_' appears to be spelled right!" : "'\u$_' does not appear to be spelled correctly.";
+    my @suggestions = $speller->suggest($_);
+    my $sug = @suggestions ? "Suggestions: " . join(', ', @suggestions[0..5]) : "No suggestions.";
+    return "$correct  $sug", html => "$correct<br/>$sug";
+};
+
+1;
