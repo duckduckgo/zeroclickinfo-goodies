116c952ba3cafc8227767f70767c58c16989939c
Dylan Lloyd <dylan@dylansserver.com>
2012-06-26 14:31:51 -0400

    added pig latin test file

---
+++ t/PigLatin.pm
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use DDG::Test::Goodie;
+
+zci answer_type => 'capitalize';
+zci is_cached => 1;
+
+ddg_goodie_test(
+	[qw(
+		DDG::Goodie::Capitalize
+	)],
+	'pig latin will this work?' => test_zci('Pig Latin: illway isthay orkway?'),
+	'piglatin i love duckduckgo' => test_zci('Pig Latin: iway ovelay uckduckgoday'),
+);
+
+done_testing;
