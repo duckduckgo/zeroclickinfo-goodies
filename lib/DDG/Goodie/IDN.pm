ddac6175300f1483e726f03b5f02efde28c03fbd
Dylan Lloyd <dylan@dylansserver.com>
2012-07-06 13:44:00 -0400

    IDN trigger now startend & result capitalized

--- lib/DDG/Goodie/IDN.pm
+++ lib/DDG/Goodie/IDN.pm
@@ -10,16 +10,16 @@ attribution web => ['http://7bits.nl/', 'Peter van Dijk'],
             github => ['https://github.com/habbie', 'habbie'],
             twitter => ['https://twitter.com/habbie', 'habbie'];
 
-triggers start => 'idn';
+triggers startend => 'idn';
 
 handle remainder => sub {
 	if(/^xn--/) {
-		return 'decoded IDN: '.encode_entities(domain_to_unicode($_));
+		return 'Decoded IDN: '.encode_entities(domain_to_unicode($_));
 	}
 	else {
-		return 'encoded IDN: '.encode_entities(domain_to_ascii($_));
+		return 'Encoded IDN: '.encode_entities(domain_to_ascii($_));
 	}
-	# return;
+	return;
 };
 
 zci is_cached => 1;
