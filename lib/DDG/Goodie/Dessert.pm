2412b50adbd4f9185317ee52361fd5ce0a428761
Joe Simpson <headbangerkenny@googlemail.com>
2012-08-19 12:36:02 +0100

    Improvements to the Dessert goodie as done by crazedpsyc

--- lib/DDG/Goodie/Dessert.pm
+++ lib/DDG/Goodie/Dessert.pm
@@ -3,73 +3,46 @@ package DDG::Goodie::Dessert;
 use DDG::Goodie;
 
 sub itemify{
-	my @a = $_[0];
-	my @i = rand scalar @a;
-	return '<a href="http://duckduckgo.com?q=' . @a[@i] . '">' . @a[@i] . '</a>';
+	my $i = rand scalar @_;
+	return '<a href="http://duckduckgo.com?q=' . $_[$i] . '">' . $_[$i] . '</a>';
 };
+
+my %desserts = (
+	a => ['Apple Turnover'],
+	b => ['Banana Split', 'Biscuits'],
+	c => ['Cherry Pie', 'Cupcake', 'Chocolate'],
+	d => ['Donut'],
+	e => ['Eclair'],
+	f => ['Froyo', 'Fudge'],
+	g => ['Gingerbread'],
+	h => ['Honeycomb'],
+	i => ['Ice Cream Sandwhich', 'Ice Cream'],
+	j => ['Jam Rolly Polly','Jelly Bean','Jelly'],
+	k => ['Key Lime Pie'],
+	l => ['Lollipop','Liqourice'],
+	m => ['Macaroons','Meringues','Marshmallow','Muffin'],
+	n => ['Nougat','Nutella'],
+	o => ['Oatmeal Pie','Oreos'],
+	p => ['Profiteroles','Pop Tart'],
+	q => ['Quiche'],
+	r => ['Rocky Road','Red Velvet Cake','Ruhbarb and Custard'],
+	s => ['Sundae','Strudel','Strawberries and Cream','Souflle'],
+	t => ['Tiramisu','Trifle','Twinkies','Taffy','Toffee'],
+	u => ['Unicorn Cake','Upside-down cake'],
+	v => ['Vanilla Swirl'],
+	w => ['Waffles','Watermelon','White Chocolate'],
+	x => ['Xmas Cake'],
+	z => ['Zepolli','Zucchinni Pie'],
+);
 
 triggers start => 'dessert', 'desserts';
 handle remainder => sub{
 	if(lc $_ =~ m/(?:that)? ?(?:start|beginn?)s?(?:ing)? ?(?:with)? ([a-zA-Z])/i){
 		my $in = lc $1;
 		my $output = 'A Desert beginning with ' . (uc $in) . ' is ';
-		my @items;
-		if($in eq "a"){
-			@items = ('Apple Turnover');
-		} elsif($in eq "b"){
-			@items = ('Banana Split', 'Biscuits');
-		} elsif($in eq "c"){
-			@items = ('Cherry Pie', 'Cupcake', 'Chocolate');
-		} elsif($in eq "d"){
-			@items = ('Donut');
-		} elsif($in eq "e"){
-			@items = ('Eclair');
-		} elsif($in eq "f"){
-			@items = ('Froyo', 'Fudge');
-		} elsif($in eq "g"){
-			@items = ('Gingerbread');
-		} elsif($in eq "h"){
-			@items = ('Honeycomb');
-		} elsif($in eq "i"){
-			@items = ('Ice Cream Sandwhich', 'Ice Cream');
-		} elsif($in eq "j"){
-			@items = ('Jam Rolly Polly','Jelly Bean','Jelly');
-		} elsif($in eq "k"){
-			@items = ('Key Lime Pie');
-		} elsif($in eq "l"){
-			@items = ('Lollipop','Liqourice');
-		} elsif($in eq "m"){
-			@items = ('Macaroons','Meringues','Marshmallow','Muffin');
-		} elsif($in eq "n"){
-			@items = ('Nougat','Nutella');
-		} elsif($in eq "o"){
-			@items = ('Oatmeal Pie','Oreos');
-		} elsif($in eq "p"){
-			@items = ('Profiteroles','Pop Tart');
-		} elsif($in eq "q"){
-			@items = ('Quiche');
-		} elsif($in eq "r"){
-			@items = ('Rocky Road','Red Velvet Cake','Ruhbarb and Custard');
-		} elsif($in eq "s"){
-			@items = ('Sundae','Strudel','Strawberries and Cream','Souflle');
-		} elsif($in eq "t"){
-			@items = ('Tiramisu','Trifle','Twinkies','Taffy','Toffee');
-		} elsif($in eq "u"){
-			@items = ('Unicorn Cake','Upside-down cake');
-		} elsif($in eq "v"){
-			@items = ('Vanilla Swirl');
-		} elsif($in eq "w"){
-			@items = ('Waffles','Watermelon','White Chocolate');
-		} elsif($in eq "x"){
-			@items = ('Xmas Cake');
-		} elsif($in eq "y"){
-			@items = ('Yoghurt');
-		} elsif($in eq "z"){
-			@items = ('Zepolli','Zucchinni Pie');
-		} else{
-			return;
-		}
-		$output .= itemify(@items);
+		
+		my $items = $desserts{lc $in};
+		$output .= itemify(@{$items});
 		return $output;
 	}
 	return;
