7238b6b1fa550dc72e9438dc9796f7d7bee1ca96
Jessica Yu <kumimoko.yo@gmail.com>
2012-08-21 15:45:28 -0700

    working Gravatar plugin

---
+++ lib/DDG/Goodie/Gravatar.pm
@@ -0,0 +1,23 @@
+package DDG::Goodie::Gravatar;
+# ABSTRACT: Return Gravatar image given an email address 
+
+use CGI qw/img/;
+use DDG::Goodie;
+use Email::Valid;
+use Gravatar::URL;
+
+triggers start => 'gravatar';
+
+handle remainder => sub {
+	if(Email::Valid->address($_)){
+		return img({src=>gravatar_url(email => $_, default => "mm")});
+	} else { 
+		return;
+	}
+};
+
+zci is_cached => 1;
+
+1;
+
+
