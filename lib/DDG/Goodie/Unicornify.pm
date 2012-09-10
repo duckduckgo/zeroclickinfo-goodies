4976597a70ce1055828158413862277d8021e442
Jessica Yu <kumimoko.yo@gmail.com>
2012-09-09 23:56:39 -0700

    made style changes

--- lib/DDG/Goodie/Unicornify.pm
+++ lib/DDG/Goodie/Unicornify.pm
@@ -13,10 +13,14 @@ handle remainder => sub {
 	my $link = 'http://unicornify.appspot.com/';
 	if (Email::Valid->address($_)) {
 		s/[\s\t]+//g; # strip whitespace from the remainder, we just need the email address.
-		return  $_ . '\'s unicorn:', 
-		html => $_ . '\'s unicorn (<a href="'.$link.'">Learn more at unicornify.appspot.com</a>):'
+		my $answer = 'This is a unique unicorn for ' . $_ . ':' . "\nLearn more at unicornify.appspot.com"; 
+		my $header =  $_ . '(Unicornify)';
+		my $html = 'This is a unique unicorn for ' . $_ . ':'
 		.'<br /><a href="' . unicornify_url(email => $_, size => 128) .'">'
-		.'<img src="'.unicornify_url(email => $_, size => "100").'" style="margin: 10px 0px 10px 20px; border-radius: 8px;" /></a>';
+		.'<img src="'.unicornify_url(email => $_, size => "100").'" style="margin: 10px 0px 10px 20px; border-radius: 8px;" /></a>'
+		. '<br /><a href="'.$link.'">Learn more at unicornify.appspot.com</a>';
+		
+		return $answer, header => $header, html => $html;
 	}
 	return;
 };
