160006f3bbbb01bb984f2ca8d9c45cc1e4331c9a
Dylan Lloyd <dylan@dylansserver.com>
2012-11-06 12:58:47 -0500

    added new attribution to NLetterWords

--- lib/DDG/Goodie/NLetterWords.pm
+++ lib/DDG/Goodie/NLetterWords.pm
@@ -7,6 +7,16 @@ triggers end => "words", "word";
 
 zci is_cached => 0;
 
+primary_example_queries => '5 letter words';
+secondary_example_queries => '12 character words';
+description 'find words of a certain length';
+name 'NLetterWords';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/NLetterWords.pm';
+topics => 'language';
+categories => 'words_and_games';
+attribution github => 'http://github.com/'
+            web => ['http://dylansserver.com/', 'Dylan Lloyd'];
+
 handle query_parts => sub {
     my $numericalized = str2nbr($_);
     return unless $numericalized =~ /^(\d{1,50}) (letter|char|character) words?$/;
