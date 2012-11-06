cfaf97e73e4fc7a3e1a606cd9cfd780c77c30ca6
Dylan Lloyd <dylan@dylansserver.com>
2012-11-06 16:17:59 -0500

    added metadata to EmToPx.pm

--- lib/DDG/Goodie/EmToPx.pm
+++ lib/DDG/Goodie/EmToPx.pm
@@ -7,6 +7,16 @@ triggers any => "em", "px";
 zci is_cached => 1;
 zci answer_type => "conversion";
 
+primary_example_queries '10 px to em';
+secondary_example_queries '12.2 px in em assuming a 12.2 font size';
+description 'convert from px to em';
+name 'EmToPx';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/EmToPx.pm';
+category 'conversions';
+topics 'programming';
+attribution twitter => 'crazedpsyc',
+            cpan    => 'CRZEDPSYC' ;
+
 handle query_raw => sub {
     s/(?![\.\s])\W//g;
     return unless /^(?:convert|change|what\s*(?:s|is|are)\s+)?(\d+\.\d*|\d*\.\d+|\d+)\s*(em|px)\s+(?:in|to)\s+(em|px)(?:\s+(?:with|at|using|assuming)(?:\s+an?)?\s+(\d+\.\d*|\d*\.\d+|\d+)\s*px)?/i;
