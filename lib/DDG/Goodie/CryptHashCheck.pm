b8394b72593024a391a89e3e95de8877ec470f65
digit4lfa1l <digit4lfa1l@gmail.com>
2012-12-04 15:00:51 -0500

    Added references to the Wikipedia articles. Changes in a module's main code.

--- lib/DDG/Goodie/CryptHashCheck.pm
+++ lib/DDG/Goodie/CryptHashCheck.pm
@@ -1,14 +1,21 @@
 package DDG::Goodie::CryptHashCheck;
-# ABSTRACT: Identifies cryptographic hash function type.
+# ABSTRACT: Identifies cryptographic hash type.
 
 use DDG::Goodie;
 
+# A comprehensive reference for hashing functions from Wikipedia.
+
+use constant MD5HERF =>  "http://en.wikipedia.org/wiki/MD5#MD5_hashes";
+use constant SHA1HREF => "http://en.wikipedia.org/wiki/SHA-1#Example_hashes";
+use constant SHA2HREF => "http://en.wikipedia.org/wiki/SHA-2#Examples_of_SHA-2_variants";
+
 zci is_cached => 1;
 
-triggers start => "hash","md5","sha";
+# Only one trigger enabled.
+triggers start => "hash";
 
 primary_example_queries 'hash 624d420035fc9471f6e16766b7132dd6bb34ea62';
-secondary_example_queries 'md5 1f9b59a2390bb77d2c446837d6aeab067f01b05732735f47099047cd7d3e9f85';
+secondary_example_queries 'hash 1f9b59a2390bb77d2c446837d6aeab067f01b05732735f47099047cd7d3e9f85';
 description 'Identifies cryptographic hash function type.';
 name 'CryptHashCheck';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/CryptHashCheck.pm';
@@ -17,20 +24,56 @@ topics 'cryptography';
 
 attribution github => ['https://github.com/digit4lfa1l', 'digit4lfa1l'];
 
+# Remainder function with links to the Wikipedia resources. 
 
 handle remainder => sub {
 	my ($md5) = /^[0-9a-f]{32}$/i;
-		return 'This is a MD5 cryptographic hash.' if $md5;
+		if ($md5){
+		  	my $text = sprintf qq(This is a MD5 cryptographic hash.);
+  			my $html = sprintf qq(This is a <a href="%s">MD5</a> cryptographic hash.),MD5HERF;	
+			
+			return $text, html => $html;			
+		}
+
 	my ($sha1)  = /^[0-9a-f]{40}$/i;
-		return 'This is a SHA-1/40 cryptographic hash.' if $sha1;
+		if ($sha1){
+		  	my $text = sprintf qq(This is a SHA-1/40 cryptographic hash.);
+  			my $html = sprintf qq(This is a <a href="%s">SHA-1/40</a> cryptographic hash.),SHA1HREF;	
+			
+			return $text, html => $html;			
+		}
+		
 	my ($sha224) = /^[0-9a-f]{56}$/i;
-		return 'This is a SHA-2/224 cryptographic hash.' if $sha224;
+		if ($sha224){
+		  	my $text = sprintf qq(This is a SHA-2/224 cryptographic hash.);
+  			my $html = sprintf qq(This is a <a href="%s">SHA-2/224</a> cryptographic hash.),SHA2HREF;	
+			
+			return $text, html => $html;			
+		}
+
 	my ($sha256) = /^[0-9a-f]{64}$/i;
-		return 'This is a SHA-2/256 cryptographic hash.' if $sha256;
+		if ($sha256){
+		  	my $text = sprintf qq(This is a SHA-2/256 cryptographic hash.);
+  			my $html = sprintf qq(This is a <a href="%s">SHA-2/256</a> cryptographic hash.),SHA2HREF;	
+			
+			return $text, html => $html;			
+		}
+
 	my ($sha384) = /^[0-9a-f]{96}$/i;
-		return 'This is a SHA-2/384 cryptographic hash.' if $sha384;
+		if ($sha384){
+		  	my $text = sprintf qq(This is a SHA-2/384 cryptographic hash.);
+  			my $html = sprintf qq(This is a <a href="%s">SHA-2/384</a> cryptographic hash.),SHA2HREF;	
+			
+			return $text, html => $html;			
+		}
+
 	my ($sha512) = /^[0-9a-f]{128}$/i;
-		return 'This is a SHA-2/512 cryptographic hash.' if $sha512; 
+		if ($sha512){
+		  	my $text = sprintf qq(This is a SHA-2/512 cryptographic hash.);
+  			my $html = sprintf qq(This is a <a href="%s">SHA-2/512</a> cryptographic hash.),SHA2HREF;	
+			
+			return $text, html => $html;			
+		}
 	return;	
 };
 
