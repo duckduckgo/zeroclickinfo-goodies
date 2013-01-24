e9d3036c73bbd1a2a73dfa9f852a1f6d8b58bcd3
matt lehning <matt.lehning@yahoo.com>
2013-01-23 19:48:32 -0500

    work ffs

--- lib/DDG/Goodie/Coin.pm
+++ lib/DDG/Goodie/Coin.pm
@@ -5,21 +5,21 @@ use DDG::Goodie;
 triggers start => 'flip', 'coin';
 
 handle remainder => sub {
-  return unless my ($a, $n) = $_ =~ /^(flip a coin|flip (/d{0,2) coins?)$/;
+  return unless my ($a, $n) = $_ =~ /^(flip a coin|flip (\d{0,2}) coins?)$/;
 	my @output;
 	my @ht = ('heads', 'tails');
 	my $flips = 1;
 	my $option = 2;
 	my $flip;
 
-	if($a = "flip a coin" || "heads or tails") {
+	if($a == "flip a coin" || "heads or tails") {
 		$n = 1;
 	}
 
 	if($n >= 1) {
 
-		for (1 .. $flips) {
-			$flip = int(rand(@choices));
+		for (1 .. $n) {
+			$flip = int(rand(@ht));
 			#$flip = $a[int rand($#a)];
 			push @output, $flip;
 		}
