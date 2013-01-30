bf0a36ded4e913cb8e0d8b8487087c7514a94400
matt lehning <matt.lehning@yahoo.com>
2013-01-30 18:23:19 -0500

    Flip a coin goodie

--- lib/DDG/Goodie/Coin.pm
+++ lib/DDG/Goodie/Coin.pm
@@ -2,18 +2,19 @@ package DDG::Goodie::Coin;
 
 use DDG::Goodie;
 
-triggers start => 'flip', 'coin', 'coins';
+triggers start => 'flip', 'toss', 'coin';
 
 handle query_lc => sub {
-  return unless my ($a, $n) = $_ =~ /^(flip a coin|flip (\d{0,2}) coins?)$/;
+  return unless my ($a, $z, $y, $n) = $_ =~ /^((flip|toss) a coin|(flip|toss) (\d{0,2}) coins?)$/;
 	my @output;
 	my @ht = ("heads", "tails");
 	my $flips = 1;
 	my $flip;
 	my $b = "flip a coin";
+	my $c = "toss a coin";
 	my $count;
 
-	if($a eq $b) {
+	if($a eq $b||$a eq $c) {
 		$n = 1;
 	}
 
