e519be79d9da9284398e3f810c59ca4fb72b7129
Brad Steinbacher <bjs5531@psu.edu>
2013-03-01 01:13:21 -0500

    changes to words completed, changed file/directory/package names, added of regexp to Randagram.pm

---
+++ lib/DDG/Goodie/Randagram.pm
@@ -0,0 +1,19 @@
+package DDG::Goodie::Randagram;
+# ABSTRACT: Take a query and spit it out randomly.
+
+use DDG::Goodie;
+use List::Util 'shuffle'; 
+
+triggers start => "randagram";
+
+handle remainder => sub {
+    s/^of\s(.*)/$1/i;
+    my @chars = split(//, $_); #convert each character of the query to an array element
+    my @garbledChars = shuffle(@chars); #randomly reorder the array
+    my $garbledAnswer = join('',@garbledChars); #convert array to string
+    return $garbledAnswer; 
+};
+
+zci is_cached => 0;
+
+1;
