b86b16c11671e21663d0ad2783d69b285835bc74
Jag Talon <talon.jag@gmail.com>
2013-03-08 17:31:30 -0500

    Added a test for HK/DK.

--- lib/DDG/Goodie/HKDK.pm
+++ lib/DDG/Goodie/HKDK.pm
@@ -22,10 +22,10 @@ handle query_nowhitespace_nodash => sub {
     my $package_number = $1;
 
     if ( $package_number =~ /hk$/i ) {
-        return heading => 'Hongkong Post Shipment Tracking', html => qq(Track this shipment at <a href="http://app3.hongkongpost.com/CGI/mt/genresult.jsp?tracknbr=$package_number">Hongkong Post</a>.);
+        return $package_number, heading => 'Hongkong Post Shipment Tracking', html => qq(Track this shipment at <a href="http://app3.hongkongpost.com/CGI/mt/genresult.jsp?tracknbr=$package_number">Hongkong Post</a>.);
     }
     elsif ( $package_number =~ /dk$/i ) {
-        return heading => 'Post Norden Shipment Tracking', html => qq(Track this shipment at <a href="http://www.postdanmark.dk/tracktrace/TrackTrace.do?i_stregkode=$package_number">Post Norden</a>.);
+        return $package_number, heading => 'Post Norden Shipment Tracking', html => qq(Track this shipment at <a href="http://www.postdanmark.dk/tracktrace/TrackTrace.do?i_stregkode=$package_number">Post Norden</a>.);
     }
 
     return;
