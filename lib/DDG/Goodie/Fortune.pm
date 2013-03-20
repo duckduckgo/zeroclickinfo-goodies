ababb16338135edfb9525d1b1fe529392fc78229
Francisco Gomez <mail@frncscgmz.me>
2013-03-19 21:41:40 -0700

    Changed triggers to any 'unix fortune' and 'fortune cookie'

--- lib/DDG/Goodie/Fortune.pm
+++ lib/DDG/Goodie/Fortune.pm
@@ -3,7 +3,15 @@ package DDG::Goodie::Fortune;
 use DDG::Goodie;
 use Fortune;
 
-triggers start => 'fortune';
+triggers any => 'unix fortune','unix fortune!','fortune cookie','fortune cookie!';
+
+primary_example_queries 'fortune';
+name 'Fortune';
+description 'get a random phrase from the original fortunes file';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/Fortune.pm';
+category 'random';
+topics 'words_and_games';
+attribution github => ['https://github.com/frncscgmz', 'frncscgmz'];
 
 zci is_cached => 1;
 zci answer_type => "fortune";
