415007470728edcad9a65a336f4eb94719ad4216
teamnigel <teamnigel@lavabit.com>
2013-05-08 13:40:22 -0400

    MinecraftSkin plugin

---
+++ lib/DDG/Goodie/MinecraftSkin.pm
@@ -0,0 +1,26 @@
+package DDG::Goodie::MinecraftSkin;
+
+use DDG::Goodie;
+
+primary_example_queries 'minecraft skin teamnigel';
+secondary_example_queries 'minotar teamnigel';
+description 'show the Minecraft skin for a given username (case sensitive!)';
+name 'Minecraft Skin';
+#code_url '';
+category 'reference';
+topics 'gaming';
+
+#attribution github => [''];
+
+triggers any => "minecraft skin", "minotar", "minecraftskin";
+
+zci is_cached => 1;
+zci answer_type => "minecraft skin";
+
+handle remainder => sub {
+	return if ($_ =~ m/[^a-zA-Z0-9]/);
+	return html => '<img src="https://minotar.net/avatar/' . $_ . '/64.png"> <img src="https://minotar.net/skin/' . $_ . '">';
+	return;
+};
+
+1;
