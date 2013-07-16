4a77b3e06ed3c7192262567c03d46b5173fa583c
chreod <chreod@lavabit.com>
2013-07-15 23:58:54 -0400

    Added protein weights, cleaned up errors, cleaned up html-making

--- lib/DDG/Goodie/BioSeq.pm
+++ lib/DDG/Goodie/BioSeq.pm
@@ -14,7 +14,7 @@ name 'BioSeq';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/lib/DDG/Goodie/BioSeq.pm';
 category 'calculations';
 topics 'science';
-attribution email => 'chreod@lavabit.com';
+email => ['chreod@lavabit.com', 'Darach Miller'];
 
 my $help = "To use the \"bioseq\" DDGoodie, enter a function name then the nucleotide or amino acid sequence."
          . "\n	r	reverse the sequence, return DNA"
@@ -23,17 +23,17 @@ my $help = "To use the \"bioseq\" DDGoodie, enter a function name then the nucle
          . "\n	tu	replace all T with U"
          . "\n	tln	translate nucleotides to amino acids in three frames"
          . "\n	t	estimate the melting temperature of sequence as DNA"
-         . "\n	w	estimate the DNA weight of sequence as DNA"
+         . "\n	w	estimate the weight of sequence of nucleotides or protein"
          . "\nFor example, \"bioseq rc ATCGCGATUCGAT\" would return the reversed complementary sequence."
          . "\nFor example, \"bioseq tln ATCGCGATUCGAT\" would return the translation of the sequence.";
 my $help_html = $help;
 $help_html =~ s/\n/<br>/g;
 
 my $nucleotides = "ATCGU";
-my $amino_acids = "FLSYCWPHQRIMTNKVADEG";
-
 my %tln_table = (
     #Each amino acid is on a row, with codons that map to it
+    #Stop codon is X
+    #Need to add attribution for the codon table to IUPAC website
     "UUU"	=>	"F",	"UUC"	=>	"F",
     "UUA"	=>	"L",	"UUG"	=>	"L",
     "UCU"	=>	"S",	"UCC"	=>	"S",	"UCA"	=>	"S",	"UCG"	=>	"S", "AGU"	=>	"S",	"AGC"	=>	"S",
@@ -57,6 +57,39 @@ my %tln_table = (
     "GAA"	=>	"E",	"GAG"	=>	"E",
     "GGU"	=>	"G",	"GGC"	=>	"G",	"GGA"	=>	"G",	"GGG"	=>	"G"
 );
+my $amino_acids = join("", values(%tln_table));	# aka "FLSYCWPHQRIMTNKVADEG"
+$amino_acids =~ s/X//g;	#Two steps so that if we use a non-canonical table above, we can still get our string of possible nucleotides
+my %amino_acid_weight = (
+	#I know the three significant figures are ridiculous, but that was how I found the values:
+	#http://web.expasy.org/protscale/pscale/Molecularweight.html
+	"A"	=>	89.000,
+	"R"	=>	174.000,
+	"N"	=>	132.000,
+	"D"	=>	133.000,
+	"C"	=>	121.000,
+	"Q"	=>	146.000,
+	"E"	=>	147.000,
+	"G"	=>	75.000,
+	"H"	=>	155.000,
+	"I"	=>	131.000,
+	"L"	=>	131.000,
+	"K"	=>	146.000,
+	"M"	=>	149.000,
+	"F"	=>	165.000,
+	"P"	=>	115.000,
+	"S"	=>	105.000,
+	"T"	=>	119.000,
+	"W"	=>	204.000,
+	"Y"	=>	181.000,
+	"V"	=>	117.000
+);
+my %nucleotide_weight = (
+	"A"	=>	313.2,
+	"T"	=>	304.2,
+	#"U"	=>	?????,	#Look up
+	"G"	=>	329.2,
+	"C"	=>	289.2
+);
 
 my %functions = (
     "reverse"            => sub { "Reversed: ".reverse },
@@ -65,18 +98,18 @@ my %functions = (
     "c"                  => sub { tr/ATCGU/TAGCA/; "Complement: $_" },
     "reverse-complement" => sub { tr/ATCGU/TAGCA/; "Reversed complement: ".reverse },
     "rc"                 => sub { tr/ATCGU/TAGCA/; "Reversed complement: ".reverse },
-    "translate"          => sub { "Translated to amino acid sequence:".translate($_) },
-    "tln"                => sub { "Translated to amino acid sequence:".translate($_) },
     "tu"                 => sub { tr/T/U/; "T's turned to U's: $_" },
-    "temp"               => sub { "Estimated melting temperature of ".temp($_)." Celsius, supposing 50mM monovalent cations." },
-    "t"                  => sub { "Estimated melting temperature of ".temp($_)." Celsius, supposing 50mM monovalent cations." },
-    "weight"             => sub { "That sequence, as DNA without a 5' phosphate, would weigh ".weight($_)." grams per mole." },
-    "w"                  => sub { "That sequence, as DNA without a 5' phosphate, would weigh ".weight($_)." grams per mole." },
+    "translate"          => sub { translate($_) },
+    "tln"                => sub { translate($_) },    
+    "temp"               => sub { temp($_) },
+    "t"                  => sub { temp($_) },
+    "weight"             => sub { weight($_) },
+    "w"                  => sub { weight($_) },
 );
 
 sub translate {
-    return "[ERROR: Non-nucleotide characters detected]" if $_[0] =~ /[^ATCGU]/;
-    $_[0] =~ tr/T/U/; # Turn DNA to RNA
+    return "BioSeq Error: Can't translate non-nucleotide characters" if $_[0] =~ /[^ATCGU]/;
+    $_[0] =~ tr/T/U/; # Turn DNA to RNA for lookup table
     my @frame;        # Make the three frames of reference
     $frame[0] = $frame[1] = $frame[2] = $_[0];
     $frame[1] =~ s/^.//;  # Offset one by deletion
@@ -97,51 +130,72 @@ sub translate {
             $tln[$frame] .= $tln_table{$1};              # If we're not changing start status, we're normal, just add the amino acid
         }
     }
-    my $report;
+    my $report = "Translated your input sequence to amino acid sequence:";
     foreach my $frame (0..$#frame) {
         # Give me the title and the translation string, if it exists
-        $report .= "\n\n\tFrame ".($frame+1).": $tln[$frame]" if $tln[$frame];
+        $report .= "\nFrame ".($frame+1).": $tln[$frame]" if $tln[$frame];
     }
     return $report;
 }
 	
-
 sub temp {
-    return "[ERROR: Non-nucleotide characters detected]" if $_[0] =~ /[^ATCGU]/;
-    my $gc_count = $_[0] =~ tr/CG/CG/;  #tr returns the number of transliterations it makes
-    return (4 * $gc_count + 2 * ((scalar length $_[0]) - $gc_count)) if (scalar length $_[0]) < 14;
-    # The above is for less than 14 length, below is for more than that
-    return (64.9 + 41 * ($gc_count - 16.4) / scalar length $_[0]);
-    # This formula is as cited in Promega's biomath page
-    # http://www.promega.com/techserv/tools/biomath/calc11.htm
+	return "BioSeq Error: Can't calculate temperature for non-nucleotide sequence (doesn't support IUPAC codes)" if $_[0] =~ /[^ATCGU]/;
+	#I can only calculate temperature for nucleotides
+	my $gc_count = $_[0] =~ tr/CG/CG/;	#Count all the Gs and Cs
+	if ((scalar length $_[0]) < 14) {
+		my $temperature = (4 * $gc_count + 2 * ((scalar length $_[0]) - $gc_count));
+		# The above is for less than 14 length, below is for 14 or more
+	} else {
+		my $temperature = (64.9 + 41 * ($gc_count - 16.4) / scalar length $_[0]);
+	}
+	$temperature =. "Estimated melting temperature of ";
+	$temperature .= " Celsius, supposing 50mM monovalent cations.";
+	return $temperature;
+	# This formula is as cited in Promega's biomath page
+	# http://www.promega.com/techserv/tools/biomath/calc11.htm
 }
 
 sub weight {
-    return "[ERROR: Non-nucleotide characters detected]" if $_[0] =~ /[^ATCGU]/;
-    my $g = $_[0] =~ tr/G/G/;  # Count each one individually
-    my $c = $_[0] =~ tr/C/C/;
-    my $a = $_[0] =~ tr/A/A/;
-    my $t = $_[0] =~ tr/TU/TU/;  # Both count as T
-    return ((329.2 * $g) + (313.2 * $a) + (304.2 * $t) + (289.2 * $c));
-    # This formula is as cited in Promega's biomath page
-    # http://www.promega.com/techserv/tools/biomath/calc11.htm
+	my $weighing_seq = $_[0];
+	my $weight = 0;
+	if ($weighing_seq =~ /[^ATCGU]/) {
+		#If it has non-nucleotides, it must be a protein, so use a different table
+		foreach (split("",$amino_acids)) {
+			my $count = $weighing_seq =~ tr/$_/$_/;
+			$weight += $count * $amino_acid_weight{$_};
+		}
+		$weight =. "That amino acid sequence weighs about ";
+		$weight	.= " dalton";
+	} else {
+		foreach (split("",$nucleotides)) {
+			if $_ eq "U" next;	#For now, need to look up the weight of dUTP
+			my $count = $weighing_seq =~ tr/$_/$_/;
+			$weight += $count * $nucleotide_weight{$_};
+		}
+		$weight =. "That amino acid sequence weighs about ";
+		$weight	.= " grams per mole";
+	}
+	return $weight;
+	#This formula is as cited in Promega's biomath page
+	#http://www.promega.com/techserv/tools/biomath/calc11.htm
 }
 
 
 handle remainder => sub {
 	my @args = split /\s+/;
 	my $function = lc shift @args;
-    return $help, html => $help_html if $function eq 'help' or $function eq 'h';
+	return $help, html => $help_html if $function eq 'help' or $function eq 'h';
 	
-	my $sequence = uc join "", @args if @args;    # The rest of argument should be seq, even with whitespace errors
+	my $sequence = uc join "", @args if @args;    # The rest of argument should be seq, even with whitespace mistakes
 	return unless $sequence;                      # If no sequence, then quit and return nothing
 	$sequence =~ s/[^$nucleotides$amino_acids]//; # Delete all non-amino_acid/non-nucleotide characters
 	$sequence =~ tr/U/T/;                         # Make to DNA, for standardization, and since tu provides return to RNA
-							                      # also, T is an amino acid, U isn't
-    for ($sequence) {
-        return $functions{$function}->()
-            if exists $functions{$function};
-    }
+						      #  also, T is an amino acid, U isn't
+	for ($sequence) {
+		my $return_value = $functions{$function}->() if exists $functions{$function};
+		my $return_value_html =~ tr/\n/<br>/g;
+		return $return_value, html => $return_value_html;
+	}
 };
 
 1;
