f529a8b2e9a41eae84bad69ca400ad9f48f618ce
John-Peter Etcheber <jetcheber@gmail.com>
2013-07-23 19:02:51 -0700

    Update POTUS.pm
    Getting changes from pr/206.

--- lib/DDG/Goodie/POTUS.pm
+++ lib/DDG/Goodie/POTUS.pm
@@ -2,11 +2,13 @@ package DDG::Goodie::POTUS;
 # ABSTRACT: Returns requested President of the United States
 
 use DDG::Goodie;
-use Scalar::Util qw(looks_like_number);
 use Lingua::EN::Numbers::Ordinate;
+use URI::Escape;
 
-triggers start => 'POTUS', 'potus';
-triggers any => 'president of the united states';
+triggers start => 'potus';
+triggers any => 'president of the united states', 'president of the us';
+
+zci is_cached => 1;
 
 name 'POTUS';
 description 'returns the President of the United States';
@@ -17,76 +19,68 @@ code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DD
 attribution github  => ['https://github.com/numbertheory', 'John-Peter Etcheber'],
             twitter => ['http://twitter.com/jpscribbles', 'John-Peter Etcheber'];
 
+#For maintenance, just add the president to the end of this array
+my @presidents = (
+    "George Washington",
+    "John Adams",
+    "Thomas Jefferson",
+    "James Madison",
+    "James Monroe",
+    "John Quincy Adams",
+    "Andrew Jackson",
+    "Martin Van Buren",
+    "William Henry Harrison",
+    "John Tyler",
+    "James K. Polk",
+    "Zachary Taylor",
+    "Millard Fillmore",
+    "Franklin Pierce",
+    "James Buchanan",
+    "Abraham Lincoln",
+    "Andrew Johnson",
+    "Ulysses S. Grant",
+    "Rutherfod B. Hayes",
+    "James A. Garfield",
+    "Chester A. Arthur",
+    "Grover Cleveland",
+    "Benjamin Harrison",
+    "Grover Cleveland",
+    "William McKinley",
+    "Theodore Roosevelt",
+    "William Howard Taft",
+    "Woodrow Wilson",
+    "Warren G. Harding",
+    "Calvin Coolidge",
+    "Herbert Hoover",
+    "Franklin D. Roosevelt",
+    "Harry S. Truman",
+    "Dwight D. Eisenhower",
+    "John F. Kennedy",
+    "Lyndon B. Johnson",
+    "Richard Nixon",
+    "Gerald Ford",
+    "Jimmy Carter",
+    "Ronald Reagan",
+    "George H.W. Bush",
+    "Bill Clinton",
+    "George W. Bush",
+    "Barack Obama",
+);
+		
 handle remainder => sub {
-	#For maintenance, just add the president to the end of this array	
-	my @prez = (
-			"George Washington",
-			"John Adams",
-			"Thomas Jefferson",
-			"James Madison",
-			"James Monroe",
-			"John Quincy Adams",
-			"Andrew Jackson",
-			"Martin Van Buren",
-			"William Henry Harrison",
-			"John Tyler",
-			"James K. Polk",
-			"Zachary Taylor",
-			"Millard Fillmore",
-			"Franklin Pierce",
-			"James Buchanan",
-			"Abraham Lincoln",
-			"Andrew Johnson",
-			"Ulysses S. Grant",
-			"Rutherfod B. Hayes",
-			"James A. Garfield",
-			"Chester A. Arthur",
-			"Grover Cleveland",
-			"Benjamin Harrison",
-			"Grover Cleveland",
-			"William McKinley",
-			"Theodore Roosevelt",
-			"William Howard Taft",
-			"Woodrow Wilson",
-			"Warren G. Harding",
-			"Calvin Coolidge",
-			"Herbert Hoover",
-			"Franklin D. Roosevelt",
-			"Harry S. Truman",
-			"Dwight D. Eisenhower",
-			"John F. Kennedy",
-			"Lyndon B. Johnson",
-			"Richard Nixon",
-			"Gerald Ford",
-			"Jimmy Carter",
-			"Ronald Reagan",
-			"George H.W. Bush",
-			"Bill Clinton",
-			"George W. Bush",
-			"Barack Obama",
-				);
-		
-	my $num = $_;
-	$num =~ s/\D+//g;
-	if (looks_like_number($num) == 0) {
-		$num = scalar @prez;
-		}	
-	#Don't use negative numbers	
-	if ($num < 1) {
-		return;
-		}
-	#Don't answer who is the 300th president of the United States
-	if ($num > scalar @prez) {
-		return;
-		}
-		
-	$num = $num - 1;
-	my $verb = " was";
-	if (($num+1) == scalar @prez){$verb = " is";}
-	return $prez[$num], html=> "<a href=\"http://en.wikipedia.org/wiki/". $prez[$num] ."\">" . $prez[$num] . "</a>" . $verb . " the " . ordinate($num+1) . " President of the United States.";
-	return;
-};
+	s/\D+//g;
+    my $num = $_;
+    $num = scalar @presidents if not $num;
+    return if --$num < 0 or $num > scalar @presidents;
 
-zci is_cached => 1;
+    my $fact = ($num + 1 == scalar @presidents ? 'is' : 'was')
+             . " the " . ordinate($num + 1) . " President of the United States.";
+
+    my $link = '<a href="https://en.wikipedia.org/wiki/'
+             . uri_escape($presidents[$num]) .'">'
+             . $presidents[$num] . '</a>';
+
+	return "$presidents[$num] $fact", html => "$link $fact";
+};
 
 1;
