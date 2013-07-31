9836b471a0803f6c9127dd66f93e07fa33471eaa
chreod <chreod@lavabit.com>
2013-07-31 16:50:48 -0400

    Rewrote as regex triggered

--- lib/DDG/Goodie/BioSeq.pm
+++ lib/DDG/Goodie/BioSeq.pm
@@ -2,35 +2,21 @@ package DDG::Goodie::BioSeq;
 # ABSTRACT: Does several simple string manipulations, transliterations, and calculations, for the convienence of biologists designing primers and other simple tasks. Takes as arguments several functions, the shorthand of which is in the help.
 use DDG::Goodie;
 
-triggers start => 'bioseq';
+#Trigger is after these data tables
 
 zci is_cached => 1;
 zci answer_type => 'bioseq';
 
-primary_example_queries 'bioseq rc ATCG';
-secondary_example_queries 'bioseq tln ATGAAACCCGGGTAG', 'bioseq temp TAATACGACTCACTATAGGG';
-description 'simple string manipulations and calculations for lazy biologists, try bioseq help';
+#primary_example_queries 'bioseq rc ATCG';
+#secondary_example_queries 'bioseq tln ATGAAACCCGGGTAG', 'bioseq temp TAATACGACTCACTATAGGG';
+#description 'simple string manipulations and calculations for lazy biologists, try bioseq help';#put help in description
 name 'BioSeq';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/lib/DDG/Goodie/BioSeq.pm';
 category 'calculations';
 topics 'science';
 attribution email => ['chreod@lavabit.com', 'chreod'];
 
-my $help = "To use the \"bioseq\" DDGoodie, type a function nickname, then the nucleotide or amino acid sequence."
-	."\n	r	reverse the sequence, return DNA"
-	."\n	c	complement the sequence, return DNA"
-	."\n	rc	reverse-complement the sequence, return DNA"
-	."\n	tu	replace all T with U"
-	."\n	ut	replace all U with T"
-	."\n	tln	translate nucleotides to amino acids in three frames"
-	."\n	t	estimate the melting temperature of sequence as DNA"
-	."\n	w	estimate the weight of sequence of nucleotides or protein"
-	."\nFor example, \"bioseq rc ATCGCGATUCGAT\" would return the reversed complementary sequence."
-	."\nFor example, \"bioseq tln ATCGCGATUCGAT\" would return the translation of the sequence.";
-my $help_html = $help;
-$help_html =~ s/\n/<br>/g;
-
-my %tln_table = (
+my %tln_table = (	#IUPAC reference
 	"UUU"	=>	"F",	"UUC"	=>	"F",
 	"UUA"	=>	"L",	"UUG"	=>	"L",
 	"UCU"	=>	"S",	"UCC"	=>	"S",	"UCA"	=>	"S",	"UCG"	=>	"S", "AGU"	=>	"S",	"AGC"	=>	"S",
@@ -54,9 +40,7 @@ my %tln_table = (
 	"GAA"	=>	"E",	"GAG"	=>	"E",
 	"GGU"	=>	"G",	"GGC"	=>	"G",	"GGA"	=>	"G",	"GGG"	=>	"G"
 );
-my %amino_acid_weight = (
-	#http://web.expasy.org/findmod/findmod_masses.html#AA
-	#Water from hydrolysis is already removed in this list
+my %amino_acid_weight = (	# from http://web.expasy.org/findmod/findmod_masses.html#AA , water from hydrolysis is already removed in this list
 	"A"	=>	71.0788,
 	"R"	=>	156.1875,
 	"N"	=>	114.1038,
@@ -80,49 +64,42 @@ my %amino_acid_weight = (
 	"U"	=>	150.0388,
 	"O"	=>	237.3018
 );
-my %nucleotide_weight = (
-	"dA"	=>	331.222,	#d is for deoxy, so dna
-	"dT"	=>	322.208,
-	"dU"	=>	308.182,	#even dUMP
-	"dG"	=>	347.2243,
-	"dC"	=>	307.197,
+my %oxy_nucleotide_weight = (	#RNA
 	"A"	=>	347.22,
 	"U"	=>	324.181,
 	"G"	=>	363.22,
 	"C"	=>	323.20,
 );
-my $amino_acids = join("", keys(%amino_acid_weight));	# aka "FLSYCWPHQRIMTNKVADEG"
-my $nucleotides = join("", keys(%nucleotide_weight));
-
-my %functions = (
-	"reverse"		=> sub { "Reversed: ".reverse },
-	"r"			=> sub { "Reversed: ".reverse },
-	"complement"		=> sub { return if /[^ATCGU]/; tr/ATCGU/TAGCA/; "Complement: $_" },
-	"c"			=> sub { return if /[^ATCGU]/; tr/ATCGU/TAGCA/; "Complement: $_" },
-	"reverse-complement"	=> sub { return if /[^ATCGU]/; tr/ATCGU/TAGCA/; "Reversed complement: ".reverse },
-	"rc"			=> sub { return if /[^ATCGU]/; tr/ATCGU/TAGCA/; "Reversed complement: ".reverse },
-	"tu"			=> sub { return if /[^ATCGU]/; tr/T/U/; "T's turned to U's: $_" },
-	"ut"			=> sub { return if /[^ATCGU]/; tr/U/T/; "U's turned to T's: $_" },
-	"translate"		=> sub { translate($_) },
-	"tln"			=> sub { translate($_) },	
-	"temp"			=> sub { temp($_) },
-	"t"			=> sub { temp($_) },
-	"weight"		=> sub { weight($_) },
-	"w"			=> sub { weight($_) },
+my %deoxy_nucleotide_weight = (	#DNA
+	"A"	=>	331.222,	
+	"T"	=>	322.208,
+	#"U"	=>	308.182,	#dUMP for handling oligos?
+	"G"	=>	347.2243,
+	"C"	=>	307.197,
 );
+my $amino_acids = lc(join("", keys(%amino_acid_weight)));	# aka lc "FLSYCWPHQRIMTNKVADEG"
+my $oxy_nucleotides = lc(join("", keys(%oxy_nucleotide_weight)));
+my $deoxy_nucleotides = lc(join("", keys(%deoxy_nucleotide_weight)));
+my %tmphash;
+@tmphash{(keys(%amino_acid_weight),keys(%oxy_nucleotide_weight),keys(%deoxy_nucleotide_weight))} = ();
+my $valid = lc(join("", sort keys %tmphash));
+undef %tmphash;
+@tmphash{(keys(%oxy_nucleotide_weight),keys(%deoxy_nucleotide_weight))} = ();
+my $nucleotides = lc(join("", sort keys %tmphash));
+undef %tmphash;
 
 my %html_references = (
-	"temp"		=> '<br><a href="http://www.promega.com/techserv/tools/biomath/calc11.htm">Temperature formula from Promega</a>',
-	"t"		=> '<br><a href="http://www.promega.com/techserv/tools/biomath/calc11.htm">Temperature formula from Promega</a>',
+	"temperature"	=> '<a href="http://www.promega.com/techserv/tools/biomath/calc11.htm">Temperature formula from Promega</a>',
 );
 
 sub translate {
-	return "BioSeq Error: Can't translate non-nucleotide characters" if $_[0] =~ /[^$nucleotides]/;
-	$_[0] =~ tr/T/U/; # Turn DNA to RNA for lookup table
-	my @frame;		# Make the three frames of reference
+	return if $_[0] =~ /[^$deoxy_nucleotides$oxy_nucleotides]/;
+	$_[0] = uc $_[0];	#Because I don't want to rewrite the codon table
+	$_[0] =~ tr/T/U/; 	#Turn DNA to RNA for lookup table
+	my @frame;		#Make the three frames of reference
 	$frame[0] = $frame[1] = $frame[2] = $_[0];
-	$frame[1] =~ s/^.//;  # Offset one by deletion
-	$frame[2] =~ s/^..//; # Offset two by deletion
+	$frame[1] =~ s/^.//;  	#Offset one by deletion
+	$frame[2] =~ s/^..//; 	#Offset two by deletion
 	my @tln;
 	my $start;				#Start with frame stopped
 	foreach my $frame (0..$#frame) {	#For each of the frames
@@ -144,13 +121,12 @@ sub translate {
 		# Give me the title and the translation string, if it exists
 		$report .= "\nFrame ".($frame+1).": $tln[$frame]" if $tln[$frame];
 	}
-	return $report;
+	return $report."\n";
 }
 	
 sub temp {
-	return "BioSeq Error: Can't calculate temperature for non-nucleotide sequence (doesn't support IUPAC codes)" if $_[0] =~ /[^ATCGU]/;
-	#I can only calculate temperature for nucleotides
-	my $gc_count = $_[0] =~ tr/CG/CG/;	#Count all the Gs and Cs
+	return if $_[0] =~ /[^atcg]/;	#I only calculate for DNA
+	my $gc_count = $_[0] =~ tr/cg/cg/;	#Count all the Gs and Cs
 	my $temperature;
 	if ((scalar length $_[0]) < 14) {
 		$temperature = (4 * $gc_count + 2 * ((scalar length $_[0]) - $gc_count));
@@ -161,7 +137,7 @@ sub temp {
 	$temperature = sprintf("%.2f", $temperature);
 	$temperature = "Estimated melting temperature of ".$temperature;
 	$temperature .= " Celsius, supposing 50mM monovalent cations";
-	return $temperature;
+	return $temperature."\n";
 	#This formula is as cited in Promega's biomath page
 	#	http://www.promega.com/techserv/tools/biomath/calc11.htm
 	#Should upgrade to this more detailed model
@@ -171,17 +147,17 @@ sub temp {
 sub weight {
 	my $weighing_seq = $_[0];
 	my $weight = 0;
-	if ($weighing_seq =~ /[^$nucleotides]/) {	#If it has non-nucleotides, it must be a protein, so use a different table
+	if ($weighing_seq =~ /[^$deoxy_nucleotides$oxy_nucleotides]/) {	#If it has non-nucleotides, it must be a protein, so use a different table
 		foreach (split(//, $weighing_seq)) {
-			$weight += $amino_acid_weight{$_};	#Water already removed in table
+			$weight += $amino_acid_weight{uc $_};	#Water already removed in table
 		}
 		$weight += 18.01528;	#Adding the ends back on, weight of water
 		$weight = sprintf("%.2f", $weight);
 		$weight = "That amino acid sequence weighs about ".$weight;
 		$weight	.= " dalton";
-	} elsif ($weighing_seq =~ /[T]/) {	#If it has at least one T, then it must be DNA, even with dU's
+	} elsif ($weighing_seq =~ /t/) {	#If it has at least one T, then it must be DNA, even with dU's
 		foreach (split(//, $weighing_seq)) {
-			$weight += $nucleotide_weight{'d'.$_} - 18.01528;	#Removing H0 from 5' phosphate and H from 3' hydroxl
+			$weight += $deoxy_nucleotide_weight{uc $_} - 18.01528;	#Removing H0 from 5' phosphate and H from 3' hydroxl
 		}
 		$weight += 18.01528 - 63.98 + 2.016;	#Adding 5' OH and 3' H back on, removing 5' phosphate
 		$weight = sprintf("%.2f", $weight);
@@ -190,32 +166,80 @@ sub weight {
 	}
 	else {	#If it only has AUCG, then it must be RNA, right?
 		foreach (split(//, $weighing_seq)) {
-			$weight += $nucleotide_weight{$_} - 18.01528;	#Removing H0 from 5' phosphate and hydrogen from 3' hydroxl
+			$weight += $oxy_nucleotide_weight{uc $_} - 18.01528;	#Removing H0 from 5' phosphate and hydrogen from 3' hydroxl
 		}
 		$weight += 18.01528 + 159.957418;	#Adding 5' OH and 3' H back on, adding 5' triphosphate
 		$weight = sprintf("%.2f", $weight);
 		$weight = "That 5' triphosphorylated single stranded RNA sequence weighs about ".$weight;
 		$weight	.= " grams per mole";
 	}
-	return $weight;
+	return $weight."\n";
 }
 
-handle remainder => sub {
-	my @args = split /\s+/;
-	return unless @args;
-	my $function = lc shift @args;
-	return $help, html => $help_html if $function eq 'help' or $function eq 'h';
-	my $sequence = uc join "", @args if @args;	# The rest of argument should be seq, even with whitespace mistakes
-	return unless $sequence;			# If no sequence, then quit and return nothing
-	$sequence =~ s/\s//g;				# Delete all whitespace
-	return if $sequence =~ /[^$nucleotides$amino_acids]/i;	#Shouldn't have anything else in there
-	for ($sequence) {
-		my $return_value = $functions{$function}->() if exists $functions{$function};
-		my $return_value_html = $return_value;
-		$return_value_html =~ s/\n/<br>/g if $return_value_html;
-		$return_value_html .= $html_references{$function} if defined $html_references{$function};
-		return $return_value, html => $return_value_html if $return_value;
+triggers query_clean => qr/[$valid]{6,}/i;
+	#qr found in another goodie, right usage?
+	#Also, we could use some better regex examples
+
+handle query_clean => sub {
+	my $query = lc $_;	#query is now lower cased input, raw
+	my $sequence;
+	my $seq_type;		#What kind of sequence?
+	if ($query =~ /(protein)|(amino\s*acid)/) {	#We look for override keywords
+		$seq_type = "Amino Acid";
+	} elsif ($query =~ /rna[\s$]/) {
+		$seq_type = "RNA";
+	} elsif ($query =~ /dna[\s$]/) {
+		$seq_type = "DNA";
 	}
+	#$query =~ s/\s//g;	#We're smushing everything together
+	if (defined $seq_type) {	#If we found a keyword, we use that seq_type
+		$query =~ /([$amino_acids]{6,})/ if $seq_type eq "Amino Acid";
+		$query =~ /([$deoxy_nucleotides]{6,})/ if $seq_type eq "DNA";
+		$query =~ /([$oxy_nucleotides]{6,})/ if $seq_type eq "RNA";
+		$sequence = $1 if $1;	# and find the first contiguous sequence
+		unless ($sequence) {
+			$query =~ /([$nucleotides]{6,})/;
+			return unless $1;
+			$sequence = $1;
+			$sequence =~ tr/u/t/ if $seq_type eq "DNA";
+			$sequence =~ tr/t/u/ if $seq_type eq "RNA";
+		}
+		return unless $sequence;
+	} else {	#Otherwise, we find the first possible sequence >= 6
+		$query =~ /([$valid]{6,})/;
+		$sequence = $1 if $1;		# and infer the seq_type
+		return unless $sequence;
+		if ($sequence =~ /[^$nucleotides]/) {
+			$seq_type = "Amino Acid";
+		} elsif ($sequence =~ /u/) {
+			$seq_type = "RNA";
+		} elsif ($sequence =~ /[$deoxy_nucleotides]/) {
+			$seq_type = "DNA";
+		}	# and then find that contiguous sequence
+	}
+
+	my $answer;		#Now we start building our answer
+	$answer .= "Recognized $seq_type sequence...\n";
+	if ($seq_type ne "Amino Acid") {
+		$answer .="Reversed: ".(uc reverse $sequence)."\n";
+		$sequence =~ tr/atcgu/tagct/;	#Complement
+		$answer .="Complement: ".(uc $sequence)."\n"
+			."Reverse Complement: ".(uc reverse $sequence)."\n";
+		$sequence =~ tr/atcgu/tagct/;	#Put back to original
+	}
+	$answer .= weight($sequence);
+	$answer .= temp($sequence) if $seq_type eq "DNA";
+	
+	$answer .= translate($sequence) if $query =~ /(translat)|(tln)/;
+	#~ "translate"		=> sub { translate($_) },
+	
+	my $answer_html = $answer;
+	$answer_html .= $html_references{"temperature"} if $seq_type eq "DNA";
+		#Add our reference if we did the temperature function
+	$answer_html =~ s/\n/<br>/g if $answer_html;
+		#Turn it into html answer as well
+	return $answer, html => $answer_html if $answer;	#Return the whole lot
+	return;	#For good measure
 };
 
 1;
