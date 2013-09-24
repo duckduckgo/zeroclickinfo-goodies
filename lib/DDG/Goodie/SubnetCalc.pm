94cb88d3c6c03ed1bb234c19950463d9f1576936
Rob Emery <rob@mintsoft.net>
2013-09-24 13:20:51 +0100

    Renaming variables as requested by @jagtalon also harmonising on underscores instead of camelcase

--- lib/DDG/Goodie/SubnetCalc.pm
+++ lib/DDG/Goodie/SubnetCalc.pm
@@ -7,45 +7,44 @@ use warnings;
 use DDG::Goodie;
 
 # TODO: This (sh|c)ould be re-written to be more precise
-triggers query => qr`^([0-9]{1,3}\.){3}([0-9]{1,3})[\s/](([1-3]?[0-9])|(([0-9]{1,3}\.){3}([0-9]{1,3})))$`;
+triggers query => qr_^([0-9]{1,3}\.){3}([0-9]{1,3})[\s/](([1-3]?[0-9])|(([0-9]{1,3}\.){3}([0-9]{1,3})))$_;
 
 zci answer_type => "subnet_info";
 zci is_cached => 1;
 
 attribution github => ['https://github.com/mintsoft', 'mintsoft'];
 
-primary_example_queries '10.1.2.3/25';
 secondary_example_queries '46.51.197.88/255.255.254.0', '176.34.131.233/32';
 
 category 'computing_tools';
 topics 'sysadmin';
 
 handle query => sub {
-	sub ip2str($) {
+	sub int_to_str($) {
 		my ($ip) = @_;
 		return sprintf "%u.%u.%u.%u", ($ip>>24&0xff),($ip>>16&0xff),($ip>>8&0xff),($ip&0xff);
 	}
-	sub array2long(@) {
+	sub ip_to_int(@) {
 		return (int($_[0])<<24) + (int($_[1])<<16) + (int($_[2])<<8) + (int($_[3]));
 	}
 
 	my ($input) = @_;
-	my ($address,$cidr) = split qr`[\s/]`, $input;
+	my ($address,$cidr) = split qr_[\s/]_, $input;
 	
 	my @octlets = split /\./,$address;
 	for (@octlets) {
 		return if int($_)>255;
 	}
 
-	my $addressRaw = array2long(@octlets);
-	my $cidrHostMaskRaw = 0;
-	my $cidrRaw = 0;
+	my $address_raw = ip_to_int(@octlets);
+	my $wildcard_mask = 0;
+	my $mask = 0;
 
 	# Convert CIDR & Subnets
 	if ($cidr =~ /^[0-9]+$/) {
 		return if ($cidr>32 || $cidr<1);
-		$cidrHostMaskRaw = 2**(32-$cidr)-1;	
-		$cidrRaw = 0xffffffff ^ $cidrHostMaskRaw;
+		$wildcard_mask = 2**(32-$cidr)-1;	
+		$mask = 0xffffffff ^ $wildcard_mask;
 	}
 	elsif ($cidr =~ /([0-9]{1,3}\.){3}([0-9]{1,3})/) {
 		my @cidr_octlets = split /\./,$cidr;
@@ -53,38 +52,38 @@ handle query => sub {
 			return if int($_)>255;
 		}
 	
-		$cidrRaw = array2long(@cidr_octlets);
-		my $cidrBinary = sprintf("%b",$cidrRaw);
-		return unless ($cidrBinary =~ /^1+0*$/);
-		$cidrHostMaskRaw = 0xffffffff ^ $cidrRaw;
-		$cidr = ($cidrBinary =~ tr/1//);	#count number of 1's
+		$mask = ip_to_int(@cidr_octlets);
+		my $mask_binary = sprintf("%b",$mask);
+		return unless ($mask_binary =~ /^1+0*$/);
+		$wildcard_mask = 0xffffffff ^ $mask;
+		$cidr = ($mask_binary =~ tr/1//);	#count number of 1's
 	}
 	else {
 		return;
 	}
 	
-	my $host = $addressRaw & $cidrHostMaskRaw;
-	my $network = $addressRaw & $cidrRaw;
+	my $host = $address_raw & $wildcard_mask;
+	my $network = $address_raw & $mask;
 	
 	my $start = $network+1;
-	my $end = $network+($cidrHostMaskRaw-1);
-	my $broadcast = $network+$cidrHostMaskRaw;
-	my $hostCount = 1+$end-$start;
+	my $end = $network+($wildcard_mask-1);
+	my $broadcast = $network+$wildcard_mask;
+	my $host_count = 1+$end-$start;
 	
-	my $whichSpecified = "Host #".($host);
-	$whichSpecified = "Network" if ($host==0);
-	$whichSpecified = "Broadcast" if ($host==$hostCount+1);
-	$whichSpecified = "Point-To-Point (".ip2str($end).", ".ip2str($start).")" if ($cidr==31);
-	$whichSpecified = "Host Only" if ($cidr==32);
+	my $which_specified = "Host #".($host);
+	$which_specified = "Network" if ($host==0);
+	$which_specified = "Broadcast" if ($host==$host_count+1);
+	$which_specified = "Point-To-Point (".int_to_str($end).", ".int_to_str($start).")" if ($cidr==31);
+	$which_specified = "Host Only" if ($cidr==32);
 	
-	my $outputStr =	"Network: ".ip2str($network)."/$cidr,".
-					" Netmask: ".ip2str($cidrRaw).",".
-					" $whichSpecified specified";
-	$outputStr .=	", Host Address Range: ".ip2str($start)."-".ip2str($end).",".
-					" $hostCount Usable Addresses,".
-					" Broadcast: ".ip2str($broadcast) 
+	my $output_str =	"Network: ".int_to_str($network)."/$cidr,".
+					" Netmask: ".int_to_str($mask).",".
+					" $which_specified specified";
+	$output_str .=	", Host Address Range: ".int_to_str($start)."-".int_to_str($end).",".
+					" $host_count Usable Addresses,".
+					" Broadcast: ".int_to_str($broadcast) 
 					unless($cidr > 30);
-	return $outputStr;
+	return $output_str;
 
 };
 
