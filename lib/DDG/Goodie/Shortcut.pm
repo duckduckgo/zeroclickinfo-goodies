2435722eda6a0d96f7bef7947a9ae212e9d781a9
dariog88a <dariog88@gmail.com>
2013-10-25 17:32:51 -0300

    Simplified code to get the column content.

--- lib/DDG/Goodie/Shortcut.pm
+++ lib/DDG/Goodie/Shortcut.pm
@@ -20,7 +20,7 @@ attribution github => ['https://github.com/dariog88a','dariog88a'],
 my @shortcuts = share('shortcuts.csv')->slurp(iomode => '<:encoding(UTF-8)');
 
 handle remainder_lc => sub {
-    #leave only letters and spaces (avoid version numbers)
+    #keep only letters and spaces (remove versioning)
     s/[^a-z ]//gi;
     #replace all OS words with starting char
     s/windows|win|xp|vista|seven|eight/W/gi;
@@ -35,36 +35,27 @@ handle remainder_lc => sub {
     $search =~ tr/ / /s; #leave just one blank between words
     $search =~ s/^\s+|\s+$//g; #trim
 
+if(!$os) {$os='';} #line added to avoid error, remove when UA detection added.
+
     my $line;
     foreach (@shortcuts) {
         if($_ =~ /^$search/i) { #matches only the start
             $line = $_;
+            $line =~ s/\R//g; #remove carriage return
             last;
         }
     }
 
-    return if !defined $line;
+    return if !$line;
 
     my @columns = split('\|',$line);
-    my %systems = (W=>'Windows',M=>'Mac OS',L=>'KDE/GNOME');
     my $answer = 'The shortcut for ' . $columns[0];
-    my $keys;
+    my %systems = (W=>'Windows',M=>'Mac OS',L=>'KDE/GNOME');
+    my %keys = (W => $columns[1], M => $columns[2], L => $columns[3]);
 
-if(!$os) {$os='';} #line added to avoid error, remove when UA detection added.
+    return if !$keys{$os};
 
-    #get column content for searched OS
-    if ($os eq 'W') {
-        $keys = $columns[1];
-    } elsif ($os eq 'M') {
-        $keys = $columns[2];
-    } elsif ($os eq 'L') {
-        $keys = $columns[3];
-        $keys =~ s/\R//g; #remove line break
-    }
-
-    return if !$keys;
-
-    if ($os) { $answer .= ' in ' . $systems{$os} . ' is ' . $keys; }
+    if ($os) { $answer .= ' in ' . $systems{$os} . ' is ' . $keys{$os}; }
 
     my $source = '<br/><a href="https://en.wikipedia.org/wiki/Table_of_keyboard_shortcuts">More at Wikipedia</a>';
     return $answer, html => "$answer $source";
