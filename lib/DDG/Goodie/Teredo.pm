f6afd465ba7edcc47d2503dcce3085edc5d19d1f
seanheaton <seanheaton@comcast.net>
2013-10-28 23:17:07 -0400

    Added html output and comments. Also verified that Net::IP doesn't appear to make any network connections.

--- lib/DDG/Goodie/Teredo.pm
+++ lib/DDG/Goodie/Teredo.pm
@@ -23,17 +23,40 @@ attribution github => ['https://github.com/seanheaton','seanheaton'],
 
 
 handle remainder => sub {
-
+	
+	my @output = ();
+	
+	# Create an IPv6 address from the query value
 	my $ip = new Net::IP ($_,6) if $_;
 
+		# Verify the query value is a valid Teredo IPv6 address
 		if ((defined $ip) && ($ip->version() == 6) && (substr($ip->ip(),0,9) eq "2001:0000")) {
 			my $binip = $ip->binip();
-			my $server = new Net::IP (Net::IP::ip_bintoip((substr $binip, 32, 32),4));
-			my $port = 65535 - cnv((substr $binip, 80, 16),2,10);
-			my $client = new Net::IP (Net::IP::ip_bintoip(~(substr $binip, 96, 32),4));
-			return "Teredo Server IPv4: " . $server->ip() . "\nNAT Public IPv4: " . $client->ip() . "\nClient Port: " . $port;
+			
+			# bits 32 to 64 designate IPv4 address of the Teredo server used
+			push @output, (new Net::IP (Net::IP::ip_bintoip((substr $binip, 32, 32),4)));
+
+			# negation of bits 80 to 96, converted to decimal, designate NAT port number
+			push @output, (65535 - cnv((substr $binip, 80, 16),2,10));
+
+			# negation of bits 96 to 128 designate IPv4 address of NAT device
+			push @output, (new Net::IP (Net::IP::ip_bintoip(~(substr $binip, 96, 32),4)));
+			return answer => to_text(@output), html => to_html(@output);
 		}
 	return;
+
+	# Params: server, port, client
+	sub to_text {
+
+		return "Teredo Server IPv4: " . $_[0]->ip() . "\nNAT Public IPv4: " . $_[2]->ip()
+		. "\nClient Port: " . $_[1];
+	}
+
+	# Params: server, port, client
+	sub to_html {
+
+		return "<div><i>Teredo Server IPv4: </i>" . $_[0]->ip() . "</div><div><i>NAT Public IPv4: </i>" . $_[2]->ip() . "</div><div><i>Client Port: </i>" . $_[1] . "</div>";
+	}
 };
 zci is_cached => 1;
 1;
