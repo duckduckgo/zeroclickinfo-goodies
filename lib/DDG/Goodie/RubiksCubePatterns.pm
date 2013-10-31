a53a0e97a79920d93261ab0d17821bfd060402c4
Rob Emery <rob@mintsoft.net>
2013-10-31 21:56:04 +0000

    Switching Rubik's cube patterns to cheatsheet and adding more patterns & html output

--- lib/DDG/Goodie/RubiksCubePatterns.pm
+++ lib/DDG/Goodie/RubiksCubePatterns.pm
@@ -6,7 +6,7 @@ use DDG::Goodie;
 
 primary_example_queries 'rcube stripes';
 secondary_example_queries 'rcube cube in a cube', 'rcube swap centers';
-description 'create interesting patterns from a solved Rubik\'s Cube';
+description "create interesting patterns from a solved Rubik's Cube";
 name 'Rubiks Cube';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/RubiksCubePatterns.pm';
 category 'random';
@@ -14,16 +14,51 @@ topics 'special_interest';
 
 attribution web => ['robert.io', 'Robert Picard'], twitter => '__rlp', github => ['https://github.com/rpicard', 'rpicard'];
 
-triggers start => "rcube", "rubik cube", "rubiks cube", "rubic cube", "rubics cube";
+triggers start =>	"rcube", "rubik", "rubiks", "rubix", 
+					"rubicks", "rubik's", "rubic's", "rubick's", 
+					"rubik cube", "rubiks cube", "rubic cube", "rubics cube";
 
-zci is_cached => 1;
 zci answer_type => "rubiks_cube";
 
 handle remainder => sub {
-	return "F U F R L2 B D' R D2 L D' B R2 L F U F" if lc($_) eq "stripes";
-	return "F L F U' R U F2 L2 U' L' B D' B' L2 U" if lc($_) eq "cube in a cube" or lc($_) eq 'in a cube';
-	return "U D' R L' F B' U D'" if lc($_) eq "swap centers";
-	return;
+
+	our %patterns = (
+		"stripes" => "F U F R L2 B D' R D2 L D' B R2 L F U F",
+		"crosses" => "U F B' L2 U2 L2 F' B U2 L2 U",
+		"swap centers" => "U D' R L' F B' U D'",
+		"checkerboard" => "F2 B2 R2 L2 U2 D2",
+		"cube in a cube" => "F L F U' R U F2 L2 U' L' B D' B' L2 U",
+		"cube in a cube in a cube" => "U' L' U' F' R2 B' R F U B2 U B' L U' F U R F'",	
+		"exchanged peaks" => "F2 R2 D R2 U D F2 D' R' D' F L2 F' D R U'",
+		"t's" => "F2 R2 U2 F' B D2 L2 F B",
+		"anaconda" => "L U B' U' R L' B R' F B' D R D' F'",
+		"python" => "F2 R' B' U R' L F' L F' B D' R B L2",
+		"black mamba" => "R D L F' R L' D R' U D' B U' R' D'",	
+	);
+
+	sub render_text($) {
+		my $name = pop;
+		return "$name: $patterns{$name} \n";
+	}
+	
+	sub render_html($) {
+		my $name = pop;
+		return "<p><i>$name</i>: $patterns{$name}</p>\n"
+	}
+
+	#support British English!
+	s/centre/center/;
+	
+	#show answer
+	return render_text($_), html => render_html($_) if ($patterns{$_});
+
+	#display the cheatsheet
+	my $output = my $html_output = "";
+	foreach my $pattern (keys %patterns) {
+		$output .= render_text($pattern);
+		$html_output .= render_html($pattern);
+	}
+	return $output, html => $html_output;
 };
 
 1;
--- t/RubiksCubePatterns.t
+++ t/RubiksCubePatterns.t
@@ -6,15 +6,23 @@ use Test::More;
 use DDG::Test::Goodie;
 
 zci answer_type => 'rubiks_cube';
-zci is_cached => 1;
 
 ddg_goodie_test(
         [qw(
                 DDG::Goodie::RubiksCubePatterns
         )],
-        'rubics cube stripes' => test_zci("F U F R L2 B D' R D2 L D' B R2 L F U F" ),
-        'rubiks cube cube in a cube' => test_zci("F L F U' R U F2 L2 U' L' B D' B' L2 U" ),
-        'rubic cube swap centers' => test_zci("U D' R L' F B' U D'" )
+        'rubics cube stripes' => test_zci(
+        	"stripes: F U F R L2 B D' R D2 L D' B R2 L F U F \n",
+        	"html" => "<p><i>stripes</i>: F U F R L2 B D' R D2 L D' B R2 L F U F</p>\n"
+       	),
+        'rubiks cube cube in a cube' => test_zci(
+        	"cube in a cube: F L F U' R U F2 L2 U' L' B D' B' L2 U \n",
+        	"html" => "<p><i>cube in a cube</i>: F L F U' R U F2 L2 U' L' B D' B' L2 U</p>\n"
+        ),
+        'rubic cube swap centers' => test_zci(
+        	"swap centers: U D' R L' F B' U D' \n",
+        	"html" => "<p><i>swap centers</i>: U D' R L' F B' U D'</p>\n"
+        )
 );
 
 done_testing;
