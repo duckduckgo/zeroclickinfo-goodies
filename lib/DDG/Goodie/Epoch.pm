080ab1e632790069aafaeac272ddfc5cb5183acd
Michael Smith <crazedpsyc@duckduckgo.com>
2013-11-13 18:52:47 -0700

    Let's not cache the time

--- lib/DDG/Goodie/Epoch.pm
+++ lib/DDG/Goodie/Epoch.pm
@@ -3,7 +3,7 @@ package DDG::Goodie::Epoch;
 use DDG::Goodie;
 use Date::Calc qw(Today_and_Now Mktime);
 
-zci is_cached => 1;
+zci is_cached => 0;
 zci answer_type => "epoch";
 
 primary_example_queries 'epoch';
