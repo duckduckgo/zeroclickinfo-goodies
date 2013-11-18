8b231478dbe4b0971fa6921eddd10ef3681429f4
Tom Bebbington <tophattedcoder@gmail.com>
2013-11-18 00:08:06 +0000

    Stricter regex; improved secondary example

--- lib/DDG/Goodie/LeapYear.pm
+++ lib/DDG/Goodie/LeapYear.pm
@@ -8,7 +8,7 @@ use warnings;
 zci answer_type => "leap_year";
 
 primary_example_queries 'is it a leap year';
-secondary_example_queries 'when is the next leap year';
+secondary_example_queries 'when were the last 50 leap years';
 description 'Check if it is a leap year';
 name 'Leap year';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/LeapYear.pm';
@@ -71,31 +71,32 @@ sub format_year {
     }
 }
 handle remainder => sub {
-    if ($_ =~ /(last|previous) ([0-9]+)/) {
+    print "\n\n$_\n\n";
+    if ($_ =~ /(last|previous) ([0-9]+)$/i) {
         my @years = search_leaps($2, -1, 0);
         @years = map(format_year, @years);
         my @pretty_years = join(', ', @years);
         return "The last $2 leap years were @pretty_years";
-    } elsif ($_ =~ /(next|future) ([0-9]+)/) {
+    } elsif ($_ =~ /(next|future) ([0-9]+)$/i) {
         my @years = search_leaps($2, 1, 0);
         @years = map(format_year, @years);
         my @pretty_years = join(', ', @years);
         return "The $1 $2 leap years will be @pretty_years";
-    } elsif ($_ =~ /(next|future|upcoming)/) {
+    } elsif ($_ =~ /(next|future|upcoming)$/i) {
         my ($nyear) = search_leaps(1, 1, 0);
         $nyear = format_year($nyear);
         return "$nyear will be the $1 leap year";
-    } elsif ($_ =~ /(latest|last|previous)/) {
+    } elsif ($_ =~ /(latest|last|previous)$/i) {
         my ($pyear) = search_leaps(1, -1, 0);
         $pyear = format_year($pyear);
         return "$pyear was the $1 leap year";
-    } elsif ($_ =~ /(most recent)/) {
+    } elsif ($_ =~ /(most recent)$/i) {
         my ($ryear) = search_leaps(1, -1, 1);
         $ryear = format_year($ryear);
         return "$ryear is the $1 leap year";
-    } elsif($_ =~ /(\-?[0-9]+) ?(ad|bce|bc|ce)?/i) {
-        my $cyear = $1;
-        if(defined($2) && $2 =~ /(bce|bc)/i) {
+    } elsif($_ =~ /^(was|is|will) ([0-9]+) ?(ad|bce|bc|ce)?( be)? a$/i) {
+        my $cyear = $2;
+        if(defined($3) && $3 =~ /^(bce|bc)$/i) {
             $cyear = -$cyear;
         }
         my $fyear = format_year($cyear);
@@ -105,7 +106,7 @@ handle remainder => sub {
         } else {
             return "$fyear $is_not_tense{$tense} a leap year";
         }
-    } elsif($_ =~ /current|now|this year|is it (currently )?a|are we in a/) {
+    } elsif($_ =~ /^is it( now | currently)? a$/i) {
         my $fyear = format_year($year);
         if(isleap($year)) {
             return "$fyear is a leap year";
