3d6af383228ea16922069a429c854482d83e8af6
Rob Emery <rob@mintsoft.net>
2013-11-21 19:23:52 +0000

    Sudoku.pm: Fixing indentation, broken css, wrapping html parsing and handling

--- lib/DDG/Goodie/Sudoku.pm
+++ lib/DDG/Goodie/Sudoku.pm
@@ -18,8 +18,32 @@ description 'show a little sudoku you can play in the browser';
 name 'Sudoku';
 category 'random';
 attribution github => ['https://github.com/DrDub', 'DrDub'],
-            web => ['http://duboue.net/', 'Pablo Duboue'],
-            github => ['https://github.com/mintsoft', 'Rob Emery'];
+	web => ['http://duboue.net/', 'Pablo Duboue'],
+	github => ['https://github.com/mintsoft', 'Rob Emery'];
+
+sub apply_css($)
+{
+	my ($html_content) = @_;
+	my $stylesheet = scalar share("style.css")->slurp;
+	return "<style type='text/css'>$stylesheet</style>\n".$html_content;
+}
+
+sub parse_to_html_table(@)
+{
+	my @sudoku_lines = @_;
+	my $html_table = "<table class='sudoku'>\n";
+	for my $line (@sudoku_lines)
+	{
+		my @chars = split(/ /, $line);
+		for my $char (@chars) 
+		{
+			$char = "<input maxlength='1'/>" if $char eq "_";
+		}
+		$html_table .= "<tr><td>" . join("</td><td>", @chars) . "</td></tr>\n";
+	}
+	$html_table .= "</table>";
+	return $html_table;
+}
 
 handle remainder => sub {
 
@@ -28,35 +52,23 @@ handle remainder => sub {
 	my($difficulty) = m/^(average|medium|hard)?$/;
 	$difficulty = "easy" unless ($difficulty);
 
-    my $sudoku = Games::Sudoku::Component->new(size => 9);
+	my $sudoku = Games::Sudoku::Component->new(size => 9);
 
-    #proportion of the grid to be blank
-    my $blanks = 0.25;
-    $blanks = 0.75 if ($difficulty eq "hard");
-    $blanks = 0.5 if ($difficulty eq "medium" || $difficulty eq "average");
+	#proportion of the grid to be blank
+	my $blanks = 0.25;
+	$blanks = 0.75 if ($difficulty eq "hard");
+	$blanks = 0.5 if ($difficulty eq "medium" || $difficulty eq "average");
 
-    $sudoku->generate(blanks => (9 ** 2) * $blanks);
+	$sudoku->generate(blanks => (9 ** 2) * $blanks);
 	my $str_output = $sudoku->as_string();
 	
 	#switch 0 to more sensible placeholders
 	$str_output =~ s/0/_/g;
 	
 	my @sudoku_lines = split(/\n/, $str_output);
-
-	my $html_table = "";
-	for my $line (@sudoku_lines)
-	{
-		my @chars = split(/ /, $line);
-		for my $char (@chars) 
-		{
-			$char = "<input maxlength='1'/>" if $char eq "_";
-		}
-		
-		$html_table .= "<tr><td>" . join("</td><td>", @chars) . "</td></tr>\n";
-	}
-	my $stylesheet = scalar share("style.css")->slurp;
-	my $html_output = "<style type='text/css'>$stylesheet</style> \n<table class='sudoku'>\n" . $html_table . "</table>";
-	return $str_output, html => $html_output;
+	my $html_table = parse_to_html_table(@sudoku_lines);
+	
+	return $str_output, html => apply_css($html_table);
 };
 
 1;
