d68edcc100ac8801bb45af9e65fc52c9444951c5
Michael Smith <crazedpsyc@duckduckgo.com>
2013-12-21 14:04:02 -0700

    Add a quick DDG removal answer

--- lib/DDG/Goodie/DuckDuckGo.pm
+++ lib/DDG/Goodie/DuckDuckGo.pm
@@ -23,6 +23,8 @@ my %data = (
     dax_html            => "<img src='https://duckduckgo.com/assets/logo_header.v101.png' alt='Dax' /><br/>I am the duck. Dax the duck.",
     irc                 => "DuckDuckGo's official IRC channel is #duckduckgo on irc.freenode.net",
     irc_html            => "DuckDuckGo's official IRC channel is <a href='http://webchat.freenode.net/?channels=duckduckgo'>#duckduckgo</a> on <a href='http://freenode.net/'>irc.freenode.net</a>",
+    remove              => "To remove DuckDuckGo from your browser, take a look at https://duck.co/help/desktop. Let us know why you are leaving: https://duckduckgo.com/feedback",
+    remove_html         => qq(To remove DuckDuckGo from your browser, look <a href="https://duck.co/help/desktop">here</a> for your web browser.<br/>In short, check your browser's addons/extensions for DuckDuckGo, then check the search settings.<br/>Please <a href="https://duckduckgo.com/feedback">let us know</a> why you are leaving!),
     (map {
         $_              => "Zero Click Info is the term DuckDuckGo uses for these boxes, which often provide useful instant answers above traditional results.",
         } qw/zeroclickinfo zeroclick 0click 0clickinfo/),
@@ -45,7 +47,7 @@ attribution twitter => 'crazedpsyc',
 handle query_lc => sub {
     s/\W//g;
     return if $_ eq 'irc';
-    s/^duckduckgo|repo(?:sitory)?//g;
+    s/^?(?:ddg|duckduckgo)\$?|repo(?:sitory)?//g;
     return unless exists $data{$_} and my $answer = $data{$_};
     return $answer unless exists $data{"${_}_html"};
     return $answer, html => $data{"${_}_html"};
