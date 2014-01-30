de482d3339368d51500bba5535a055b9c03394dc
Michael Smith <crazedpsyc@duckduckgo.com>
2014-01-30 08:45:41 -0700

    Add a WhereAmI goodie

---
+++ lib/DDG/Goodie/WhereAmI.pm
@@ -0,0 +1,34 @@
+package DDG::Goodie::WhereAmI;
+# ABSTRACT: Display the user's perceived location from GeoIP
+
+use DDG::Goodie;
+
+zci is_cached => 0;
+
+triggers startend => 'where am i', 'my location';
+
+primary_example_queries 'Where am I?';
+secondary_example_queries 'my location';
+description 'display your perceived location';
+name 'WhereAmI';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/WhereAmI.pm';
+category 'computing_tools';
+topics 'travel';
+attribution twitter => 'crazedpsyc',
+            cpan    => 'CRZEDPSYC' ;
+
+handle remainder => sub {
+    return if length($_) or !$loc or !$loc->city;
+
+    p $loc;
+    my $answer = 'You appear to be in ' .
+        $loc->city.', '.
+        ($loc->region_name ? $loc->region_name.', ' : '').
+        $loc->country_name.
+        ' (Lat:'.$loc->latitude.', Lon:'.$loc->longitude.').'
+        ;
+
+    return $answer, html => $answer . '<br/><i>This is the perceived source of your connection. It is never saved here!</i>'
+};
+
+1;
