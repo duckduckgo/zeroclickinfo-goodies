f092bf7aab8023b0099250ae720c6743b5500d9c
elohmrow <bradley@pvnp.us>
2014-01-31 19:14:55 +0000

    re-factored

--- lib/DDG/Goodie/Conversions.pm
+++ lib/DDG/Goodie/Conversions.pm
@@ -2,74 +2,67 @@ package DDG::Goodie::Conversions;
 # ABSTRACT: convert between various weights and measures
 
 # @todo: set significant digits
-# @todo: handle multi-word units
 # @todo: handle plural units
 
 use DDG::Goodie;
 use Scalar::Util qw(looks_like_number);
 
+# metric ton is base unit
 # known SI units and aliases / plurals / common uses:
-my @units = (
-        {
-            'unit'          => 'metric ton',
-            'multiple'      => '1',
-            'aliases'       => ['tonne', 't', 'mt', 'te', 'metric'],
-        },  
-        { 
-            'unit'          => 'kilogram',
-            'multiple'      => '1000',  
-            'aliases'       => ['kilo', 'kg', 'kilogramme', 'kilogram'],
-        }, 
-        {
-            'unit'          => 'gram',                          # units
-            'multiple'      => '1000000',                       # number of units in 1 metric ton
-            'aliases'       => ['gram', 'g', 'gm', 'gramme'],   # alternate units
-        },
-        {
-            'unit'          => 'milligram',
-            'multiple'      => '1000000000',
-            'aliases'       => ['milligram', 'mg'],
-        },
-        {
-            'unit'          => 'microgram',
-            'multiple'      => '1000000000000',
-            'aliases'       => ['microgram', 'mcg'],
-        },
-        {
-            'unit'          => 'long ton',
-            'multiple'      => '0.984207',
-            'aliases'       => ['long', 'weight', 'imperial'],
-        },  
-        {
-            'unit'          => 'short ton', 
-            'multiple'      => '1.10231',
-            'aliases'       => ['ton',  'short'],
-        },  
-        {
-            'unit'          => 'stone',
-            'multiple'      => '157.473',
-            'aliases'       => ['st', 'stone'],
-        },
-        {
-            'unit'          => 'pound',
-            'multiple'      => '2204.62',
-            'aliases'       => ['lb', 'pound', 'lbm'], 
-        }, 
-        {
-            'unit'          => 'ounce',
-            'multiple'      => '35274',
-            'aliases'       => ['ounce', 'oz'],
-        },
-);
-# to limit math, convert entered unit to metric ton, then multiply factor
-       
-# build triggers based on available conversion units:
-my @triggers = ();
-foreach my $unit (@units) {
-    push(@triggers, @{$unit->{'aliases'}});
-}
+my %units = (
+    # metric ton:
+    'metric ton'   => '1',
+    'tonne'        => '1',
+    't'            => '1',
+    'mt'           => '1',
+    'te'           => '1',
+    
+    # kilogram:
+    'kilogram'     => '1000',
+    'kg'           => '1000',
+    'kilo'         => '1000',
+    'kilogramme'   => '1000',
+     
+    # gram:
+    'gram'         => '1000000',
+    'g'            => '1000000',
+    'gm'           => '1000000',
+    'gramme'       => '1000000',
+    
+    # milligram:
+    'milligram'    => '1000000000',
+    'mg'           => '1000000000',
+    
+    # microgram:
+    'microgram'    => '1000000000',
+    'mcg'          => '1000000000',
+    
+    # long ton:
+    'long ton'     => '0.984207',
+    'weight ton'   => '0.984207',
+    'imperial ton' => '0.984207',
+    
+    # short ton:
+    'short ton'    => '1.10231',
+    'ton'          => '1.10231',
 
-triggers startend => @triggers;
+    # stone:
+    'stone'        => '157.473',
+    'st'           => '157.473',
+
+    # pound:
+    'pound'        => '2204.62',
+    'lb'           => '2204.62',
+    'lbm'          => '2204.62',
+    'pound mass'   => '2204.62',
+
+    # ounce:
+    'ounce'        => '35274',
+    'oz'           => '35274',
+);
+
+# build triggers based on available conversion units:
+triggers startend => keys %units;
 
 name                      'Conversions';
 description               'convert between various weights and measures';
@@ -81,21 +74,16 @@ code_url                  'https://github.com/duckduckgo/zeroclickinfo-goodies/b
 attribution                github  => ['https://github.com/elohmrow', '/bda'],
                            email   => ['bradley@pvnp.us'];
 
+zci answer_type => 'conversions';
+
 handle query => sub {
     my @args = split(/\s+/, $_);
-    
-    ###
-    ###my %args = map { $_ => 1 } @args;
-    ###@args{@args} = undef;
-    ###my @matches = grep { exists $args{$_} } @triggers;
-    ###
-    ###return unless scalar @matches == 2;
-    ###
 
     my @matches = ();
     my $factor = 1;
+ 
     foreach my $arg (@args) {
-        if (lc($arg) ~~ @triggers) {
+        if (exists $units{lc($arg)}) {
             push(@matches, lc($arg));
         }
         if (looks_like_number($arg)) {
@@ -105,20 +93,8 @@ handle query => sub {
     
     return unless scalar @matches == 2; # minimum conversion requires two @triggers
 
-    # either pass over array again here, or %units must contain all aliases as keys.
-    my $fromMultiple = 1;
-    my $toMultiple = 1;
-    foreach my $unit (@units) {
-        if ($matches[0] ~~ @{$unit->{'aliases'}}) {
-            $fromMultiple = $unit->{'multiple'}; 
-        }
-        if ($matches[1] ~~ @{$unit->{'aliases'}}) {
-            $toMultiple = $unit->{'multiple'}; 
-        }
-    }
-
     # run the conversion:
-    return "$factor $matches[0] is " . $factor * ($toMultiple / $fromMultiple) . " $matches[1]";
+    return "$factor $matches[0] is " . $factor * ($units{$matches[1]} / $units{$matches[0]}) . " $matches[1]";
 };
 
 
--- t/Conversions.t
+++ t/Conversions.t
@@ -6,6 +6,8 @@ use warnings;
 use Test::More;
 use DDG::Test::Goodie;
 
+zci answer_type => 'conversions';
+
 ddg_goodie_test(
     [
         'DDG::Goodie::Conversions'
@@ -13,67 +15,14 @@ ddg_goodie_test(
     
     # @todo: could create a complete test suite for all toUnits->fromUnits
 
-    # failure cases first, as they are most important ...
-    # [ 1]: good $factor, bad $toUnits, bad $fromUnits
-    'convert 12 pipes to tokes' =>
-        test_zci(
-            '',
-        );
-    
-    # [ 2]: good $factor, bad $toUnits, good $fromUnits
-    'convert 0.99 dogs to ounces' =>
-        test_zci(
-            '',
-        );
-    
-    # [ 3]: good $factor, good $toUnits, bad $fromUnits
-    'convert 1 ton to kitty litter' =>
-        test_zci(
-            '',
-        );
-    
-    # [ 4]: bad $factor, bad $toUnits, bad $fromUnits
-    'convert bradley david to andersen' =>
-        test_zci(
-            '',
-        );
-    
-    # [ 5]: bad $factor, bad $toUnits, good $fromUnits
-    'convert ddg ducks to metric tons' =>
-        test_zci(
-            '',
-        );
-    
-    # [ 6]: bad $factor, good $toUnits, bad $fromUnits
-    'convert soil grams to beans' =>
-        test_zci(
-            '',
-        );
-
-    # and now a few success cases ... let's round it up to 10:
-    # [ 7]: good $factor, good $toUnits, good $fromUnits
-    'convert 5 oz to g' =>
-        test_zci(
-            '5 ozs is 141.747462720417 gs.',
-        );
-    
-    # [ 8]: good $factor, good $toUnits, good $fromUnits
-    'convert 1 ton to long tons' =>
-        test_zci(
-            '1 ton is 0.892858633233845 long ton.',
-        );
-
-    # [ 9]: good $factor, good $toUnits, good $fromUnits
-    'convert 158 ounces to lbms' =>
-        test_zci(
-            '158 ounces is 9.87497760390089 lbms.',
-        );
-
-    # [10]: good $factor, good $toUnits, good $fromUnits
-    'convert 0.111 stone to pound' =>
-        test_zci(
-            '0.111 stone is 1.55399859023452 pounds.',
-        );
+    'convert 5 oz to g'            => test_zci('5 oz is 141.747462720417 g',),
+    'convert 1 ton to long ton'    => test_zci('1 ton is 0.892858633233845 long ton',),
+    'convert 158 ounce to lbm'     => test_zci('158 ounce is 9.87497760390089 lbm',),
+    'convert 0.111 stone to pound' => test_zci('0.111 stone is 1.55399859023452 pound',),
+    'mcg to mcg'                   => test_zci('1 mcg is 1 mcg',),
+    '3 kilogramme to pound'        => test_zci('3 kilogramme is 6.61386 pound',),
+    '1.3 metric ton to short ton'  => test_zci('1.3 metric ton is 1.433 short ton',),
+    'stone pound'                  => test_zci('1 stone is 13.9999872994101 pound',),
 );
 
 done_testing;
