5df21bd93b8c83f9f35d3afa16634636e4ac1dd5
Matthew Ramina <mattramina@gmail.com>
2014-01-31 19:33:35 -0500

    MakeMeASandwich- tabs to spaces, fixed code link

--- lib/DDG/Goodie/MakeMeASandwich.pm
+++ lib/DDG/Goodie/MakeMeASandwich.pm
@@ -8,20 +8,20 @@ description 'Responds in accordance to xkcd #149';
 primary_example_queries 'make me a sandwich', 'sudo make me a sandwich';
 category 'special';
 topics 'geek';
-code_url 'https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Goodie/MakeMeASandwich.pm';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/MakeMeASandwich.pm';
 attribution twitter => 'mattr555',
-			github => ['mattr555', 'Matt Ramina'];
+            github => ['https://github.com/mattr555/', 'Matt Ramina'];
 
 zci is_cached => 1;
 
 triggers end => 'make me a sandwich';
 
 handle remainder => sub {
-	return 'Okay.', 
-		html => 'Okay. <a href="http://xkcd.com/149/">XKCD</a>' if $_ eq 'sudo';
-	return 'What? Make it yourself.', 
-		html => 'What? Make it yourself. <a href="http://xkcd.com/149/">XKCD</a>' if not $_;
-	return;
+    return 'Okay.', 
+        html => 'Okay. <a href="http://xkcd.com/149/">XKCD</a>' if $_ eq 'sudo';
+    return 'What? Make it yourself.', 
+        html => 'What? Make it yourself. <a href="http://xkcd.com/149/">XKCD</a>' if not $_;
+    return;
 };
 
 1;
