8e37201c1eae287b199e7dc893aaf7a99e05d0b7
Matthew Ramina <mattramina@gmail.com>
2014-02-15 19:54:21 -0500

    Added Stopwatch, an interactive stopwatch

---
+++ lib/DDG/Goodie/Stopwatch.pm
@@ -0,0 +1,22 @@
+package DDG::Goodie::Stopwatch;
+
+use DDG::Goodie;
+
+name 'Stopwatch';
+description 'Displays a stopwatch';
+primary_example_queries 'stopwatch';
+category 'special';
+topics 'everyday', 'science', 'words_and_games';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/Stopwatch.pm';
+attribution twitter => 'mattr555',
+            github => ['https://github.com/mattr555/', 'Matt Ramina'];
+
+triggers end => 'stopwatch';
+
+my $html = share('stopwatch.html')->slurp;
+
+handle remainder => sub {
+    return 'stopwatch', html => $html if $_ eq '';
+};
+
+1;
