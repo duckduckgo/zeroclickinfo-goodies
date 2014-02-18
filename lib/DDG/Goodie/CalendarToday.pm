0719a58dedce0020f998afb2aec5a738e60b826c
Harald Ulver <webmaster@quovadit.org>
2014-02-18 18:58:02 +0100

    move data outside of handle function

--- lib/DDG/Goodie/CalendarToday.pm
+++ lib/DDG/Goodie/CalendarToday.pm
@@ -17,16 +17,15 @@ attribution email   => ['webmaster@quovadit.org'];
 triggers start => 'calendar';
 
 
-handle remainder => sub {
-  
-  # define days & months
-  my @weekDays = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
-  my %monthsShort = ("jan" => 1, "feb" => 2, "mar" => 3, "apr" => 4, "may" => 5, "jun" => 6, 
+# define days & months
+my @weekDays = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+my %monthsShort = ("jan" => 1, "feb" => 2, "mar" => 3, "apr" => 4, "may" => 5, "jun" => 6, 
 	"jul" => 7, "aug" => 8, "sep" => 9, "oct" => 10, "nov" => 11, "dec" => 12);
   
-  my %monthsLong = ("january" => 1, "february" => 2, "march" => 3, "april" => 4, "may" => 5, "june" => 6, 
+my %monthsLong = ("january" => 1, "february" => 2, "march" => 3, "april" => 4, "may" => 5, "june" => 6, 
 	"july" => 7, "august" => 8, "september" => 9, "october" => 10, "november" => 11, "december" => 12);
-  
+
+handle remainder => sub {
   
   # Default: today
   my ($day, $monthId, $yearOffset) = (localtime)[3,4,5];
