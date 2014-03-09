abf09813ca6b9b1682296bd891dc1ccfe21b5df4
Lars Jansøn Engvik <lars@engvik.nu>
2014-03-09 17:05:19 +0000

    Adding FIGlet support.

---
+++ lib/DDG/Goodie/FIGlet.pm
@@ -0,0 +1,41 @@
+package DDG::Goodie::FIGlet;
+# ABSTRACT: Uses FIGlet to make large letters out of ordinary text.
+
+use DDG::Goodie;
+use Text::FIGlet;
+
+triggers startend => 'figlet';
+primary_example_queries 'figlet DuckDuckGo';
+secondary_example_queries 'figlet graceful DuckDuckGo';
+
+name 'FIGlet';
+description 'Uses FIGlet to make large letters out of ordinary text.';
+category 'transformations';
+topics 'words_and_games';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/Figlet.pm';
+attribution
+    web => ['http://engvik.nu', 'Lars Jansøn Engvik'],
+    github => [ 'larseng', 'Lars Jansøn Engvik'];
+
+zci answer_type => 'figlet';
+zci is_cached => 1;
+
+my @fonts = share()->children;
+my $figlet;
+
+handle remainder => sub {
+    m/^\s*(\w+)/;
+
+    # Checks if the first word is a font and uses it if it's not the only
+    # word to figify. Else uses the standard font.
+    if ($1 && $_ ne $1 && grep /$1/i, @fonts) {
+        $figlet = Text::FIGlet->new(-f=>lc $1, -d=>share())->figify(-A=>substr $_, length $1, length $_)
+    } elsif ($_) {
+        $figlet = Text::FIGlet->new(-f=>"standard", -d=>share())->figify(-A=>$_)
+    }
+    
+    return $figlet, html => "<pre>$figlet</pre>" if $figlet;
+    return;
+};
+
+1;
