076a76fa5d834d099772fe8ae783d2a312f281b1
Jag Talon <jagtalon@duckduckgo.com>
2014-04-17 10:51:12 -0400

    ParseCron: Revert to old version.

--- lib/DDG/Goodie/ParseCron.pm
+++ lib/DDG/Goodie/ParseCron.pm
@@ -16,18 +16,7 @@ triggers start => 'crontab', 'cron', 'cronjob';
 
 zci is_cached => 0;
 
-primary_example_queries   'crontab * */3 * * *';
-=comment
-                                    * */3 * * *
-                                    * * * * * *
-                                    | | | | | | 
-                                    | | | | | +-- Year              (range: 1900-3000)
-                                    | | | | +---- Day of the Week   (range: 1-7, 1 standing for Monday)
-                                    | | | +------ Month of the Year (range: 1-12)
-                                    | | +-------- Day of the Month  (range: 1-31)
-                                    | +---------- Hour              (range: 0-23)
-                                    +------------ Minute            (range: 0-59)
-=cut
+primary_example_queries 'crontab * */3 * * *';
 secondary_example_queries 'crontab 42 12 3 Feb Sat';
 description 'show the next occurrence of a cron job in human-readable form';
 name 'ParseCron';
@@ -39,35 +28,23 @@ attribution web     => [ 'http://indeliblestamp.com', 'Arun S' ],
 
 handle remainder => sub {
     my $crontab = $_;
-    
     # We replace Jan,Feb.. and Mon,Tue.. with 1,2..
-    foreach (0 .. $#mon) {
-	    my $newmonth = $_ + 1;
-	    $crontab =~ s/$mon[$_]/$newmonth/;
+    foreach (0..$#mon) {
+	my $newmonth=$_+1;
+	$crontab =~ s/$mon[$_]/$newmonth/;
     }
-
-    foreach (0 .. $#day) {
-	    my $newday = $_ + 1;
-	    $crontab =~ s/$day[$_]/$newday/;
+    foreach (0..$#day) {
+	my $newday=$_+1;
+	$crontab =~ s/$day[$_]/$newday/;
     }
-
-    print STDERR 'CRONTAB', "\n", $crontab, "\n"; 
     my $cron = Schedule::Cron::Events->new($crontab) or return;
-    print STDERR 'CRON', "\n", $cron, "\n";
-    use Data::Dumper; print STDERR Dumper($cron);
     my $text;
-    
     # Fix for issue #95: Show the next 3 events instead of just one.
-    for (my $count = 1; $count <= 10; $count++) {
-        my ($sec, $min, $hour, $day, $month, $year) = $cron->nextEvent;
-        
-        $text .= sprintf("%2d:%02d:%02d on %d %s, %d\n", $hour, $min, $sec, $day, $mon[$month], ($year+1900));
+    for (my $count=1;$count<=3;$count++) {
+      my ($sec, $min, $hour, $day, $month, $year) = $cron->nextEvent;
+      $text .= sprintf("%2d:%02d:%02d on %d %s, %d\n", $hour, $min, $sec, $day, $mon[$month], ($year+1900));
     }
-    print STDERR 'TEXT', "\n", $text, "\n";
-    return "Cron will schedule the job at this frequency: \n$text";
+    return "Cron will schedule the job at this frequency: \n$text" if $_;
     return;
 };
-
-
-
 1;
