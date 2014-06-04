b4a4bc282673200d7eb5b53aab63f47d1f41b908
David Wilkins <david@wilkox.org>
2014-06-04 10:22:39 +0800

    Add Reverse Complement goodie with tests

---
+++ lib/DDG/Goodie/ReverseComplement.pm
@@ -0,0 +1,45 @@
+package DDG::Goodie::ReverseComplement;
+# ABSTRACT: Give the DNA reverse complement of a DNA or RNA sequence.
+
+use DDG::Goodie;
+
+triggers start => 'reverse complement', 'revcomp';
+zci is_cached => 0;
+
+name 'Reverse Complement';
+description 'Give the DNA reverse complement of a DNA or RNA sequence';
+primary_example_queries 'revcomp AAAACCCGGT';
+category 'transformations';
+topics 'science';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/ReverseComplement.pm';
+attribution github => ['http://github.com/wilkox', 'wilkox'];
+
+handle remainder => sub {
+
+    #Remove 'of' if supplied
+    $_ =~ s/^of\s//g;
+
+    my $sequence = $_;
+
+    #Remove whitespace and dashes and make uppercase
+    $sequence =~ s/\s|-//g;
+    $sequence = uc($sequence);
+
+    #Return nothing if sequence contains characters
+    # other than DNA/RNA bases or standard IUPAC ambiguity codes
+    return if $sequence =~ /[^ATCGURYKMSWBVDH]/;
+
+    #Return nothing if sequence contains both
+    # thymine and uracil (more likely an error than a real molecule)
+    return if $sequence =~ /T/ && $sequence =~ /U/;
+
+    #Complement
+    $sequence =~ tr/ATUCGRYKMSWBVHD/TAAGCYRMKSWVBDH/;
+
+    #Reverse
+    $sequence = reverse($sequence);
+
+    return 'DNA reverse complement of "' . $_ . '": ' . $sequence;
+};
+
+1;
