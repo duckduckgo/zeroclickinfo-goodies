15d416052a66721ccae58658055ea06a1eb1ecb5
David Wilkins <david@wilkox.org>
2014-06-07 21:02:01 +0800

    Add Chinese Zodiac goodie

---
+++ lib/DDG/Goodie/ChineseZodiac.pm
@@ -0,0 +1,118 @@
+package DDG::Goodie::ChineseZodiac;
+# ABSTRACT: Return the Chinese zodiac animal for a given year.
+
+use DDG::Goodie;
+use DateTime::Calendar::Chinese;
+use DateTime::Event::Chinese qw(chinese_new_year_before chinese_new_year_after);
+use utf8;
+
+triggers startend => 'chinese zodiac';
+triggers any => 'chinese zodiac';
+zci is_cached => 1;
+
+name 'Chinese Zodiac';
+description 'Return the Chinese zodiac animal for a given year';
+primary_example_queries 'chinese zodiac for 1969';
+secondary_example_queries '2004 chinese zodiac animal', 'what was the chinese zodiac animal in 1992', 'what will the chinese zodiac animal be for 2056';
+category 'dates';
+topics 'special_interest';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/ChineseZodiac.pm';
+attribution github => ['http://github.com/wilkox', 'wilkox'];
+
+my %standard = (
+  'hare' => 'Rabbit',
+  'dragon' => 'Dragon',
+  'snake' => 'Snake',
+  'horse' => 'Horse',
+  'sheep' => 'Goat',
+  'monkey' => 'Monkey',
+  'fowl' => 'Rooster',
+  'dog' => 'Dog',
+  'pig' => 'Pig',
+  'rat' => 'Rat',
+  'ox' => 'Ox',
+  'tiger' => 'Tiger'
+);
+
+my %character = (
+  'Rabbit' => "\x{5154}",
+  'Dragon' => "\x{9F99}",
+  'Snake' => "\x{86C7}",
+  'Horse' => "\x{9A6C}",
+  'Goat' => "\x{7F8A}",
+  'Monkey' => "\x{7334}",
+  'Rooster' => "\x{9E21}",
+  'Dog' => "\x{72D7}",
+  'Pig' => "\x{732A}",
+  'Rat' => "\x{9F20}",
+  'Ox' => "\x{725B}",
+  'Tiger' => "\x{864E}"
+);
+
+handle remainder => sub {
+
+  #Remove extra words if supplied
+  $_ =~ s/(what)?
+          (is)?
+          (was)?
+          (will)?
+          (the)?
+          (be)?
+          (animal)?
+          (for)?
+          (of)?
+          (a)?
+          (people|someone|person)?
+          (born)?
+          (in)?
+          (year)?
+        //gix;
+
+  #Remove whitespace
+  $_ =~ s/\s//g;
+
+  #Must be a year in the current era (0-9999)
+  return unless $_ =~ /^\d{1,4}$/;
+
+  #Find the Chinese year that aligns 
+  # with the query (presumed Gregorian) year
+  my $year_gregorian = DateTime->new(year => $_, month => 6, time_zone => 'Asia/Shanghai');
+  my $year_chinese = DateTime::Calendar::Chinese->from_object(object => $year_gregorian);
+
+  #Get the inclusive Gregorian date range for the Chinese year
+  #Note that returned dates will be for the 'Asia/Shanghai'
+  # time zone (China Standard Time/CST) as this is where
+  # Chinese New Year is calculated
+  my $year_start = chinese_new_year_before($year_gregorian)->set_time_zone('Asia/Shanghai');
+  my $year_end = chinese_new_year_after($year_gregorian)->subtract(days => 1)->set_time_zone('Asia/Shanghai');
+
+  my $animal = $standard{$year_chinese->zodiac_animal};
+  my $character = $character{$animal};
+
+  my $statement = 'Chinese zodiac animal for ' . format_datetime($year_start) . "\x{2013}" . format_datetime($year_end);
+
+  return answer => $animal, html => wrap_html($character, $animal, $statement);
+};
+
+sub format_datetime {
+  my $dt = shift;
+  my $formatted = $dt->strftime('%b %e, %Y');
+  $formatted =~ s/\s\s/ /g;
+  return $formatted;
+}
+
+# This function adds some HTML and styling to our output
+# so that we can make it prettier (copied from the Conversions
+# goodie)
+sub append_css {
+  my $html = shift;
+  my $css = share("style.css")->slurp;
+  return "<style type='text/css'>$css</style>$html";
+}
+
+sub wrap_html {
+  my ($character, $animal, $statement) = @_;
+  return append_css("<div class='zci--chinesezodiac'><div class='zodiaccharacter'>$character ($animal)</div><span class='statement'>$statement</span></div>");
+}
+
+1;
---
+++ t/ChineseZodiac.t
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use DDG::Test::Goodie;
+
+zci answer_type => 'chinesezodiac';
+zci is_cached => 1;
+
+ddg_goodie_test(
+	[qw(
+		DDG::Goodie::ChineseZodiac
+	)],
+
+  #Primary example
+  'chinese zodiac for 1969' => test_zci('Rooster', html => qr/Rooster/),
+
+  #Secondary examples
+  '2004 chinese zodiac animal' => test_zci('Monkey', html => qr/Monkey/),
+  'what was the chinese zodiac animal in 1992' => test_zci('Monkey', html => qr/Monkey/),
+  'what will the chinese zodiac animal be for 2056' => test_zci('Rat', html => qr/Rat/),
+
+  #Primary example with different query formats
+  '1969 chinese zodiac animal' => test_zci('Rooster', html => qr/Rooster/),
+  'what was the chinese zodiac animal for 1969' => test_zci('Rooster', html => qr/Rooster/),
+  'what will the chinese zodiac animal be for people born in the year 1969' => test_zci('Rooster', html => qr/Rooster/),
+  'chinese zodiac for a person born in 1969' => test_zci('Rooster', html => qr/Rooster/),
+  'chinese zodiac of 1969' => test_zci('Rooster', html => qr/Rooster/),
+
+  #Test some different years
+  # Taken from http://www.chinesezodiac.com/calculator.php
+  'chinese zodiac animal for 1924' => test_zci('Rat', html => qr/Rat/),
+  'chinese zodiac animal for 1929' => test_zci('Snake', html => qr/Snake/),
+  'chinese zodiac animal for 1934' => test_zci('Dog', html => qr/Dog/),
+  'chinese zodiac animal for 1939' => test_zci('Rabbit', html => qr/Rabbit/),
+  'chinese zodiac animal for 1944' => test_zci('Monkey', html => qr/Monkey/),
+  'chinese zodiac animal for 1949' => test_zci('Ox', html => qr/Ox/),
+  'chinese zodiac animal for 1954' => test_zci('Horse', html => qr/Horse/),
+  'chinese zodiac animal for 1959' => test_zci('Pig', html => qr/Pig/),
+  'chinese zodiac animal for 1964' => test_zci('Dragon', html => qr/Dragon/),
+  'chinese zodiac animal for 1969' => test_zci('Rooster', html => qr/Rooster/),
+  'chinese zodiac animal for 1974' => test_zci('Tiger', html => qr/Tiger/),
+  'chinese zodiac animal for 2027' => test_zci('Goat', html => qr/Goat/),
+  'chinese zodiac animal for 2040' => test_zci('Monkey', html => qr/Monkey/),
+
+  #Test for correct date ranges
+  # Taken from http://www.chinesezodiac.com/calculator.php
+  'chinese zodiac animal for 1925' => test_zci('Ox', html => qr/Jan\s24,\s1925.Feb\s12,\s1926/),
+  'chinese zodiac animal for 1937' => test_zci('Ox', html => qr/Feb\s11,\s1937.Jan\s30,\s1938/),
+  'chinese zodiac animal for 1953' => test_zci('Snake', html => qr/Feb\s14,\s1953.Feb\s2,\s1954/),
+  'chinese zodiac animal for 1973' => test_zci('Ox', html => qr/Feb\s3,\s1973.Jan\s22,\s1974/),
+  'chinese zodiac animal for 1997' => test_zci('Ox', html => qr/Feb\s7,\s1997.Jan\s27,\s1998/),
+  'chinese zodiac animal for 2013' => test_zci('Snake', html => qr/Feb\s10,\s2013.Jan\s30,\s2014/),
+  'chinese zodiac animal for 2017' => test_zci('Rooster', html => qr/Jan\s28,\s2017.Feb\s15,\s2018/),
+  'chinese zodiac animal for 2041' => test_zci('Rooster', html => qr/Feb\s1,\s2041.Jan\s21,\s2042/),
+
+);
+
+done_testing;
+
