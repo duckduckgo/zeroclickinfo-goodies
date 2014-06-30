3fcb5dc28bb37ad82dfc60b8df2ccca53c944ac0
pixunil <pixunil@gmx.de>
2014-06-30 13:03:53 +0200

    Geometry.pm -  Added temporarily a 'evil' line for bugtracking

--- lib/DDG/Goodie/Geometry.pm
+++ lib/DDG/Goodie/Geometry.pm
@@ -195,6 +195,6 @@ handle remainder => sub {
 		}
 	}
 
-	return;
+	return '', html => '<div>Lorem Ipsum</div><svg width="100" height="100"><rect x="0" y="0" width="100" height="100"></rect></svg><span>foobar</span>';
 };
 1;
