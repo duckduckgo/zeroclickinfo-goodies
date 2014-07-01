8a375d89c0f115932d06fe12173c2535dbaa92c4
loganom <logan@mccamon.org>
2014-07-01 01:24:26 +0000

    Paper.pm: add metadata

--- lib/DDG/Goodie/Paper.pm
+++ lib/DDG/Goodie/Paper.pm
@@ -1,8 +1,25 @@
 package DDG::Goodie::Paper;
+# ABSTRACT: Return the dimensions of a defined paper size
 
 use DDG::Goodie;
 
-triggers any => 'paper', 'size';
+zci answer_type => "paper";
+zci is_cached   => 1;
+
+triggers any => 'paper size', 'dimensions', 'paper dimension', 'paper dimensions';
+
+primary_example_queries 'letter paper size';
+secondary_example_queries 'a1 paper size', 'a9 paper dimension';
+description 'Lookup the size of standard paper sizes';
+name 'Paper';
+code_url
+    'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/Paper.pm';
+category 'conversions';
+topics 'special_interest';
+attribution github => 'loganom',
+			twitter => 'loganmccamon',
+            github => 'mattlehnig';
+
 
 handle query_lc => sub {
 	return unless my ($s, $l, $n) = $_ =~ /^((?:(a|b|c)(\d{0,2}))|legal|letter|junior\s*legal|ledger|tabloid|hagaki)\s+paper\s+(?:size|dimm?ensions?)$/i;
