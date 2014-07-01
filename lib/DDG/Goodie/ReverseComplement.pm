fbf46d35d9fca7568c41daeb72f07a76e9f5d45a
David Wilkins <david@wilkox.org>
2014-07-01 18:13:42 +0800

    Improve triggers for ReverseComplement goodie

--- lib/DDG/Goodie/ReverseComplement.pm
+++ lib/DDG/Goodie/ReverseComplement.pm
@@ -4,7 +4,7 @@ package DDG::Goodie::ReverseComplement;
 use DDG::Goodie;
 use feature 'state';
 
-triggers startend => 'reverse complement', 'revcomp';
+triggers any => 'reverse complement', 'revcomp';
 zci is_cached => 1;
 
 name 'Reverse Complement';
@@ -17,33 +17,36 @@ attribution github => ['http://github.com/wilkox', 'wilkox'];
 
 handle remainder => sub {
 
-    #Remove 'of' if supplied
-    $_ =~ s/^of\s//g;
+  my $sequence = $_;
 
-    my $sequence = $_;
+  #Remove extra words if supplied
+  $sequence =~ s/\bof\b//gi;
+  $sequence =~ s/\bsequence\b//gi;
+  $sequence =~ s/\b[DR]NA\b//gi;
+  $sequence =~ s/\bnucleotide\b//gi;
 
-    #Remove whitespace and dashes and make uppercase
-    $sequence =~ s/\s|-//g;
-    $sequence = uc($sequence);
+  #Remove whitespace and dashes and make uppercase
+  $sequence =~ s/\s|-//g;
+  $sequence = uc($sequence);
 
-    #Return nothing if sequence contains characters
-    # other than DNA/RNA bases or standard IUPAC ambiguity codes
-    return if $sequence =~ /[^ATCGURYKMSWBVDHN]/;
+  #Return nothing if sequence contains characters
+  # other than DNA/RNA bases or standard IUPAC ambiguity codes
+  return if $sequence =~ /[^ATCGURYKMSWBVDHN]/;
 
-    #DNA contains thymine (T) but not uracil (U);
-    # RNA contains U but not T (with some extremely
-    # rare exceptions). Hence, if the sequence
-    # contains both U and T it's more likely to be an
-    # error than a real molecule so should return nothing.
-    return if $sequence =~ /T/ && $sequence =~ /U/;
+  #DNA contains thymine (T) but not uracil (U);
+  # RNA contains U but not T (with some extremely
+  # rare exceptions). Hence, if the sequence
+  # contains both U and T it's more likely to be an
+  # error than a real molecule so should return nothing.
+  return if $sequence =~ /T/ && $sequence =~ /U/;
 
-    #Complement, using standard IUPAC codes
-    $sequence =~ tr/ATUCGRYKMBVHD/TAAGCYRMKVBDH/;
+  #Complement, using standard IUPAC codes
+  $sequence =~ tr/ATUCGRYKMBVHD/TAAGCYRMKVBDH/;
 
-    #Reverse
-    $sequence = reverse($sequence);
+  #Reverse
+  $sequence = reverse($sequence);
 
-    return $sequence, html => wrap_html('DNA reverse complement:', $sequence);
+  return $sequence, html => wrap_html('DNA reverse complement:', $sequence);
 };
 
 # This function adds some HTML and styling to our output
