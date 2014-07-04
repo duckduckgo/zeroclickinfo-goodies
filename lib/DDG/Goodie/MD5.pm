801c3d561c4893de90f8d0097bb2c6ed34099e58
gdrooid <gdrooid@openmailbox.org>
2014-07-04 23:05:13 +0200

    MD5: Add tests and fix previous commit.

--- lib/DDG/Goodie/MD5.pm
+++ lib/DDG/Goodie/MD5.pm
@@ -31,7 +31,7 @@ sub html_output {
 }
 
 handle remainder => sub {
-    s/of "?([^"]*)"?/$1/;
+    s/^of "?([^"]*)"?$/$1/;
     if (/^\s*(.*)\s*$/) {
         # Exit unless a string is found
         return unless $1;
