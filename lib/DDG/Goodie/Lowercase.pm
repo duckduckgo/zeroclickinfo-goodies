6c93e4480959c84420b91531e47ae73ed0ac8a33
Zaahir Moolla <moollaza@fastmail.fm>
2014-07-23 17:31:06 -0400

    add CSS styles for Lowercase Goodie

--- lib/DDG/Goodie/Lowercase.pm
+++ lib/DDG/Goodie/Lowercase.pm
@@ -16,8 +16,21 @@ zci answer_type => "lowercase";
 
 triggers start => 'lowercase', 'lower case', 'lc', 'strtolower', 'tolower';
 
+my $css = share("style.css")->slurp();
+sub append_css {
+    my $html = shift;
+    return "<style type='text/css'>$css</style>\n" . $html;
+};
+
 handle remainder => sub {
-    return lc $_ if $_;
+    return unless $_;
+    my $lower = lc $_;
+    my $text = "$lower";
+    my $html = qq(<div class="zci--lowercase"><span class="text--primary">$lower</span></div>);
+    $html = append_css($html);
+
+    return $text, html => $html;
+
     return;
 };
 
