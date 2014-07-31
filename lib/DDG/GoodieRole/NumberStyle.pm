1ad7f82e0680d3437d09cebd7c75c2e7f5fbd373
Matt Miller <mwmiller@outlook.com>
2014-07-31 15:36:57 -0400

    NumberStyle: more hackiness for negative numbers.
    Basically, we're only going to recognize negative numbers when they
    appear at the beginning of an expression.  Otherwise we assume we don't
    needs to deal with them.
    
    I'm sure this causes other problems, but resovles the testing issues for
    now.

--- lib/DDG/GoodieRole/NumberStyle.pm
+++ lib/DDG/GoodieRole/NumberStyle.pm
@@ -105,7 +105,7 @@ sub _add_html_exponents {
     # because of associativity and power-to-power, we need to scan nearly the whole thing
     for my $index (1 .. $#chars - 1) {
         my $this_char = $chars[$index];
-        if ($this_char =~ $number_re or $this_char eq '-') {
+        if ($this_char =~ $number_re or ($newly_up && $this_char eq '-')) {
             if ($newly_up) {
                 $in_exp_number = 1;
                 $newly_up      = 0;
