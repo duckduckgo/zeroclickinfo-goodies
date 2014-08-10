df28204b42c0124ed3e2e2c9ec3011ac20e94aba
Matt Miller <mwmiller@outlook.com>
2014-08-10 08:07:54 -0400

    DatesRole: reorganize parsing slightly.
    This reorganizes and clarifies some things to make adding extra
    cases in later easier (hopefully).

--- lib/DDG/GoodieRole/Dates.pm
+++ lib/DDG/GoodieRole/Dates.pm
@@ -11,11 +11,15 @@ use DateTime::Format::HTTP;
 
 # Reused lists and components for below
 my $short_day_of_week = qr#Mon|Tue|Wed|Thu|Fri|Sat|Sun#i;
-my %long_to_short = map { lc $_ => substr($_, 0, 3) } qw(January February March April May June July August September October November December);
+my %full_month_to_short = map { lc $_ => substr($_, 0, 3) } qw(January February March April May June July August September October November December);
 my $short_month = qr#Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec#i;
 my $full_month = qr#January|February|March|April|May|June|July|August|September|October|November|December#i;
 my $time_24h = qr#(?:(?:[0-1][0-9])|(?:2[0-3]))[:]?[0-5][0-9][:]?[0-5][0-9]#i;
 my $time_12h = qr#(?:(?:0[1-9])|(?:1[012])):[0-5][0-9]:[0-5][0-9]\s?(?:am|pm)#i;
+# Covering the ambiguous formats, like:
+# DMY: 27/11/2014 with a variety of delimiters and month formats
+# MDY: 11/27/2014 -- fundamentally non-sensical date format, for americans
+my $ambiguous_dates =  qr#^([0-3]?[0-9])[\\/\,_-]([0-3]?[0-9])[\\/\,_-]([0-9]{4})$#i;
 
 # like: 1st 2nd 3rd 4-20,24-30th 21st 22nd 23rd 31st
 my $number_suffixes = qr#(st|nd|rd|th)#i;
@@ -27,57 +31,54 @@ my $tz_suffixes = qr#(?:[+-][0-9]{4})|ACDT|ACST|ACT|ADT|AEDT|AEST|AFT|AKDT|AKST|
 #  not "are valid dates"; expects normalised whitespace
 sub date_regex {
     my @regexes = ();
-    
-    ## unambigous and awesome date formats: 
+
+    ## unambigous and awesome date formats:
     # ISO8601: 2014-11-27
     push @regexes, qr#[0-9]{4}-?[0-1][0-9]-?[0-3][0-9]([ T]$time_24h)?( ?$tz_suffixes)?#i;
-    
+
     # HTTP: Sat, 09 Aug 2014 18:20:00
     push @regexes, qr#$short_day_of_week, [0-9]{2} $short_month [0-9]{4} $time_24h?#i;
-    
+
     # RFC850 08-Feb-94 14:15:29 GMT
     push @regexes, qr#[0-9]{2}-$short_month-([0-9]{2}|[0-9]{4}) $time_24h?( ?$tz_suffixes)#i;
-    
-    ## acceptable date formats
-    # DMY: 27/11/2014 with a variety of delimiters and month formats
-    push @regexes, qr#[0-3]?[0-9][\\/\._-][0-1]?[0-9][\\/\._-][0-9]{4}#i;
+
+    # month-first date formats
     push @regexes, qr#[0-3][0-9][\\/\._-]$short_month[\\/\._-][0-9]{4}#i;
     push @regexes, qr#[0-3][0-9][\\/\._-]$full_month[\\/\._-][0-9]{4}#i;
-    push @regexes, qr#[0-3]?[0-9](?: ?$number_suffixes)? (?:$short_month|$full_month) [0-9]{4}#i;
-    
-    ## fundamentally non-sensical date formats, for americans
-    # MDY: 11/27/2014
-    push @regexes, qr#[0-1]?[0-9][\\/\,_-][0-3]?[0-9][\\/\,_-][0-9]{4}#i;
+    push @regexes, qr#(?:$short_month|$full_month) [0-3]?[0-9](?: ?$number_suffixes)? [0-9]{4}#i;
+
+    # day-first date formats
     push @regexes, qr#$short_month[\\/\,_-][0-3][0-9][\\/\,_-][0-9]{4}#i;
     push @regexes, qr#$full_month\\/\,_-][0-3][0-9][\\/\,_-][0-9]{4}#i;
-    push @regexes, qr#(?:$short_month|$full_month) [0-3]?[0-9](?: ?$number_suffixes)? [0-9]{4}#i;
-        
+    push @regexes, qr#[0-3]?[0-9](?: ?$number_suffixes)? (?:$short_month|$full_month) [0-9]{4}#i;
+
+    ## Ambiguous, but potentially valid date formats
+    push @regexes, $ambiguous_dates;
+
     my $returned_regex = join '|', @regexes;
     return qr/$returned_regex/i;
 }
 
 sub parse_string_to_date {
     my ($d) = @_;
-    
-    # guesswork for ambigous DMY/MDY and switch to ISO 
-    if($d =~ qr#^([0-3]?[0-9])[\\/\,_-]([0-3]?[0-9])[\\/\,_-]([0-9]{4})$#i)
-    {
-        my $month = $1;
-        my $day = $2;
-        my $year = $3;
-        
-        return if($1 > 12 && $2 > 12);
-        #assume it's in DMY unless it's impossible
-        if($month > 12){
-            my $tmp = $day;
-            $day = $month;
-            $month = $tmp;
+
+    # guesswork for ambigous DMY/MDY and switch to ISO
+    if ($d =~ $ambiguous_dates) {
+        my $month = $1;    # Assume MDY, even though it's crazy, for backward compatibility
+        my $day   = $2;
+        my $year  = $3;
+
+        if ($month > 12) {
+            # Months over 12 don't make any sense, so must not be MDY
+            return if ($day > 12);    # what we took as day must not be month, either.  No idea how to proceed.
+            ($day, $month) = ($month, $day);    # month and day must be swapped, then.
         }
-        $d = sprintf("%04d-%02d-%02d",$year,$month,$day);
+
+        $d = sprintf("%04d-%02d-%02d", $year, $month, $day);
     }
 
-    $d =~ s/(\d+)\s?$number_suffixes/$1/i;            # Strip ordinal text.
-    $d =~ s/($full_month)/$long_to_short{lc $1}/i;    # Parser deals better with the shorter month names.
+    $d =~ s/(\d+)\s?$number_suffixes/$1/i;                  # Strip ordinal text.
+    $d =~ s/($full_month)/$full_month_to_short{lc $1}/i;    # Parser deals better with the shorter month names.
     return DateTime::Format::HTTP->parse_datetime($d);
 }
 
