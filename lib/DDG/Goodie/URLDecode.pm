f70fb32b11439d834c14d041e3adeff6d66a0e8b
Rob Emery <rob@mintsoft.net>
2014-09-11 20:16:14 +0100

    URLDecode: Workaround for 100 char limit within `query` by switching to query_raw

--- lib/DDG/Goodie/URLDecode.pm
+++ lib/DDG/Goodie/URLDecode.pm
@@ -8,7 +8,7 @@ use strict;
 
 my $trigger_words = qr#urlunescape|unescapeurl|(unescape url)|decodeurl|(decode url)|urldecode|(url decode)|(url unescape)#;
 
-triggers query => qr#%[0-9A-Fa-f]{2}#;
+triggers query_raw => qr#%[0-9A-Fa-f]{2}#;
 primary_example_queries 'url decode https%3A%2F%2Fduckduckgo.com%2F', 'decode url xkcd.com%2Fblag';
 secondary_example_queries 'http%3A%2F%2Farstechnica.com%2F url unescape', 'linux.com%2Ftour%2F unescape url';
 
@@ -26,9 +26,9 @@ sub append_css {
     return "<style type='text/css'>$css</style>\n" . $html;
 };
 
-handle query => sub {
+handle query_raw => sub {
     #remove triggers and trim
-    s/(^$trigger_words)|($trigger_words$)//;
+    s/(^$trigger_words)|($trigger_words$)//i;
     s/(^\s+)|(\s+$)//;
     
     my $decoded_url = decodeURIComponent($_);
