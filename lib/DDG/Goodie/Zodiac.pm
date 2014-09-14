b5d820d1805ca37b2032145ebdd16422ddf55e90
n0mady <nomady@zoho.com>
2014-09-14 01:51:18 +0000

    Replaced Tabs with Spaces with Zodiac.pm

--- lib/DDG/Goodie/Zodiac.pm
+++ lib/DDG/Goodie/Zodiac.pm
@@ -17,26 +17,26 @@ triggers startend => "zodiac","zodiac sign","starsign","star sign";
 #File MetaData
 primary_example_queries "zodiac 21st June";
 secondary_example_queries "31 Dec starsign","1st Jan 1981 star sign","zodiac sign 1 Nov";
-description	"Find the Zodiac Sign for the Respective Date given as Input based on Tropical Zodiac(2011) Source : https://en.wikipedia.org/wiki/Zodiac";
+description "Find the Zodiac Sign for the Respective Date given as Input based on Tropical Zodiac(2011) Source : https://en.wikipedia.org/wiki/Zodiac";
 name "zodiac";
 category "special";
 topics "science";
-attribution email => 'nomady@zoho.com',
-	    github => ['https://github.com/n0mady','NOMADY'];
-			
+attribution email => 'nomady@zoho.com'
+            github => ['https://github.com/n0mady','NOMADY'];
+
 handle remainder => sub {
-	
-		#User Entered Date/Query
-		my $query=$_;								
-		
-	        my $zodiacdate=Date::Horoscope::locate($query);				
-	
-		#Return Nothing if the User Provided Date is Invalid	
-		return if $zodiacdate eq "";
-		
-		#Return the Star Sign
-		$zodiacdate=ucfirst($zodiacdate);
-		return "Star Sign : $zodiacdate";
+
+                    #User Entered Date/Query
+                    my $query=$_;
+
+                    my $zodiacdate=Date::Horoscope::locate($query);
+
+                    #Return Nothing if the User Provided Date is Invalid
+                    return if $zodiacdate eq "";
+
+                   #Return the Star Sign
+                   $zodiacdate=ucfirst($zodiacdate);
+                   return "Star Sign : $zodiacdate";
 };
 
 1;
