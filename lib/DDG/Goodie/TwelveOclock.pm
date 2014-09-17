2d28a92a45e178d1b0de75278eb26c776fafa072
Matt Miller <mwmiller@outlook.com>
2014-09-17 08:31:20 -0400

    TwelveOclock: improve friendliness of answers.
    - Answer questions "when is noon?" and "when is midnight?"
    - Include such questions as secondary_example_queries
    - Be less aggressive in the answer with a comma instead of a period
    
    Includes some internal renaming and reorganization, plus comments to
    better reflect the purpose of each bit.

--- lib/DDG/Goodie/TwelveOclock.pm
+++ lib/DDG/Goodie/TwelveOclock.pm
@@ -4,6 +4,7 @@ package DDG::Goodie::TwelveOclock;
 use DDG::Goodie;
 
 primary_example_queries "is 12:00am noon?", "is 1200pm midnight?";
+secondary_example_queries "when is noon?", "when is midnight?";
 description "Succinct explanation of what this instant answer does";
 name "TwelveOclock";
 code_url "https://github.com/duckduckgo/zeroclickinfo-spice/blob/master/lib/DDG/Spice/TwelveOclock.pm";
@@ -14,29 +15,38 @@ attribution github => ['https://github.com/duckduckgo', 'duckduckgo'];
 # Triggers
 triggers any => "midnight", "noon";
 
-my $twelve_oclock_re = qr/\b12(?:00|:00)?\s?(?<mer>[ap]m)\b/;
-my %answers          = (
+# 12am; 12:00PM; when is noon?; when is midnight
+my $question_re = qr/(?:\b12(?:00|:00)?\s?(?<mer>[ap]m)\b|^when is (?<q>noon|midnight)\??$)/;
+my %answers     = (
     am => 'midnight',
     pm => 'noon',
 );
+%answers = (%answers, reverse %answers);    # Point both ways to answer either direction.
 
 # Handle statement
 handle query => sub {
     my $query = lc shift;
 
     $query =~ s/\.//g;    # Strip any dots.
-    return unless ($query =~ $twelve_oclock_re);
-    my $meridian = $+{'mer'};
-    my $answer   = $answers{$meridian};
-    return unless $answer;
+    return unless ($query =~ $question_re);
+    my $included_mer = $+{'mer'};
+    my $meridian     = $included_mer || $answers{$+{'q'}};    # No included meridian implies straight-forward question.
+    my $to_show      = $answers{$meridian};
+    return unless $to_show;
 
-    my $noon     = ($query =~ qr/\bnoon\b/);
-    my $midnight = ($query =~ qr/\bmidnight\b/);
+    my $guess_result = '';
+    if ($included_mer) {
+        # If they included a meridian with their 12 o'clock, we need to figure out if they were guessing.
+        my $noon     = ($query =~ qr/\bnoon\b/);
+        my $midnight = ($query =~ qr/\bmidnight\b/);
 
-    my $guess = ($noon && !$midnight) ? 'noon' : ($midnight && !$noon) ? 'midnight' : '';
-    my $guess_status = (!$guess) ? '' : ($guess eq $answer) ? 'Yes. ' : 'No. ';
+        # It's only a guess if they mention only one or the other.
+        my $guess = ($noon && !$midnight) ? 'noon' : ($midnight && !$noon) ? 'midnight' : '';
+        # If they guessed, we need to answer the question they asked.
+        $guess_result = (!$guess) ? '' : ($guess eq $to_show) ? 'Yes, ' : 'No, ';
+    }
 
-    return $guess_status . '12:00' . $meridian . ' is ' . $answer . '.';
+    return $guess_result . '12:00' . $meridian . ' is ' . $to_show . '.';
 };
 
 1;
