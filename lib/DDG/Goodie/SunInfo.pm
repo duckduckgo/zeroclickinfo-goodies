0561d8ffec0281af646738f85b47669b84ff1d2a
Matt Miller <mwmiller@outlook.com>
2014-10-01 22:26:03 +0200

    SunInfo: use less precise, but friendlier time.

--- lib/DDG/Goodie/SunInfo.pm
+++ lib/DDG/Goodie/SunInfo.pm
@@ -20,7 +20,7 @@ category 'calculations';
 topics 'everyday';
 attribution github => ['https://github.com/duckduckgo', 'duckduckgo'];
 
-my $time_format = '%T %Z';
+my $time_format = '%l:%M%P';
 my $datestring_regex = datestring_regex();
 
 handle remainder => sub {
