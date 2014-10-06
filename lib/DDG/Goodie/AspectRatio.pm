d72a90658b34898ce67c2533d8055af59933ebe6
Matt Miller <mwmiller@outlook.com>
2014-10-06 08:55:08 +0200

    AspectRatio: update with structured answer.

--- lib/DDG/Goodie/AspectRatio.pm
+++ lib/DDG/Goodie/AspectRatio.pm
@@ -18,22 +18,29 @@ attribution github => [ 'https://github.com/mrshu', 'mrshu' ],
             twitter => [ 'https://twitter.com/mr__shu', 'mr__shu' ];
 
 handle remainder => sub {
-    my $input = $_;
+    my $input  = $_;
     my $result = 0;
-    my $ratio = 0;
+    my $ratio  = 0;
 
-    if ($input =~ /^(\d+(?:\.\d+)?)\s*\:\s*(\d+(?:\.\d+)?)\s*(?:(?:(\?)\s*:\s*(\d+(?:\.\d+)?))|(?:(\d+(?:\.\d+)?)\s*:\s*(\?)))$/){
+    if ($input =~ /^(\d+(?:\.\d+)?)\s*\:\s*(\d+(?:\.\d+)?)\s*(?:(?:(\?)\s*:\s*(\d+(?:\.\d+)?))|(?:(\d+(?:\.\d+)?)\s*:\s*(\?)))$/) {
         $ratio = $1 / $2;
+        my $pretty_ratio = $1 . ':' . $2;
 
+        my $result;
         if ($6 && $6 eq "?") {
-            $result = $5 / $ratio;
-            return "Aspect ratio: $5:$result ($1:$2)";
+            $result = $5 . ':' . ($5 / $ratio);
         } elsif ($3 && $3 eq "?") {
-            $result = $4 * $ratio;
-            return "Aspect ratio: $result:$4 ($1:$2)";
+            $result = ($4 * $ratio) . ':' . $4;
         }
+        return unless $result;
+
+        return "Aspect ratio: $result ($pretty_ratio)",
+          structured_answer => {
+            input     => [$pretty_ratio],
+            operation => 'aspect ratio',
+            result    => $result
+          };
     }
-    return;
 };
 
 1;
