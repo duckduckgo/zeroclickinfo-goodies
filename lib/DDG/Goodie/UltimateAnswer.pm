635c61335df5de9cf13e475bfcd27061692fef8a
Matt Miller <mwmiller@outlook.com>
2014-10-07 10:58:48 +0200

    UltimateAnswer: add strcutured answer.

--- lib/DDG/Goodie/UltimateAnswer.pm
+++ lib/DDG/Goodie/UltimateAnswer.pm
@@ -3,8 +3,8 @@ package DDG::Goodie::UltimateAnswer;
 
 use DDG::Goodie;
 triggers start => 'what is the ultimate answer', 'what is the ultimate answer to life the universe and everything', 'what is the answer to the ultimate question of life the universe and everything';
-primary_example_queries 'what is the answer to the ultimate question of life the universe and everything';
 
+primary_example_queries 'what is the answer to the ultimate question of life the universe and everything';
 
 name 'Ultimate Answer';
 description 'Hichhiker\'s Guide to the Galaxy reference.';
@@ -14,12 +14,20 @@ code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DD
 attribution github  => ['https://github.com/jfeeneywm/', 'jfeeneywm'],
             twitter => ['https://twitter.com/jfeeneywm', 'jfeeneywm'];
 
-zci answer_type => 'UltimateAnswer';
+zci answer_type => 'ultimate_answer';
 zci is_cached   => 1;
 
 handle remainder => sub {
     return unless ($_ eq '' || $_ eq '?');
-    return 'Forty-two', html => '<span class="zci--ultanswer">Forty-two</span>';
 
+    my $answer = 'Forty-two';
+
+    return $answer,
+      structured_answer => {
+        input     => [],
+        operation => 'the answer to the ultimate question of life, the universe and everything',
+        result    => $answer
+      };
 };
+
 1;
--- t/UltimateAnswer.t
+++ t/UltimateAnswer.t
@@ -6,20 +6,22 @@ use warnings;
 use Test::More;
 use DDG::Test::Goodie;
 
-zci answer_type => "UltimateAnswer";
+zci answer_type => "ultimate_answer";
 zci is_cached   => 1;
 
 ddg_goodie_test(
-    [
-        'DDG::Goodie::UltimateAnswer'
-    ],
+    ['DDG::Goodie::UltimateAnswer'],
 
-    'what is the answer to the ultimate question of life the universe and everything'=>
-        test_zci(
-            'Forty-two', html => '<span class="zci--ultanswer">Forty-two</span>'
-        ),
+    'what is the answer to the ultimate question of life the universe and everything' => test_zci(
+        'Forty-two',
+        structured_answer => {
+            input     => [],
+            operation => 'the answer to the ultimate question of life, the universe and everything',
+            result    => 'Forty-two',
+        }
+    ),
     'what is the answer to my homework question' => undef,
-    'why?' => undef,
+    'why?'                                       => undef,
 );
 
 done_testing;
