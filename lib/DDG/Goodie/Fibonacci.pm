191b2779cc6c915e7c9e4d8464400acee31be11a
Matt Miller <mwmiller@outlook.com>
2014-10-14 18:21:34 +0200

    Fibonacci: use structured answer.

--- lib/DDG/Goodie/Fibonacci.pm
+++ lib/DDG/Goodie/Fibonacci.pm
@@ -7,8 +7,9 @@ use DDG::Goodie;
 use Lingua::EN::Numbers::Ordinate qw(ordsuf);
 
 triggers any => 'fib', 'fibonacci';
-zci is_cached => 1;
+
 zci answer_type => 'fibonacci';
+zci is_cached   => 1;
 
 primary_example_queries 'fib 7';
 secondary_example_queries 'fibonacci 33';
@@ -19,23 +20,28 @@ code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DD
 topics 'math';
 category 'calculations';
 
+my @fib = (0, 1);
+
 handle remainder => sub {
     s/^\s+//;
     s/\s+$//;
-    return unless /^(?:what(?:'s| is) the )?(\d+)(?:th|rd|st)?(?: number)?(?: in the (?:series|sequence))?\??$/ && $1 <= 1470;
-    my @fib;
-    my $n = $1;
-    $#fib = $n;
-    $fib[0] = 0;
-    $fib[1] = 1;
+    return unless /^(?:what(?:'s| is) the )?(?<which>\d+)(?:th|rd|st)?(?: number)?(?: in the (?:series|sequence))?\??$/ && $1 <= 1470;
+    my $n = $+{'which'};
     # Instead of calling a typical recursive function,
     # use simple dynamic programming to improve performance
-    for my $i (2..$#fib) {
-        $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
+    if ($#fib < $n) {
+        for my $i ($#fib .. $n) {
+            $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
+        }
     }
-    my $suf = ordsuf($_);
+
+    my $suf = ordsuf($n);
     return "The $n$suf fibonacci number is ${fib[$n]} (assuming f(0) = 0).",
-           html => "The $n<sup>$suf</sup> fibonacci number is ${fib[$n]} (assuming f(0) = 0).";
+      structured_answer => {
+        input     => [$n . $suf],
+        operation => 'Fibonacci number',
+        result    => $fib[$n],
+      };
 };
 
 1;
