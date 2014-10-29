8c60194d76c428d46c948394cde20ab86738a202
tarun29061990 <tarun29061990@gmail.com>
2014-10-29 20:23:36 +0000

    added category as entertainment

--- lib/DDG/Goodie/IsAwesome/tarun29061990.pm
+++ lib/DDG/Goodie/IsAwesome/tarun29061990.pm
@@ -12,7 +12,7 @@ zci is_cached   => 1;
 name "IsAwesome tarun29061990";
 description "My first Goodie, it let's the world know that tarun29061990 is awesome";
 primary_example_queries "duckduckhack tarun29061990";
-category "";
+category "entertainment";
 topics "";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/tarun29061990.pm";
 attribution github => ["https://github.com/tarun29061990", "tarun29061990"],
