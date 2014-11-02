c08c3e5546e90021aab6ae51755fb1073d87c07e
Usman Raza <usman.r123@gmail.com>
2014-11-02 19:17:33 +0500

    added comments

--- lib/DDG/Goodie/CountryCodes.pm
+++ lib/DDG/Goodie/CountryCodes.pm
@@ -1,9 +1,8 @@
 package DDG::Goodie::CountryCodes;
-# ABSTRACT: Matches country names to international calling codes
+# ABSTRACT: Matches country names to ISO 3166 codes and vice versa
 
 use DDG::Goodie;
 use Locale::Country qw/country2code code2country/;
-use Data::Dumper;
 
 use constant WPHREF => "https://en.wikipedia.org/wiki/ISO_3166-1";
     
@@ -18,7 +17,7 @@ category    "geography";
 topics      "travel", "geography";
 
 primary_example_queries   "country code Japan", "iso code for Spain";
-secondary_example_queries "numeric country code for Russia", "3 letter country code of China";
+secondary_example_queries "Russia two letter country code", "3 letter country code of China";
 
 attribution github  => ["killerfish", "Usman Raza"],
             twitter     => ["f1shie", "Usman Raza"];
@@ -29,10 +28,13 @@ handle remainder => sub {
     return unless /^([a-zA-Z]+)*\s*(?:(2|3|two|three)\sletter)?$|^(?:(2|3|two|three)\sletter)?\s*(?:of|for)?\s*([a-zA-Z]+)*$/;
     
     my (@answer, $expr);
-    my $query = $4;
     
-    $expr = '2' unless $expr = ($2 || $3);
+    # Get the user input which could be a country or code like France or fra or fr
+    my $query = $4;
     $query = $1 if $1;
+
+    # Default to alpha-2 if user has not specified Codeset
+    $expr = '2' unless $expr = ($2 || $3);
 
     if($expr && ($expr eq '2' || $expr eq 'two' )) {
         @answer = result($query, "alpha-2");
@@ -42,11 +44,13 @@ handle remainder => sub {
         @answer = result($query, "alpha-3");
     }
     
+    # Return if user input was neither country or code
     return if @answer < 2;
-    ($query, $answer[0]) = ($answer[0], $query) if $answer[1] == 1;
+
+    # Swap country and code, if user had entered code
+    ($query, $answer[0]) = ($answer[0], $query) if $answer[1] eq 'code';
     
     
-    print Dumper \@answer;
     my $text = sprintf qq(ISO 3166: %s - %s), ucfirst $query, $answer[0],
     my $html = sprintf qq(<a href="%s">ISO 3166</a>: %s - %s), WPHREF, ucfirst $query, $answer[0];
     return $text, html => $html;
@@ -56,7 +60,9 @@ handle remainder => sub {
 sub result {
     my ($query, $sw) = @_;
     my $result;
-    ($result = country2code($query, $sw)) ? return ($result, 0) : ($result = code2country($query, $sw)) ? return ($result, 1) : return -1;
+    
+    # Validate user input and return result accordingly
+    ($result = country2code($query, $sw)) ? return ($result, 'country') : ($result = code2country($query, $sw)) ? return ($result, 'code') : return -1;
 }
 
 1;
