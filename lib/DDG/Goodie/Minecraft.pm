3b30ca297c20aa2101e966647a424a6f70fd6f52
Lars Jansøn Engvik <lars@engvik.nu>
2014-11-03 14:12:39 +0100

    Fixed indentation.

--- lib/DDG/Goodie/Minecraft.pm
+++ lib/DDG/Goodie/Minecraft.pm
@@ -29,18 +29,18 @@ my %recipes = map{ lc $_->{'name'} => $_ } (@{ $decoded->{'items'} });
 
 # Create a hash with good words.
 sub populate_good_words() {
-	my @good_words;
+    my @good_words;
 
-	foreach my $recipe (keys %recipes) {
-		if ($recipe !~ /\s/) {
-			push (@good_words, $recipe);
-		} else {
-			my @words = split /\s+/, $recipe;
-			push (@good_words, @words);
-		}
-	}
+    foreach my $recipe (keys %recipes) {
+        if ($recipe !~ /\s/) {
+            push (@good_words, $recipe);
+        } else {
+            my @words = split /\s+/, $recipe;
+            push (@good_words, @words);
+        }
+    }
 
-	return map { $_ => 1 } @good_words;
+    return map { $_ => 1 } @good_words;
 }
 
 my %bad_words = qw(download server tutorial mod mods skins skin texture pack packs project projects);
@@ -63,31 +63,31 @@ sub make_html {
 }
 
 handle remainder => sub {
-	my @query = split /\s+/, lc $_; # Split on whitespaces.
-	my $lookup = '';
+    my @query = split /\s+/, lc $_; # Split on whitespaces.
+    my $lookup = '';
 
-	# Loop through the query.
-	foreach (@query) {
-		return if(exists($bad_words{$_})); # Not looking for a recipe.
-		$lookup .= $_ . ' ' if(exists($good_words{$_})); # Words exists in a recipe, add it.
-	}
+    # Loop through the query.
+    foreach (@query) {
+        return if(exists($bad_words{$_})); # Not looking for a recipe.
+        $lookup .= $_ . ' ' if(exists($good_words{$_})); # Words exists in a recipe, add it.
+    }
 
-	chop $lookup; # Remove trailing whitespace.
+    chop $lookup; # Remove trailing whitespace.
 
-	my $recipe = $recipes{$lookup}; # Check if this actually is a recipe.
+    my $recipe = $recipes{$lookup}; # Check if this actually is a recipe.
 
-	# No recipe found, let's try again without the okay words.
-	if (!$recipe) {
-		$lookup =~ s/$okay_words//g;
-		$recipe = $recipes{$lookup};
-		return unless $recipe; # Definitely not a recipe.
-	}
+    # No recipe found, let's try again without the okay words.
+    if (!$recipe) {
+        $lookup =~ s/$okay_words//g;
+        $recipe = $recipes{$lookup};
+        return unless $recipe; # Definitely not a recipe.
+    }
 
-	# Recipe found, let's return an answer.
-	my $plain = $recipe->{'name'} . ' are made from ' . $recipe->{'ingredients'} . '.';
-	my $html = make_html($recipe);
+    # Recipe found, let's return an answer.
+    my $plain = $recipe->{'name'} . ' are made from ' . $recipe->{'ingredients'} . '.';
+    my $html = make_html($recipe);
 
-	return $plain, html => $html;
-	return;
+    return $plain, html => $html;
+    return;
 };
 1;
