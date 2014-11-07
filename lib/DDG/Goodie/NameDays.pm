f94f2e531d7be8c952e00ed7a41de3ac31ea9980
W25 <kankowski@gmail.com>
2014-11-07 07:07:09 +0000

    Initial version

---
+++ lib/DDG/Goodie/NameDays.pm
@@ -0,0 +1,74 @@
+package DDG::Goodie::NameDays;
+# ABSTRACT: Display Name Days for a given name or date
+
+use strict;
+use DateTime;
+use DDG::Goodie;
+with 'DDG::GoodieRole::Dates';
+
+zci answer_type => "name_days_w25";
+zci is_cached   => 1;
+
+# Metadata
+name "Name Days";
+source "https://en.wikipedia.org/wiki/Name_days_in_Poland";
+description "Name Days for a given name or date";
+primary_example_queries "name day Maria", "1 June name day";
+secondary_example_queries "name days today";
+category "dates";
+topics "social", "everyday";
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/NameDays/W25/W25.pm";
+attribution github => ["http://github.com/W25", "W25"];
+
+# Triggers
+triggers any => "name day", "name days", "nameday", "namedays", "imieniny";
+
+# Load the data file
+my @names = share('Poland.txt')->slurp(iomode => '<:encoding(UTF-8)'); # Names indexed by day
+my %dates = (); # Days indexed by name
+
+sub load_days {
+	my $day_of_year = 1;
+
+	# Read names for each day and add them to the hash
+	for my $names_for_date (@names) {
+		for my $name (split(' ', $names_for_date)) {
+			push(@{$dates{$name}}, $day_of_year);
+		}
+	
+		# Advance to the next day
+		$day_of_year++;
+	}
+}
+
+load_days();
+
+my $datestring_regex = datestring_regex();
+
+# Handle statement
+handle remainder => sub {
+	if (exists $dates{lc($_)}) {
+		# Return all dates corresponding to this name
+		my @ret = ();
+		foreach (@{$dates{lc($_)}}) {
+			# Any leap year here, because the text file includes February, 29
+			my $d = DateTime->from_day_of_year(year => 2000, day_of_year => $_);
+			push(@ret, $d->strftime('%e %b'));
+		}
+		return join(', ', @ret);
+	}
+    
+    return unless $_ =~ qr/^($datestring_regex)$/;
+
+	my $day = parse_datestring_to_date($1);
+    
+    $day->set_year(2000); # Any leap year here
+    
+    my $ret = $names[$day->day_of_year()];
+    
+    chomp($ret);
+    
+    return $ret;
+};
+
+1;
---
+++ t/NameDays.t
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use DDG::Test::Goodie;
+
+zci answer_type => "name_days_w25";
+zci is_cached   => 1;
+
+ddg_goodie_test(
+    [qw( DDG::Goodie::NameDays::W25 )],
+    'name day mieszko' => test_zci(' 1 Jan'),
+    'maria imieniny' => test_zci('23 Jan,  2 Feb, 11 Feb, 25 Mar, 14 Apr, 26 Apr, 28 Apr,  3 May, 24 May, 25 May, 29 May,  2 Jun, 13 Jun, 27 Jun,  2 Jul, 16 Jul, 17 Jul, 22 Jul, 29 Jul
+,  2 Aug,  4 Aug,  5 Aug, 15 Aug, 22 Aug, 26 Aug,  8 Sep, 12 Sep, 15 Sep, 24 Sep,  7 Oct, 11 Oct, 16 Nov, 21 Nov,  8 Dec, 10 Dec'),
+    # Try to include some examples of queries on which it might
+    # appear that your answer will trigger, but does not.
+    'name day' => undef,
+);
+
+done_testing;
