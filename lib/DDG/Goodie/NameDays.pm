9d15c531368943a88846ed49983361ddfd9af984
W25 <kankowski@gmail.com>
2014-11-12 07:46:05 +0000

    Support for more date formats

--- lib/DDG/Goodie/NameDays.pm
+++ lib/DDG/Goodie/NameDays.pm
@@ -1,6 +1,7 @@
 package DDG::Goodie::NameDays;
 # ABSTRACT: Display Name Days for a given name or date
 
+use utf8;
 use strict;
 use DateTime;
 use DDG::Goodie;
@@ -14,7 +15,7 @@ name "Name Days";
 source "https://en.wikipedia.org/wiki/Name_days_in_Poland";
 description "Name Days for a given name or date";
 primary_example_queries "name day Maria", "1 June name day";
-secondary_example_queries "name days today";
+secondary_example_queries "name days today", "imieniny 9 stycznia", "imieniny Marii";
 category "dates";
 topics "social", "everyday";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/NameDays/W25/W25.pm";
@@ -22,6 +23,8 @@ attribution github => ["http://github.com/W25", "W25"];
 
 # Triggers
 triggers any => "name day", "name days", "nameday", "namedays", "imieniny";
+
+
 
 # Load the data file
 my @names = share('Poland.txt')->slurp(iomode => '<:encoding(UTF-8)'); # Names indexed by day
@@ -69,19 +72,69 @@ sub load_days {
     }
 }
 
+
 load_days();
+
+
+
+sub parse_other_date_formats {
+    # Quick fix for the date formats not supported by parse_datestring_to_date.
+    # If parse_datestring_to_date will be improved, you can remove some of the following code.
+    
+    # US date format ("month/day")
+    if (/^([0-1]?[0-9])\s?\/\s?([0-3]?[0-9])$/) {
+        # Suppress errors for invalid dates with eval
+        return eval { new DateTime(year => 2000, day => $2, month => $1) };
+    }
+    
+    # Polish date format ("day.month")
+    if (/^([0-3]?[0-9])\s?\.\s?([0-1]?[0-9])$/) {
+        return eval { new DateTime(year => 2000, day => $1, month => $2) };
+    }
+    
+    # Parse_datestring_to_date uses the current year if the year is not specified, so
+    # it will not parse "29 Feb" in a non-leap year. Fix this problem here.
+    if (/^29\s?(?:th)?\s*(Feb|February)/ || /(Feb|February)\s*29\s?(?:th)?$/) {
+        return new DateTime(year => 2000, day => 29, month => 2);
+    }
+    
+    # Polish month names
+    s/\b(styczeń|stycznia)\b/Jan/;
+    s/\b(luty|lutego)\b/Feb/;
+    s/\b(marzec|marca)\b/Mar/;
+    s/\b(kwiecień|kwietnia)\b/Apr/;
+    s/\b(maj|maja)\b/May/;
+    s/\b(czerwiec|czerwca)\b/Jun/;
+    s/\b(lipiec|lipca)\b/Jul/;
+    s/\b(sierpień|sierpnia)\b/Aug/;
+    s/\b(wrzesień|września)\b/Sep/;
+    s/\b(październik|października)\b/Oct/;
+    s/\b(listopad|listopada)\b/Nov/;
+    s/\b(grudzień|grudnia)\b/Dec/;
+        
+    return parse_datestring_to_date($_)
+}
+
+
 
 # Handle statement
 handle remainder => sub {
+    # Search by name first
     if (exists $dates{lc($_)}) {
         return $dates{lc($_)};
     }
     
+    # Then, search by date
     my $day = parse_datestring_to_date($_);
+    
+    if (!$day) {
+        $day = parse_other_date_formats($_);
+    }
     
     return unless $day;
     
-    $day->set_year(2000); # Any leap year here
+    # Any leap year here, because the array includes February, 29
+    $day->set_year(2000);
     
     return $names[$day->day_of_year() - 1];
 };
