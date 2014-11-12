c00c4986ee5e7421cbce3aff6ed4652f735ba6c8
W25 <kankowski@gmail.com>
2014-11-12 06:16:05 +0000

    Genetive case and variations of the names

--- lib/DDG/Goodie/NameDays.pm
+++ lib/DDG/Goodie/NameDays.pm
@@ -29,19 +29,43 @@ my %dates = (); # Days indexed by name
 
 # File format: 366 lines (one for each day of a year).
 # Each line contains names separated with a space.
+# A line may contain the names in genitive case or variations of a name.
+# These variations are placed after vertical bar character (|); they are
+# not shown when searching for this day, but you can search for them.
 
 sub load_days {
     my $day_of_year = 1;
 
     # Read names for each day and add them to the hash
-    for my $names_for_date (@names) {
+    for (@names) {
+        # Add all names, including the names after vertical bar
+        my $names_for_date = $_;
         $names_for_date =~ s/\|/ /;
         for my $name (split(' ', $names_for_date)) {
             push(@{$dates{$name}}, $day_of_year);
         }
     
+        # Remove the names after vertical bar (|)
+        chomp;
+        s/\s*\|.*$//;
+        
+        # Convert the first letter to uppercase
+        s/\b(\w)/\u\L$1/g;
+        
         # Advance to the next day
         $day_of_year++;
+    }
+    
+    # Convert the dates to string
+    for (keys %dates) {
+        # Find all dates corresponding to this name
+        my @ret = ();
+        foreach (@{$dates{$_}}) {
+            # Any leap year here, because the text file includes February, 29
+            my $d = DateTime->from_day_of_year(year => 2000, day_of_year => $_);
+            push(@ret, $d->strftime('%e %b'));
+        }
+        $dates{$_} = join(', ', @ret);
     }
 }
 
@@ -50,14 +74,7 @@ load_days();
 # Handle statement
 handle remainder => sub {
     if (exists $dates{lc($_)}) {
-        # Return all dates corresponding to this name
-        my @ret = ();
-        foreach (@{$dates{lc($_)}}) {
-            # Any leap year here, because the text file includes February, 29
-            my $d = DateTime->from_day_of_year(year => 2000, day_of_year => $_);
-            push(@ret, $d->strftime('%e %b'));
-        }
-        return join(', ', @ret);
+        return $dates{lc($_)};
     }
     
     my $day = parse_datestring_to_date($_);
@@ -66,13 +83,7 @@ handle remainder => sub {
     
     $day->set_year(2000); # Any leap year here
     
-    my $ret = $names[$day->day_of_year() - 1];
-    
-    $ret =~ s/\b(\w)/\u\L$1/g; # Convert the first letter to uppercase
-    
-    chomp($ret);
-    
-    return $ret;
+    return $names[$day->day_of_year() - 1];
 };
 
 1;
--- t/NameDays.t
+++ t/NameDays.t
@@ -15,6 +15,11 @@ ddg_goodie_test(
     '3 June name day' => test_zci('Konstantyn Leszek Paula Tamara'),
     'Name Day Tamara' => test_zci(' 3 Jun'),
     'namedays dec 30' => test_zci('Dawid Eugeniusz Katarzyna Uniedrog'),
+    
+    # Genetive case
+    'imieniny marii' => test_zci('23 Jan,  2 Feb, 11 Feb, 25 Mar, 14 Apr, 26 Apr, 28 Apr,  3 May, 24 May, 25 May, 29 May,  2 Jun, 13 Jun, 27 Jun,  2 Jul, 16 Jul, 17 Jul, 22 Jul, 29 Jul,  2 Aug,  4 Aug,  5 Aug, 15 Aug, 22 Aug, 26 Aug,  8 Sep, 12 Sep, 15 Sep, 24 Sep,  7 Oct, 11 Oct, 16 Nov, 21 Nov,  8 Dec, 10 Dec'),
+    'imieniny Tamary' => test_zci(' 3 Jun'),
+    
     # Try to include some examples of queries on which it might
     # appear that your answer will trigger, but does not.
     'my name day' => undef,
