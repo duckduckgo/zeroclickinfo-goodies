2022d6e853ca894d8609382127668fbda33fe14d
moollaza <moollaza@fastmail.fm>
2014-11-13 04:29:07 +0000

    Small cleanup of IsAwesome Goodies - Removed unneeded comments - Added ABSTRACT where missing - Filled in Metadata where missing

--- lib/DDG/Goodie/IsAwesome/JohnCarlosReed.pm
+++ lib/DDG/Goodie/IsAwesome/JohnCarlosReed.pm
@@ -6,20 +6,17 @@ use DDG::Goodie;
 zci answer_type => "is_awesome_john_carlos_reed";
 zci is_cached   => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "IsAwesome JohnCarlosReed";
 description "My first Goodie, it let's the world know that JohnCarlosReed is awesome";
 primary_example_queries "duckduckhack JohnCarlosReed";
-#category "";
-#topics "";
-code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/JohnCarlosReed/JohnCarlosReed.pm";
+category "entertainment";
+topics "entertainment", "special_interest";
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/JohnCarlosReed.pm";
 attribution github => ["https://github.com/JohnCarlosReed", "John Reed"],
             twitter => "johnnycarlos";
 
-# Triggers
 triggers start => "duckduckhack johncarlosreed";
 
-# Handle statement
 handle remainder => sub {
     return if $_;
     return "JohnCarlosReed is awesome and has successfully completed the DuckDuckHack Goodie tutorial!";
