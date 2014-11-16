2f38dc6766cdddda7d8bf81af2d876af31ac2a00
ymzong <yzong@cmu.edu>
2014-11-16 23:03:14 +0000

    Edited first Goodie for pull request.

--- lib/DDG/Goodie/IsAwesome/ymzong.pm
+++ lib/DDG/Goodie/IsAwesome/ymzong.pm
@@ -1,27 +1,19 @@
 package DDG::Goodie::IsAwesome::ymzong;
-# ABSTRACT: Write an abstract here
-# Start at https://duck.co/duckduckhack/goodie_overview if you are new
-# to instant answer development
+# ABSTRACT: ymzong's first Goodie!
 
 use DDG::Goodie;
 
 zci answer_type => "is_awesome_ymzong";
 zci is_cached   => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "IsAwesome ymzong";
-description "Succinct explanation of what this instant answer does";
-primary_example_queries "first example query", "second example query";
-secondary_example_queries "optional -- demonstrate any additional triggers";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
-# category "";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
-# topics "";
+description "My first Goodie, it let's the world know that ymzong is awesome!!";
+primary_example_queries "duckduckhack ymzong";
+category "special";
+topics "special_interest", "geek", "math", "programming";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/ymzong.pm";
-attribution github => ["GitHubAccount", "Friendly Name"],
-            twitter => "twitterhandle";
+attribution github => ["GitHubAccount", "ymzong"];
 
-# Triggers
 triggers start => "duckduckhack ymzong";
 
 handle remainder => sub {
--- t/IsAwesome/ymzong.t
+++ t/IsAwesome/ymzong.t
@@ -10,12 +10,8 @@ zci is_cached   => 1;
 
 ddg_goodie_test(
     [qw( DDG::Goodie::IsAwesome::ymzong )],
-    # At a minimum, be sure to include tests for all:
-    # - primary_example_queries
-    # - secondary_example_queries
     'duckduckhack ymzong' => test_zci('ymzong is awesome and has successfully completed the DuckDuckHack Goodie tutorial!'),
-    # Try to include some examples of queries on which it might
-    # appear that your answer will trigger, but does not.
+
     'duckduckhack ymzong is awesome' => undef,
     'bad example query' => undef,
     'duckduckgo' => undef
