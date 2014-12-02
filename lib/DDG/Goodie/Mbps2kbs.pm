255780bdbd7820d98a23b72100968a21f0feb112
Eric Newport <kethinov@gmail.com>
2014-12-02 10:55:28 -0500

    adding category and topics

--- lib/DDG/Goodie/Mbps2kbs.pm
+++ lib/DDG/Goodie/Mbps2kbs.pm
@@ -9,10 +9,8 @@ zci is_cached   => 1;
 name "Mbps2kbs";
 description "Convert mbps bandwidth into kb/s";
 primary_example_queries "1mbps in kb/s";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
-# category "";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
-# topics "";
+category "conversions";
+topics "computing";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/Mbps2kbs.pm";
 attribution github => ["kethinov", "Eric Newport"],
             twitter => "kethinov";
