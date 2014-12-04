db5c579aab48d0da3c1ede988e5b375405af14a3
stefolof <stefolof@gmail.com>
2014-12-04 13:02:22 +0000

    Added bpm definition. Added new triggers. All millisecond values are now integers.

--- lib/DDG/Goodie/BPMToMs.pm
+++ lib/DDG/Goodie/BPMToMs.pm
@@ -16,58 +16,70 @@ code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DD
 attribution github => ["https://github.com/stefolof", "stefolof"],
             twitter => "stefolof";
 
-triggers end => "bpm to ms";
+triggers end => "bpm to ms", "bpm to milliseconds", "bpm to note values", "bpm to note lengths", "bpm", "bpm timings", "beats per minute to milliseconds", 
+                "beats per minute to ms", "beats per minute to note values", "beats per minute to note lengths", "beats per minute", "beats per minute timings";
 
 handle remainder => sub {
     return unless $_; # Guard against "no answer"
     return unless $_ =~ /^\d+$/i; # Only integer values accepted
     
+    my $bpm = $_;
+    
+    my @divisors = map { 2 ** $_ } 0 .. 6;
+    
+    my @straight_values = map { int( 240000 / ($bpm * $_) + 0.5) } @divisors;
+    my @triplet_values = map { int( 160000 / ($bpm * $_) + 0.5) } @divisors;
+    my @dotted_values = map { int( 360000 / ($bpm * $_) + 0.5) } @divisors;
+    
     return html => "<div class=\"record\">
+                    <div>
+                       $bpm BPM as quarter notes per minute
+                    </div>
                     <table style=\"border-spacing: 10px; border-collapse: separate;\" class=\"record__body\">
                         <tr class=\"record__row record__highlight\">
-                            <td>Whole Note:</td><td>" . 240000 / $_ . " ms</td>
-                            <td>Triplet:</td><td>" . 160000 / $_ . " ms</td>
-                            <td>Dotted:</td><td>" . 360000 / $_ . " ms</td>
+                            <td>Whole Note:</td><td>" . $straight_values[0] . " ms</td>
+                            <td>Triplet:</td><td>" . $triplet_values[0] . " ms</td>
+                            <td>Dotted:</td><td>" . $dotted_values[0] . " ms</td>
                         </tr>
                         <tr class=\"record__row record__highlight\">
-                            <td>Half Note:</td><td>" . 120000 / $_ . " ms</td>
-                            <td>Triplet:</td><td>" . 80000 / $_ . " ms</td>
-                            <td>Dotted:</td><td>" . 180000 / $_ . " ms</td>
+                            <td>Half Note:</td><td>" . $straight_values[1] . " ms</td>
+                            <td>Triplet:</td><td>" . $triplet_values[1] . " ms</td>
+                            <td>Dotted:</td><td>" . $dotted_values[1] . " ms</td>
                         </tr>
                         <tr class=\"record__row record__highlight\">
-                            <td>Quarter Note:</td><td>" . 60000 / $_ . " ms</td>
-                            <td>Triplet:</td><td>" . 40000 / $_ . " ms</td>
-                            <td>Dotted:</td><td>" . 90000 / $_ . " ms</td>
+                            <td>Quarter Note:</td><td>" . $straight_values[2] . " ms</td>
+                            <td>Triplet:</td><td>" . $triplet_values[2] . " ms</td>
+                            <td>Dotted:</td><td>" . $dotted_values[2] . " ms</td>
                         </tr>
                         <tr class=\"record__row record__highlight\">
-                            <td>1/8 Note:</td><td>" . 30000 / $_ . " ms</td>
-                            <td>Triplet:</td><td>" . 20000 / $_ . " ms</td>
-                            <td>Dotted:</td><td>" . 45000 / $_ . " ms</td>
+                            <td>1/8 Note:</td><td>" . $straight_values[3] . " ms</td>
+                            <td>Triplet:</td><td>" . $triplet_values[3] . " ms</td>
+                            <td>Dotted:</td><td>" . $dotted_values[3] . " ms</td>
                         </tr>
                         <tr class=\"record__row record__highlight\">
-                            <td>1/16 Note:</td><td>" . 15000 / $_ . " ms</td>
-                            <td>Triplet:</td><td>" . 10000 / $_ . " ms</td>
-                            <td>Dotted:</td><td>" . 22500 / $_ . " ms</td>
+                            <td>1/16 Note:</td><td>" . $straight_values[4] . " ms</td>
+                            <td>Triplet:</td><td>" . $triplet_values[4] . " ms</td>
+                            <td>Dotted:</td><td>" . $dotted_values[4] . " ms</td>
                         </tr>
                         <tr class=\"record__row record__highlight\">
-                            <td>1/32 Note:</td><td>" . 7500 / $_ . " ms</td>
-                            <td>Triplet:</td><td>" . 5000 / $_ . " ms</td>
-                            <td>Dotted:</td><td>" . 11250 / $_ . " ms</td>
+                            <td>1/32 Note:</td><td>" . $straight_values[5] . " ms</td>
+                            <td>Triplet:</td><td>" . $triplet_values[5] . " ms</td>
+                            <td>Dotted:</td><td>" . $dotted_values[5] . " ms</td>
                         </tr>
                         <tr class=\"record__row record__highlight\">
-                            <td>1/64 Note:</td><td>" . 3750 / $_ . " ms</td>
-                            <td>Triplet:</td><td>" . 2500 / $_ . " ms</td>
-                            <td>Dotted:</td><td>" . 5625 / $_ . " ms</td>
+                            <td>1/64 Note:</td><td>" . $straight_values[6] . " ms</td>
+                            <td>Triplet:</td><td>" . $triplet_values[6] . " ms</td>
+                            <td>Dotted:</td><td>" . $dotted_values[6] . " ms</td>
                         </tr>
                    </table>
                    </div>",
-                   answer => "Whole Note: " . 240000 / $_ . " ms, Triplet: " . 160000 / $_ . " ms, Dotted: " . 360000 / $_ . " ms
-Half Note: " . 120000 / $_ . " ms, Triplet: " . 80000 / $_ . " ms, Dotted: " . 180000 / $_ . " ms
-Quarter Note: " . 60000 / $_ . " ms, Triplet: " . 40000 / $_ . " ms, Dotted: " . 90000 / $_ . " ms
-1/8 Note: " . 30000 / $_ . " ms, Triplet: " . 20000 / $_ . " ms, Dotted: " . 45000 / $_ . " ms
-1/16 Note: " . 15000 / $_ . " ms, Triplet: " . 10000 / $_ . " ms, Dotted: " . 22500 / $_ . " ms
-1/32 Note: " . 7500 / $_ . " ms, Triplet: " . 5000 / $_ . " ms, Dotted: " . 11250 / $_ . " ms
-1/64 Note: " . 3750 / $_ . " ms, Triplet: " . 2500 / $_ . " ms, Dotted: " . 5625 / $_ . " ms";
+                   answer => "$bpm BPM as quarter notes per minute\n" . "Whole Note: " . $straight_values[0] . " ms, Triplet: " . $triplet_values[0] . " ms, Dotted: " . $dotted_values[0] . " ms
+Half Note: " . $straight_values[1] . " ms, Triplet: " . $triplet_values[1] . " ms, Dotted: " . $dotted_values[1] . " ms
+Quarter Note: " . $straight_values[2] . " ms, Triplet: " . $triplet_values[2] . " ms, Dotted: " . $dotted_values[2] . " ms
+1/8 Note: " . $straight_values[3] . " ms, Triplet: " . $triplet_values[3] . " ms, Dotted: " . $dotted_values[3] . " ms
+1/16 Note: " . $straight_values[4] . " ms, Triplet: " . $triplet_values[4] . " ms, Dotted: " . $dotted_values[4] . " ms
+1/32 Note: " . $straight_values[5] . " ms, Triplet: " . $triplet_values[5] . " ms, Dotted: " . $dotted_values[5] . " ms
+1/64 Note: " . $straight_values[6] . " ms, Triplet: " . $triplet_values[6] . " ms, Dotted: " . $dotted_values[6] . " ms";
 };
 
 1;
