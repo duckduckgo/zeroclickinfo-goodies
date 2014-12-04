757d0627ac4787138928842e0b5ecc6f21e211b2
bitflipped <jon.aykroyd@gmail.com>
2014-12-04 02:28:04 +0000

    #799 removed comments per @moollaza request

--- lib/DDG/Goodie/IsAwesome/Bitflipped.pm
+++ lib/DDG/Goodie/IsAwesome/Bitflipped.pm
@@ -1,14 +1,11 @@
 package DDG::Goodie::IsAwesome::Bitflipped;
-# ABSTRACT: bitflipped's first Goodie
-# Start at https://duck.co/duckduckhack/goodie_overview if you are new
-# to instant answer development
 
 use DDG::Goodie;
 
 zci answer_type             => "is_awesome_bitflipped";
 zci is_cached               => 1;
 
-# Metadata.                 See https://duck.co/duckduckhack/metadata for help in filling out this section.
+
 name                        "IsAwesome Bitflipped";
 description                 "Prints nice message";
 primary_example_queries     "duckduckhack bitflipped";
@@ -19,14 +16,11 @@ code_url                    "https://github.com/duckduckgo/zeroclickinfo-goodies
 attribution github          => ["bitflipped", "Jon A"];
 
 
-# Triggers
+
 triggers start      => "duckduckhack bitflipped";
 
-# Handle statement
-handle remainder    => sub {
 
-	# optional - regex guard
-	#return unless qr/^\w+/;
+handle remainder    => sub {
 
 	return if $_; # Guard against "no answer"
 
