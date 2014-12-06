b8af7cec13713027a400551ec833a60dd056859c
flippingtables <fragflop@yahoo.dk>
2014-12-06 13:51:35 +0000

    Cleanup

--- lib/DDG/Goodie/IsAwesome/flippingtables.pm
+++ lib/DDG/Goodie/IsAwesome/flippingtables.pm
@@ -25,53 +25,50 @@ triggers start => "how many days are in";
 # Handle statement
 handle remainder => sub {
     return unless $_; # Guard against "no answer"
-    my $query = $_;	
-    #print $query;
-    #return unless $query =~ /^(in|) +(\w+)$/i;;
-    #return if $_;
+    my $query = $_; 
     my $days = calculateNumberOfDaysForMonthString($query);
     if ($days != -1){
-            return $days;
-    } else {
-    return;
+      return $days;
+      } else {
+        return;
+      }
+      
+    };
+
+    sub calculateNumberOfDays{
+     my $x = $_[0];
+     
+     my $float = $x/8;
+     my $roundedFirst = int($float + $float/($float*2));
+     my $float2 = 1/$x;
+     my $roundedSecond = int($float2);
+     return (28 + ($x + $roundedFirst) % 2) + (2 % $x) + (2 * $roundedSecond);
+   }
+
+   sub calculateNumberOfDaysForMonthString{
+    my $month = $_[0];
+    my $i;
+    switch($month){
+      case("january") {$i =     1;}
+      case("february") {$i =    2;}
+      case("march") {$i =       3;}
+      case("april") {$i =       4;}
+      case("may") {$i =         5;}
+      case("june") {$i =        6;}
+      case("july") {$i =        7;}
+      case("august") {$i =      8;}
+      case("september") {$i =   9;}
+      case("october") {$i =     10;}
+      case("november") {$i =    11;}
+      case("december") {$i =    12;}
+      else    { $i = -1; }
     }
-    
-};
+    if ($i != -1){
+      return calculateNumberOfDays($i);
+      } else {
+        return -1;
+      }
+      
+    }
 
-sub calculateNumberOfDays{
- my $x = $_[0];
- 
-    my $float = $x/8;
-    my $roundedFirst = int($float + $float/($float*2));
-    my $float2 = 1/$x;
-    my $roundedSecond = int($float2);
-    return (28 + ($x + $roundedFirst) % 2) + (2 % $x) + (2 * $roundedSecond);
-}
-
-sub calculateNumberOfDaysForMonthString{
-        my $month = $_[0];
-        my $i;
-        switch($month){
-          case("january") {$i =     1;}
-          case("february") {$i =    2;}
-          case("march") {$i =       3;}
-          case("april") {$i =       4;}
-          case("may") {$i =         5;}
-          case("june") {$i =        6;}
-          case("july") {$i =        7;}
-          case("august") {$i =      8;}
-          case("september") {$i =   9;}
-          case("october") {$i =     10;}
-          case("november") {$i =    11;}
-          case("december") {$i =    12;}
-          else    { $i = -1; }
-         }
-          if ($i != -1){
-            return calculateNumberOfDays($i);
-            } else {
-                return -1;
-            }
-            
-}
-
-1;
+    1;
