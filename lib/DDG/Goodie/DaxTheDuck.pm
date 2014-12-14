f3efcdb69c8b25cbc6ea1eb4e29ba0b0d184c6e5
Thomas Denizou <javathunderman@users.noreply.github.com>
2014-12-14 14:53:08 -0500

    Update DaxTheDuck.pm

--- lib/DDG/Goodie/DaxTheDuck.pm
+++ lib/DDG/Goodie/DaxTheDuck.pm
@@ -1,11 +1,9 @@
 package DDG::Goodie::DaxTheDuck;
 
-
 use DDG::Goodie;
 
 zci answer_type => "dax_the_duck";
 zci is_cached   => 1;
-
 
 name "Dax the Duck";
 description "When you type in Dax the Duck, it tells you where Dax is. ";
@@ -15,16 +13,12 @@ code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DD
 attribution github => ["javathunderman", "Thomas Denizou"],
             twitter => "Emposoft";
 
-
 triggers any => "dax the duck";
-
 
 handle remainder => sub {
 
-
 	return "Dax is DuckDuckGo's mascot. Check him out in the top left corner or at the homepage!";
 
-	
 };
 
 1;
