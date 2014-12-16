6d099567d391f079da8be63cc22446612e822710
mohamadissawi <mohamad.issawi@outlook.com>
2014-12-16 22:26:59 +0000

    added styling in a separate CSS file, test files now succeed, and fixed a bunch of little things.

--- lib/DDG/Goodie/SiUnits.pm
+++ lib/DDG/Goodie/SiUnits.pm
@@ -3,13 +3,13 @@ package DDG::Goodie::SiUnits;
 use DDG::Goodie;
 
 zci answer_type => 'si_base_units';
-zci is_cached => 0;
+zci is_cached => 1;
 
 name 'Si Units';
-source 'conversions.txt';
+source 'http://en.wikipedia.org/wiki/International_System_of_Units';
 description 'convert composite units into si base units';
 primary_example_queries 'newtons in si base units';
-secondary_example_queries 'newtons si';
+secondary_example_queries 'si joules';
 category 'conversions';
 topics 'math', 'science';
 code_url 'https://github.com/mohamadissawi/zeroclickinfo-spice/blob/master/lib/DDG/Goodie/SiUnits.pm';
@@ -21,17 +21,18 @@ triggers start => 'si', 'si units', 'si base units';
 triggers end => 'si', 'in si', 'si units', 'in si units', 'si base units', 'in si base units'; 
 
 #read conversion statements from share directory
-open(DAT, share('conversions.txt'));
-my @lines = <DAT>;
+my @lines = split /\n/, share("conversions.txt")->slurp;
 
 #picks unit name out of remainder
 handle remainder => sub {
 
     #greps the text file for the unit of interest
-    my @thing = grep /<div style="font-size:15px">@_ /i, @lines;
+    my @thing = grep /<div class="zci--statement">@_ /i, @lines;
+    
+    chomp (@thing);
 
     #returns the instant answer, and treats the text as html
-    return (html => "@thing") if @thing;
+    return "", html => "@thing" if @thing;
     return ;
-    };
+};
 1;
--- t/SiUnits.t
+++ t/SiUnits.t
@@ -6,13 +6,14 @@ use Test::More;
 use DDG::Test::Goodie;
 
 zci answer_type => 'si_base_units';
-zci is_cached => 0;
+zci is_cached => 1;
 
 ddg_goodie_test(
     [qw(
         DDG::Goodie::SiUnits
     )],
-    'newtons si' => test_zci("", html => "<div style=\"font-size:15px\">Newtons expressed in SI base units: <div style=\"font-size:20px; padding-top:5px;\">kg&#183;m&#183;s<sup>-2</sup></div></div>"),
+    "newtons in si base units" => test_zci("",html => qq(<div class="zci--statement">Newtons expressed in SI base units: <div class="zci--unit">kg&#183;m&#183;s<sup>-2</sup></div></div>)),
+    "si joules" => test_zci("",html => qq(<div class="zci--statement">Joules expressed in SI base units: <div class="zci--unit">kg&#183;m<sup>2</sup>&#183;s<sup>-2</sup></div>)),
 );
 
 done_testing;
