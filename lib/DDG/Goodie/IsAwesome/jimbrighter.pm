81c4835eca3b742f8d4841c79bc3e4f5d89e6d5e
jim-brighter <jbrighter92@gmail.com>
2014-12-16 04:40:29 +0000

    fixed bad formatting

--- lib/DDG/Goodie/IsAwesome/jimbrighter.pm
+++ lib/DDG/Goodie/IsAwesome/jimbrighter.pm
@@ -8,18 +8,20 @@ zci is_cached   => 1;
 
 name "IsAwesome jim-brighter";
 description "First Goodie, tells the world that jim-brighter is awesome";
-primary_example_queries "duckduckhack jimbrighter";
 category "special";
 topics "special_interest", "geek";
+
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/jim-brighter.pm";
-attribution github => ["https://github.com/jim-brighter", "jim-brighter"],
+attribution github  => ["https://github.com/jim-brighter", "jim-brighter"],
             twitter => "jim_brighter";
+
+primary_example_queries "duckduckhack jimbrighter";
 
 triggers start => "duckduckhack jimbrighter";
 
 handle remainder => sub {
     return if $_;
-	return "jim-brighter is awesome and has successfully completed the DuckDuckHack Goodie tutorial!"; # Guard against "no answer"
+    return "jim-brighter is awesome and has successfully completed the DuckDuckHack Goodie tutorial!";
 
 };
 
