fb5cfacd9cc71a31fafc55e1a8b6c27709f6e0c7
jim-brighter <jbrighter92@gmail.com>
2014-12-17 23:25:40 +0000

    fixed a bunch of formatting problems

--- lib/DDG/Goodie/EmacsCheatSheet.pm
+++ lib/DDG/Goodie/EmacsCheatSheet.pm
@@ -14,7 +14,7 @@ category "cheat_sheets";
 topics "computing", "geek", "programming", "sysadmin";
 
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/EmacsCheatSheet.pm";
-attribution github => ["https://github.com/jim-brighter", "Jim Brighter"],
+attribution github  => ["https://github.com/jim-brighter", "Jim Brighter"],
             twitter => "jim_brighter";
 
 primary_example_queries "emacs cheatsheet", "emacs commands", "emacs help";
