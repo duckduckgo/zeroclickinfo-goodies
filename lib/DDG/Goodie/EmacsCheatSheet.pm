926b29b1bf16b42535d5fbbdfbf4a5a01e142400
jim-brighter <jbrighter92@gmail.com>
2014-12-17 22:40:35 +0000

    first trial

--- lib/DDG/Goodie/EmacsCheatSheet.pm
+++ lib/DDG/Goodie/EmacsCheatSheet.pm
@@ -8,24 +8,34 @@ zci is_cached   => 1;
 
 name "EmacsCheatSheet";
 description "emacs cheatsheet";
-source "ADD SOURCE";
-primary_example_queries "emacs cheatsheet", "emacs commands", "emacs help";
-secondary_example_queries "emacs reference", "emacs guide";
+source "http://www.rgrjr.com/emacs/emacs_cheat.html";
+
 category "cheat_sheets";
 topics "computing", "geek", "programming", "sysadmin";
+
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/EmacsCheatSheet.pm";
 attribution github => ["https://github.com/jim-brighter", "Jim Brighter"],
             twitter => "jim_brighter";
+
+primary_example_queries "emacs cheatsheet", "emacs commands", "emacs help";
+secondary_example_queries "emacs reference", "emacs guide";
 
 triggers startend => "emacs cheatsheet", "emacs cheat sheet",
                      "emacs help", "emacs commands", "emacs guide",
                      "emacs reference", "emacs quick reference";
 
 handle remainder => sub {
-
-	return unless $_; # Guard against "no answer"
-
-	return $_;
+	return 
+        heading => 'Emacs Cheat Sheet',
+        html    => html_cheat_sheet();
 };
+
+my $HTML;
+
+sub html_cheat_sheet {
+    $HTML //= share("emacs_cheat_sheet.html")
+            ->slurp(iomode => '<:encoding(UTF-8)');
+        return $HTML;
+}
 
 1;
