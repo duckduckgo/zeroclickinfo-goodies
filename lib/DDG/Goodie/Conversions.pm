1748eef7c0ecf8da74912133bc07c7a20c7ec385
Rob Emery <rob@mintsoft.net>
2014-12-22 21:20:31 +0000

    Conversions: Moving subs to under the handle

--- lib/DDG/Goodie/Conversions.pm
+++ lib/DDG/Goodie/Conversions.pm
@@ -55,6 +55,95 @@ my %plural_exceptions = (
     'pounds force'           => 'pounds force',
 );
 my %singular_exceptions = reverse %plural_exceptions;
+
+handle query_lc => sub {
+    # hack around issues with feet and inches for now
+    $_ =~ s/"/inches/;
+    $_ =~ s/'/feet/;
+
+    # hack support for "degrees" prefix on temperatures
+    $_ =~ s/ degrees (celsius|fahrenheit)/ $1/;
+    
+    # guard the query from spurious matches
+    return unless $_ =~ /$guard/;
+    
+    my @matches = ($+{'left_unit'}, $+{'right_unit'});
+    return if ("" ne $+{'left_num'} && "" ne $+{'right_num'});
+    my $factor = $+{'left_num'};
+
+    # if the query is in the format <unit> in <num> <unit> we need to flip
+    # also if it's like "how many cm in metre"; the "1" is implicitly metre so also flip
+    # But if the second unit is plural, assume we want the the implicit one on the first
+    # It's always ambiguous when they are both countless and plural, so shouldn't be too bad.
+    if (
+        "" ne $+{'right_num'}
+        || (   "" eq $+{'left_num'}
+            && "" eq $+{'right_num'}
+            && $+{'question'} !~ qr/convert/i
+            && !looks_plural($+{'right_unit'})))
+    {
+        $factor = $+{'right_num'};
+        @matches = ($matches[1], $matches[0]);
+    }
+    $factor = 1 if ($factor =~ qr/^(a[n]?)?$/);
+
+    # fix precision and rounding:
+    my $precision = 3;
+    my $nearest = '.' . ('0' x ($precision-1)) . '1';
+
+    my $styler = number_style_for($factor);
+    return unless $styler;
+
+    my $result = convert({
+        'factor' => $styler->for_computation($factor),
+        'from_unit' => $matches[0],
+        'to_unit' => $matches[1],
+    });
+    
+    return if !$result->{'result'};
+
+    my $f_result;
+
+    # if $result = 1.00000 .. 000n, where n <> 0 then $result != 1 and throws off pluralization, so:
+    $result->{'result'} = nearest($nearest, $result->{'result'});
+
+    if ($result->{'result'} == 0 || length($result->{'result'}) > 2*$precision + 1) {
+        if ($result->{'result'} == 0) {
+            # rounding error
+            $result = convert( {
+                'factor' => $styler->for_computation($factor),
+                'from_unit' => $matches[0],
+                'to_unit' => $matches[1],
+            } );
+        }
+
+        # We only display it in exponent form if it's above a certain number.
+        # We also want to display numbers from 0 to 1 in exponent form.
+        if($result->{'result'} > 1_000_000 || $result->{'result'} < 1) {
+            $f_result = (sprintf "%.${precision}g", $result->{'result'});
+        } else {
+            $f_result = (sprintf "%.${precision}f", $result->{'result'});
+        }
+    }
+
+    # handle pluralisation of units
+    # however temperature is never plural and does require "degrees" to be prepended
+    if ($result->{'type'} ne 'temperature') {
+        $result->{'from_unit'} = set_unit_pluralisation($result->{'from_unit'}, $factor);
+        $result->{'to_unit'}   = set_unit_pluralisation($result->{'to_unit'},   $result->{'result'});
+    } else {
+        $result->{'from_unit'} = "degrees $result->{'from_unit'}" if ($result->{'from_unit'} ne "kelvin");
+        $result->{'to_unit'} = "degrees $result->{'to_unit'}" if ($result->{'to_unit'} ne "kelvin");
+    }
+
+    $result->{'result'} = defined($f_result) ? $f_result : sprintf("%.${precision}f", $result->{'result'});
+    $result->{'result'} =~ s/\.0{$precision}$//;
+    $result->{'result'} = $styler->for_display($result->{'result'});
+
+    my $output = $styler->for_display($factor)." $result->{'from_unit'} = $result->{'result'} $result->{'to_unit'}";
+    return $output, html => wrap_html($factor, $result, $styler);
+};
+
 sub looks_plural {
     my $unit = shift;
 
@@ -176,92 +265,5 @@ sub set_unit_pluralisation {
     return $proper_unit;
 }
 
-handle query_lc => sub {
-    # hack around issues with feet and inches for now
-    $_ =~ s/"/inches/;
-    $_ =~ s/'/feet/;
-
-    # hack support for "degrees" prefix on temperatures
-    $_ =~ s/ degrees (celsius|fahrenheit)/ $1/;
-    
-    # guard the query from spurious matches
-    return unless $_ =~ /$guard/;
-    
-    my @matches = ($+{'left_unit'}, $+{'right_unit'});
-    return if ("" ne $+{'left_num'} && "" ne $+{'right_num'});
-    my $factor = $+{'left_num'};
-
-    # if the query is in the format <unit> in <num> <unit> we need to flip
-    # also if it's like "how many cm in metre"; the "1" is implicitly metre so also flip
-    # But if the second unit is plural, assume we want the the implicit one on the first
-    # It's always ambiguous when they are both countless and plural, so shouldn't be too bad.
-    if (
-        "" ne $+{'right_num'}
-        || (   "" eq $+{'left_num'}
-            && "" eq $+{'right_num'}
-            && $+{'question'} !~ qr/convert/i
-            && !looks_plural($+{'right_unit'})))
-    {
-        $factor = $+{'right_num'};
-        @matches = ($matches[1], $matches[0]);
-    }
-    $factor = 1 if ($factor =~ qr/^(a[n]?)?$/);
-
-    # fix precision and rounding:
-    my $precision = 3;
-    my $nearest = '.' . ('0' x ($precision-1)) . '1';
-
-    my $styler = number_style_for($factor);
-    return unless $styler;
-
-    my $result = convert({
-        'factor' => $styler->for_computation($factor),
-        'from_unit' => $matches[0],
-        'to_unit' => $matches[1],
-    });
-    
-    return if !$result->{'result'};
-
-    my $f_result;
-
-    # if $result = 1.00000 .. 000n, where n <> 0 then $result != 1 and throws off pluralization, so:
-    $result->{'result'} = nearest($nearest, $result->{'result'});
-
-    if ($result->{'result'} == 0 || length($result->{'result'}) > 2*$precision + 1) {
-        if ($result->{'result'} == 0) {
-            # rounding error
-            $result = convert( {
-                'factor' => $styler->for_computation($factor),
-                'from_unit' => $matches[0],
-                'to_unit' => $matches[1],
-            } );
-        }
-
-        # We only display it in exponent form if it's above a certain number.
-        # We also want to display numbers from 0 to 1 in exponent form.
-        if($result->{'result'} > 1_000_000 || $result->{'result'} < 1) {
-            $f_result = (sprintf "%.${precision}g", $result->{'result'});
-        } else {
-            $f_result = (sprintf "%.${precision}f", $result->{'result'});
-        }
-    }
-
-    # handle pluralisation of units
-    # however temperature is never plural and does require "degrees" to be prepended
-    if ($result->{'type'} ne 'temperature') {
-        $result->{'from_unit'} = set_unit_pluralisation($result->{'from_unit'}, $factor);
-        $result->{'to_unit'}   = set_unit_pluralisation($result->{'to_unit'},   $result->{'result'});
-    } else {
-        $result->{'from_unit'} = "degrees $result->{'from_unit'}" if ($result->{'from_unit'} ne "kelvin");
-        $result->{'to_unit'} = "degrees $result->{'to_unit'}" if ($result->{'to_unit'} ne "kelvin");
-    }
-
-    $result->{'result'} = defined($f_result) ? $f_result : sprintf("%.${precision}f", $result->{'result'});
-    $result->{'result'} =~ s/\.0{$precision}$//;
-    $result->{'result'} = $styler->for_display($result->{'result'});
-
-    my $output = $styler->for_display($factor)." $result->{'from_unit'} = $result->{'result'} $result->{'to_unit'}";
-    return $output, html => wrap_html($factor, $result, $styler);
-};
 
 1;
