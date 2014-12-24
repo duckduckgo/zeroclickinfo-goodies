7cbf6d88a588d7cf273061fed02052f6c4db5d29
Zaahir Moolla <moollaza@fastmail.fm>
2014-12-24 13:37:25 -0500

    whoops, remove lingering sudo

--- install_mpfr.sh
+++ install_mpfr.sh
@@ -4,4 +4,4 @@ set -ex
 # download and install mpfr for Chinese Zodiac Goodie
 wget http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.gz
 tar -xzvf mpfr-3.1.2.tar.gz
-cd mpfr-3.1.2 && ./configure --prefix=/usr && make && sudo make install
+cd mpfr-3.1.2 && ./configure --prefix=/usr && make && make check && make install
