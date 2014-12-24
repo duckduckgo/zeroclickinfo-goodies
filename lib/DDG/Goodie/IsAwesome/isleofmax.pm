9b71eaa1e3c81c60e70cbabe3c348e883724686a
isleofmax <isleofmax@gmail.com>
2014-12-24 19:33:23 +0000

    Created my first Goodie

---
+++ lib/DDG/Goodie/IsAwesome/isleofmax.pm
@@ -0,0 +1,27 @@
+package DDG::Goodie::IsAwesome::isleofmax;
+# ABSTRACT: isleofmax's first Goodie
+
+use DDG::Goodie;
+
+zci answer_type => "is_awesome_isleofmax";
+zci is_cached   => 1;
+
+name "IsAwesome isleofmax";
+description "My first Goodie, it let's the world know that isleofmax is awesome";
+primary_example_queries "duckduckhack isleofmax";
+category "special";
+topics "special_interest", "geek";
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/isleofmax.pm";
+attribution github => ["https://github.com/isleofmax", "isleofmax"];
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/isleofmax.pm";
+attribution github => ["GitHubAccount", "Friendly Name"],
+            twitter => "twitterhandle";
+
+triggers start => "duckduckhack isleofmax";
+
+handle remainder => sub {
+    return if $_;
+    return "isleofmax is awesome and has successfully completed the DuckDuckHack Goodie tutorial!";
+};
+
+1;
