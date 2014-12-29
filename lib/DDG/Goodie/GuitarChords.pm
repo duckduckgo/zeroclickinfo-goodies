a20db411e811ab41735e6e3dbc301ed3577b057b
Charles L <chuckiels2011@gmail.com>
2014-12-29 15:24:49 -0500

    restructured code

--- lib/DDG/Goodie/GuitarChords.pm
+++ lib/DDG/Goodie/GuitarChords.pm
@@ -39,28 +39,31 @@ my $gtr = GD::Tab::Guitar->new;
 
 sub check_chord
 {
-    if ($_[0] =~ /([a-gA-G])(#|b)?(dim|m|min|aug|add)?(M|maj|m|min)?([0-9])?\s*((#|b)?[0-9]+)?/) {
-        my $r = "";
-        $r .= uc($1) if $1;
-        $r .= $2 if $2;
-        if ($3) {
-            if($3 eq "min") {
-                $r .= "m";
-            } else {
-                $r .= $3;
-            }
+    if ($_[0] =~ /(?<a>[a-gA-G])(?<b>#|b)?(?<c>dim|m|min|aug|add)?(?<d>M|maj|m|min)?(?<e>[0-9])?\s*(?<f>(#|b)?[0-9]+)?/) {
+        my ($a,$b,$c,$d,$e,$f,$r) = "";
+        $a = uc($+{'a'}) if $+{'a'};
+        if ($+{'b'}) {
+            $b = $+{'b'} if $+{'b'};
         }
-        if ($4) {
-            if ($4 eq "maj") {
-                $r .= "M";
-            } elsif ($4 eq "min") {
-                $r .= "m";
-            }
+        if ($+{'c'}) {
+            $c = 'm' if ($+{'c'} =~ /(min|m)/);
         }
-        $r .= $5 if defined $5;
-        if ($6) {
-            $r .= '('.$6.')';
+        if ($+{'d'}) {
+            $d = 'M' if ($+{'d'} =~ /(maj|M)/);
         }
+        if ($+{'e'}) {
+            $e = $+{'e'};
+        }
+        if ($+{'f'}) {
+            $f = '('.$+{'f'}.')';
+        }
+        # 'cause perl will throw `$c is not defined` even though $c is initialized to an empty string?
+        $r = $a if $a;
+        $r .= $b if $b;
+        $r .= $c if $c;
+        $r .= $d if $d;
+        $r .= $e if $e;
+        $r .= $f if $f;
         if ($r ~~ @{$gtr->all_chords}) {
             return $r;
         }
