16fba0ab83c0e9513c5c42edaff929ed8e3f4055
Russell Holt <russell@duckduckgo.com>
2015-01-07 13:59:10 -0500

    semi

--- lib/DDG/Goodie/Conversions.pm
+++ lib/DDG/Goodie/Conversions.pm
@@ -16,7 +16,7 @@ primary_example_queries   'convert 5 oz to grams';
 secondary_example_queries '5 ounces to g', '0.5 nautical miles in km';
 code_url                  'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/Conversions.pm';
 attribution                github  => 'https://github.com/elohmrow',
-                           github => ['https://github.com/mintsoft', 'Rob Emery'];
+                           github => ['https://github.com/mintsoft', 'Rob Emery'],
                            email   => 'bradley@pvnp.us';
 
 zci answer_type => 'conversions';
