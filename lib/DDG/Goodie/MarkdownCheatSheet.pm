4aa6735e864a39b638eefb9de367f30966901f66
marianosimone <marianosimone@gmail.com>
2015-01-12 02:36:19 +0000

    use short individual snippets instead of full cheatsheet

--- lib/DDG/Goodie/MarkdownCheatSheet.pm
+++ lib/DDG/Goodie/MarkdownCheatSheet.pm
@@ -28,14 +28,56 @@ triggers startend => (
 
 attribution github  => ["marianosimone", "Mariano Simone"];
 
-my $txt = scalar share('markdown_cheat_sheet.txt')->slurp,
-my $html = scalar share('markdown_cheat_sheet.html')->slurp;
+my %snippets = (
+    'header', '<h1>This is an H1</h1><h2>This is an H2</h2><pre># This is an H1
+## This is an H2</pre>',
+    'em', '<em>Emphasis</em> or <em>ephasis</em><pre>_emphasis_ or *emphasis*</pre>',
+    'strong', '<strong>Strong</strong> or <strong>strong</strong><pre>**strong** or __strong__</pre>',
+    'list', '<ul><li>First</li><li>Second</li><li>Third</li></ul><pre>- First
+- Second
+- Third</pre><ol><li>First</li><li>Second</li><li>Third</li></ol><pre>1. First
+2. Second
+3. Third</pre>',
+    'image', '<img src="http://duckduckgo.com/assets/badges/logo_square.64.png"></img><pre>![Image Alt](http://duckduckgo.com/assets/badges/logo_square.64.png)</pre>',
+    'link', '<a href="http://www.duckduckgo.com" title="Example Title">This is an example inline link</a><pre>[This is an example inline link](http://www.duckduckgo.com "Example Title")</pre>',
+    'blockquote', '<blockquote>This is the first level of quoting.<blockquote>This is nested blockquote.</blockquote>Back to the first level.</blockquote><pre>> This is the first level of quoting.
+>
+> > This is nested blockquote.
+>
+> Back to the first level.</pre>'
+);
+
+sub load_synonyms {
+    my %mappings = (
+        "header", ['h1', 'heading'],
+        "em", ['emphasis', 'emphasize'],
+        "strong", [],
+        "image", ["img", "images", "insert image"],
+        "link", ["a", "href", "links"],
+        "blockquote", ["quote", "quotation"],
+        "list", ["lists", "ordered list", "unordered list", "ul", "ol"]
+    );
+
+    my %synonims = ();
+    foreach my $key (keys(%mappings)) {
+        $synonims{$key} = $key;
+        foreach my $v (@{$mappings{$key}}) {
+            $synonims{$v} = $key;
+        }
+    }
+    return %synonims;
+};
+
+my %synonyms = load_synonyms();
 
 handle remainder => sub {
+    return unless $_;
+    my $requested = $synonyms{$_};
+    return unless $requested;
     return
         heading => 'Markdown Cheat Sheet',
-        html    => $html,
-        answer  => $txt,
+        html    => $snippets{$requested}."<br>See full <a href='http://daringfireball.net/projects/markdown/syntax'>Markdown Syntax Documentation</a>",
+        answer  => $snippets{$requested},  # TODO: make simpler txt response
 };
 
 1;
--- t/MarkdownCheatSheet.t
+++ t/MarkdownCheatSheet.t
@@ -2,21 +2,32 @@
 
 use strict;
 use warnings;
+
 use Test::More;
 use DDG::Test::Goodie;
 
-zci answer_type => "markdown_cheat_sheet";
+zci answer_type => 'markdown_cheat';
 zci is_cached   => 1;
 
+sub test_success {
+    return test_zci(
+        qr/^<$_[0] ?.*>.*<\/\w*><pre>.*<\/pre>/s,
+	    heading => 'Markdown Cheat Sheet',
+		html => qr/^<$_[0] ?.*>.*<\/\w*><pre>.*<\/pre>/s,
+    );
+}
+
 ddg_goodie_test(
-    [qw( DDG::Goodie::MarkdownCheatSheet )],
-    # At a minimum, be sure to include tests for all:
-    # - primary_example_queries
-    # - secondary_example_queries
-    'example query' => test_zci('query'),
-    # Try to include some examples of queries on which it might
-    # appear that your answer will trigger, but does not.
-    'bad example query' => undef,
+	[ 'DDG::Goodie::MarkdownCheatSheet' ],
+    'markdown cheat sheet header' => test_success('h1'),
+    'markdown cheat sheet h1' => test_success('h1'),
+    'markdown cheat sheet list' => test_success('ul'),
+    'markdown cheat sheet em' => test_success('em'),
+    'markdown cheat sheet strong' => test_success('strong'),
+    'markdown cheat sheet blockquote' => test_success('blockquote'),
+    'markdown cheat sheet insert image' => test_success('img'),
+    'markdown cheat sheet link' => test_success('a'),
+    'markdown cheat sheet random stuff' => undef,
 );
 
 done_testing;
