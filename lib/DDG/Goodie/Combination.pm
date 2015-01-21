0c5b7fb5ad6e2221aaeff8bb76acd331a22ee713
Collin Richards <richardscollin@gatech.edu>
2015-01-21 13:10:07 -0500

    Fix whitespace

--- lib/DDG/Goodie/Combination.pm
+++ lib/DDG/Goodie/Combination.pm
@@ -42,50 +42,50 @@ handle query => sub {
     #Ensure both are non-negative integers
     return unless $p1 =~ /^\d+$/ && $p2 =~ /^\d+$/;
 
-	#Do not try to calculate undefined combinations
-	return unless $p1 >= $p2;
+    #Do not try to calculate undefined combinations
+    return unless $p1 >= $p2;
 
     my $result;
     my %struct_ans;
 
-	if ('choose' eq $operation) {
+    if ('choose' eq $operation) {
         $result = $style->for_display(choose($p1, $p2));
-	} else { #must be permute
+    } else { #must be permute
         $result = $style->for_display(permute($p1, $p2));
-	}
+    }
 
     return $result,
-      structured_answer => {
+    structured_answer => {
         input     => [$style->for_display($p1) . " $operation " . $style->for_display($p2)],
         operation => $operation,
         result    => $style->with_html($result),
-      };
+    };
 };
 
 #Computes the cumulative product of the numbers from $_[0] to $_[1] inclusive
 #Do not call when first parameter is greater then second.
 sub cumprod {
-	my $acc = 1;
-	for (my $i = $_[0]; $i <= $_[1]; $i++) {
-		$acc *= $i;
-	}
-	return $acc;
+    my $acc = 1;
+    for (my $i = $_[0]; $i <= $_[1]; $i++) {
+        $acc *= $i;
+    }
+    return $acc;
 }
 
 sub choose {
-	my ($n, $k) = @_;
+    my ($n, $k) = @_;
 
-	#optimiztion combination is semetric
-	my $diff = $n - $k;
-	if ($k > $diff) {
-		$k = $diff;
-	}
-	return cumprod($n - $k + 1, $n) / cumprod(1, $k);
+    #optimiztion combination is semetric
+    my $diff = $n - $k;
+    if ($k > $diff) {
+        $k = $diff;
+    }
+    return cumprod($n - $k + 1, $n) / cumprod(1, $k);
 }
 
 sub permute {
-	my ($n, $k) = @_;
-	return cumprod($n - $k + 1, $n);
+    my ($n, $k) = @_;
+    return cumprod($n - $k + 1, $n);
 }
 
 1;
