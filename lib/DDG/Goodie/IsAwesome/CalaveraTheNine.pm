d6841fd202cf3b9c0ca3bc68614273ba364f13d1
CalaveraTheNine <swift_shady@hotmail.com>
2015-02-05 10:58:08 +0000

    Update CalaveraTheNine.pm

--- lib/DDG/Goodie/IsAwesome/CalaveraTheNine.pm
+++ lib/DDG/Goodie/IsAwesome/CalaveraTheNine.pm
@@ -7,20 +7,15 @@ use DDG::Goodie;
 zci answer_type => "is_awesome_calavera_the_nine";
 zci is_cached   => 1;
 
-
 name "IsAwesome CalaveraTheNine";
-description "My first Goodie, it let's the world know that GitHubUsername is awesome";
+description "My first Goodie, it let's the world know that CalaveraTheNine is awesome";
 primary_example_queries "duckduckhack CalaveraTheNine";
 category "special";
 topics "special_interest", "geek";
-code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/GitHubUsername.pm";
-attribution github => ["https://github.com/GitHubUsername", "GitHubUserName"],
-;
-;
-
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/CalaveraTheNine.pm";
+attribution github => ["https://github.com/CalaveraTheNine", "CalaveraTheNine"];
 
 triggers start => "duckduckhack calaverathenine";
-
 
 handle remainder => sub {
     return if $_;
