9ea4f143a8db2eb70bf41076688c06713d44e9f7
puskin94 <giovanni.puskin@gmail.com>
2015-02-10 20:58:17 +0100

    modified but some problem

--- lib/DDG/Goodie/Pi.pm
+++ lib/DDG/Goodie/Pi.pm
@@ -1,3 +1,4 @@
+package DDG::Goodie::Pi;
 # ABSTRACT: This Goodie returns 'pi' to a user-specified number of decimal places
 
 use DDG::Goodie;
@@ -19,10 +20,33 @@ triggers any => "pi";
 # Handle statement
 handle remainder => sub {
 
+    # $PI has 399 decimals
+
+    my $PI1 = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+    my $PI2 = 8214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196;
+    my $PI3 = 4428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273;
+    my $PI4 = 724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609;
+
+    my $answer;
+    my $num;
+
     my $decimal = $_;
-    my $answer = substr pi, 0, ( $decimal + 2 );
-    
-    return "$answer";
+    if (($decimal  > 0) && ($decimal < 400)) {
+        if ($decimal > 301) {
+            $num = $PI1 . $PI2 . $PI3 . $PI4;
+            $answer = substr $num, 0, ( $decimal + 2 );
+        } elsif ($decimal > 201) {
+            $num = $PI1 . $PI2 . $PI3;
+            $answer = substr $num, 0, ( $decimal + 2 );
+        } elsif ($decimal > 101) {
+            $num = $PI1 . $PI2;
+            $answer = substr $num, 0, ( $decimal + 2 );
+        } else {
+            $answer = substr $PI1, 0, ( $decimal + 2 );
+        }
+        return "$answer";
+    }
+    return;
     
 };
 
