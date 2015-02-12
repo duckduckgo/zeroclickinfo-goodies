75e6cdfa47ddd7d1f22d8ca485629d72b9f48d99
jee1mr <14.jeevan@gmail.com>
2015-02-12 20:04:36 +0000

    Lorem Ipsum generator IA added

---
+++ lib/DDG/Goodie/LoremIpsum.pm
@@ -0,0 +1,26 @@
+package DDG::Goodie::LoremIpsum;
+# ABSTRACT: Lorem Ipsum Generator
+
+use DDG::Goodie;
+use Text::Lorem;
+zci answer_type => "lorem_ipsum";
+zci is_cached   => 1;
+
+name "LoremIpsum";
+description "Generates Lorem Ipsum text";
+primary_example_queries "lorem ipsum", "lipsum", "lorem ipsum 6";
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/LoremIpsum.pm";
+attribution github => ["https://github.com/jee1mr", "Jeevan M R"],
+            twitter => ["https://twitter.com/jee1mr","Jeevan M R"];
+
+
+triggers any => "lorem ipsum", "lipsum";
+
+handle remainder => sub {
+    
+    my $text = Text::Lorem->new();
+    return $text->paragraphs($_) if $_ && $_ =~ /^\d{10}$/; 
+    return $text->paragraphs(4);
+};
+
+1;
