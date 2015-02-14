65e55cc46b2ffc8f8f75b65ef9a73f90c2b36499
murz <m@murz.net>
2015-02-14 02:09:20 +0000

    get rid of another unnecessary comment and update the attribution metadata

--- lib/DDG/Goodie/IsAwesome/murz.pm
+++ lib/DDG/Goodie/IsAwesome/murz.pm
@@ -5,14 +5,13 @@ use DDG::Goodie;
 zci answer_type => "is_awesome_murz";
 zci is_cached   => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "IsAwesome murz";
 description "My first Goodie, it lets the world know that murz is awesome";
 primary_example_queries "duckduckhack murz";
 category "special";
 topics "special_interest", "geek";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/murz.pm";
-attribution github => ["GitHubAccount", "murz"];
+attribution github => ["murz", "Mike Murray"];
 
 # Triggers
 triggers start => "duckduckhack murz";
