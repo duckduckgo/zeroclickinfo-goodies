5d536195e3bf125d51359871a7e0970e8eaa8751
chercolvin <rstars70@hotmail.com>
2015-02-16 21:33:49 -0500

    Update chercolvin.pm

--- lib/DDG/Goodie/IsAwesome/chercolvin.pm
+++ lib/DDG/Goodie/IsAwesome/chercolvin.pm
@@ -6,7 +6,6 @@ use DDG::Goodie;
 zci answer_type => "is_awesome_chercolvin";
 zci is_cached   => 1;
 
-# Metadata
 name "IsAwesome chercolvin";
 description "My first Goodie, it lets the world know that chercolvin is awesome";
 primary_example_queries "duckduckhack chercolvin";
@@ -16,10 +15,8 @@ code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DD
 attribution github => ["chercolvin", "Cheryl Colvin"],
             twitter => "chercolvin";
 
-# Triggers
 triggers start => "duckduckhack chercolvin";
 
-# Handle statement
 handle remainder => sub {
 
     return if $_;
