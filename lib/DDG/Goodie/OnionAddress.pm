a712d40d93f6e08a41950baedbc0e875ae1d3635
ilv <ilv@users.noreply.github.com>
2015-03-06 19:23:25 +0000

    Made http(s) optional (but no other protos). Fixed wrong grouping in regex

--- lib/DDG/Goodie/OnionAddress.pm
+++ lib/DDG/Goodie/OnionAddress.pm
@@ -7,7 +7,7 @@ use DDG::Goodie;
 zci is_cached => 1;
 zci answer_type => "onion_address";
 
-primary_example_queries 'https://3g2upl4pq6kufc4m.onion';
+primary_example_queries '3g2upl4pq6kufc4m.onion';
 secondary_example_queries 'How do I access https://3g2upl4pq6kufc4m.onion:81/?q=dont+track+us?';
 description 'Provide quick access to HTTP onion services';
 name 'OnionAddress';
@@ -19,16 +19,18 @@ attribution email => 'ilv@torproject.org';
 # regex to detect an onion address
 # this cover several cases, including addresses with ports and/or paths
 # e.g. https://3g2upl4pq6kufc4m.onion:5000/?q=dont+track+us
-my $onion_address_qr = qr/\bhttps?:\/\/([a-z0-9]{16})\.onion[:\d+]?[\/.*]?\b/;
+my $onion_address_qr = qr/(\w+:\/\/)?([a-z0-9]{16})\.onion(:\d+)?(\/.*)?/;
 
 # add an optional question mark in case it's at the end of a question sentence
 # e.g. How do I access https://0123456789abcdef.onion?
-triggers query_lc => qr/$onion_address_qr[\?]?/;
+triggers query_lc => qr/\b$onion_address_qr(\?)?\b/;
 
 handle query_lc => sub {
 
-    return unless $1;
-    return $1, html => "<div class='zci__caption'>Access $1.onion using the <a href='https://www.torproject.org/projects/torbrowser.html.en'>Tor Browser</a> or via <a href='https://$1.tor2web.org'>Tor2web</a>.</div>";
+	# we only accept queries for web onion services
+	# we also assume that an onion service without protocol:// should be web
+	return unless !$1 or $1 eq 'https://' or $1 eq 'http://' or $2;
+    return $2, html => "<div class='zci__caption'>Access $2.onion using the <a href='https://www.torproject.org/projects/torbrowser.html.en'>Tor Browser</a> or via <a href='https://$2.tor2web.org'>Tor2web</a>.</div>";
 
 };
 
