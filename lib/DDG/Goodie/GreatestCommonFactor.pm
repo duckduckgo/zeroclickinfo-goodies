cdcdd82c31e1ec0af0891cf8f708e1f25224a5dc
Christian Aistleitner <pfirsichbluete@lirum.at>
2015-03-11 11:14:55 +0100

    GreatestCommonFactor: Drop unneeded uppercase trigger
    Since there is a lowercase 'gcf' trigger, the uppercase 'GCF' trigger
    is unnecessary.

--- lib/DDG/Goodie/GreatestCommonFactor.pm
+++ lib/DDG/Goodie/GreatestCommonFactor.pm
@@ -7,7 +7,7 @@ use DDG::Goodie;
 zci answer_type => "greatest_common_factor";
 zci is_cached   => 1;
 
-triggers startend => 'greatest common factor', 'GCF', 'gcf';
+triggers startend => 'greatest common factor', 'gcf';
 
 primary_example_queries 'GCF 121 11';
 secondary_example_queries '99 9 greatest common factor';
