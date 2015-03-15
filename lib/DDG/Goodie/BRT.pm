158df3037b3814f183698ffe2a9fa6a4dec1d02e
Alessio Felicioni <afelicioni@users.noreply.github.com>
2015-03-15 23:09:31 +0100

    Add BRT Shipment Tracking

---
+++ lib/DDG/Goodie/BRT.pm
@@ -0,0 +1,44 @@
+package DDG::Goodie::BRT;
+# ABSTRACT: Track a shipment from BRT (formerly Bartolini)
+
+use DDG::Goodie;
+
+zci answer_type => "brt";
+zci is_cached   => 1;
+
+name "BRT";
+description "Track a shipment from BRT";
+icon_url "https://afelicioni.github.io/assets/i/www.brt.it.ico";
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/BRT.pm";
+primary_example_queries "brt 114090393162";
+secondary_example_queries "brtcode 1511409039316255850";
+source "BRT";
+category 'ids';
+topics 'special_interest';
+attribution web => ["https://afelicioni.github.io/", "Alessio Felicioni"],
+            github => ["afelicioni", "Alessio Felicioni"],
+            twitter => ["afelicioni_pro", "Alessio Felicioni"];
+
+triggers start => "brt", "brtcode";
+
+handle query_lc => sub {
+    return unless /^(?:brt\s+(?:([0-9]{12})|([0-9]{19}))|brtcode\s+([0-9]{19}))$/;
+    my $brt_num = $1;
+    my $brtcode_num = $3 // $2;
+
+    # BRT Shipment Tracking (using 12 digits basic code)
+    if ( $brt_num ) {
+        return $brt_num,
+            heading => "BRT Tracking",
+            html    => qq|Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.hsm?referer=sped_numspe_par.htm&Nspediz=$brt_num&RicercaNumeroSpedizione=Ricerca">BRT</a>.|;
+    }
+    # BRT Shipment Tracking (using 19 digits "BRTcode")
+    if ( $brtcode_num ) {
+        return $brtcode_num,
+            heading => "BRT Tracking",
+            html    => qq|Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?brtCode=$brtcode_num&urltype=a">BRT</a>.|;
+    }
+    return;
+};
+
+1;
---
+++ t/BRT.t
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use DDG::Test::Goodie;
+
+zci answer_type => "brt";
+zci is_cached   => 1;
+
+ddg_goodie_test(
+    [qw( DDG::Goodie::BRT )],
+    "brt 123456789012" => test_zci(
+        "123456789012",
+        heading => 'BRT Tracking',
+        html    => 'Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.hsm?referer=sped_numspe_par.htm&Nspediz=123456789012&RicercaNumeroSpedizione=Ricerca">BRT</a>.'
+    ),
+    "brtcode 1234567890123456789" => test_zci(
+        "1234567890123456789",
+        heading => 'BRT Tracking',
+        html    => 'Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?brtCode=1234567890123456789&urltype=a">BRT</a>.'
+    ),
+    "brt 1234567890123456789" => test_zci(
+        "1234567890123456789",
+        heading => 'BRT Tracking',
+        html    => 'Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?brtCode=1234567890123456789&urltype=a">BRT</a>.'
+    ),
+    "brt 1234" => undef,
+    "brtcode 1234" => undef,
+    "brt 1234" => undef
+);
+
+done_testing;
