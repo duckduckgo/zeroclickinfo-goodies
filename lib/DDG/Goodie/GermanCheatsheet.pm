ec520d8604163827a9a6bca16f8fc03764ee3ed9
AdamSC1-ddg <adamscottcochran+ddg@gmail.com>
2015-03-16 05:29:49 +0000

    Created German Cheatsheet Goodie

---
+++ lib/DDG/Goodie/GermanCheatsheet.pm
@@ -0,0 +1,30 @@
+package DDG::Goodie::GermanCheatsheet;
+# ABSTRACT: German Cheatsheet Goodie
+
+use DDG::Goodie;
+
+zci answer_type => "german_cheatsheet";
+zci is_cached   => 1;
+
+name "GermanCheatsheet";
+description "A Cheatsheet with the 100 most common German to English words and a few key phrases";
+primary_example_queries "German Cheatsheet", "English to German Cheatsheet";
+
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/GermanCheatsheet.pm";
+attribution github => ["AdamSC1-DDG", "Adam Cochran"],
+            twitter => "adamscochran";
+
+# Triggers
+triggers any => "german cheatsheet", "cheatsheet german";
+
+my $HTML = share("german_cheatsheet.html")->slurp(iomode => "<:encoding(UTF-8)");
+
+
+handle remainder => sub {
+    return
+        heading => "English to German Cheat Sheet",
+        html    => $HTML,
+
+};
+
+1;
