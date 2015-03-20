d782f0d1e7323db5e500b897839432386d35a91f
Alessio Felicioni <afelicioni@users.noreply.github.com>
2015-03-20 16:02:08 +0100

    Strict and cleaner update

--- lib/DDG/Goodie/BRT.pm
+++ lib/DDG/Goodie/BRT.pm
@@ -1,6 +1,7 @@
 package DDG::Goodie::BRT;
 # ABSTRACT: Track a shipment from BRT (formerly Bartolini)
 
+use strict;
 use DDG::Goodie;
 
 zci answer_type => "brt";
@@ -10,8 +11,8 @@ name "BRT";
 description "Track a shipment from BRT";
 icon_url "https://afelicioni.github.io/assets/i/www.brt.it.ico";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/BRT.pm";
-primary_example_queries "brt 114090393162";
-secondary_example_queries "brtcode 1511409039316255850";
+primary_example_queries "brt 123456789012";
+secondary_example_queries "brt 1234567890123456789";
 source "BRT";
 category 'ids';
 topics 'special_interest';
@@ -19,24 +20,36 @@ attribution web => ["https://afelicioni.github.io/", "Alessio Felicioni"],
             github => ["afelicioni", "Alessio Felicioni"],
             twitter => ["afelicioni_pro", "Alessio Felicioni"];
 
-triggers start => "brt", "brtcode";
+triggers start => "brt";
 
 handle query_lc => sub {
-    return unless /^(?:brt\s+(?:(?<c_brt>[0-9]{12})|(?<c_brtcode>[0-9]{19}))|brtcode\s+(?<c_brtcode>[0-9]{19}))$/;
+    return unless /^brt\s+(?:(?<c_brt>[0-9]{12})|(?<c_brtcode>[0-9]{19}))$/;
     my $brt_num = $+{'c_brt'};
     my $brtcode_num = $+{'c_brtcode'};
 
     # BRT Shipment Tracking (using 12 digits basic code)
     if ( $brt_num ) {
-        return $brt_num,
-            heading => "BRT Tracking",
-            html    => qq|Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?referer=sped_numspe_par.htm&Nspediz=$brt_num&RicercaNumeroSpedizione=Ricerca">BRT</a>.|;
+        my $result_text    = $brt_num;
+        my $operation_text = qq|Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?referer=sped_numspe_par.htm&Nspediz=$brt_num&RicercaNumeroSpedizione=Ricerca">BRT</a>.|;
+
+        return $result_text,
+            structured_answer => {
+                input     => [],
+                operation => $operation_text,
+                result    => $result_text,
+            };
     }
     # BRT Shipment Tracking (using 19 digits "BRTcode")
     if ( $brtcode_num ) {
-        return $brtcode_num,
-            heading => "BRT Tracking",
-            html    => qq|Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?brtCode=$brtcode_num&urltype=a">BRT</a>.|;
+        my $result_text    = $brtcode_num;
+        my $operation_text = qq|Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?brtCode=$brtcode_num&urltype=a">BRT</a>.|;
+
+        return $result_text,
+            structured_answer => {
+                input     => [],
+                operation => $operation_text,
+                result    => $result_text,
+            };
     }
     return;
 };
--- t/BRT.t
+++ t/BRT.t
@@ -12,22 +12,22 @@ ddg_goodie_test(
     [qw( DDG::Goodie::BRT )],
     "brt 123456789012" => test_zci(
         "123456789012",
-        heading => 'BRT Tracking',
-        html    => 'Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?referer=sped_numspe_par.htm&Nspediz=123456789012&RicercaNumeroSpedizione=Ricerca">BRT</a>.'
-    ),
-    "brtcode 1234567890123456789" => test_zci(
-        "1234567890123456789",
-        heading => 'BRT Tracking',
-        html    => 'Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?brtCode=1234567890123456789&urltype=a">BRT</a>.'
+        structured_answer => {
+            input     => [],
+            operation => 'Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?referer=sped_numspe_par.htm&Nspediz=123456789012&RicercaNumeroSpedizione=Ricerca">BRT</a>.',
+            result    => '123456789012'
+        }
     ),
     "brt 1234567890123456789" => test_zci(
         "1234567890123456789",
-        heading => 'BRT Tracking',
-        html    => 'Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?brtCode=1234567890123456789&urltype=a">BRT</a>.'
+        structured_answer => {
+            input     => [],
+            operation => 'Shipment tracking available at <a href="http://as777.brt.it/vas/sped_det_show.htm?brtCode=1234567890123456789&urltype=a">BRT</a>.',
+            result    => '1234567890123456789'
+        }
     ),
     "brt 1234" => undef,
-    "brtcode 1234" => undef,
-    "brt 1234" => undef
+    "brt 12345678901234567890" => undef
 );
 
 done_testing;
