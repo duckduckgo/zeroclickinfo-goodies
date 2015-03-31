754cf8954b10312036d34e9101f38d345b7c2130
puskin94 <giovanni.puskin@gmail.com>
2015-03-31 13:32:31 +0200

    better js source....removed breaks

--- lib/DDG/Goodie/2048.pm
+++ lib/DDG/Goodie/2048.pm
@@ -21,42 +21,42 @@ triggers any => "game", "play";
 # Handle statement
 handle remainder => sub {
 
-	(my $inputNum, my $dimension) = split/ /;
+    (my $inputNum, my $dimension) = split/ /;
 
-	return unless $inputNum;
+    return unless $inputNum;
 
-	my $base = 128;
+    my $base = 128;
 
-	return unless (($inputNum == $base) || # play 128
-					($inputNum == $base*2) || # play 256
-					($inputNum == $base*4) || # play 512
-					($inputNum == $base*8) || # play 1024
-					($inputNum == $base*16) || # play 2048
-					($inputNum == $base*32) || # play 4096
-					($inputNum == $base*64)); # play 8192
-
-
-	if (!$dimension || $dimension > 10 || $dimension < 3) {
-		$dimension = 4;
-	}
+    return unless (($inputNum == $base) || # play 128
+                    ($inputNum == $base*2) || # play 256
+                    ($inputNum == $base*4) || # play 512
+                    ($inputNum == $base*8) || # play 1024
+                    ($inputNum == $base*16) || # play 2048
+                    ($inputNum == $base*32) || # play 4096
+                    ($inputNum == $base*64)); # play 8192
 
 
-	my $play = '<i><b>Play <span id="game">'. $inputNum .'</span> <br><br><span id="dimension">'. $dimension .'</span> x '. $dimension. '</b></i>';
-	my $text = 'Play '.$inputNum;
+    if (!$dimension || $dimension > 10 || $dimension < 3) {
+        $dimension = 4;
+    }
 
-	return $text,
-	structured_answer => {
-		id => '2048',
-		name => '2048',
-		data => $play, # I don't know how push this to the html
-		templates => {
-			group => 'base',
-			detail => 0
-			#options => {			this is not useful, duckpan takes the content.handlebars
-			#	content => $play.'Goodie.2048.content'
-			#}
-		}
-	};
+
+    my $play = '<i><b>Play <span id="game">'. $inputNum .'</span> <br><br><span id="dimension">'. $dimension .'</span> x '. $dimension. '</b></i>';
+    my $text = 'Play '.$inputNum;
+
+    return $text,
+    structured_answer => {
+        id => '2048',
+        name => '2048',
+        data => $play, # I don't know how push this to the html
+        templates => {
+            group => 'base',
+            detail => 0
+            #options => {           this is not useful, duckpan takes the content.handlebars
+            #   content => $play.'Goodie.2048.content'
+            #}
+        }
+    };
 
 };
 
