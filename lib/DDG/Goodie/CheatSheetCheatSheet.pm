7ba0740f702102c0f547f8b3c3c5cc42ad241b2b
ngzhian <ngzhian@gmail.com>
2015-04-04 22:13:43 +0000

    Use html template to render results

--- lib/DDG/Goodie/CheatSheetCheatSheet.pm
+++ lib/DDG/Goodie/CheatSheetCheatSheet.pm
@@ -19,33 +19,68 @@ attribution github => ["https://github.com/ngzhian", "ngzhian"],
 
 triggers start => "duckduckgo cheat sheets", "ddg cheat sheets";
 
-# maps cheat sheet names to their url
-my %mapping = (
-    'CronTab' => 'https://duckduckgo.com/?q=crontab+help&ia=answer',
-    'GIMP' => 'https://duckduckgo.com/?q=gimp+cheat+sheet&ia=answer',
-    'RegEx' => 'https://duckduckgo.com/?q=regex&ia=answer',
-    'TMux' => 'https://duckduckgo.com/?q=tmux+help&ia=answer',
-    'Emacs' => 'https://duckduckgo.com/?q=emacs+cheat+sheet&ia=answer',
-    'Vim' => 'https://duckduckgo.com/?q=vim+help&ia=answer',
-    'Color Codes' => 'https://duckduckgo.com/?q=color+codes',
-    'Private Networks' => 'https://duckduckgo.com/?q=private+networks&ia=answer',
-    'Public DNS' => 'https://duckduckgo.com/?q=public+dns&ia=answer',
-    'DuckDuckGo Shortcuts' => 'https://duckduckgo.com/?q=DuckDuckGo+shortcuts&ia=answer',
-    'Markdown Syntax' => 'https://duckduckgo.com/?q=markdown+syntax+list&ia=answer'
-);
+# list of mappings from cheat sheet names to their url
+my @list = [{
+    text => 'CronTab',
+    url => 'https://duckduckgo.com/?q=crontab+help&ia=answer'
+},
+{
+    text => 'GIMP',
+    url => 'https://duckduckgo.com/?q=gimp+cheat+sheet&ia=answer'
+},
+{
+    text => 'RegEx',
+    url => 'https://duckduckgo.com/?q=regex&ia=answer'
+},
+{
+    text => 'TMux',
+    url => 'https://duckduckgo.com/?q=tmux+help&ia=answer'
+},
+{
+    text => 'Emacs',
+    url => 'https://duckduckgo.com/?q=emacs+cheat+sheet&ia=answer'
+},
+{
+    text => 'Vim',
+    url => 'https://duckduckgo.com/?q=vim+help&ia=answer'
+},
+{
+    text => 'Color Codes',
+    url => 'https://duckduckgo.com/?q=color+codes',
+},
+{
+    text => 'Private Networks',
+    url => 'https://duckduckgo.com/?q=private+networks&ia=answer'
+},
+{
+    text => 'Public DNS',
+    url => 'https://duckduckgo.com/?q=public+dns&ia=answer'
+},
+{
+    text => 'DuckDuckGo Shortcuts',
+    url => 'https://duckduckgo.com/?q=DuckDuckGo+shortcuts&ia=answer'
+},
+{
+    text => 'Markdown Syntax',
+    url => 'https://duckduckgo.com/?q=markdown+syntax+list&ia=answer'
+}];
 
 
 handle remainder => sub {
-    my $html_output = '';
-    my $text_output = '';
     my $heading = 'Cheat sheets on DuckDuckGo';
-    
-    foreach my $cheatsheet (sort keys %mapping) {
-        $html_output .= '<p><a href="' . $mapping{$cheatsheet} . '">' . $cheatsheet . '</a></p>';
-        $text_output .= $cheatsheet . ' - ' . $mapping{$cheatsheet} . '\n';
+    return structured_answer => {
+        id => 'cheat_sheet_cheat_sheet',
+        name => "CheatSheet CheatSheet",
+        data => {
+            list => @list,
+        },
+        templates => {
+            group => 'list',
+            options => {
+                list_content => 'DDH.cheat_sheet_cheat_sheet.list_content'
+            }
+        }
     }
-
-    return answer => $text_output, html => $html_output, heading => $heading;
 };
 
 1;
