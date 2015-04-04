3898c18876ce982713249261fe69bd0a8d95416c
Zaahir Moolla <moollaza@duckduckgo.com>
2015-04-04 17:35:33 +0000

    finished up working detail view cheatsheets

--- lib/DDG/Goodie/CheatSheets.pm
+++ lib/DDG/Goodie/CheatSheets.pm
@@ -3,17 +3,19 @@ package DDG::Goodie::CheatSheets;
 
 use JSON::XS;
 use DDG::Goodie;
+use DDP;
 
 zci answer_type => 'cheat_sheet';
 zci is_cached   => 1;
 
 name 'CheatSheets';
 description 'Cheat sheets';
+primary_example_queries 'universal help', 'universal cheat sheet', 'universal example';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/CheatSheets.pm';
 category 'cheat_sheets';
 topics qw'computing geek programming sysadmin';
+attribution github  => [zachthompson => 'Zach Thompson'];
 
-primary_example_queries 'universal help', 'universal cheat sheet', 'universal example';
 
 triggers startend => (
 	'char', 'chars',
@@ -27,8 +29,6 @@ triggers startend => (
 	'symbol', 'symbols',
 );
 
-attribution github  => [zachthompson => 'Zach Thompson'];
-
 handle remainder => sub {
     # If needed we could jump through a few more hoops to check
     # terms against file names.
@@ -36,29 +36,19 @@ handle remainder => sub {
     open my $fh, $json_path or return;
     my $json = do { local $/;  <$fh> };
     my $data = decode_json($json);
-	my @data;
-	for my $s (@{$data->{section_order}}){
-		my %record_data;
-		for my $t (@{$data->{sections}{$s}}){
-			$record_data{$t->{key}} = $t->{val};
-		}
-		push @data, {title => $s, record_data => \%record_data};	
-	}
+
     return 'Vim Cheat Sheet', structured_answer => {
     	id => 'cheat_sheets',
 		name => 'Cheat Sheet',
-		data => \@data,
+		data => $data,
+		meta => \%{$data->{meta}},
 		templates => {
-			group => 'base',
-			options => {
-				content => 'record',
-				moreAt => true,
-				rowHighlight => true
-			}
-		},
-		meta => {
-			sourceName => 'duckduckgo',
-			sourceURL => 'https://duckduckgo.com'
+            group => 'base',
+            item => 0,
+            options => {
+                content => 'DDH.cheat_sheets.detail',
+                moreAt => 0
+            }
 		}
     };
 };
