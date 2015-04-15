87884cfc1dd33feef3080cb98e7ca8ac8e28fcb6
valcrist73 <valcrist73@gmail.com>
2015-04-15 05:47:24 +0000

    corrected and tested my first goodie

--- lib/DDG/Goodie/IsAwesome/valcrist73.pm
+++ lib/DDG/Goodie/IsAwesome/valcrist73.pm
@@ -7,13 +7,12 @@ zci answer_type => "is_awesome_valcrist73";
 zci is_cached   => 1;
 
 name "IsAwesome valcrist73";
-description "My first Goodie, it lets the world know that valcrist73 is awesome";
-primary_example_queries "duckduckhack GitHubUsername";
+description "My first Goodie, it lets the world know that valcrist73 is specifically awesome";
+primary_example_queries "duckduckhack valcrist73";
 category "special";
 topics "special_interest", "geek";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/valcrist73.pm";
-attribution github => ["valcrist73", "Jonathan"],
-                   => ["valcrist73@gmail.com", "valcrist73 at gmail.com"];
+attribution github => ["valcrist73", "Jonathan"];
 
 triggers start => "duckduckhack valcrist73";
 
@@ -21,5 +20,4 @@ handle remainder => sub {
     return if $_;
     return "Valcrist73 is awesome and has successfully completed the DuckDuckHack Goodie tutorial! - Valcrist73 es genial y ha completado exitosamente el tutorial DuckDuckHack Goodie!";
 };
-
 1;
