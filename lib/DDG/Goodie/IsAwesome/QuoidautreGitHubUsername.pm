1536d89b98cc705155e52d8fff0432282146e291
quoidautre <dev.fabrice@yahoo.fr>
2015-04-16 08:26:07 +0000

    Writing Your First Goodie Test File

--- lib/DDG/Goodie/IsAwesome/QuoidautreGitHubUsername.pm
+++ lib/DDG/Goodie/IsAwesome/QuoidautreGitHubUsername.pm
@@ -1,25 +1,28 @@
 package DDG::Goodie::IsAwesome::QuoidautreGitHubUsername;
-ABSTRACT: QuoidautreGitHubUsername''s first Goodie
+#ABSTRACT: QuoidautreGitHubUsername first Goodie;
 
 use DDG::Goodie;
 
-zci answer_type => "is_awesome_quoidautre_git_hub_username";
+zci answer_type => "is_awesome_git_hub_username";
 zci is_cached   => 1;
 
-name "IsAwesome QuoiduGitHubUsername";
-description "My first Goodie, it lets the world know that QuoidautreGitHubUsername is awesome";
-primary_example_queries "duckduckhack QuoidautreGitHubUsername";
-category "special";
-topics "special_interest", "geek";
-code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/QuoidautreGitHubUsername.pm";
-attribution github => ["https://github.com/QuoidautreGitHubUsername", "QuoidautreGitHubUsername"],
-            twitter => "quoidautre_test";
+name "IsAwesome QuoidautreGitHubUsername";
+description "Succinct explanation of what this Instant Answer does";
+primary_example_queries "first example query", "second example query";
+secondary_example_queries "optional -- demonstrate any additional triggers";
+# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
+# category "";
+# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
+# topics "";
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/GitHubUsername.pm";
+attribution github => ["GitHubAccount", "Friendly Name"],
+            twitter => "twitterhandle";
 
+#triggers start => "duckduckhack quoidautregithubusername";
 triggers start => "duckduckhack quoidautregithubusername";
 
 handle remainder => sub {
     return if $_;
-    return "QuoidautreGitHubUsername is awesome and has successfully completed the DuckDuckHack Goodie tutorial !!!!!";
+    return "QuoidautreGitHubUsername is awesome and has successfully completed the DuckDuckHack Goodie tutorial!";
 };
-
 1;
