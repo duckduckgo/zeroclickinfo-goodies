b3ef1f837e322d6ef1076b59a6ca4e2a42593972
quoidautre <dev.fabrice@yahoo.fr>
2015-04-16 16:31:01 +0000

    Created my first Goodie | rename variables & filenames

--- lib/DDG/Goodie/IsAwesome/Quoidautre.pm
+++ lib/DDG/Goodie/IsAwesome/Quoidautre.pm
@@ -1,4 +1,4 @@
-package DDG::Goodie::IsAwesome::QuoidautreGitHubUsername;
+package DDG::Goodie::IsAwesome::Quoidautre;
 #ABSTRACT: QuoidautreGitHubUsername's first Goodie;
 
 use DDG::Goodie;
@@ -7,18 +7,18 @@ use strict;
 zci answer_type => "is_awesome_git_hub_username";
 zci is_cached   => 1;
 
-name "IsAwesome QuoidautreGitHubUsername";
-description "My first Goodie, it lets QuoidautreGitHubUsername";
+name "IsAwesome Quoidautre";
+description "My first Goodie, it lets Quoidautre";
 category "special";
 topics "special_interest", "geek";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/GitHubUsername.pm";
 attribution github => ["https://github.com/quoidautre", "quoidautre"],
             twitter => "quoidautre_test";
 
-triggers start => "duckduckhack quoidautregithubusername";
+triggers start => "duckduckhack quoidautre";
 
 handle remainder => sub {
     return if $_;
-    return "QuoidautreGitHubUsername is awesome and has successfully completed the DuckDuckHack Goodie tutorial!";
+    return "Quoidautre is awesome and has successfully completed the DuckDuckHack Goodie tutorial!";
 };
 1;
