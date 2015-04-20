6200735eb9d55abe8fd46b47f617d7929ee4b744
Julien Deswaef <juego@requiem4tv.com>
2015-04-20 15:16:17 -0400

    bombed unecessary comments

--- lib/DDG/Goodie/BlenderCheatSheet.pm
+++ lib/DDG/Goodie/BlenderCheatSheet.pm
@@ -1,20 +1,14 @@
 package DDG::Goodie::BlenderCheatSheet;
 # ABSTRACT: Provide a cheatsheet for common Blender shortcuts
-# Start at https://duck.co/duckduckhack/goodie_overview if you are new
-# to instant answer development
 
 use DDG::Goodie;
 
 zci answer_type => "blender_cheat_sheet";
 zci is_cached   => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "BlenderCheatSheet";
 description "Blender most common shortcuts";
 primary_example_queries "blender shortcuts", "blender key bindings";
-# secondary_example_queries "optional -- demonstrate any additional triggers";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
 category "cheat_sheets";
 topics "computing", "geek", "special_interest";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/BlenderCheatSheet.pm";
