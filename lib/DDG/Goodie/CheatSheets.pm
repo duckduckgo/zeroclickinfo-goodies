1f2f8e487cede2c86df3f856f885b28d58bb7519
Zach Thompson <zach@duckduckgo.com>
2015-05-01 14:11:45 -0600

    CheatSheets: back to dynamic

--- lib/DDG/Goodie/CheatSheets.pm
+++ lib/DDG/Goodie/CheatSheets.pm
@@ -28,11 +28,14 @@ handle remainder => sub {
     my $json = do { local $/;  <$fh> };
     my $data = decode_json($json);
 
+	my $meta = delete $data->{meta};
+
     return 'Cheat Sheet', structured_answer => {
+	    id => 'cheat_sheets',
         dynamic_id => $data->{id},
         name => 'Cheat Sheet',
         data => $data,
-        meta => \%{$data->{meta}},
+        meta => $meta,
         templates => {
             group => 'base',
             item => 0,
