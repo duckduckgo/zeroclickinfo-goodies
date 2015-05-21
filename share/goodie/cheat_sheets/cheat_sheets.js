ae9f3d6738e4b16bfb4c06f5582e2004e02f0466
Zaahir Moolla <moollaza@duckduckgo.com>
2015-05-21 21:02:39 +0000

    simplify and refactor -- small bug to fix

--- share/goodie/cheat_sheets/cheat_sheets.js
+++ share/goodie/cheat_sheets/cheat_sheets.js
@@ -12,35 +12,63 @@ DDH.cheat_sheets.build = function(ops) {
         return result;
     });
 
-    var re_lbrackets   = /(^|[^\\])(\[|\{)/gi, // search for [ or {, but not \[ or \{
-        re_rbrackets   = /([^\\])(\]|\})/gi,   // search for ] or ], but not \] or \}
-        re_escbrackets = /(?:\\(\[|\{|\]|\}))/gi, // search for \[, \], \{, and \}
-        re_whitespace  = /\s+/,
-        re_codeblock   = /<code>(.+?)<\/code>/g;
-        _escape = Handlebars.Utils.escapeExpression;
+    var re_brackets    = /(?:(?:\[)(.+?)(?:\])|(?:\{)(.+?)(?:\}))/g, // search for [] or {}
+        re_escbrackets = /(?:\\(\[|\{|\]|\}))/g,     // search for \[, \], \{, and \}
+        re_whitespace  = /\s+/,                      // search for spaces
+        re_codeblock   = /<code>(.+?)<\/code>/g;     // search for <code></code>
 
     Spice.registerHelper('cheatsheets_codeblock', function(string, options) {
 
         var out;
 
-        if (re_lbrackets.test(string) && re_rbrackets.test(string)) {
-
-            // replace '[' or '{' with '<code>'
-            string = string.replace(re_lbrackets, '$1<code>');
-
-            // replace ']' or '}' with '</code>'
-            string = string.replace(re_rbrackets, '$1</code>');
-
-            // replace '\[' or '\{' with '[' or '{'
-            // replace '\]' or '\}' with ']' or '}'
-            string = string.replace(re_escbrackets, '$1');
-
-            out = string.replace(re_codeblock, function(match, p1, offset, string){
-                return "<code>" + _escape(p1) + "</code>";
-            });
-        } else {
-           out = "<code>" + _escape(string) + "</code>";
+        // has escaped brackets
+        // e.g. "[Ctrl-B] {\[}"
+        //  --> replace escaped brackets
+        if (re_escbrackets.test(string)){
+            console.log("escaped brackets: " + string);
+            string = string
+                .replace(/\\\[/, "&#91;")
+                .replace(/\\\]/, "&#93;")
+                .replace(/\\\{/, "&#123;")
+                .replace(/\\\}/, "&#125;");
+            console.log("now: " + string);
         }
+
+        // no spaces
+        // OR
+        // spaces and no brackets
+        // e.g "?()", ":sp filename"
+        //  --> wrap whole sting in <code></code>
+        if ( re_whitespace.test(string) === false || re_brackets.test(string) === false ){
+            console.log("No spaces OR no bracket: " + string);
+            out = "<code>" + string + "</code>";
+            console.log("now: " + out);
+
+        // spaces
+        // AND
+        // brackets
+        // e.g "[Ctrl] [B]"
+        //  --> replace [] & {} with <code></code>
+        } else {
+            console.log("spaces AND brackets: " + string);
+            out = string
+                .replace(/\[|\{/g, "<code>")
+                .replace(/\]|\}/g, "</code>");
+            console.log("now: " + out);
+        }
+
+        console.log("out: " + out);
+
+        out = out.replace(re_codeblock, function(match, p1, offset, string){
+            console.log("escaping codeblocks...");
+            console.log("match: " + match);
+            console.log("string: " + string);
+            var escaped = Handlebars.Utils.escapeExpression(p1);
+            console.log("escaped: " + escaped);
+            return "<code>" + escaped + "</code>";
+        });
+
+        console.log("final: " + out);
 
         return new Handlebars.SafeString(out);
     });
