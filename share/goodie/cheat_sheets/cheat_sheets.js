70e606898f139cb2cecbb79c95c45e0b34b4c042
Zaahir Moolla <moollaza@duckduckgo.com>
2015-05-22 22:35:33 +0000

    codeblock creation fixed and working

--- share/goodie/cheat_sheets/cheat_sheets.js
+++ share/goodie/cheat_sheets/cheat_sheets.js
@@ -12,37 +12,22 @@ DDH.cheat_sheets.build = function(ops) {
         return result;
     });
 
-    var re_brackets    = /(?:(?:\[)(.+?)(?:\])|(?:\{)(.+?)(?:\}))/g, // search for [] or {}
-        re_escbrackets = /(?:\\(\[|\{|\]|\}))/g,     // search for \[, \], \{, and \}
-        re_whitespace  = /\s+/,                      // search for spaces
-        re_codeblock   = /<code>(.+?)<\/code>/g;     // search for <code></code>
+    var re_brackets    = /(?:\[|\{|\}|\])/,      // search for [, {, }, or }
+        re_escbrackets = /(?:\\(\[|\{|\]|\}))/g, // search for \[, \], \{, and \}
+        re_whitespace  = /\s+/,                  // search for spaces
+        re_codeblock   = /<code>(.+?)<\/code>/g; // search for <code></code>
 
     Spice.registerHelper('cheatsheets_codeblock', function(string, options) {
 
         var out;
-
-        // has escaped brackets
-        // e.g. "[Ctrl-B] {\[}"
-        //  --> replace escaped brackets
-        if (re_escbrackets.test(string)){
-            console.log("escaped brackets: " + string);
-            string = string
-                .replace(/\\\[/, "&#91;")
-                .replace(/\\\]/, "&#93;")
-                .replace(/\\\{/, "&#123;")
-                .replace(/\\\}/, "&#125;");
-            console.log("now: " + string);
-        }
 
         // no spaces
         // OR
         // spaces and no brackets
         // e.g "?()", ":sp filename"
         //  --> wrap whole sting in <code></code>
-        if ( re_whitespace.test(string) === false || re_brackets.test(string) === false ){
-            console.log("No spaces OR no bracket: " + string);
+        if ( !re_whitespace.test(string) || !re_brackets.test(string) ){
             out = "<code>" + string + "</code>";
-            console.log("now: " + out);
 
         // spaces
         // AND
@@ -50,25 +35,21 @@ DDH.cheat_sheets.build = function(ops) {
         // e.g "[Ctrl] [B]"
         //  --> replace [] & {} with <code></code>
         } else {
-            console.log("spaces AND brackets: " + string);
             out = string
-                .replace(/\[|\{/g, "<code>")
-                .replace(/\]|\}/g, "</code>");
-            console.log("now: " + out);
+                // replace pairs of brackets with <code></code>
+                .replace(/(?:\[|\{)([^\[\]\{\}].*?)(?:\]|\})/g, "<code>$1</code>")
+                // un-escape remaining brackets
+                .replace(/\\\[/, "[")
+                .replace(/\\\{/, "{")
+                .replace(/\\\]/, "]")
+                .replace(/\\\}/, "}");
+
         }
 
-        console.log("out: " + out);
-
-        out = out.replace(re_codeblock, function(match, p1, offset, string){
-            console.log("escaping codeblocks...");
-            console.log("match: " + match);
-            console.log("string: " + string);
+        out = out.replace(re_codeblock, function esc_codeblock (match, p1, offset, string){
             var escaped = Handlebars.Utils.escapeExpression(p1);
-            console.log("escaped: " + escaped);
             return "<code>" + escaped + "</code>";
         });
-
-        console.log("final: " + out);
 
         return new Handlebars.SafeString(out);
     });
