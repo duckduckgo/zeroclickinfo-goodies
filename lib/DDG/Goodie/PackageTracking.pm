76effb862e7b054d990fdda1c3225c8df40606aa
Mikhail Dronov <m@dronov.net>
2015-05-26 21:23:10 +0300

    start working on fix #1138 and added Russian Post

---
+++ lib/DDG/Goodie/PackageTracking.pm
@@ -0,0 +1,45 @@
+package DDG::Goodie::PackageTracking;
+# ABSTRACT: Track a package through some sources trhrough tracking number.
+
+use strict;
+use DDG::Goodie;
+
+use YAML qw( Load );
+
+zci is_cached => 1;
+zci answer_type => "package_tracking";
+
+primary_example_queries 'CU123456789DK';
+secondary_example_queries 'EE123456789HK';
+description 'Track a package from some post sources';
+name 'PackageTracking';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/PackageTracking.pm';
+category 'ids';
+topics 'special_interest';
+attribution github => [ 'https://github.com/duckduckgo', 'duckduckgo'];
+
+my $responses = Load(scalar share("responses.yml")->slurp);
+my @response_triggers = ();
+
+foreach my $keyword (keys %$responses) {
+    my $response = $responses->{$keyword};
+    my $string_trigger = quotemeta $response->{trigger};
+    my $regex_trigger = qr/^$response->{trigger}$/i;
+
+    push @response_triggers, $regex_trigger;
+}
+
+triggers query_nowhitespace_nodash => @response_triggers;
+
+handle query_nowhitespace_nodash => sub {
+    foreach my $keyword (keys %$responses) {
+        my $package_number = $1;
+        my $response = $responses->{$keyword};
+        if ( $package_number =~ /$response->{short_code}/i  ) {
+            return $package_number, heading => 'Tracking Package', html => qq(Track this shipment at <a href="$response->{tracking_url}$package_number">$response->{name}</a>.);
+        }
+    }
+    return;
+};
+
+1;
--- t/PackageTracking.t
+++ t/PackageTracking.t
@@ -5,20 +5,25 @@ use warnings;
 use Test::More;
 use DDG::Test::Goodie;
 
-zci answer_type => 'hkdk';
+zci answer_type => 'package_tracking';
 zci is_cached => 1;
 
 ddg_goodie_test(
-        [qw( DDG::Goodie::HKDK )],
+        [qw( DDG::Goodie::PackageTracking )],
         'CU123456789DK' => test_zci(
         	"CU123456789DK",
-        	heading => 'Post Norden Shipment Tracking',
+        	heading => 'Tracking Package',
         	html => qq(Track this shipment at <a href="http://www.postdanmark.dk/tracktrace/TrackTrace.do?i_stregkode=CU123456789DK">Post Norden</a>.)
         ),
         'EE123456789HK' => test_zci(
         	"EE123456789HK",
-        	heading => 'Hongkong Post Shipment Tracking',
+        	heading => 'Tracking Package',
         	html => qq(Track this shipment at <a href="http://app3.hongkongpost.com/CGI/mt/genresult.jsp?tracknbr=EE123456789HK">Hongkong Post</a>.)
+        ),
+        'YF123456789RU' => test_zci(
+        	"YF123456789RU",
+        	heading => 'Tracking Package',
+        	html => qq(Track this shipment at <a href="http://www.russianpost.ru/tracking20/?YF123456789RU">Russian Post</a>.)
         ),
 );
 
