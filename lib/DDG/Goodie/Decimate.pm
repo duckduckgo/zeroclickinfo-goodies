b6d701eea01740d4b2d39746260812bf664d893a
Matthew Vega <matthew@fairvega.com>
2015-05-30 14:36:19 -0700

    Polish for a pull request
    Added comments.
    Improved answer notes about repeating/non-repeating decimals.
    Smarter preparation of the working numerator.

--- lib/DDG/Goodie/Decimate.pm
+++ lib/DDG/Goodie/Decimate.pm
@@ -29,74 +29,71 @@ handle remainder => sub {
 
     # optional - regex guard
     return unless qr/^\w+/;
-
-    return unless $_; # Guard against "no answer"
-
-    my @ml = /(\d+)\s*(?:\/|by)\s*(\d+)/;
-    return unless @ml;
     
-    my $numer = $ml[0];
-    my $denom = $ml[1];
-    my @nl = split("", $numer);
-    my $dotAt = @nl;
-    my $nd = 0;
-    my $rem = $numer; # initialize to anything true-ish
-    my %map = ();
-    my @ans = ();
-    my $i;
-    for ($i = 0; $rem != 0; $i++) {
-        if ($nd) {
-            $nd = $nd . ($i < @nl ? $nl[$i] : "0");
-        } else {
-            $nd = $nl[$i];
-        }
-        my $quo = int($nd / $denom);
-        $rem = $nd % $denom;
-        push @ans, $quo;
-        if ($rem != 0) { # into the decimal expansion
-            $nd = $rem;
-            if ($i >= @nl) {
-                if ($map{$rem}) {
-                    pop @ans;
-                    last;
-                } else {
-                    $map{$rem} = $i;
+    return unless $_; # Guard against "no answer"
+    
+    my @ml = /(\d+)\s*(?:\/|by)\s*(\d+)/; # numerator / denominator, "/" or "by"
+    return unless @ml;                    # quit if no match
+    
+    my $numer = $ml[0];         # the numerator
+    my $denom = $ml[1];         # the denominator
+    my @nl = split("", $numer); # list of digits in the numerator
+    my $dotAt = @nl;            # position of the decimal in the numerator
+    my $nd = "";                # working portion of the numerator
+    my $rem = $numer;           # remainder, initial value must be != 0
+    my %map = ();               # record of which remainders have occurred
+    my @ans = ();               # list of digits in the answer
+    my $i;                      # iterator position
+    for ($i = 0; $rem != 0; $i++) {      # until no remainder
+        $nd .= $i < @nl ? $nl[$i] : "0"; # get the next digit, or zero
+        my $quo = int($nd / $denom); # the working quotient, without remainder
+        $rem = $nd % $denom;         # find the remainder
+        push @ans, $quo;             # remember the working quotient
+        if ($rem != 0) {             # work still left to do
+            $nd = $rem;              # remainder becomes the working numerator
+            if ($i >= @nl) {         # into the decimal expansion
+                if ($map{$rem}) {    # this remainder has occurred before
+                    pop @ans;        # forget the last (duplicate) quotient
+                    last;            # and we're done
+                } else {             # haven't seen this remainder,
+                    $map{$rem} = $i; # but we have now, so remember it
                 }
             }
         }
     }
-    my $lz = 1;
-    my $postDot = 0;
-    my @repetend = ();
-    my $answer = "$numer / $denom = ";
-    for ($i = 0; $i < @ans; $i++) {
-        if ($lz) {
-            if ($i == $dotAt) {
-                $lz = 0;
-                $answer .= "0";
-            } elsif ($ans[$i] != 0) {
-                $lz = 0;
+    my $lz = 1;                        # leading zeros, until we see a non-zero
+    my $postDot = 0;                   # past the decimal point, not at first
+    my @repetend = ();                 # list of decimal places, if they repeat
+    my $answer = "$numer / $denom = "; # start building the answer text
+    for ($i = 0; $i < @ans; $i++) {    # for each digit in the answer
+        if ($lz) {                     # have we seen non-zero?
+            if ($i == $dotAt) {        # are we at the decimal position?
+                $lz = 0;               # zeros not leading anymore after the dot
+                $answer .= "0";        # add a zero to the answer before the dot
+            } elsif ($ans[$i] != 0) {  # found a non-zero
+                $lz = 0;               # remember that we have
             }
         }
-        if (!$lz) {
-            if ($i == $dotAt) {
-                $answer .= ".";
-                $postDot = 1;
+        if (!$lz) {                       # done with leading zeros?
+            if ($i == $dotAt) {           # are we at the decimal position?
+                $answer .= ".";           # if so, add the dot to the answer
+                $postDot = 1;             # remember that we've past the dot
             }
-            if ($postDot) {
-                push @repetend, $ans[$i];
+            if ($postDot) {               # are we past the dot?
+                push @repetend, $ans[$i]; # if so, remember in case of repeats
             }
-            $answer .= $ans[$i];
+            $answer .= $ans[$i];          # finally, add the current digit
         }
     }
-    if (@repetend == 1) {
+    if (@repetend == 1 && $rem) {            # only one repeating digit
         $answer .= " (last digit repeats)";
-    } elsif (@repetend > 1) {
-        $answer .= " (last " . @repetend . " digits repeat)";
-    } else {
+    } elsif (@repetend > 1 && $rem) {        # more than one repeating digit
+        $answer .= " (last " . @repetend .
+            " digits repeat)";
+    } elsif ($postDot) {                     # a non-repeating decimal
         $answer .= " (non-repeating)";
     }
-    return $answer;
+    return $answer;                          # all done!
 };
 
 1;
