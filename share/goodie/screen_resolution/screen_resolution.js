6aef6e830a3594949a29d78faaf9a6b40afa816d
Matthew Ramina <mattramina@gmail.com>
2015-06-02 22:26:53 -0400

    ScreenResolution: do insert without DOM

--- share/goodie/screen_resolution/screen_resolution.js
+++ share/goodie/screen_resolution/screen_resolution.js
@@ -1,13 +1,11 @@
 DDH.screen_resolution = DDH.screen_resolution || {};
 
 DDH.screen_resolution.build = function(ops){
-    return {
-        onShow: function(){
-            var res = 'Your screen resolution is ' + window.screen.width + 'x' + window.screen.height;
-            if (window.devicePixelRatio && window.devicePixelRatio > 1){
-                res += ' (pixel ratio x' + window.devicePixelRatio + ')';
-            }
-            $('#zci-screen_resolution').find('h3').html(res);
-        }
+    var res = 'Your screen resolution is ' + window.screen.width + 'x' + window.screen.height;
+    if (window.devicePixelRatio && window.devicePixelRatio > 1){
+        res += ' (pixel ratio x' + window.devicePixelRatio + ')';
     }
+
+    ops.data.title = res;
+    return ops;
 }
