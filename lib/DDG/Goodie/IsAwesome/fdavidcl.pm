7885d979791cd229f1cee9433868ff5815d63a8a
fdavidcl <fdavidcl@outlook.com>
2015-06-08 17:50:03 +0000

    Added Quick Start Goodie for user @fdavidcl

---
+++ lib/DDG/Goodie/IsAwesome/fdavidcl.pm
@@ -0,0 +1,34 @@
+package DDG::Goodie::IsAwesome::fdavidcl;
+# ABSTRACT: Write an abstract here
+# Start at https://duck.co/duckduckhack/goodie_overview if you are new
+# to instant answer development
+
+use DDG::Goodie;
+use strict;
+
+zci answer_type => "is_awesome_fdavidcl";
+zci is_cached   => 1;
+
+# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
+name "IsAwesome fdavidcl";
+description "Succinct explanation of what this instant answer does";
+primary_example_queries "first example query", "second example query";
+secondary_example_queries "optional -- demonstrate any additional triggers";
+# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
+# category "";
+# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
+# topics "";
+code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/fdavidcl.pm";
+attribution github => ["fdavidcl", "David Charte"],
+            twitter => "fdavidcl";
+
+# Triggers
+triggers start => "duckduckhack fdavidcl";
+
+# Handle statement
+handle remainder => sub {
+    return if $_;
+    return "fdavidcl is awesome and has successfully completed the DuckDuckHack Goodie tutorial!";
+};
+
+1;
