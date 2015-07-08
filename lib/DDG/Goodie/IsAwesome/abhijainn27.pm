7f8c7ac74fabd7def1db724dec135f9bb0a2354b
abhijainn27 <ajolstorage@gmail.com>
2015-07-08 13:15:29 +0000

    Added Description and attributes

--- lib/DDG/Goodie/IsAwesome/abhijainn27.pm
+++ lib/DDG/Goodie/IsAwesome/abhijainn27.pm
@@ -8,11 +8,11 @@ zci is_cached   => 1;
 
 
 name "IsAwesome abhijainn27";
-description "Succinct explanation of what this instant answer does";
+description "My First Goodie Hack !! ";
 primary_example_queries "duckduckhack abhijainn27";
 
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/abhijainn27.pm";
-attribution github => ["GitHubAccount", "Friendly Name"],
+attribution github => ["https://github.com/abhijainn27", "abhijainn27"],
             twitter => "abhijainn27";
 
 
@@ -23,9 +23,6 @@ handle remainder => sub {
     return if $_;
 	return "abhijainn27 is a DDG user and has successfully completed the DuckDuckHack Goodie tutorial!";
 
-	return unless $_; # Guard against "no answer"
-
-	return $_;
 };
 
 1;
