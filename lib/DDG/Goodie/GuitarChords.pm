d346183eace359498bbad2c157eb40f360a297ac
Charles S <chuckiels2011@gmail.com>
2015-07-09 22:37:30 +0000

    Addressed issue #1143

--- lib/DDG/Goodie/GuitarChords.pm
+++ lib/DDG/Goodie/GuitarChords.pm
@@ -34,6 +34,8 @@ handle remainder => sub
         my $f = $c;
         # filenames use '_' instead of parenthesies
         $f =~ s/[\(\)]/_/g;
+        $f =~ s/([^dim])m/$1minor/g;
+        $f =~ s/M/major/g;
 
         return
             heading => "$c",
@@ -52,12 +54,12 @@ sub check_chord
         $a = uc($+{'a'});
         $b = $+{'b'} if $+{'b'};
         if ($c = $+{'c'}) {
-            $c = 'm' if ($c =~ /^min$/);
-            $c = 'M' if ($c =~ /^maj$/);
+            $c = 'm' if ($c =~ /^(min|m)$/);
+            $c = 'M' if ($c =~ /^(maj|M)$/);
         }
         if ($d = $+{'d'}) {
-            $d = 'm' if ($d =~ /^min$/);
-            $d = 'M' if ($d =~ /^maj$/);
+            $d = 'm' if ($d =~ /^(min|m)$/);
+            $d = 'M' if ($d =~ /^(maj|M)$/);
         }
         $e = $+{'e'} if $+{'e'};
         $f = '('.$+{'f'}.')' if $+{'f'};
