6ba6aca449a39fda02cfaea76b24c4aae7f77ba1
ettienne <ettpitts@gmail.com>
2015-07-10 11:52:40 +0000

    Abstract altered and some comments removed

--- lib/DDG/Goodie/IsAwesome/Sloff.pm
+++ lib/DDG/Goodie/IsAwesome/Sloff.pm
@@ -1,7 +1,5 @@
 package DDG::Goodie::IsAwesome::Sloff;
-# ABSTRACT: Write an abstract here
-# Start at https://duck.co/duckduckhack/goodie_overview if you are new
-# to instant answer development
+# ABSTRACT: Sloff's first goodie
 
 use DDG::Goodie;
 use strict;
@@ -9,13 +7,10 @@ use strict;
 zci answer_type => "is_awesome_sloff";
 zci is_cached   => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "IsAwesome Sloff";
 description "My first goodie";
 primary_example_queries "duckduckhack sloff";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
 category "special";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
 topics "special_interest";
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/Sloff.pm";
 attribution github => ["https://github.com/Sloff/", "Sloff"];
