3fecfe85aa5462936c7eb24fc023e0da8c514a50
ankushg07 <adi10204@gmail.com>
2015-07-31 13:47:04 +0000

    Fixes for test file

--- lib/DDG/Goodie/Morse.pm
+++ lib/DDG/Goodie/Morse.pm
@@ -27,6 +27,7 @@ handle remainder => sub {
 
     return unless $input;
     return if($input eq 'cheat sheet');
+    return if($input eq 'cheatsheet');
     my $convertor = is_morse($input) ? \&as_ascii : \&as_morse;
     my $result = $convertor->($input);
 
