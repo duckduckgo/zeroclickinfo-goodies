903498f0fe8a26eb97e960352919de82f44f05f3
AhmedBenmoussa <ahmbenmoussa@msn.com>
2015-08-21 15:45:11 +0000

    My DuckDuckHack instant answer message.

--- lib/DDG/Goodie/IsAwesome/AhmedBenmoussa.pm
+++ lib/DDG/Goodie/IsAwesome/AhmedBenmoussa.pm
@@ -1,7 +1,4 @@
 package DDG::Goodie::IsAwesome::AhmedBenmoussa;
-# ABSTRACT: Write an abstract here
-# Start at https://duck.co/duckduckhack/goodie_overview if you are new
-# to instant answer development
 
 use DDG::Goodie;
 use strict;
@@ -9,15 +6,12 @@ use strict;
 zci answer_type => "is_awesome_ahmed_benmoussa";
 zci is_cached   => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
 name "IsAwesome AhmedBenmoussa";
 description "Succinct explanation of what this instant answer does";
 primary_example_queries "first example query", "second example query";
 secondary_example_queries "optional -- demonstrate any additional triggers";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
-# category "";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
-# topics "";
+
+
 code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/IsAwesome/AhmedBenmoussa.pm";
 attribution github => ["GitHubAccount", "Friendly Name"],
             twitter => "twitterhandle";
