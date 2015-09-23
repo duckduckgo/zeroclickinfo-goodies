c1cf583aadd3ab5626eb31302339e0a56f85683c
ettienne <ettpitts@gmail.com>
2015-09-23 06:44:05 +0000

    Colors changed

--- lib/DDG/Goodie/ChineseZodiac.pm
+++ lib/DDG/Goodie/ChineseZodiac.pm
@@ -21,19 +21,19 @@ code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DD
 attribution github => ['http://github.com/wilkox', 'wilkox'],
             github => ['https://github.com/Sloff', 'Sloff'];
 
-my %animal_to_language_and_path = (
-    'hare' => { en => 'Rabbit', zh => '兔' , path => "share/goodie/chinese_zodiac/rabbit.png"},
-    'dragon' => { en => 'Dragon', zh => '龙' , path => "share/goodie/chinese_zodiac/dragon.png"},
-    'snake' => { en => 'Snake', zh => '蛇' , path => "share/goodie/chinese_zodiac/snake.png"},
-    'horse' => { en => 'Horse', zh => '马' , path => "share/goodie/chinese_zodiac/horse.png"},
-    'sheep' => { en => 'Goat', zh => '羊' , path => "share/goodie/chinese_zodiac/goat.png"},
-    'monkey' => { en => 'Monkey', zh => '猴' , path => "share/goodie/chinese_zodiac/monkey.png"},
-    'fowl' => { en => 'Rooster', zh => '鸡' , path => "share/goodie/chinese_zodiac/rooster.png"},
-    'dog' => { en => 'Dog', zh => '狗' , path => "share/goodie/chinese_zodiac/dog.png"},
-    'pig' => { en => 'Pig', zh => '猪' , path => "share/goodie/chinese_zodiac/pig.png"},
-    'rat' => { en => 'Rat', zh => '鼠' , path => "share/goodie/chinese_zodiac/rat.png"},
-    'ox' => { en => 'Ox', zh => '牛' , path => "share/goodie/chinese_zodiac/ox.png"},
-    'tiger' => { en => 'Tiger', zh => '虎' , path => "share/goodie/chinese_zodiac/tiger.png"}
+my %animal_to_language_and_path_and_class = (
+    'hare' => { en => 'Rabbit', zh => '兔' , path => "share/goodie/chinese_zodiac/rabbit.png", class => "bg-clr--wood"},
+    'dragon' => { en => 'Dragon', zh => '龙' , path => "share/goodie/chinese_zodiac/dragon.png", class => "bg-clr--green"},
+    'snake' => { en => 'Snake', zh => '蛇' , path => "share/goodie/chinese_zodiac/snake.png", class => "bg-clr--red"},
+    'horse' => { en => 'Horse', zh => '马' , path => "share/goodie/chinese_zodiac/horse.png", class => "bg-clr--red"},
+    'sheep' => { en => 'Goat', zh => '羊' , path => "share/goodie/chinese_zodiac/goat.png", class => "bg-clr--green"},
+    'monkey' => { en => 'Monkey', zh => '猴' , path => "share/goodie/chinese_zodiac/monkey.png", class => "bg-clr--grey"},
+    'fowl' => { en => 'Rooster', zh => '鸡' , path => "share/goodie/chinese_zodiac/rooster.png", class => "bg-clr--grey"},
+    'dog' => { en => 'Dog', zh => '狗' , path => "share/goodie/chinese_zodiac/dog.png", class => "bg-clr--green"},
+    'pig' => { en => 'Pig', zh => '猪' , path => "share/goodie/chinese_zodiac/pig.png", class => "bg-clr--blue-light"},
+    'rat' => { en => 'Rat', zh => '鼠' , path => "share/goodie/chinese_zodiac/rat.png", class => "bg-clr--blue-light"},
+    'ox' => { en => 'Ox', zh => '牛' , path => "share/goodie/chinese_zodiac/ox.png", class => "bg-clr--green"},
+    'tiger' => { en => 'Tiger', zh => '虎' , path => "share/goodie/chinese_zodiac/tiger.png", class => "bg-clr--wood"}
 );
 
 my $chinese_zodiac_tz            = 'Asia/Shanghai';
@@ -80,17 +80,18 @@ handle remainder => sub {
     my $year_end = chinese_new_year_after($date_gregorian)->subtract(days => 1)->set_time_zone($chinese_zodiac_tz);
 
     my $animal = $year_chinese->zodiac_animal;
-    my $english = $animal_to_language_and_path{$animal}{'en'};
-    my $character = $animal_to_language_and_path{$animal}{'zh'};
-    my $path = $animal_to_language_and_path{$animal}{'path'};
+    my $english = $animal_to_language_and_path_and_class{$animal}{'en'};
+    my $character = $animal_to_language_and_path_and_class{$animal}{'zh'};
+    my $path = $animal_to_language_and_path_and_class{$animal}{'path'};
+    my $class = $animal_to_language_and_path_and_class{$animal}{'class'};
 
     my $statement = $year_start->strftime("%b %d, %Y") . " – " . $year_end->strftime("%b %d, %Y");
 
-    return format_answer($character, $english, $statement, $path);
+    return format_answer($character, $english, $statement, $path, $class);
 };
 
 sub format_answer {
-    my ($character, $english, $statement, $path) = @_;
+    my ($character, $english, $statement, $path, $class) = @_;
 
     return "$character ($english)", structured_answer => {
         id => "chinese_zodiac",
@@ -110,7 +111,7 @@ sub format_answer {
                 iconImage => 'large'
             },
             elClass => {
-                iconImage => 'bg-clr--slate circle'
+                iconImage => $class.' circle'
             }
         },
         meta => {
--- t/ChineseZodiac.t
+++ t/ChineseZodiac.t
@@ -10,27 +10,28 @@ use utf8;
 zci answer_type => 'chinesezodiac';
 zci is_cached   => 0;
 
-my %animal_to_language_and_path = (
-    'hare' => { en => 'Rabbit', zh => '兔' , path => "share/goodie/chinese_zodiac/rabbit.png"},
-    'dragon' => { en => 'Dragon', zh => '龙' , path => "share/goodie/chinese_zodiac/dragon.png"},
-    'snake' => { en => 'Snake', zh => '蛇' , path => "share/goodie/chinese_zodiac/snake.png"},
-    'horse' => { en => 'Horse', zh => '马' , path => "share/goodie/chinese_zodiac/horse.png"},
-    'sheep' => { en => 'Goat', zh => '羊' , path => "share/goodie/chinese_zodiac/goat.png"},
-    'monkey' => { en => 'Monkey', zh => '猴' , path => "share/goodie/chinese_zodiac/monkey.png"},
-    'fowl' => { en => 'Rooster', zh => '鸡' , path => "share/goodie/chinese_zodiac/rooster.png"},
-    'dog' => { en => 'Dog', zh => '狗' , path => "share/goodie/chinese_zodiac/dog.png"},
-    'pig' => { en => 'Pig', zh => '猪' , path => "share/goodie/chinese_zodiac/pig.png"},
-    'rat' => { en => 'Rat', zh => '鼠' , path => "share/goodie/chinese_zodiac/rat.png"},
-    'ox' => { en => 'Ox', zh => '牛' , path => "share/goodie/chinese_zodiac/ox.png"},
-    'tiger' => { en => 'Tiger', zh => '虎' , path => "share/goodie/chinese_zodiac/tiger.png"}
+my %animal_to_language_and_path_and_class = (
+    'hare' => { en => 'Rabbit', zh => '兔' , path => "share/goodie/chinese_zodiac/rabbit.png", class => "bg-clr--wood"},
+    'dragon' => { en => 'Dragon', zh => '龙' , path => "share/goodie/chinese_zodiac/dragon.png", class => "bg-clr--green"},
+    'snake' => { en => 'Snake', zh => '蛇' , path => "share/goodie/chinese_zodiac/snake.png", class => "bg-clr--red"},
+    'horse' => { en => 'Horse', zh => '马' , path => "share/goodie/chinese_zodiac/horse.png", class => "bg-clr--red"},
+    'sheep' => { en => 'Goat', zh => '羊' , path => "share/goodie/chinese_zodiac/goat.png", class => "bg-clr--green"},
+    'monkey' => { en => 'Monkey', zh => '猴' , path => "share/goodie/chinese_zodiac/monkey.png", class => "bg-clr--grey"},
+    'fowl' => { en => 'Rooster', zh => '鸡' , path => "share/goodie/chinese_zodiac/rooster.png", class => "bg-clr--grey"},
+    'dog' => { en => 'Dog', zh => '狗' , path => "share/goodie/chinese_zodiac/dog.png", class => "bg-clr--green"},
+    'pig' => { en => 'Pig', zh => '猪' , path => "share/goodie/chinese_zodiac/pig.png", class => "bg-clr--blue-light"},
+    'rat' => { en => 'Rat', zh => '鼠' , path => "share/goodie/chinese_zodiac/rat.png", class => "bg-clr--blue-light"},
+    'ox' => { en => 'Ox', zh => '牛' , path => "share/goodie/chinese_zodiac/ox.png", class => "bg-clr--green"},
+    'tiger' => { en => 'Tiger', zh => '虎' , path => "share/goodie/chinese_zodiac/tiger.png", class => "bg-clr--wood"}
 );
 
 sub build_answer {
     my ($animal, $statement) = @_;
     
-    my $character = $animal_to_language_and_path{$animal}{'zh'};
-    my $english = $animal_to_language_and_path{$animal}{'en'};
-    my $path = $animal_to_language_and_path{$animal}{'path'};
+    my $character = $animal_to_language_and_path_and_class{$animal}{'zh'};
+    my $english = $animal_to_language_and_path_and_class{$animal}{'en'};
+    my $path = $animal_to_language_and_path_and_class{$animal}{'path'};
+    my $class = $animal_to_language_and_path_and_class{$animal}{'class'};
 
     return test_zci("$character ($english)", structured_answer => {
         id => "chinese_zodiac",
@@ -50,7 +51,7 @@ sub build_answer {
                 iconImage => 'large'
             },
             elClass => {
-                iconImage => 'bg-clr--slate circle'
+                iconImage => $class.' circle'
             }
         },
         meta => {
