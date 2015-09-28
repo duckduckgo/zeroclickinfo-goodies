b051cf75f470accbb6a524429fad1c50ab0bcf78
Burak Ozdemir <mail@burakozdemir.co.uk>
2015-09-28 21:51:36 +0300

    UN: fix indent

--- lib/DDG/Goodie/UN.pm
+++ lib/DDG/Goodie/UN.pm
@@ -6,7 +6,7 @@ use DDG::Goodie;
 use Number::UN 'get_un';
 
 attribution github => ['tantalor', 'John Tantalo'],
-            github => ['https://github.com/ozdemirburak', "Burak Özdemir"];
+github => ['https://github.com/ozdemirburak', 'Burak Özdemir'];
 
 primary_example_queries 'UN Number 0009';
 description 'gives a description for a given UN number';
@@ -23,39 +23,39 @@ zci is_cached => 1;
 zci answer_type => 'united_nations';
 
 handle remainder => sub {
-  my $num = shift or return;
-  $num =~ s/^number\s+//gi;
-  return unless $num =~ /^\d+$/;
+    my $num = shift or return;
+    $num =~ s/^number\s+//gi;
+    return unless $num =~ /^\d+$/;
 
-  my %un = get_un($num) or return;
-  $un{description} =~ s/\.$//;
+    my %un = get_un($num) or return;
+    $un{description} =~ s/\.$//;
 
-  return $un{description}, structured_answer => {
-    id => 'un',
-    name => 'UN Number',
-    data => {
-      title => "UN Number: " . $un{number},
-      description => $un{description}
-    },
-    meta => {
-      sourceName => "Wikipedia",
-      sourceUrl => wphref($num)
-    },
-    templates => {
-      group => 'info',
-      options => {
-        moreAt => 1
-      }
-    }
-  };
+    return $un{description}, structured_answer => {
+        id => 'un',
+        name => 'UN Number',
+        data => {
+            title => "UN Number: " . $un{number},
+            description => $un{description}
+        },
+        meta => {
+            sourceName => "Wikipedia",
+            sourceUrl => wphref($num)
+        },
+        templates => {
+            group => 'info',
+            options => {
+                moreAt => 1
+            }
+        }
+    };
 };
 
 # Wikipedia attribution per CC-BY-SA
 sub wphref {
-  my $num = shift;
-  my $lower = int($num / 100) * 100 + 1;
-  my $upper = $lower + 99;
-  return sprintf WPHREF, $lower, $upper;
+    my $num = shift;
+    my $lower = int($num / 100) * 100 + 1;
+    my $upper = $lower + 99;
+    return sprintf WPHREF, $lower, $upper;
 }
 
 1;
