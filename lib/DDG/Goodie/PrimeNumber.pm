edbd70b4253eced8241e3a90e13045e852165d42
shjnyr <shjnyr@me.com>
2015-10-18 13:39:07 +0000

    Updates support to negative numbers support in PrimeNumber

--- lib/DDG/Goodie/PrimeNumber.pm
+++ lib/DDG/Goodie/PrimeNumber.pm
@@ -3,6 +3,7 @@ package DDG::Goodie::PrimeNumber;
 
 use DDG::Goodie;
 use strict;
+use POSIX;
 
 zci answer_type => "prime";
 zci is_cached   => 1;
@@ -22,30 +23,31 @@ triggers start => "prime", "prime numbers";
 
 handle query_lc => sub {
     # q_check (as opposed to q_internal) Allows for decimals.
-    return unless ($_ =~ /^\!?(?:prime(?: num(?:bers|)|)(?: between|))( [\d\.]+|)(?: and|)( [\d\.]+|)$/i);
+    return unless ($_ =~ /^\!?(?:prime num(?:ber(?:s|)|) between|)[\s]*([-]{0,1}[\d\.]+|)(?: and|)[\s]*([-]{0,1}[\d\.]+|)$/i);
 
-    my $start = $1 || 0;
-    my $end   = $2 || 0;
+    my $start = $1 || 1;
+    my $end   = $2 || 1;
 
     $start = 1000000000 if $start > 1000000000;
-    $start = 0          if $start < 0;
-    $start += 0;
-
     $end = 1000000000 if $end > 1000000000;
-    $end = 0          if $end < 0;
-    $end = 1          if !$end;
-    $end += 0;
-
     ($end, $start) = ($start, $end) if ($start > $end);
-
-    my @sieve = (1) x ($end + 1);
+    
+    my $s = ceil($start);
+    my $e = floor($end);
+    
+    $s = 1          if $s <= 0;
+    $s += 0;
+    $e = 1          if $e <= 0;
+    $e += 0;
+    
+    my @sieve = (1) x ($e + 1);
     my @primes = ();
     my $p = 2;
     my $j = 0;
     
-    while ($p * $p <= $end) {
+    while ($p * $p <= $e) {
         $j = $p + $p;
-        while ($j <= $end) {
+        while ($j <= $e) {
             $sieve[$j] = 0;
             $j += $p;
         }
@@ -54,20 +56,35 @@ handle query_lc => sub {
             $p += 1;
         }
     }
-    for ($p = $start; $p < $end; $p++) {
+    for ($p = $s; $p <= $e; $p++) {
         if ($sieve[$p] == 1) {
             push @primes, $p;
         }
     }
+    
+    return "None",
+      structured_answer => {
+        input     => [$start, $end],
+        operation => 'Prime numbers between',
+        result    => "None"
+      } unless @primes;
+      
     if ($primes[0] == 1){
         splice @primes, 0, 1;
     }
     
-    return join(",  ", @primes) . " (prime numbers)",
+    return "None",
       structured_answer => {
         input     => [$start, $end],
         operation => 'Prime numbers between',
-        result    => join(",  ", @primes)
+        result    => "None"
+      } unless @primes;
+    
+    return join(", ", @primes),
+      structured_answer => {
+        input     => [$start, $end],
+        operation => 'Prime numbers between',
+        result    => join(", ", @primes)
       };
 };
 
