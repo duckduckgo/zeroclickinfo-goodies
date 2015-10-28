6aba25664fe2c02dfbfeac80219f84e054d7c1aa
jlbaez <jlb406@gmail.com>
2015-10-28 00:55:45 +0000

    moved responses outside of handle

--- lib/DDG/Goodie/MagicEightBall.pm
+++ lib/DDG/Goodie/MagicEightBall.pm
@@ -18,32 +18,32 @@ attribution github => ["jlbaez", "Jose Baez"];
 
 triggers start => "magic eight ball", "magic 8 ball", "magic eight-ball", "magic 8-ball", "magic 8ball";
 
+#These are the standard responses found in a magic eight ball
+my @eightBallresponses = ("It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+);
+
 handle remainder => sub {
     #only 2 words or more
     return unless /\S \S/;
-
-    #These are the standard responses found in a magic eight ball
-    my @eightBallresponses = ("It is certain",
-        "It is decidedly so",
-        "Without a doubt",
-        "Yes, definitely",
-        "You may rely on it",
-        "As I see it, yes",
-        "Most likely",
-        "Outlook good",
-        "Yes",
-        "Signs point to yes",
-        "Reply hazy try again",
-        "Ask again later",
-        "Better not tell you now",
-        "Cannot predict now",
-        "Concentrate and ask again",
-        "Don't count on it",
-        "My reply is no",
-        "My sources say no",
-        "Outlook not so good",
-        "Very doubtful"
-    );
 
     my $response = $eightBallresponses[int rand scalar @eightBallresponses];
 
