fb33450f8dd7fc46d587c0eb809ff7bf88b008ca
Zaahir Moolla <moollaza@duckduckgo.com>
2015-11-14 11:52:21 -0500

    Convert tabs to spaces

--- template/share/goodie/example/example.js
+++ template/share/goodie/example/example.js
@@ -5,7 +5,7 @@ DDH.'<: $lia_id :>' = DDH.'<: $lia_id :>' || {};
 
     console.log("DDH.'<: $lia_id :>'.build");
 
-	// define private variables and functions here
+    // define private variables and functions here
 
     DDH.'<: $lia_id :>'.build = function(ops) {
 
@@ -13,25 +13,25 @@ DDH.'<: $lia_id :>' = DDH.'<: $lia_id :>' || {};
             id: "life",
 
             meta: {
-				sourceName: "Source Domain",
-				sourceUrl: "https://source.website.com"
-			},
+                sourceName: "Source Domain",
+                sourceUrl: "https://source.website.com"
+            },
 
-			// data: {
-			// already defined in Perl Package
-			// you can re-define it here
-			// or access/modify 'ops.data'
-			// },
+            // data: {
+            // already defined in Perl Package
+            // you can re-define it here
+            // or access/modify 'ops.data'
+            // },
 
-			// normalize: function(item){
-			// use this to map your 'data'
-			// to the properties required for your chosen template
-			//
-			// 	return {
-			// 		title: data.myTitle
-			// 		subtitle: data.foo.subtitle
-			// 	};
-			// },
+            // normalize: function(item){
+            // use this to map your 'data'
+            // to the properties required for your chosen template
+            //
+            //     return {
+            //         title: data.myTitle
+            //         subtitle: data.foo.subtitle
+            //     };
+            // },
 
             templates: {
                 group: 'text',
@@ -45,15 +45,15 @@ DDH.'<: $lia_id :>' = DDH.'<: $lia_id :>' || {};
                 // }
             },
 
-			// Function that executes after template content is displayed
-			onShow: function() {
+            // Function that executes after template content is displayed
+            onShow: function() {
 
-				// define any callbacks or event handlers here
-				//
-				// var $dom = $(".zci--'<: $lia_id :>'");
-				// $dom.find(".my-special-class").click(funtcion(){
-				//
-				// });
+                // define any callbacks or event handlers here
+                //
+                // var $dom = $(".zci--'<: $lia_id :>'");
+                // $dom.find(".my-special-class").click(funtcion(){
+                //
+                // });
 
             }
         };
