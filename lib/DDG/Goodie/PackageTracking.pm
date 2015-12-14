35356a53d11e391f6f415ffbb86db54f8d4001be
Mailkov <miticomilko@hotmail.com>
2015-12-14 16:58:08 +0000

    create file share/couriers.yml

--- lib/DDG/Goodie/PackageTracking.pm
+++ lib/DDG/Goodie/PackageTracking.pm
@@ -10,10 +10,28 @@ zci is_cached   => 1;
 
 triggers any => 'triggerWord', 'trigger phrase';
 
+primary_example_queries "123456789012";
+secondary_example_queries "1234567890123456789";
+description	"Track a shipment from any courier";
+name "packagetracking";
+category "ids";
+topics "special_interest";
+attribution github => ["https://github.com/Mailkov", "Melchiorre Alastra"];
+
+#load all couriers
+
+
 # Handle statement
 handle remainder => sub {
 
     my $remainder = $_;
+    
+    my @packagecouriers;
+    #validation package for couriers
+    
+    
+    
+    
 
     return "plain text response",
         structured_answer => {
@@ -23,10 +41,11 @@ handle remainder => sub {
             data => {
               title => "My Instant Answer Title",
               subtitle => "My Subtitle",
+              list => 
             },
 
             templates => {
-                group => "text",
+                group => "list",
             }
         };
 };
