81dca33febda0623331095e2c890d26308fa299b
Charlie Garrison <cngarrison@gmail.com>
2015-12-28 01:36:32 +1100

    New Goodie to calculate day of the week for a date
    Signed-off-by: Charlie Garrison <cngarrison@gmail.com>

--- lib/DDG/Goodie/DayOfWeek.pm
+++ lib/DDG/Goodie/DayOfWeek.pm
@@ -1,59 +1,53 @@
 package DDG::Goodie::DayOfWeek;
+# ABSTRACT: Calculates what day of the week a given day falls on
 
-# ABSTRACT: Write an abstract here
-# Start at https://duck.co/duckduckhack/goodie_overview if you are new
-# to instant answer development
-
-use DDG::Goodie;
 use strict;
+use DDG::Goodie;
+with 'DDG::GoodieRole::Dates';
 
-zci answer_type => '';
 
-# Caching - https://duck.co/duckduckhack/spice_advanced_backend#caching-api-responses
+zci answer_type => 'day_of_week';
 zci is_cached   => 1;
 
-# Triggers - https://duck.co/duckduckhack/goodie_triggers
-triggers any => 'triggerWord', 'trigger phrase';
+primary_example_queries 'day of week for June 22 1907';
+secondary_example_queries 'day of week for 1/1/2012', 'what day will 1/1/2025 be', 'what day was 1/1/2005', '1/1/2005 was what day';
+description 'calculate day of the week for a date';
+name 'DayOfWeek';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/DayOfWeek.pm';
+category 'time_sensitive';
+topics 'everyday';
+attribution github => ['http://github.com/cngarrison', 'cngarrison'];
+
+
+triggers startend => 'day of week', 'day of the week', 'what day will', 'what day was', 'was what day';
+
+my $datestring_regex = datestring_regex();
 
 # Handle statement
 handle remainder => sub {
-
     my $remainder = $_;
+    return unless $remainder && 
+                  $remainder =~ qr/(?<date>$datestring_regex)/x;
+    
+    my $input_date   = parse_datestring_to_date($+{date});
+    return unless $input_date;
+    
+    my $out_date     = date_output_string($input_date);
+    my $day_of_week  = $input_date->day_name;
 
-    # Optional - Guard against no remainder
-    # I.E. the query is only 'triggerWord' or 'trigger phrase'
-    #
-    # return unless $remainder;
+    my $text = "Day of the Week: $day_of_week";
 
-    # Optional - Regular expression guard
-    # Use this approach to ensure the remainder matches a pattern
-    # I.E. it only contains letters, or numbers, or contains certain words
-    #
-    # return unless qr/^\w+|\d{5}$/;
-
-    return "plain text response",
+    return $text,
         structured_answer => {
-
-            # ID - Must be unique and match Instant Answer page
-            # E.g. https://duck.co/ia/view/calculator has `id => 'calculator'``
-            id => '',
-
-            # Name - Used for Answer Bar Tab
-            # Value should be chosen from existing Instant Answer topics
-            # see https://duck.co/duckduckhack/display_reference#codenamecode-emstringem-required
+            id   => 'day_of_week',
             name => 'Answer',
-
             data => {
-              title => "My Instant Answer Title",
-              subtitle => "My Subtitle",
-              # image => "http://website.com/image.png"
+              title => $day_of_week,
+              subtitle => "Day of the week for: $out_date",
             },
-
             templates => {
                 group => "text",
-                # options => {
-                #
-                # }
+                moreAt => 0
             }
         };
 };
--- t/DayOfWeek.t
+++ t/DayOfWeek.t
@@ -4,19 +4,59 @@ use strict;
 use warnings;
 use Test::More;
 use DDG::Test::Goodie;
+use DateTime;
 
-zci answer_type => "";
+zci answer_type => "day_of_week";
 zci is_cached   => 1;
+
+sub build_answer {
+    my ($answer, $sub) = @_;
+    $sub = '' unless $sub;
+
+    return sprintf("Day of the Week: %s",$answer) , structured_answer => {
+        id => 'day_of_week',
+        name => 'Answer',
+        data => {
+            title => $answer,
+            subtitle => "Day of the week for: $sub"
+        },
+        templates => {
+            group => 'text',
+            moreAt => 0
+        }
+    }
+}
+
+
+# my $dt = DateTime->now(time_zone => $loc->time_zone);
+my $dt_today     = DateTime->now;
+my $dt_tomorrow  = $dt_today->clone->add( days => 1 );
+my $dt_yesterday = $dt_today->clone->subtract( days => 1 );
 
 ddg_goodie_test(
     [qw( DDG::Goodie::DayOfWeek )],
-    # At a minimum, be sure to include tests for all:
-    # - primary_example_queries
-    # - secondary_example_queries
-    'example query' => test_zci('query'),
-    # Try to include some examples of queries on which it might
-    # appear that your answer will trigger, but does not.
-    'bad example query' => undef,
+
+    'day of week june 22 1907'   => test_zci( build_answer( 'Saturday', '22 Jun 1907' ) ),
+    'day of week for 1/1/2012'   => test_zci( build_answer( 'Sunday',   '01 Jan 2012' ) ),
+    'day of week for 01/01/2012' => test_zci( build_answer( 'Sunday',   '01 Jan 2012' ) ),
+
+    'what day was 1/1/2005' => test_zci( build_answer( 'Saturday', '01 Jan 2005' ) ),
+    '1/1/2005 was what day' => test_zci( build_answer( 'Saturday', '01 Jan 2005' ) ),
+
+    'what day will 1/1/2025 be'          => test_zci( build_answer( 'Wednesday', '01 Jan 2025' ) ),
+    'What day will November 23 2050 be?' => test_zci( build_answer( 'Wednesday', '23 Nov 2050' ) ),
+    'What day will 23 November 2050 be?' => test_zci( build_answer( 'Wednesday', '23 Nov 2050' ) ),
+
+    'day of week 1/12/2005' => test_zci( build_answer( 'Wednesday', '12 Jan 2005' ) ),
+    'day of week 12/1/2005' => test_zci( build_answer( 'Thursday',  '01 Dec 2005' ) ),
+    'day of week 13/1/2005' => test_zci( build_answer( 'Thursday',  '13 Jan 2005' ) ),
+
+    'day of week today'     => test_zci( build_answer( $dt_today->day_name,     $dt_today->strftime("%d %b %Y") ) ),
+    'day of week tomorrow'  => test_zci( build_answer( $dt_tomorrow->day_name,  $dt_tomorrow->strftime("%d %b %Y") ) ),
+    'day of week yesterday' => test_zci( build_answer( $dt_yesterday->day_name, $dt_yesterday->strftime("%d %b %Y") ) ),
+
+    'day of week' => undef,
+    'day of week' => undef,
 );
 
 done_testing;
