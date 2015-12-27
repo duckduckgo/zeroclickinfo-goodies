5292d8ede7f1162e8a0e9ad3ff3d4f25edc0d283
Ben Moon <guiltydolphin@gmail.com>
2015-12-27 13:56:48 +0000

    Implement basic nonsense
    Provides support for basic queries to generate gibberish in
    all supported languages.

--- lib/DDG/Goodie/GibberishGenerator.pm
+++ lib/DDG/Goodie/GibberishGenerator.pm
@@ -5,13 +5,65 @@ use strict;
 use DDG::Goodie;
 use utf8;
 
+use Silly::Werder qw(sentence get_werd);
+
 triggers any => qw(nonsense gibberish);
 
 zci is_cached => 0;
 zci answer_type => "gibberish_generator";
 
+my %languages = (
+    'english'       => ['English', 'small'],
+    'german'        => ['German',  ''],
+    'french'        => ['French',  ''],
+    'shakespearean' => ['Shakespeare', ''],
+    'swedish'       => ['Swedish', ''],
+);
+
+sub pluralise {
+    my ($amount, $to_pluralise) = @_;
+    return $amount == 1 ? $to_pluralise : ($to_pluralise . 's');
+}
+
+my $types       = qr/(?<type>word|sentence)/i;
+my $modifier_re = qr/(?<modifier>English|German|French|Swedish|Shakespearean)/i;
+my $amountre    = qr/(?<amount>\d+)/;
+my $nonsense_re = qr/(?:nonsense|gibberish)/i;
+
+my $forms = qr/^(?:
+     (((?<amount>\d+)\s)?($types)s?\sof((?!$modifier_re).)*(?<modifier>$modifier_re)?+.*($nonsense_re))
+    |((?<amount>\d+)\s(?<modifier>$modifier_re)?\s*($nonsense_re)\s($types)s?)
+    )$/xi;
+
+
+# Generates a string containing all the nonsense words/sentences.
+sub generate_werds {
+    my ($amount, $modifier, $type) = @_;
+    my @modifier_args = @{$languages{$modifier}};
+
+    my $werd = Silly::Werder->new();
+    my $werds;
+    if ($type eq 'sentence') {
+        $werds .= join ' ', map { sentence() } (1..$amount);
+    } elsif ($type eq 'word') {
+        $werd->set_werds_num($amount, $amount);
+        $werd->set_language(@modifier_args);
+        $werds = $werd->sentence();
+    };
+    return $werds;
+}
+
 handle query_lc => sub {
     my $query   = $_;
+    return unless $query =~ $forms;
+    my $amount   = $+{'amount'} // 1;
+    my $modifier = $+{'modifier'} // 'english';
+    my $type     = $+{'type'};
+    my $result = generate_werds($amount, $modifier, $type);
+    # Proper-case modifier (english -> English)
+    my $fmodifier = $modifier =~ s/^\w/\u$&/r;
+    # E.g, "3 words of Swedish gibberish"
+    my $formatted_input = "$amount @{[pluralise $amount, $type]} of $fmodifier gibberish";
 
     return $result, structured_answer => {
         id   => 'gibberishgenerator',
--- t/GibberishGenerator.t
+++ t/GibberishGenerator.t
@@ -23,8 +23,30 @@ sub build_result {
 
 sub build_test { test_zci(build_result(qr/^$_[0]$/)) }
 
+my $shake_word   = qr/[a-z'-]+/i;
+my $french_word  = qr/[a-zàâæçéèêëîïôœùûüÿ]+/i;
+my $english_word = qr/[a-z]+/i;
+my $swedish_word = qr/[a-zåäö]+/i;
+my $german_word  = qr/[a-zäöüß]+/i;
+
+sub spaced {
+    my ($reg, $end, $amount) = @_;
+    return qr/($reg ){@{[$amount - 1]}}$reg$end/;
+}
+sub sentence  { qr/($_[0] )+$_[0]\./ }
+sub werds     { spaced($_[0], qr/\./, $_[1]) }
+sub paragraph { spaced(sentence($_[0]), qr//, $_[1]) }
+
 ddg_goodie_test(
     [qw( DDG::Goodie::GibberishGenerator )],
+    # First form
+    '5 words of gibberish'                    => build_test(werds($english_word, 5)),
+    'sentence of nonsense'                    => build_test(sentence($english_word)),
+    '3 words of utter Shakespearean nonsense' => build_test(werds($shake_word, 3)),
+    '2 words of french gibberish'             => build_test(werds($french_word, 2)),
+    # Second form
+    '3 nonsense words'               => build_test(werds($english_word, 3)),
+    '7 Swedish nonsense words'       => build_test(werds($swedish_word, 7)),
     # Non-matchers
     'utter nonsense'                        => undef,
     'nonsense word'                         => undef,
