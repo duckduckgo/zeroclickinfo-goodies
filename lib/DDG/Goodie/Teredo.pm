b954752411132d5e5b0087d082a916b5dda724ce
Mailkov <miticomilko@hotmail.com>
2015-12-29 16:41:36 +0000

    add template list

--- lib/DDG/Goodie/Teredo.pm
+++ lib/DDG/Goodie/Teredo.pm
@@ -26,7 +26,7 @@ zci is_cached   => 1;
 
 # Params: server, port, client
 sub to_text {
-	return "Teredo Server IPv4: " . $_[0]->ip() . "\nNAT Public IPv4: " . $_[2]->ip()
+	return "Teredo Server IPv4: " . $_[0] . "\nNAT Public IPv4: " . $_[2]
 		. "\nClient Port: " . $_[1];
 }
 
@@ -41,14 +41,41 @@ handle remainder => sub {
 	    my $binip = $ip->binip();
 
 	    # bits 32 to 64 designate IPv4 address of the Teredo server used
-	    push @output, (new Net::IP (Net::IP::ip_bintoip((substr $binip, 32, 32),4)));
+	    my $netip = new Net::IP (Net::IP::ip_bintoip((substr $binip, 32, 32),4));
+        my $teredo = $netip->ip();
+	    push @output, $teredo;
 
 	    # negation of bits 80 to 96, converted to decimal, designate NAT port number
-	    push @output, (65535 - cnv((substr $binip, 80, 16),2,10));
+	    my $port = (65535 - cnv((substr $binip, 80, 16),2,10));
+	    push @output, $port;
 
 	    # negation of bits 96 to 128 designate IPv4 address of NAT device
-	    push @output, (new Net::IP (Net::IP::ip_bintoip(~(substr $binip, 96, 32),4)));
-	    return answer => to_text(@output), #html => to_html(@output);
+	    $netip = new Net::IP (Net::IP::ip_bintoip(~(substr $binip, 96, 32),4));
+        my $nat = $netip->ip();
+	    push @output, $netip->ip();
+        
+        
+        my %output  =  ( 
+            'Teredo Server IPv4:' => $teredo,
+            'Nat Public IPv4:' => $nat,
+            'Client Port:' => $port,  
+        );
+               
+	    return to_text(@output), #html => to_html(@output);
+        structured_answer => {
+            id => 'teredo',
+            name => 'Answer',
+            data => {
+                record_data => \%output,
+                record_keys => ['Teredo Server IPv4:', 'Nat Public IPv4:', 'Client Port:'],
+            },
+            templates => {
+                group => 'list',
+                options => {
+                    content => 'record',
+                }
+            }
+        };
     }
     return;
 
