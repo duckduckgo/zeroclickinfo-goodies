764a03da732520657b0c8e8e7729eca257782f42
Ben Moon <guiltydolphin@gmail.com>
2015-12-29 15:50:58 +0000

    Add main functionality
    Adds the body to provide the functionality for the Goodie.

--- lib/DDG/Goodie/LoremIpsum.pm
+++ lib/DDG/Goodie/LoremIpsum.pm
@@ -13,6 +13,54 @@ triggers any => 'lorem ipsum', 'lipsum', 'latin';
 zci is_cached => 0;
 zci answer_type => 'lorem_ipsum';
 
+sub pluralise {
+    my ($amount, $to_pluralise) = @_;
+    return $amount == 1 ? $to_pluralise : ($to_pluralise . 's');
+}
+
+my $types       = qr/(?<type>word|sentence|paragraph)/i;
+my $modifier_re = qr/(?<modifier>random|regular)/i;
+my $amountre    = qr/(?<amount>\d+)/;
+my $lorem_re = qr/(?:latin|l(?:orem )?ipsum)/i;
+
+my $forms = qr/^(?:
+     (((?<amount>\d+)\s)?($types)s?\sof((?!$modifier_re).)*(?<modifier>$modifier_re)?+.*($lorem_re))
+    |((?<amount>\d+)\s(?<modifier>$modifier_re)?\s*($lorem_re)\s($types)s?)
+    )$/xi;
+
+
+# Generates a string containing all the nonsense words/sentences.
+sub generate_latin {
+    my ($amount, $modifier, $type) = @_;
+    my $word = Text::Lorem::More->new();
+    my $words;
+    if ($type eq 'sentence') {
+        $words = $word->sentences($amount);
+    } elsif ($type eq 'word') {
+        $words = $word->words($amount);
+    } elsif ($type eq 'paragraph') {
+        $words = $word->paragraphs($amount);
+    };
+    return $words;
+}
+
+sub get_result_and_formatted {
+    my $query = shift;
+    $query =~ s/^a /one /i;
+    # Convert initial words into numbers if possible.
+    my $formatted = $query =~ s/^\w+[a-rt-z](?=\b)/str2nbr($&)/ier;
+    return unless $formatted =~ $forms;
+    my $amount   = $+{'amount'} // 1;
+    my $modifier = $+{'modifier'} // 'regular';
+    my $type     = $+{'type'};
+    my $result = generate_latin($amount, $modifier, $type);
+    # Proper-case modifier (latin -> Latin)
+    my $fmodifier = $modifier =~ s/^\w/\u$&/r;
+    # E.g, "3 words of lorem ipsum"
+    my $formatted_input = "$amount @{[pluralise $amount, $type]} of Lorem Ipsum";
+    return ($result, $formatted_input);
+}
+
 handle query_lc => sub {
     my $query   = $_;
     # Treat 'a' as 'one' for the purposes of amounts.
