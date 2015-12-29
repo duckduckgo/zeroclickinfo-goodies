a602c9c5648731020b421da258ff986c82b991a7
Mailkov <miticomilko@hotmail.com>
2015-12-29 14:07:35 +0000

    correct indentation

--- lib/DDG/Goodie/Teredo.pm
+++ lib/DDG/Goodie/Teredo.pm
@@ -32,24 +32,24 @@ handle remainder => sub {
 
     # Verify the query value is a valid Teredo IPv6 address
     if ((defined $ip) && ($ip->version() == 6) && (substr($ip->ip(),0,9) eq "2001:0000")) {
-	my $binip = $ip->binip();
+	    my $binip = $ip->binip();
 
-	# bits 32 to 64 designate IPv4 address of the Teredo server used
-	push @output, (new Net::IP (Net::IP::ip_bintoip((substr $binip, 32, 32),4)));
+	    # bits 32 to 64 designate IPv4 address of the Teredo server used
+	    push @output, (new Net::IP (Net::IP::ip_bintoip((substr $binip, 32, 32),4)));
 
-	# negation of bits 80 to 96, converted to decimal, designate NAT port number
-	push @output, (65535 - cnv((substr $binip, 80, 16),2,10));
+	    # negation of bits 80 to 96, converted to decimal, designate NAT port number
+	    push @output, (65535 - cnv((substr $binip, 80, 16),2,10));
 
-	# negation of bits 96 to 128 designate IPv4 address of NAT device
-	push @output, (new Net::IP (Net::IP::ip_bintoip(~(substr $binip, 96, 32),4)));
-	return answer => to_text(@output), #html => to_html(@output);
+	    # negation of bits 96 to 128 designate IPv4 address of NAT device
+	    push @output, (new Net::IP (Net::IP::ip_bintoip(~(substr $binip, 96, 32),4)));
+	    return answer => to_text(@output), #html => to_html(@output);
     }
     return;
 
     # Params: server, port, client
     sub to_text {
-	return "Teredo Server IPv4: " . $_[0]->ip() . "\nNAT Public IPv4: " . $_[2]->ip()
-		. "\nClient Port: " . $_[1];
+	    return "Teredo Server IPv4: " . $_[0]->ip() . "\nNAT Public IPv4: " . $_[2]->ip()
+		    . "\nClient Port: " . $_[1];
     }
 
     # Params: server, port, client
