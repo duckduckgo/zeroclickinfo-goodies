f4586191f11a2b490567cdf194f83aeeac7a10fa
Mailkov <miticomilko@hotmail.com>
2015-12-30 16:41:29 +0000

    fix indentation

--- lib/DDG/Goodie/FIGlet.pm
+++ lib/DDG/Goodie/FIGlet.pm
@@ -17,7 +17,7 @@ topics 'words_and_games';
 code_url 'https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/Figlet.pm';
 attribution
     web => ['http://engvik.nu', 'Lars Jansøn Engvik'],
-	github => [ 'larseng', 'Lars Jansøn Engvik'],
+    github => [ 'larseng', 'Lars Jansøn Engvik'],
     github => ["https://github.com/Mailkov", "Melchiorre Alastra"];
 
 zci answer_type => 'figlet';
@@ -33,42 +33,42 @@ closedir DIR;
 # Renders a figlet.
 sub render_figlet {
     my ($font, $text) = @_;
-	return Text::FIGlet->new(-f=>$font, -d=>share())->figify(-w=>$width, -A=>$text);
+    return Text::FIGlet->new(-f=>$font, -d=>share())->figify(-w=>$width, -A=>$text);
 }
 
 handle query => sub {
-	my $font;
-	my $text;
-	my $figlet;
-	my $html;
+    my $font;
+    my $text;
+    my $figlet;
+    my $html;
 
-	# Return if no input provided.
-	return if ((lc $_ eq 'figlet') || (lc $_ eq 'bigtext') || (lc $_ eq 'big text'));
+    # Return if no input provided.
+    return if ((lc $_ eq 'figlet') || (lc $_ eq 'bigtext') || (lc $_ eq 'big text'));
 
-	# Parse query.
-	$_ =~ m/^(?:figlet|bigtext|big text)(?:\-|\s+)(.*)|(.*)\s+(?:figlet|bigtext|big text)$/i;
-	$text = $1 if $1;
-	$text = $2 if $2;
+    # Parse query.
+    $_ =~ m/^(?:figlet|bigtext|big text)(?:\-|\s+)(.*)|(.*)\s+(?:figlet|bigtext|big text)$/i;
+    $text = $1 if $1;
+    $text = $2 if $2;
 
-	# Checks if the first word is a font.
-	$text =~ m/^\s*(\w+)/;
-	$font = lc $1 if grep /\b$1\b/i, @fonts;
+    # Checks if the first word is a font.
+    $text =~ m/^\s*(\w+)/;
+    $font = lc $1 if grep /\b$1\b/i, @fonts;
 
-	# Strip the font from the text to render if we're using a font.
-	if ($font && $font ne $text) {
-		$text = substr $text, length ($font)+1, length $text;
-	} else {
-		$font = "standard";
-	}
+    # Strip the font from the text to render if we're using a font.
+    if ($font && $font ne $text) {
+        $text = substr $text, length ($font)+1, length $text;
+    } else {
+        $font = "standard";
+    }
 
-	# Render the FIGlet
-	$figlet = render_figlet($font, $text);
-	$figlet = html_enc($figlet) if grep /^$font$/i, qw(rot13 mnemonic term); #ensure we escape input for plaintext formatted results
+    # Render the FIGlet
+    $figlet = render_figlet($font, $text);
+    $figlet = html_enc($figlet) if grep /^$font$/i, qw(rot13 mnemonic term); #ensure we escape input for plaintext formatted results
 
-	$html = "<div id='figlet-wrapper'><span>Font: </span><span id='figlet-font'>$font</span><pre contenteditable='true'>$figlet</pre></div>";
+    $html = "<div id='figlet-wrapper'><span>Font: </span><span id='figlet-font'>$font</span><pre contenteditable='true'>$figlet</pre></div>";
 
-	return $figlet, html => $html if $figlet;
-	return;
+    return $figlet, html => $html if $figlet;
+    return;
 };
 
 1;
