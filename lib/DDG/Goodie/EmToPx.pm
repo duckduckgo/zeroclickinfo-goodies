609070ba8045d30abc4856ce6f66882536857d30
Ben Moon <guiltydolphin@gmail.com>
2016-01-02 12:34:52 +0000

    Update regexes and format of answer
    Fixes a few bugs with the regexes (namely the font-size).
    
    Adds support for using "base pixel size" instead of "font size".
    
    Pulls out some of the expressions into smaller pieces.

--- lib/DDG/Goodie/EmToPx.pm
+++ lib/DDG/Goodie/EmToPx.pm
@@ -20,12 +20,6 @@ topics 'programming';
 attribution twitter => ['crazedpsyc','crazedpsyc'],
             cpan    => ['CRZEDPSYC','crazedpsyc'];
 
-handle query_raw => sub {
-    my $q = lc $_;
-    $q =~ s/(?![\.\s])\W//g;
-    return unless $q =~ /^(?:convert|change|what\s*(?:s|is|are)\s+)?(?<num>\d+\.\d*|\d*\.\d+|\d+)\s*(?<source>em|px)\s+(?:in|to)\s+(?<target>em|px)(?:\s+(?:with|at|using|assuming)(?:\s+an?)?\s+(?<fs>\d+\.\d*|\d*\.\d+|\d+)\s*px)?/;
-    my ($target, $num, $source, $fontsize) = map { $+{$_} } qw(target num source fs);
-    $fontsize ||= 16;
 
 sub em_to_px {
     my ($to_convert, $fontsize) = @_;
@@ -37,11 +31,27 @@ sub px_to_em {
     return sprintf('%.3f', $to_convert / $fontsize) =~ s/\.?0+$//r;
 }
 
+my $number = qr/(\d++\.\d*+|\d*+\.\d++|\d++)/;
+my $whatis = qr/(convert|change|what\s*('?s|is|are))?/i;
+my $amount = qr/(?<amount>$number)/i;
+my $source = qr/(?<source>em|px)/i;
+my $target = qr/(?<target>em|px)/i;
+my $fs = qr/((font[ -]?|base[- ]?pixel[- ]?)size)/i;
+my $fontsize = qr/((with|at|using|assuming)(\s*an?)?)?\s*(?<fm>$fs\s*(of)?\s*)?((?<fontsize>$number)\s*(px)?)\s*(?(<fm>)|($fs))?/i;
+
+handle query_lc => sub {
+    my $query = $_;
+    $query =~ s/(?![\.\s])\W//g;
+    return unless $query =~ /^${whatis}?\s*${amount}\s*${source}\s*(?:in|to)\s*${target}\s*${fontsize}?\s*$/i;
+    my $target   = $+{target};
+    my $num      = $+{amount};
+    my $source   = $+{source};
+    my $fontsize = $+{fontsize} // 16;
     return if ($target eq $source || !$num);
 
-    my $plur   = $result == 1      ? "is"             : "are";
     my $result = ($target eq 'px') ? em_to_px($num, $fontsize) . 'px'
                                    : px_to_em($num, $fontsize) . 'em';
+    my $formatted_input = "Convert $num $source to $target with a font-size of ${fontsize}px";
 
     return $result,
         structured_answer => {
