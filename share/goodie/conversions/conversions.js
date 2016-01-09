93389963f7299fae6f642aeb01a6e802af70e4a7
Rob Emery <rob@mintsoft.net>
2016-01-09 17:15:05 +0000

    Conversions: Adding skeleton files for JS

---
+++ share/goodie/conversions/conversions.js
@@ -0,0 +1,9 @@
+DDH.conversions = DDH.conversions || {};
+
+DDH.conversions.build = function(ops){
+	// http://api.duckduckgo.com/?q=minecraft+cake&format=json&pretty=1
+	$.ajax({
+		url: "http://api.duckduckgo.com/?q=10%20metres%20in%20feet&format=json&pretty=1",
+	});
+	return ops;
+};
