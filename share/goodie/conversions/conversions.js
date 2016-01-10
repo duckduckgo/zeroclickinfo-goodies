e34c6ed2bcb34d8c5e7f8a13802f5871b942db9b
Rob Emery <rob@mintsoft.net>
2016-01-10 19:32:31 +0000

    Conversions: text boxes galore & AJAX being triggered

--- share/goodie/conversions/conversions.js
+++ share/goodie/conversions/conversions.js
@@ -1,9 +1,28 @@
 DDH.conversions = DDH.conversions || {};
 
-DDH.conversions.build = function(ops){
+DDH.conversions.build = function(){
 	// http://api.duckduckgo.com/?q=minecraft+cake&format=json&pretty=1
-	$.ajax({
-		url: "http://api.duckduckgo.com/?q=10%20metres%20in%20feet&format=json&pretty=1",
-	});
-	return ops;
+	
+	return {
+		onShow: function() {
+			$("#zci__conversions-left-in").change(function(){
+				console.log("onchange executed");
+				var query = $(this).val() + " " + $("#zci__conversions-left-unit").val() + " in " + $("#zci__conversions-right-unit").val();
+				
+				$.ajax({
+					url: "https://api.duckduckgo.com/",
+					data: { 
+						q: query,
+						format: 'json'
+					},
+					method: 'get',
+					dataType: 'json',
+					done: function(data) {
+						console.log("AJAX success",data);
+					}
+				});
+				
+			});
+		}
+	};
 };
