dac58d4622c297c6468f810774a2833e5a46a531
Zach Thompson <zach@duckduckgo.com>
2016-01-12 14:15:06 -0700

    Remove boilerplate. Suppress warnings

--- lib/DDG/Goodie/Bin2Unicode.pm
+++ lib/DDG/Goodie/Bin2Unicode.pm
@@ -4,53 +4,31 @@ package DDG::Goodie::Bin2Unicode;
 use DDG::Goodie;
 use strict;
 
-use warnings FATAL => 'non_unicode';
+no warnings 'non_unicode';
 
 zci answer_type => 'bin2unicode';
 zci is_cached   => 1;
 
-# Triggers - https://duck.co/duckduckhack/goodie_triggers
 triggers query => qr{[ 01]+(?:(?:to\s+)?unicode|text|ascii)?};
 
-# Handle statement
-handle remainder => sub {
+handle query_lc => sub {
 
-    my $remainder = $_;
+    return unless /([\s01]+)(?:(?:to\s+)?unicode|text|ascii)?/;
 
-    # Optional - Guard against no remainder
-    # I.E. the query is only 'triggerWord' or 'trigger phrase'
-    #
-    # return unless $remainder;
-
-    # Optional - Regular expression guard
-    # Use this approach to ensure the remainder matches a pattern
-    # I.E. it only contains letters, or numbers, or contains certain words
-    #
-    # return unless qr/^\w+|\d{5}$/;
-
-    return "plain text response",
+    my $bin = my $unicode = $1;
+    $unicode =~ s/([01]+)/chr(oct("0b$1"))/ge;
+    return "Binary '$bin' converted to unicode is '$unicode'",,
         structured_answer => {
-
-            # ID - Must be unique and match Instant Answer page
-            # E.g. https://duck.co/ia/view/calculator has `id => 'calculator'``
             id => 'bin2unicode',
-
-            # Name - Used for Answer Bar Tab
-            # Value should be chosen from existing Instant Answer topics
-            # see https://duck.co/duckduckhack/display_reference#codenamecode-emstringem-required
             name => 'Answer',
-
             data => {
-              title => "My Instant Answer Title",
-              subtitle => "My Subtitle",
-              # image => "http://website.com/image.png"
+              title => $unicode,
+              subtitle => "Input: $bin",
             },
 
             templates => {
-                group => "text",
-                # options => {
-                #
-                # }
+                group => 'text',
+                moreAt => 0
             }
         };
 };
