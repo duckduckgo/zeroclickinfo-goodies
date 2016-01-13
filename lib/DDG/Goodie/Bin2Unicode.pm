fc1abffad899e2c59ff99ed13b9759ecd21f739c
Zach Thompson <zach@duckduckgo.com>
2016-01-13 14:57:07 -0700

    Default to ascii if binary number is larger than max code point for unicode Suppress warnings about overflow, non-portability, non_unicode Add tests

--- lib/DDG/Goodie/Bin2Unicode.pm
+++ lib/DDG/Goodie/Bin2Unicode.pm
@@ -4,24 +4,47 @@ package DDG::Goodie::Bin2Unicode;
 use DDG::Goodie;
 use strict;
 
-no warnings 'non_unicode';
+no warnings qw'non_unicode overflow portable';
 
 zci answer_type => 'bin2unicode';
 zci is_cached   => 1;
 
-triggers query => qr{^([01\s]{8,})(?:(?:to\s+)?(?:unicode|text|ascii))?$};
+triggers query => qr{^([01\s]{8,})(?:\s+(?:to\s+)?(?:unicode|text|ascii))?$};
+
+my $MAX_CODE_PT = 1114111;
 
 handle matches => sub {
+    my $q = $_; # orginal query
+    my $bin_string = shift @_; # captured binary string
 
-    my $bin = my $unicode = shift;
-    $unicode =~ s/([01]+)/chr(oct("0b$1"))/ge;
-    return "Binary '$bin' converted to unicode is '$unicode'",,
+    my $want_ascii = $q =~ /\bascii\b/;
+
+    my @bins = $bin_string =~ /([01]+|\s+)/g;
+    my $str;
+    for my $b (@bins){
+        if($b =~ /^[01]+$/){
+		    # Overflow/non-portable warnings expected
+            my $i = oct("0b$b");
+            # Assume ascii if out of range or explicitly requested.
+            # This will work for characters all in the same string
+            # but will not print the right non-ascii characters *if*
+            # they are also in the string.
+            $str .= (($i > $MAX_CODE_PT) || $want_ascii)
+                ? pack('B*', $b)
+                : chr($i);
+        }
+        else{
+            $str .= $b;
+        }
+    }
+
+    return "Binary '$bin_string' converted to " . $want_ascii ? 'ascii' : 'unicode' . " is '$str'",,
         structured_answer => {
             id => 'bin2unicode',
             name => 'Answer',
             data => {
-              title => $unicode,
-              subtitle => "Input: $bin",
+              title => $str,
+              subtitle => "Input: $q",
             },
 
             templates => {
