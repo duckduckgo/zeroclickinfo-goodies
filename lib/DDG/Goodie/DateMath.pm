1dae5db923b4d0b91a41de9ab1322988dcfd02ce
Ben Moon <guiltydolphin@gmail.com>
2016-01-14 19:26:00 +0000

    Implement hour/minute/second support
    Closes #2056

--- lib/DDG/Goodie/DateMath.pm
+++ lib/DDG/Goodie/DateMath.pm
@@ -39,8 +39,41 @@ sub get_duration {
     );
 }
 
+sub get_action_for {
+    my $action = shift;
+    return '+' if $action =~ /\+|plus|from/i;
+    return '-' if $action =~ /\-|minus|ago/i;
+}
 
+sub normalize_number {
+    my $number = shift;
+    return $number =~ s/(\w+(?!s)\w)/str2nbr($1)/gre;
+}
 
+sub is_clock_unit {
+    my $unit = shift;
+    return $unit =~ /hour|minute|second/i;
+}
+
+sub format_result {
+    my ($out_date, $unit) = @_;
+    my $clock_rep = $out_date->strftime("%T");
+    $out_date = date_output_string($out_date);
+    return "${out_date}@{[is_clock_unit($unit) ? ' ' . $clock_rep : '']}";
+}
+
+sub get_clock_time {
+    my $date = shift;
+    return $date->strftime("%T");
+}
+
+sub format_input {
+    my ($input_date, $action, $unit, $input_number) = @_;
+    my $in_date    = date_output_string($input_date);
+    $in_date = "$in_date @{[get_clock_time $input_date]}" if is_clock_unit $unit;
+    my $out_action = "$action $input_number $unit";
+    return "$in_date $out_action";
+}
 
 my $relative_regex = qr/(?<number>\d+|[a-z\s-]+)\s+$units/;
 
@@ -78,32 +111,25 @@ handle query_lc => sub {
         return build_result($out_date, $+{date});
     }
 
-    my $input_date   = parse_datestring_to_date($+{date});
-    my $input_number = str2nbr($+{number});
+    my $action = get_action_for $+{action} or return;
+    my $input_date;
+    unless (defined $+{date}) {
+        $input_date = parse_datestring_to_date("today");
+    } else {
+        $input_date   = parse_datestring_to_date($+{date});
+    };
+    my $input_number = normalize_number $+{number};
     my $unit = $+{unit};
-
-    # check/tweak other (non-date) input
-    my %action_map = (
-        plus  => '+',
-        '+'   => '+',
-        minus => '-',
-        '-'   => '-',
-        from  => '+'
-    );
-    my $action = $action_map{$+{action}} || return;
 
     my $number = $action eq '-' ? 0 - $input_number : $input_number;
 
-    $unit =~ s/s$//g;
     my $dur = get_duration $number, $unit;
 
+    $unit .= 's' if $input_number > 1;
+    my $out_date = $input_date->clone->add_duration($dur);
+    my $result = format_result $out_date, $unit;
+    my $formatted_input = format_input $input_date, $action, $unit, $input_number;
 
-    $unit .= 's' if $input_number > 1;    # plural?
-    my $out_date   = date_output_string($input_date->clone->add_duration($dur));
-    my $in_date    = date_output_string($input_date);
-    my $out_action = "$action $input_number $unit";
-    my $result = $out_date;
-    my $formatted_input = "$in_date $out_action";
     return build_result($result, $formatted_input);
 };
 
