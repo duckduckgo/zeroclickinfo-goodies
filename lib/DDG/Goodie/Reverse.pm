c681f283a85e0795a9985bdf1946d325351cca40
Ben Moon <guiltydolphin@gmail.com>
2016-01-17 15:17:25 +0000

    Update Reverse to use WhatIs

--- lib/DDG/Goodie/Reverse.pm
+++ lib/DDG/Goodie/Reverse.pm
@@ -3,23 +3,32 @@ package DDG::Goodie::Reverse;
 
 use strict;
 use DDG::Goodie;
+with 'DDG::GoodieRole::WhatIs';
 
 triggers startend => "reverse text";
 
 zci answer_type => "reverse";
 zci is_cached   => 1;
 
-handle remainder => sub {
-  my $in = $_;
+my $matcher = wi_custom(
+    groups => ['imperative', 'prefix', 'postfix'],
+    options => {
+        command => 'reverse text',
+        primary => qr/(?!complement\s(of )?[ATCGURYKMSWBVDHN\s-]+).+/i,
+    },
+);
 
-  return unless $in;    # Guard against empty query.
+handle query => sub {
+  my $query = shift;
+  my $match = $matcher->full_match($query) or return;
+
   #Filter out requests for DNA/RNA reverse complements, handled
   # by the ReverseComplement goodie
-  return if $in =~ /^complement\s(of )?[ATCGURYKMSWBVDHN\s-]+$/i;
+  my $in = $match->{value};
 
   my $out = reverse $in;
 
-  return qq|Reversed "$_": | . $out,
+  return qq|Reversed "$in": | . $out,
     structured_answer => {
       input     => [html_enc($in)],
       operation => 'Reverse string',
