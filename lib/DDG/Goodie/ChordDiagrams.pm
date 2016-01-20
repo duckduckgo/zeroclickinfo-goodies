502ae8550a22a5a6b2c6d83bc210cc2a9a057163
charlie <chuckiels2011@gmail.com>
2016-01-20 18:27:15 -0500

    removed unused code

--- lib/DDG/Goodie/ChordDiagrams.pm
+++ lib/DDG/Goodie/ChordDiagrams.pm
@@ -28,49 +28,12 @@ open(my $fh, '<', share('guitar.json'));
 my $json_text = <$fh>;
 my $new_chords = decode_json($json_text);
 
-# Map note letters to indexes
-my %notes = (
-    "c"  => 0,    #
-    "d"  => 2,
-    "e"  => 4,  #
-    "f"  => 5,
-    "g"  => 7,#
-    "a"  => 9,
-    "b"  => 11,
-);
-
-# Map the distance that the root of a chord is from each note
-my %chords = (
-    "augmented"  => [0, 4, 8, 11],
-    "major"      => [0, 4, 7, 11, 2, 5, 9],
-    "dominant"   => [0, 4, 7, 10, 2, 5, 9],
-    "minor"      => [0, 3, 7, 10, 2, 5, 9],
-    "diminished" => [0, 3, 6, 10],
-    "sus2"       => [0, 2, 7],
-    "sus4"       => [0, 5, 7],
-);
-
 # Store the instruments that the program will respond to, with a
 # list storing the note of each string in order. (Add one to note
 # for sharps, and subtract one for flats)
-my %instruments = (
-    "guitar"  => [$notes{"e"}, $notes{"a"}, $notes{"d"}, $notes{"g"}, $notes{"b"}, $notes{"e"}],
-    "ukulele" => [$notes{"g"}, $notes{"c"}, $notes{"e"}, $notes{"a"}],
+my @instruments = (
+    "guitar"
 );
-my %instrument_aliases = (
-    "uke" => "ukulele"
-);
-# Find the smallest element in an array
-sub minimum{
-    my @sorted = sort{ $a <=> $b } (@_);
-    return $sorted[0];
-};
-
-# Find the largest element in an array
-sub maximum{
-    my @sorted = sort{ $a <=> $b } (@_);
-    return $sorted[-1];
-};
 
 # create svg X for muted strings
 sub mk_x {
@@ -185,10 +148,7 @@ sub items{
     if (defined $mod) {
         $mod = $mod_hash{$mod} || 0;
     }
-    my @chordList = grep($chords{$_}, @words);
-    if(defined $chordList[0]){
-        $chord = $chordList[0];
-    }elsif(defined $chord && ($chord eq "m" || $chord =~ /(min|minor)/i)){
+    if(defined $chord && ($chord eq "m" || $chord =~ /(min|minor)/i)){
         $chord = "min";
     }elsif(defined $chord && ($chord eq "M" || $chord =~ /(maj|major)/i)){
         $chord = "maj";
@@ -202,81 +162,13 @@ sub items{
     if(!$dom){
         $dom = 5;
     };
-    my @instr = grep($instruments{$_}, @words);
-    if(!@instr){
-        if($_ && $instrument_aliases{$_}){
-            @instr = $instrument_aliases{(grep($instrument_aliases{$_}, @words))[0]};
-        };
-    };
-    return $instr[0], $chord, uc $key, $mod, $dom;
-};
-
-# Turns a root notes, and a chord (such as from the chord hash), and
-# turns it into a chord in the key of that root
-sub chord{
-    my $root = $_[0];
-    my $dis = $_[1];
-    return map{($_ + $root) % 12} @$dis;
-};
-
-# Takes a starting fret, a chord, such as from the chord function, and an instrument, such
-# as from the instrument hash.
-# Determines which frets would need to be pressed on that instrument to
-# form that chord.
-# The starting fret determines the lowest fret that the function will try to
-# put a note on.
-sub _frets{
-    my ($start, $instrument, $values) = @_;
-    my @final = ();
-    foreach my $s (@$instrument){
-        for(my $f = 0; $f < 12; $f+= $f?1:$start){ #$f starts at zero, then skips to the value of $start
-            foreach my $n (@$values){
-                if($n == ($s+$f) % 12){
-                    push(@final, $f);
-                    $f += 12;
-                    last;
-                };
-            };
-        };
-    };
-    return @final;
-};
-
-# Takes in all the same vales as _fret, besides a start value. Passes them to
-# _fret with slowly increasing start values.
-# For each array returned by _fret, determines a "distance" between the notes
-# on an instrument.
-# A lower distance then all the previous distances will get that array added
-# to the return array.
-sub all_frets{
-    my @values;
-    my $small_d = 9999999;
-    for my $d (1 .. 8){
-        my @value = _frets (($d), @_);
-        my $distance = 0;
-        my $l = 0;
-        my $n = -1;
-        for my $i (0 .. $#value){
-            if($n >= 0 and $value[$i]){
-                $distance += $i-$n + abs($value[$i]-$l)
-            };
-            if($value[$i]){
-                $l = $value[$i];
-                $n = $i;
-            };
-        };
-        if($distance < $small_d){
-            $small_d = $distance;
-            push(@values, @value);
-        };
-    };
-    return @values;
+    my $instr = grep(@instruments, @words);
+    return $instr, $chord, uc $key, $mod, $dom;
 };
 
 sub get_chord {
     my $chord = shift;
     my $name = shift; # maj, 5, min, etc.
-    $chord =~ tr/a-z/A-Z/;
     foreach(@$new_chords) {
         if (grep(/^$chord$/, @{%$_{'root'}})) {
             foreach(@{%$_{'types'}}) {
@@ -286,16 +178,26 @@ sub get_chord {
             }
         }
     }
+    return undef;
 };
 
 # Handle statement
 handle remainder => sub {
     my ($instr_name, $chord_name, $key_name, $mod, $dom) = items($_);
     if((defined $instr_name) && (defined $chord_name) && (defined $key_name)){
-        my $strings = 0+@{$instruments{$instr_name}};
+        my $strings = 6;
         my $length = 4;
         my $input = join(" ", (uc $key_name) . (($mod == -1)? "b" :(($mod == 1)? "#" : "" )), $chord_name);
 
+        if ($mod == -1) {
+            $mod = 'b';
+        } elsif ($mod == 1) {
+            $mod = '#'
+        } else {
+            $mod = '';
+        }
+        my $results = \@{@{get_chord($key_name . $mod, $chord_name)}[0]};
+        return if not defined get_chord($key_name . $mod, $chord_name);
         return 'chord_diagrams', structured_answer => {
             id => 'chord_diagrams',
             name => 'Music',
@@ -305,7 +207,7 @@ handle remainder => sub {
                 'height'=>120,
                 'frets'=>$length,
                 'strings'=>$strings,
-                'points'=>\@{@{get_chord($key_name, $chord_name)}[0]}
+                'points'=> $results
                 )->xmlify,
                 input => $input
             },
