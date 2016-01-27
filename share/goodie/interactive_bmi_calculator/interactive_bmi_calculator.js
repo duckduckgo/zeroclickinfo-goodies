ddeab1377e4907ac87fc10821779d1d01f74e279
Maria Grazia Alastra <maria@duckduckgo.com>
2016-01-27 10:09:58 -0500

    BMI Calculator - switch between metric and imperial units

--- share/goodie/interactive_bmi_calculator/interactive_bmi_calculator.js
+++ share/goodie/interactive_bmi_calculator/interactive_bmi_calculator.js
@@ -5,6 +5,8 @@ DDH.interactive_bmi_calculator.build = function(ops) {
         onShow: function(){
             var $height = $("#bmi_height");
             var $weight = $("#bmi_weight");
+            var is_metric = DDG.settings.get('kaj') === 'm';
+            updateSwitches();
 
             $(".bmi_var").keydown(function(evt) {
                 if (evt.keyCode === 13) { //Enter
@@ -20,6 +22,22 @@ DDH.interactive_bmi_calculator.build = function(ops) {
                     $("#bmi_result").val(bmi);
                 }
             });
+
+            $(".bmi_switch").click(function(evt) {
+                is_metric = $(this).attr("id") === "bmi_metric";
+                updateSwitches();
+                
+                //Update settings in the cloud too
+                DDG.settings.set('kaj', is_metric? 'm' : 'u', { saveToCloud: true });
+            });
+
+            function updateSwitches() {
+                var selected_class = "bmi_selected_switch";
+                $("." + selected_class).removeClass(selected_class);
+
+                var $active_switch = is_metric? $("#bmi_metric") : $("#bmi_imperial");
+                $active_switch.addClass(selected_class);
+            }
         }
     };
 };
