ae7821dad2cee218957f2fc8ca782cf682a81911
Andrey Pissantchev <pissantchev@gmail.com>
2016-02-04 18:33:28 +0000

    got more complex regexes to match as well

--- lib/DDG/Goodie/IaHints.pm
+++ lib/DDG/Goodie/IaHints.pm
@@ -9,22 +9,31 @@ zci answer_type => 'ia_hints';
 
 zci is_cached => 1;
 
+# define a regex that matches for each query hint
 my %iahints = (
     'news' => 'obama news',
     'air quality' => 'air quality 33434',
     '(iphone|ipad|android) apps' => 'productivity apps',
     'gifs?' => 'cat gifs',
-    '(english)? dictionary' => 'define obsequious'
+    '(english )?dictionary' => 'define obsequious'
 );
 
+# join them into a massive regex that we use for triggers
 my $re = '^('.join('|', keys(%iahints)).')$';
 
 triggers query_lc => qr/$re/;
 
 # Handle statement
 handle matches => sub {
+    my $match = $_;
+    my $suggestion;
 
-    my $suggestion = %iahints->{$_};
+    # check which of the regexes we matched to get the query hint
+    while (my ($key, $val) = each %iahints) {
+        if ($match =~ /$key/) {
+            $suggestion = $val;
+        }
+    }
 
     return unless $suggestion;
 
