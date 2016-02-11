0b76d356cdc5f86c42be8b5a9b9376ae890f7ea9
Zaahir Moolla <moollaza@duckduckgo.com>
2016-02-11 18:27:36 -0500

    Remove Perl metadata, handled by IA Page

--- lib/DDG/Goodie/CoffeeToWaterRatio.pm
+++ lib/DDG/Goodie/CoffeeToWaterRatio.pm
@@ -1,4 +1,5 @@
 package DDG::Goodie::CoffeeToWaterRatio;
+
 # ABSTRACT: Either return a default drip coffee to water ratio (no weight entered)
 # or return a volume of liquid proportional to the coffee weight entered
 
@@ -9,19 +10,6 @@ with 'DDG::GoodieRole::NumberStyler';
 zci answer_type => "coffee_to_water_ratio";
 zci is_cached   => 1;
 
-# Metadata.  See https://duck.co/duckduckhack/metadata for help in filling out this section.
-name "CoffeeToWaterRatio";
-description "Finds the amount of water required for the weight of coffee used.";
-primary_example_queries "29g coffee to water", "coffee to water ratio 1 ounce";
-secondary_example_queries "29 grams coffee to water ratio";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#category
-category "calculations";
-# Uncomment and complete: https://duck.co/duckduckhack/metadata#topics
-topics "food_and_drink";
-code_url "https://github.com/duckduckgo/zeroclickinfo-goodies/blob/master/lib/DDG/Goodie/CoffeeToWaterRatio.pm";
-attribution github => ["nickselpa", "Nick Selpa"], twitter => "nickselpa";
-
-# Triggers
 triggers startend => "coffee to water", "coffee to water ratio";
 
 my $imperial_fluid_units = 'fl. oz.';
@@ -30,7 +18,7 @@ my $metric_fluid_units = 'ml';
 my $metric_precision =  1;
 my $imperial_precision = 0.1;
 
-my $imperial_to_water = 16;   
+my $imperial_to_water = 16;
 my $metric_to_water = 16.6945;
 
 my %wt = (
@@ -80,29 +68,29 @@ sub convertResult {
 
 
 # Handle statement
-handle remainder => sub { 
-        
+handle remainder => sub {
+
     return "1 g to 16.7 ml (0.035 oz. to 0.56 fl. oz.)", structured_answer => {
         input     => [''],
         operation => 'Coffee to water ratio per gram (0.035 ounces)',
         result    => "16.7 ml (0.56 fl. oz.)",
     } unless $_;
-    
+
     return unless my ($weight_ns) = $_ =~ qr/($weight_re)/;
     my $weight_styler = number_style_for($weight_ns);
     my $weight = $weight_styler->for_computation($weight_ns);
     my $weight_display = $weight_styler->for_display($weight);
 
     my ($unit) = $_ =~ /(ounce[s]?|gram[s]?|oz|g)/i;
-    
+
     return unless defined $unit and defined $weight;
-    
+
     my $lc_unit = lc($unit);
-   
+
     return convertResult($unit, $weight, $weight_display, $wt{$lc_unit}{'ratio'}, $wt{$lc_unit}{'precision'}, $wt{$lc_unit}{'fluid_units'}) if defined $wt{$lc_unit};
-    
+
     return;
- 
+
 };
 
 1;
