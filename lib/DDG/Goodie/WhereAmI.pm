a0204e981662a794b5cecb012679b4a06fb1d620
amoraleda <alvaro.moraleda@gmail.com>
2016-03-02 11:22:57 +0000

    Map template added

--- lib/DDG/Goodie/WhereAmI.pm
+++ lib/DDG/Goodie/WhereAmI.pm
@@ -9,17 +9,33 @@ zci is_cached => 0;
 triggers start => 'where am i', 'my location', 'current location', 'my current location';
 
 handle remainder => sub {
-    return if length($_) or !$loc or !$loc->city;
+    return if length($_) or !$loc or !$loc->city or !$loc->latitude or !$loc->longitude;
 
     my $answer = 'Lat: ' . $loc->latitude . ', Lon: ' . $loc->longitude
                  . ' (near ' . join(', ', $loc->city, $loc->region || $loc->country_name) . ')';
-
+    
     return $answer,
-      structured_answer => {
-        input     => [],
-        operation => 'Apparent current location',
-        result    => $answer
-      };
+        structured_answer => {
+            id => 'whereami',
+            name => 'Map',
+            model => 'Place',
+            view => 'Map',
+            data => {
+                id => 'whereami_id',
+                name => 'Apparent current location',
+                city => $loc->city,
+                lat => $loc->latitude,
+                lon => $loc->longitude
+            },
+            meta => {
+                zoomValue => 3,
+                sourceName => 'DDG GeoIP',
+                sourceUrl => 'http://duckduckgo.com'
+            },
+            templates => {
+                group => 'places'
+            }
+        };
 };
 
 1;
