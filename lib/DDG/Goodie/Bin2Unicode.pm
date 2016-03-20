07f2d3a08e6d7cecfd8d7e74ca77504cbab119c3
Zach Thompson <zach@duckduckgo.com>
2016-03-20 12:41:21 -0600

    Add link

--- lib/DDG/Goodie/Bin2Unicode.pm
+++ lib/DDG/Goodie/Bin2Unicode.pm
@@ -121,7 +121,8 @@ handle matches => sub {
 
     DONE:
 
-    return if $str =~ /^[\x{0}-\x{20}\x{7F}-\x{9F}]+$/; # all control/space
+    # return if all control/space (https://en.wikipedia.org/wiki/List_of_Unicode_characters#Control_codes)
+    return if $str =~ /^[\x{0}-\x{20}\x{7F}-\x{9F}]+$/;
 
     return "Binary '$bin_string' converted to " . $want_ascii ? 'ascii' : 'unicode' . " is '$str'",
         structured_answer => {
