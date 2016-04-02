bb7821769f4b09bd8b6e1ef9165002b604fad199
Ben Moon <guiltydolphin@gmail.com>
2016-04-02 10:43:44 +0100

    Remove provision of additional regexes

--- lib/DDG/GoodieRole/Dates.pm
+++ lib/DDG/GoodieRole/Dates.pm
@@ -10,29 +10,6 @@ use DDG::GoodieRole::Dates::Parser;
 
 my $parser = DDG::GoodieRole::Dates::Parser->new();
 
-# Accessors for useful regexes
-sub full_year_regex {
-    return date_format_to_regex('%Y');
-}
-sub full_month_regex {
-    return date_format_to_regex('%B');
-}
-sub short_month_regex {
-    return date_format_to_regex('%B');
-}
-sub full_day_of_week_regex {
-    return date_format_to_regex('%A');
-}
-sub short_day_of_week_regex {
-    return date_format_to_regex('%a');
-}
-sub time_24h_regex {
-    return date_format_to_regex('%T');
-}
-sub time_12h_regex {
-    return date_format_to_regex('%r');
-}
-
 sub is_valid_year {
 	my ($year) = @_;
 	return ($year =~ /^[0-9]{1,4}$/)
