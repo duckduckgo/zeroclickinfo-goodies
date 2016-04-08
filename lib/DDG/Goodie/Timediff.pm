8e001ed6520430da1a8ffa278e0be8214e88317f
Rob Emery <rob@mintsoft.net>
2016-04-08 20:25:27 +0100

    Timediff: Rough first version

---
+++ lib/DDG/Goodie/Timediff.pm
@@ -0,0 +1,42 @@
+package DDG::Goodie::Timediff;
+# ABSTRACT: provides the duration between dates
+
+use strict;
+use DDG::Goodie;
+with 'DDG::GoodieRole::Dates';
+with 'DDG::GoodieRole::NumberStyler';
+
+triggers any => qw(timediff);
+
+zci is_cached   => 0;
+zci answer_type => 'timediff';
+
+
+my $datestring_regex = datestring_regex();
+
+handle remainder => sub {
+    my $query = $_;
+
+    return unless $query =~ /^(?:between\s)?($datestring_regex)\s(?:and\s)?($datestring_regex)$/i;
+    
+    my @dates = parse_all_datestrings_to_date($1,$2);
+    my $duration = $dates[0]->epoch - $dates[1]->epoch;
+    
+    return "$duration seconds",
+        structured_answer => {
+            data => {
+                seconds => $duration,
+                minutes => $duration/60,
+                hours => $duration/3600,
+                days => $duration/8400
+            },
+            templates => {
+                group => 'text',
+                options => {
+                    content => 'DDH.timediff.content'
+                }
+            }
+        };
+};
+
+1;
