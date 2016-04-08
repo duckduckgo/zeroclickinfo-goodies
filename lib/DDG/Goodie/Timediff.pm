dcc49e7ddf8b76271cb59af9952ac61be8181207
Rob Emery <rob@mintsoft.net>
2016-04-08 20:52:06 +0100

    Timediff: Switching to record template

--- lib/DDG/Goodie/Timediff.pm
+++ lib/DDG/Goodie/Timediff.pm
@@ -11,8 +11,8 @@ triggers any => qw(timediff);
 zci is_cached   => 0;
 zci answer_type => 'timediff';
 
-
 my $datestring_regex = datestring_regex();
+my $style = number_style_for("1");
 
 handle remainder => sub {
     my $query = $_;
@@ -22,18 +22,25 @@ handle remainder => sub {
     my @dates = parse_all_datestrings_to_date($1,$2);
     my $duration = abs $dates[0]->epoch - $dates[1]->epoch;
     
+    my %answer = {
+        seconds => $style->for_display($duration),
+        minutes => $style->for_display($duration/60),
+        hours => $style->for_display($duration/3600),
+        days => $style->for_display($duration/86400)
+    };
+    my @keys = sort keys %answer;
+    
     return "$duration seconds",
         structured_answer => {
             data => {
-                seconds => $duration,
-                minutes => $duration/60,
-                hours => $duration/3600,
-                days => $duration/86400
+                record_data => \%answer,
+                record_keys => \@keys,
             },
             templates => {
-                group => 'text',
+                group => 'list',
                 options => {
-                    content => 'DDH.timediff.content'
+                    content => 'record',
+                    moreAt => 0
                 }
             }
         };
