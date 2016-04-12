728f4a069d6cc42368b0634c4156a2b5d5cbeb62
rpohankar <rasikapohankar@gmail.com>
2016-04-12 11:05:30 +0000

    Added date string in data.

--- lib/DDG/Goodie/Countdown.pm
+++ lib/DDG/Goodie/Countdown.pm
@@ -18,33 +18,25 @@ triggers any => 'countdown to','time until';
 my $datestring_regex = datestring_regex();
 my $relative_dates_regex = relative_dates_regex();
 my $time_regex = time_12h_regex();
+my $then;
 
 sub get_initial_difference {
     my $user_input = $_;    
-    #my $now = DateTime->now( time_zone => 'UTC' );
-    my $now = DateTime->now( time_zone => 'local' )->set_time_zone('floating');
-    #$now->set_time_zone(_get_timezone());
-    my $then;        
+    my $now = DateTime->now(time_zone => _get_timezone());        
+    #my $then;        
     
     #user input a combination of time and date string
     my $time = $user_input =~ s/($datestring_regex)//ir;      
     
     #datestring_regex matched somewhere in the input
     if($1) { 
-        $then = parse_datestring_to_date($1);      
-        #$then->set_time_zone('local');
-        print "\n\t aalela tz ".$then->time_zone();
+        $then = parse_datestring_to_date($1);              
     } 
-   
-#     if(!$time) {
-#         $time = $user_input =~ /($time_regex)/i;
-#     }
+       
     if($time =~ /($time_regex)/) {
-        #create date object and change hr,min,sec of $then
-        if(!$then) {
-            my $now = DateTime->now( time_zone => 'local' )->set_time_zone('floating');
-            #$then = DateTime->now( time_zone => 'UTC' );            
-            #$then->set_time_zone(_get_timezone());
+        #create date object and change hr,min,sec of $then       
+       if(!$then) {            
+            $then = DateTime->now(time_zone => _get_timezone());            
         }
         my ($hours, $minutes, $seconds, $meridiem) = split(/:|[\s]/, $1);
         if($hours == 12) {
@@ -53,21 +45,18 @@ sub get_initial_difference {
         if($meridiem eq 'pm') {
             $hours += 12;
         }
-        my $new_dur = DateTime::Duration->new(
-            days => 1,    
-        );        
-        print "then ".$then->year()."-".$then->month()."-".$then->day()."-".$then->hour()."-".$then->minute()."-".$then->second()."   entered ".$hours;
-       # $then->hour() >= $hours ? $then->add_duration($new_dur) : print "false";
+        my $new_dur = DateTime::Duration->new(days => 1);             
+        if($then->hour() > $hours || ($then->hour() == $hours && $then->minute() >= $minutes)) {
+            $then->add_duration($new_dur);
+        }            
         $then->set_hour($hours);
         $then->set_minute($minutes);
-        $then->set_second($seconds);        
-        
-    } 
-#    print "\n\tValue of compare ".DateTime->compare($then, $now);
+        $then->set_second($seconds);                
+    }     
     if(!$then || DateTime->compare($then, $now) != 1) {
         return;
     }        
-    my $dur = $then->subtract_datetime_absolute($now);        
+    my $dur = $then->subtract_datetime_absolute($now);                
     return $dur->in_units( 'nanoseconds' );
 }
 
@@ -83,7 +72,8 @@ handle remainder => sub {
         structured_answer => {
             data => {
                 remainder => $_,
-                difference => $diff
+                difference => $diff,
+                countdown_to => date_output_string($then,1)
             },
             templates => {
                 group => "text",
