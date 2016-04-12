a7244d67da98749dc74127c89f31300eb4802e1f
Ben Moon <guiltydolphin@gmail.com>
2016-04-12 18:49:43 +0100

    Add support for 'standard' queries
    Things like "random date", "random month" etc.

--- lib/DDG/Goodie/ExampleDate.pm
+++ lib/DDG/Goodie/ExampleDate.pm
@@ -6,17 +6,36 @@ use DDG::Goodie;
 use strict;
 
 use DateTime;
+use List::Util qw(first);
 
 zci answer_type => 'example_date';
 
 zci is_cached => 0;
 
+triggers start => 'random';
 triggers any   => 'date';
+
+my %standard_queries = (
+    'week ?day|day( of the week)?' => '%A',
+    'month( of the year)?'         => '%B',
+    'date'                         => '%x',
+    'time'                         => '%X',
+    'week'                         => '%W',
+);
+
+my $standard_re = join '|', map { "($_)" } (keys %standard_queries);
 
 handle query => sub {
     my $query = shift;
-    return unless $query =~ /^((random|example) )?date for (?<format>.+)$/i;
-    my $format = $+{'format'};
+    my $format;
+    if ($query =~ /^random ($standard_re)$/i) {
+        my $standard_query = $1;
+        my $k = first { $standard_query =~ qr/^$_$/i } (keys %standard_queries);
+        $format = $standard_queries{$k};
+    } else {
+        return unless $query =~ /^((random|example) )?date for (?<format>.+)$/i;
+        $format = $+{'format'};
+    }
     my $random_date = get_random_date();
     my $formatted = $random_date->strftime($format);
 
@@ -36,8 +55,10 @@ handle query => sub {
         };
 };
 
+# 9999-12-31T23:59:59Z
+my $MAX_DATE = 253_402_300_799;
 sub get_random_date {
-    my $rand_num = int(rand(1_000_000_000));
+    my $rand_num = int(rand($MAX_DATE));
     return DateTime->from_epoch(epoch => $rand_num);
 }
 
--- t/ExampleDate.t
+++ t/ExampleDate.t
@@ -28,15 +28,25 @@ sub build_structured_answer {
 sub build_test { test_zci(build_structured_answer(@_)) }
 
 my $time_24 = qr/\d{2}:\d{2}:\d{2}/;
+my $time_12 = qr/\d{1,2}:\d{2}:\d{2} [AP]M/;
 my $short_name = qr/[A-Z][a-z]{2}/;
+my $long_name = qr/[A-Z][a-z]{2,}/;
+my $week = qr/\d{2}/;
 
 ddg_goodie_test(
     [qw( DDG::Goodie::ExampleDate )],
     'random date for %Y'  => build_test('%Y', re(qr/\d{4}/)),
     'date for %a, %b %T'  => build_test('%a, %b %T', re(qr/$short_name, $short_name $time_24/)),
     'example date for %a' => build_test('%a', re(qr/$short_name/)),
+    # 'Standard' Queries
+    'random weekday' => build_test('%A', re($long_name)),
+    'random month'   => build_test('%B', re($long_name)),
+    'random time'    => build_test('%X', re($time_12)),
+    'random week'    => build_test('%W', re($week)),
     # Invalid Queries
-    'date for %K' => undef,
+    'date for %K'   => undef,
+    'random number' => undef,
+    'example date'  => undef,
 );
 
 done_testing;
