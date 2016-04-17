7e9a85ab20a01d82a4fd405496f2e28fbea3ddb9
rgaul <gaulrobe@gmail.com>
2016-04-17 05:12:54 +0000

    Improvements to AltCalendars IA

--- lib/DDG/Goodie/AltCalendars.pm
+++ lib/DDG/Goodie/AltCalendars.pm
@@ -3,33 +3,26 @@ package DDG::Goodie::AltCalendars;
 
 use strict;
 use DDG::Goodie;
+use JSON;
 
-triggers any => 'juche', 'minguo', 'meiji', 'taisho', 'taishou', 'showa', 'shouwa', 'heisei';
+my $definitions_json = share('definitions.json')->slurp();
+my $year_definitions = decode_json($definitions_json);
+
+triggers any => keys $year_definitions;
 
 zci answer_type => 'date_conversion';
 zci is_cached => 1;
-
-my %eras = (
-    'Meiji' => [1867, 'Meiji_period'], # Japanese Meiji era
-    'Taisho' => [1911, 'Taisho_period'], # Japanese Taisho era
-    'Taishou' => [1911, 'Taisho_period'], # Alternative spelling of "Taisho"
-    'Showa' => [1925, 'Showa_period'], # Japanese Showa era
-    'Shouwa' => [1925, 'Showa_period'], # Alternative spelling of "Showa"
-    'Heisei' => [1988, 'Heisei_period'], # Japanese Heisei era
-    'Juche' => [1911, 'North_Korean_calendar'], # North Korean Juche era
-    'Minguo' => [1911, 'Minguo_calendar'], # ROC (Taiwanese) Minguo era
-);
 
 handle query_parts => sub {
     # Ignore single word queries
     return unless scalar(@_) > 1;
 
-    if ($_ =~ /^(.*\b)(meiji|taisho|taishou|showa|shouwa|heisei|juche|minguo)\s+(\d*[1-9]\d*)(\b.*)$/i) {
-        my $era_name = ucfirst($2);
-        my $era_year = $3;
-        my $year = $eras{$era_name}[0] + $era_year;
-        my $result = $1.$year.$4;
-        my $wiki = 'https://en.wikipedia.org/wiki/';
+    if ($_ =~ /^([A-Za-z]+)\s+(\d*[1-9]\d*)$/i) {
+        my $era_name = $1;
+        my $era_year = $2;
+        my $gregorian_year_started = $year_definitions->{$era_name}->{'gregorian_year_started'};
+        my $wikipedia_link = $year_definitions->{$era_name}->{'wikipedia_link'};
+        my $year = $gregorian_year_started + $era_year;
 
         my $answer = "$era_name $era_year is equivalent to $year in the Gregorian Calendar";
 
@@ -37,11 +30,11 @@ handle query_parts => sub {
             structured_answer => {
                 data => {
                     title => $year,
-                    subtitle => "$era_name $era_year - Equivalent Gregorian Year"
+                    subtitle => "$era_name Year $era_year"
                 },
                 meta => {
                     sourceName => "Wikipedia",
-                    sourceUrl => "$wiki$eras{$era_name}[1]"
+                    sourceUrl => "$wikipedia_link"
                 },
                 templates => {
                     group => 'info',
