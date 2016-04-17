e0041a911e35a2f6f09d57d2fa3cd29f2e9da9be
Ben Moon <guiltydolphin@gmail.com>
2016-04-17 19:33:10 +0100

    Improve initial date display
    Instead of having a bunch of (visible) fields the user can edit, we let
    them directly modify the starting date.

--- share/goodie/date_math/date_math.js
+++ share/goodie/date_math/date_math.js
@@ -49,9 +49,9 @@ DDH.date_math = DDH.date_math || {};
                     $resultDate = $dom.find('.date--result'),
                     $opTemplate = $dom.find('.template--op').clone(),
                     $ops = $dom.find('.op--container'),
-                    $hour = $dom.find('.input--time .input--hour'),
-                    $minute = $dom.find('.input--time .input--minute'),
-                    $second = $dom.find('.input--time .input--second');
+                    $hour = $dom.find('.input--hour'),
+                    $minute = $dom.find('.input--minute'),
+                    $second = $dom.find('.input--second');
 
                 $dom.find('form').submit(function(e) {
                     e.preventDefault();
@@ -62,13 +62,6 @@ DDH.date_math = DDH.date_math || {};
 
                 function getYear() { return $year.val(); }
 
-                function isValidYear(yearText) {
-                    if (yearText.match(/^\d{1,4}$/)) {
-                        return true;
-                    }
-                    return false;
-                }
-
                 function isAmountValid(amountText) {
                     if (amountText.match(/^\d+$/)) {
                         return true;
@@ -76,15 +69,8 @@ DDH.date_math = DDH.date_math || {};
                     return false;
                 }
 
-                $year.keyup(function() {
-                    var yearText = $(this).val();
-                    if (isValidYear(yearText)) {
-                        setFieldValid($(this));
-                    } else {
-                        setFieldInvalid($(this));
-                    }
-                });
-
+                var monthFormat = 'MMMM';
+                var dayFormat   = 'Do';
                 DDG.require('moment.js', function() {
                     function getDate() {
                         var month = getMonth();
@@ -95,9 +81,11 @@ DDH.date_math = DDH.date_math || {};
                         var second = $second.val();
                         var time = hour + ':' + minute + ':' + second;
                         var date = moment(year + "-" + month + "-" + day +
-                                ' ' + time, "YYYY-MM-DD HH:mm:ss");
+                                ' ' + time, "YYYY-" + monthFormat + '-' + dayFormat + ' HH:mm:ss');
                         return date;
                     }
+
+                    var months = moment.months();
 
                     function calculateResult(date) {
                         var resultDate = moment(date);
@@ -135,7 +123,7 @@ DDH.date_math = DDH.date_math || {};
                     }
 
                     function formatDate(date) {
-                        return date.format('dddd MMMM Do YYYY HH:mm:ss');
+                        return date.format('dddd ' + monthFormat + ' ' + dayFormat + ' YYYY HH:mm:ss');
                     }
 
                     function allFieldsValid() {
@@ -145,23 +133,74 @@ DDH.date_math = DDH.date_math || {};
                         return true;
                     }
 
+                    $('input').focusout(function() {
+                        if ($(this).val() === '') {
+                            $(this).val($(this).data('prev'));
+                        } else {
+                            $(this).data('prev', $(this).val());
+                        }
+                    });
+
+                    function revertVal($elt) {
+                        $elt.val($elt.data('prev'));
+                    }
+
+                    function updateVal($elt, val) {
+                        $elt.val(val);
+                        $elt.data('prev', val);
+                    }
+
+                    $month.change(function() {
+                        var monthDate = moment($(this).val(), 'MMM');
+                        if (!monthDate.isValid()) {
+                            revertVal($(this));
+                        } else {
+                            updateVal($(this), (months[monthDate.month()]));
+                        }
+                    });
+
+                    $('input .input--date').change(function() {
+                        if ($(this).hasClass('.input--invalid')) {
+                            $(this).val($(this).data('prev'));
+                            setFieldValid($(this));
+                        }
+                    });
+                    $day.focusin(function() {
+                        $(this).val(function(idx, value) {
+                            return value.replace(/\D/g, '');
+                        });
+                    });
+                    var savedDate = {};
+
+                    $day.focusout(function() {
+                        $day.val(function(idx, value) {
+                            return DDG.getOrdinal(value);
+                        });
+                    });
+
+                    function updateStartDate(date) {
+                        $startDate.find('.date--start-weekday').text(date.format('dddd'));
+                        updateVal($month, date.format(monthFormat));
+                        updateVal($day, date.format(dayFormat));
+                        updateVal($year, date.format('YYYY'));
+                        updateVal($hour, date.format('HH'));
+                        updateVal($minute, date.format('mm'));
+                        updateVal($second, date.format('ss'));
+                    }
+
                     function performCalculation() {
                         if (allFieldsValid() !== true) {
                             return;
                         }
                         var date = getDate();
-                        if (date === undefined) {
+                        if (date === undefined || !date.isValid()) {
                             return;
                         }
                         var result = calculateResult(date);
-                        $startDate.find('.date--start-date').text(formatDate(date));
                         $resultDate.text(formatDate(result));
                     }
 
                     function initializeForm($par) {
-                        $par.find('form').submit(function(e) {
-                            e.preventDefault();
-                        });
                         $par.find('.date--form *').change(function() {
                             performCalculation();
                         });
@@ -213,15 +252,13 @@ DDH.date_math = DDH.date_math || {};
                         saData.actions.map(function(modifier) {
                             addModifier(modifier.operation, modifier.amount, modifier.type);
                         });
-                        moment.months().map(function(month, idx) {
-                            $('<option value="' + padZero(idx+1, 2) + '">' + month + '</option>')
-                                .appendTo($month);
-                        });
-                        var i;
-                        for (i=1; i<=31; i++) {
-                            $('<option value="' + padZero(i, 2) + '">' + i + '</option>')
-                                .appendTo($day);
-                        }
+                        updateStartDate(moment.unix(saData.start_date));
+                        $month.attr('maxlength', Math.max.apply(null, months.map(function(month) {
+                            return month.length;
+                        })));
+                        $dom.find('.input--date').addClass('tx-clr--slate-light');
+                        $dom.find('.input--numeric, .input--numeric .input--date').data('reject', '\\D');
+                        $dom.find('.input--time *').attr('pattern', '\\d{1,2}');
                     }
                     $dom.find('.op--add').click(function() {
                         var $lastOp = $ops.find('.date--form').last();
@@ -261,6 +298,15 @@ DDH.date_math = DDH.date_math || {};
                         });
                     });
 
+                    $dom.find('.date--start-date').click(function() {
+                        var $popup = $dom.find('.date--popup');
+                        if ($popup.hasClass('is-showing')) {
+                            $popup.removeClass('is-showing');
+                        } else {
+                            $popup.addClass('is-showing');
+                        }
+                    });
+
                     $dom.find('.input--time input').change(function() {
                         $(this).val(function(idx, value) {
                             return padZero(value, 2);
@@ -270,25 +316,20 @@ DDH.date_math = DDH.date_math || {};
                     function getNumDaysMonthYear() {
                         var month = getMonth();
                         var year = getYear();
-                        return moment(year + '-' + month, 'YYYY-MM').daysInMonth();
+                        return moment(year + '-' + month, 'YYYY-' + monthFormat).daysInMonth();
                     }
-
-                    $day.change(function() {
-                        setFieldValid($day.parent());
-                    });
 
                     // Ensure day is within range
                     $('.input--year,.input--month').change(function() {
                         var numDays = getNumDaysMonthYear();
-                        $day.find('option[value="28"] ~ option').show();
-                        $day.find('option[value="' + numDays + '"] ~ option').hide();
-                        if ($day.find('option:selected').is(':hidden')) {
-                            setFieldInvalid($day.parent());
+                        $day.attr('max', numDays);
+                        if (Number($day.val()) > numDays) {
+                            setFieldInvalid($day);
                         } else {
-                            setFieldValid($day.parent());
+                            setFieldValid($day);
                         }
                     });
-                    $('.date--form *').change(function() {
+                    $('.input--date, .date--form *').change(function() {
                         performCalculation();
                     });
                     initializeForms();
