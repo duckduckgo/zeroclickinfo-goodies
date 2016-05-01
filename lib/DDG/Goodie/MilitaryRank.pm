6af33b47901ddee605998774bc436ee149ab75f9
lowellosaurus <LowellWood@gmail.com>
2016-05-01 10:37:13 +0430

    Updated image urls

--- lib/DDG/Goodie/MilitaryRank.pm
+++ lib/DDG/Goodie/MilitaryRank.pm
@@ -29,84 +29,84 @@ my $DATA = {
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-02.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/9/91/Army-USA-OR-02.svg',
                     title => 'Private',
                     subtitle => 'PV2',
                     altSubtitle => 'E-2 | OR-2',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-03.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Army-USA-OR-03.svg',
                     title => 'Private First Class',
                     subtitle => 'PFC',
                     altSubtitle => 'E-3 | OR-3',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-04b.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Army-USA-OR-04b.svg',
                     title => 'Specialist',
                     subtitle => 'SPC',
                     altSubtitle => 'E-4 | OR-4',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-04a.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/a/a2/Army-USA-OR-04a.svg',
                     title => 'Corporal',
                     subtitle => 'CPL',
                     altSubtitle => 'E-4 | OR-4',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-05.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/2/27/Army-USA-OR-05.svg',
                     title => 'Sergeant',
                     subtitle => 'SGT',
                     altSubtitle => 'E-5 | OR-5',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-06.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Army-USA-OR-06.svg',
                     title => 'Staff Sergeant',
                     subtitle => 'SSG',
                     altSubtitle => 'E-6 | OR-6',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-07.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/7/71/Army-USA-OR-07.svg',
                     title => 'Sergeant First Class',
                     subtitle => 'SFC',
                     altSubtitle => 'E-7 | OR-7',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-08b.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/8/8a/Army-USA-OR-08b.svg',
                     title => 'Master Sergeant',
                     subtitle => 'MSG',
                     altSubtitle => 'E-8 | OR-8',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-08a.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Army-USA-OR-08a.svg',
                     title => 'First Sergeant',
                     subtitle => '1SG',
                     altSubtitle => 'E-8 | OR-8',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-09c.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/5/5d/Army-USA-OR-09c.svg',
                     title => 'Sergeant Major',
                     subtitle => 'SGM',
                     altSubtitle => 'E-9 | OR-9',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-09b.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/3/31/Army-USA-OR-09b.svg',
                     title => 'Command Sergeant Major',
                     subtitle => 'CSM',
                     altSubtitle => 'E-9 | OR-9',
 
                 },
                 {
-                    image => 'https://en.wikipedia.org/wiki/File:Army-USA-OR-09a.svg',
+                    image => 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Army-USA-OR-09a.svg',
                     title => 'Sergeant Major of the Army',
                     subtitle => 'SMA',
                     altSubtitle => 'E-9 | OR-9',
