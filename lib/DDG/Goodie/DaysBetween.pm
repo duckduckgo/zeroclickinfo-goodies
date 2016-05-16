1080c459e1fc7ef88084c516f27f68393202243a
GreatDanton <pribosek.jan@gmail.com>
2016-05-16 23:39:44 +0200

    on new branch

--- lib/DDG/Goodie/DaysBetween.pm
+++ lib/DDG/Goodie/DaysBetween.pm
@@ -1,41 +1,32 @@
 package DDG::Goodie::DaysBetween;
-# ABSTRACT: Give the number of days between two given dates.
+# ABSTRACT: Return number of years/days/hours/minutes/seconds between two chosen dates.
 
 use strict;
 use DDG::Goodie;
 with 'DDG::GoodieRole::Dates';
 
-triggers start => "days between", "days", "daysbetween", "days_between", "number of days between", "how many days between", "number of days from", "days from";
+triggers start => "dates calculator", "date calculator", "days between", "daysbetween", "days_between", "number of days between", "how many days between", "number of days from", "days from", "minutes between", "hours between", "years between";
+
+triggers end => "in minutes", "in seconds", "in hours", "in years", "in days", "in minutes";
 
 zci is_cached => 1;
 zci answer_type => "days_between";
 
-my $datestring_regex = datestring_regex();
-
-handle remainder => sub {
-    return unless $_ =~ qr/^($datestring_regex) (?:(?:and|to) )?($datestring_regex)(?:[,]? inclusive)?$/i;
-
-    my ($date1, $date2) = parse_all_datestrings_to_date($1, $2);
-    return unless ($date1 && $date2);
-
-    ($date1, $date2) = ($date2, $date1) if ( DateTime->compare($date1, $date2) == 1 );
-
-    my $difference = $date1->delta_days($date2);
-    my $daysBetween = abs($difference->in_units('days'));
-    my $inclusive = '';
-    if(/inclusive/) {
-        $daysBetween += 1;
-        $inclusive = ', inclusive';
-    }
-    my $startDate = date_output_string($date1);
-    my $endDate   = date_output_string($date2);
-
-    return "There are $daysBetween days between $startDate and $endDate$inclusive.",
-      structured_answer => {
-        input     => [$startDate, $endDate,],
-        operation => 'Days between' . $inclusive,
-        result    => $daysBetween
-      };
+handle query_lc => sub {
+    return "plain text response",
+           structured_answer => {
+               data => {
+                 title => "Date Math",
+                   },
+               templates => {
+                   group => "list",
+                   options => {
+                     subtitle_content => 'DDH.days_between.content',
+                     content => "record",
+                     rowHighlight => 'true',
+                   }
+               }
+           };
 };
 
 1;
