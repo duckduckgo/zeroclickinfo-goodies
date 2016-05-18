a913c6af02fc41d075d297f1ec51ffd2962c8a86
Rob Emery <rob@mintsoft.net>
2016-05-18 12:40:39 +0100

    PaleoIngredientCheck: encoding the input string for XSS protection (we think this is still neded)

--- lib/DDG/Goodie/PaleoIngredientCheck.pm
+++ lib/DDG/Goodie/PaleoIngredientCheck.pm
@@ -41,7 +41,7 @@ handle remainder => sub {
     return $result, structured_answer => {
         data => {
             title => $result,
-            subtitle => "Paleo Friendly: $item"
+            subtitle => "Paleo Friendly: ".html_enc($item)
         },
         templates => {
             group => 'text'
