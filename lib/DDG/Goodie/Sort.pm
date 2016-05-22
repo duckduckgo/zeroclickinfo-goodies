ba8fe158bf0af8b4e4680cdf5ae0a2a317123b77
Rob <rob@mintsoft.net>
2016-05-22 20:37:21 +0100

    Sort: Convert to text template

--- lib/DDG/Goodie/Sort.pm
+++ lib/DDG/Goodie/Sort.pm
@@ -43,12 +43,15 @@ handle remainder => sub {
     my $sorted_list = join($delim, sort { $ascending ? $a <=> $b : $b <=> $a } @numbers);
     my $dir = $ascending ? 'ascendingly' : 'descendingly';
 
-    return "$sorted_list (Sorted $dir)",
-      structured_answer => {
-        input     => [$unsorted_list],
-        operation => 'Sort ' . $dir,
-        result    => $sorted_list
-      };
+    return "$sorted_list (Sorted $dir)", structured_answer => {
+        data => {
+            title => "$sorted_list",
+            subtitle => "Sort $dir: $unsorted_list"
+        },
+        templates => {
+            group => 'text'
+        }
+    };
 };
 
 1;
