32d1b25a6035e2c5fa535916366f49e204ff908b
jonk1993 <jonkalfayan@gmail.com>
2016-05-27 02:03:36 +0000

    made regex capture names more clear

--- lib/DDG/Goodie/Base.pm
+++ lib/DDG/Goodie/Base.pm
@@ -32,18 +32,18 @@ my $prefix_keys = join '|', keys %prefix_map;
 triggers any => 'base', keys %base_map;
 
 handle query_clean => sub {
-    return unless /^(?<la>$prefix_keys)?(?<inp>[0-9A-Za-z]+)\s*((?:(?:in|as)\s+)?(?:(?<lt>$map_keys)|(?:base\s*(?<ln>[0-9]+)))\s+)?(?:(?:in|as|to)\s+)?(?:(?<rt>$map_keys)|(?:base\s*(?<rn>[0-9]+)))$/;
+    return unless /^(?<prefixKey>$prefix_keys)?(?<inp>[0-9A-Za-z]+)\s*((?:(?:in|as)\s+)?(?:(?<inpBaseKey>$map_keys)|(?:base\s*(?<inpBaseNum>[0-9]+)))\s+)?(?:(?:in|as|to)\s+)?(?:(?<outBaseKey>$map_keys)|(?:base\s*(?<outBaseNum>[0-9]+)))$/;
     my $input = uc $+{'inp'}; # uc is necessary as Int2Base doesnt support lowercase
     my $from_base = 10;
-    if (defined $+{'la'}) {
-        return if (defined $+{'ln'} || defined $+{'lt'}); #to avoid input like '0xFF in binary' 
-        $from_base = $prefix_map{$+{'la'}};
-    } elsif (defined $+{'ln'}) {
-        $from_base = $+{'ln'};
-    } elsif (defined $+{'lt'}) {
-        $from_base = $base_map{$+{'lt'}};
+    if (defined $+{'prefixKey'}) {
+        return if (defined $+{'inpBaseNum'} || defined $+{'inpBaseKey'}); #to avoid input like '0xFF in binary' 
+        $from_base = $prefix_map{$+{'prefixKey'}};
+    } elsif (defined $+{'inpBaseNum'}) {
+        $from_base = $+{'inpBaseNum'};
+    } elsif (defined $+{'inpBaseKey'}) {
+        $from_base = $base_map{$+{'inpBaseKey'}};
     }
-    my $to_base = $+{'rn'} // $base_map{$+{'rt'}};
+    my $to_base = $+{'outBaseNum'} // $base_map{$+{'outBaseKey'}};
     return if $to_base < 2 || $to_base > 36 || $from_base < 2 || $from_base > 36;
     my $output;
     eval { $output = convert_base($input, $from_base, $to_base) }; return if $@;
