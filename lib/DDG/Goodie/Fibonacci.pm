71b855660c9dfa4791e7bde8ba4417193d3a0ade
mhunsinger <mike.hunsinger@gmail.com>
2016-06-10 07:00:31 +0000

    Added IA for "is n a Fibonacci number?".

--- lib/DDG/Goodie/Fibonacci.pm
+++ lib/DDG/Goodie/Fibonacci.pm
@@ -12,27 +12,53 @@ triggers any => 'fib', 'fibonacci';
 zci answer_type => 'fibonacci';
 zci is_cached   => 1;
 
-handle remainder_lc => sub {
-    s/^\s+//;
-    s/\s+$//;
-    my $limit = 25000; # larger numbers degrade performance
-    
-    return unless /^(?:what(?:'s| is) the )?(?<which>\d+)(?:th|rd|st|nd)?(?: number)?(?: in the (?:series|sequence))?\??$/ && $1 <= $limit;
-    my $n = $+{'which'};
-    my $fib_seq = Math::NumSeq::Fibonacci->new;
-    my $val = $fib_seq->ith($n);
-    
-    my $suf = ordsuf($n);
-    my $text_answer ="The $n$suf fibonacci number is $val (assuming f(0) = 0).";
-    return $text_answer, structured_answer => {
+sub answer {
+    my ($text_ans, $title, $subtitle) = @_;
+
+    return $text_ans, structured_answer => {
         data => {
-            title => $val,
-            subtitle => "$n$suf Fibonacci number"  
+            title => $title,
+            subtitle => $subtitle
         },
         templates => {
             group => 'text'
         }
     };
+}
+
+handle remainder_lc => sub {
+    my $ith_limit = 25000;      # limit for nth fibonacci numbers 
+    my $pred_limit = 10**22;    # limit for if n is a fibonacci number
+    my $fib_seq = Math::NumSeq::Fibonacci->new;
+
+    # trim whitespace around remainder
+    s/^\s+//;
+    s/\s+$//;
+    
+    # check "what is the nth fibonacci number"
+    if (/^(?:what(?:'s| is) the )?(?<which>\d+)(?:st|nd|rd|th)?(?: number)?(?: in the (?:series|sequence))?\??$/ && $1 <= $ith_limit) 
+    {
+        my $n = $+{'which'};
+        my $val = $fib_seq->ith($n);
+        my $suf = ordsuf($n);
+        my $text_answer ="The $n$suf fibonacci number is $val (assuming f(0) = 0).";
+
+	return answer($text_answer, $val, "$n$suf Fibonacci number");
+    }
+    # check "is n a fibonacci number"
+    elsif (/^is (?<which>\d+) (?:a|in the)? ?(?:number|sequence)?\??$/ && $1 <= $pred_limit)
+    {
+        my $n = $+{'which'};
+        my $val = $fib_seq->pred($n);
+        my $is_fib = $val ? "is" : "is not";
+        my $text_answer ="$n $is_fib a Fibonacci number.";
+
+	return answer($text_answer, $val ? "true":"false", "$n $is_fib a Fibonacci number");
+    } 
+    else
+    {
+        return;                 # didn't match anything
+    }
 };
 
 1;
--- t/Fibonacci.t
+++ t/Fibonacci.t
@@ -23,6 +23,20 @@ sub build_test
     });
 }
 
+sub build_pred_test
+{
+    my ($answer, $subtitle) = @_;
+    return test_zci("$subtitle a Fibonacci number.", structured_answer => {
+	data => {
+	    title => $answer,
+	    subtitle => "$subtitle a Fibonacci number"
+	},
+        templates => {
+            group => 'text'
+        }
+    });
+}
+
 ddg_goodie_test(
     [qw(DDG::Goodie::Fibonacci)],
     'fib 7' => build_test(13, '7th'),
@@ -31,6 +45,11 @@ ddg_goodie_test(
     '10th number in the fibonacci sequence?' => build_test(55, '10th'),
     'what is the 18th number in the fibonacci series' => build_test(2584, '18th'),
     'What is the 2nd fibonacci number' => build_test(1, '2nd'),
+    'what is the 250001 fib' => undef,
+    'is 14 a fibonacci number' => build_pred_test("false", '14 is not'),
+    'is 13 in the fibonacci sequence?' => build_pred_test("true", '13 is'),
+    'is 6 a fib?' => build_pred_test("false", "6 is not"),
+    'is 1000000000000000000000000000000000000000000000 a fib' => undef,
     'tell a fib'                  => undef,
     'what are fibonacci numbers?' => undef,
 );
