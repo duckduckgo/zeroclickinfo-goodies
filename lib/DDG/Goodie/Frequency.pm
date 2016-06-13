b601f0a070beb4def7e5635b5c553b5a4670b5bf
sdua <sahildua2305@gmail.com>
2016-06-13 20:23:04 +0000

    Update to use List template

--- lib/DDG/Goodie/Frequency.pm
+++ lib/DDG/Goodie/Frequency.pm
@@ -7,36 +7,60 @@ use DDG::Goodie;
 triggers start => 'frequency', 'freq';
 
 handle remainder => sub {
-    if ($_ =~ /^of ([a-z]|(?:all ?|)(?:letters|characters|chars|)) in (.+)/i)
-    {
+    if ($_ =~ /^of ([a-z]|(?:all ?|)(?:letters|characters|chars|)) in (.+)/i) {
 
-	my $collect = lc $1;
-	my $target_str = lc $2;
+        my $collect = lc $1;
+        my $target_str = lc $2;
 
-#	warn qq($collect\t$target_str\n);
+#     	warn qq($collect\t$target_str\n);
 
-	my $count = 0;
-	my %freq;
-	my @chars = split //, $target_str;
+        my $count = 0;
+        my %freq;
+        my @chars = split //, $target_str;
 
-	foreach (@chars)
-	{
-	    if ($_ =~ /[a-z]/)
-	    {
-		if ($collect =~ /all|letters|characters|chars/) { ++$freq{$_}; }
-		else { ++$freq{$_} if $_ eq $collect; }
+        foreach (@chars) {
+            if ($_ =~ /[a-z]/) {
+                if ($collect =~ /all|letters|characters|chars/) {
+                    ++$freq{$_};
+                }
+                else {
+                    ++$freq{$_} if $_ eq $collect;
+                }
 
-		++$count;
-	    };
-	};
+                ++$count;
+            };
+        };
 
-	my @out;
-	foreach my $key (keys %freq)
-	{
-	    push @out, join ":", $key, $freq{$key} . "/" . $count;
-	};
+        my $list_data = [];
+        foreach my $key (sort keys %freq) {
+            my $temp_hash = {
+                alphabet => "$key",
+                frequency => "$freq{$key}"
+            };
+            push( @$list_data, $temp_hash );
+        }
 
-	return "Frequency: " . join ' ',sort(@out) if @out;
+        my @out;
+        foreach my $key (keys %freq) {
+        push @out, join " ", $key, $freq{$key} . "/" . $count;
+        };
+
+        my $plaintext = join ' ', @out if @out;
+    	
+        return $plaintext,
+        structured_answer => {
+            data => {
+                title => "Frequency",
+                list => $list_data
+            },
+            meta => {},
+            templates => {
+                group => 'list',
+                options => {
+                    list_content => "DDH.frequency.list_content"
+                },
+            },
+        };;
     };
 
     return;
