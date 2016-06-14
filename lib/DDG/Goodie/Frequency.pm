09e9a93b0796f4fb96f8ee9dd11ff6bb56a4c133
sdua <sahildua2305@gmail.com>
2016-06-14 00:01:45 +0000

    Update module to use record template

--- lib/DDG/Goodie/Frequency.pm
+++ lib/DDG/Goodie/Frequency.pm
@@ -18,6 +18,8 @@ handle remainder => sub {
         my %freq;
         my @chars = split //, $target_str;
 
+        my ($title, $subtitle, @record_data, @record_keys);
+
         foreach (@chars) {
             if ($_ =~ /[a-z]/) {
                 if ($collect =~ /all|letters|characters|chars/) {
@@ -26,41 +28,38 @@ handle remainder => sub {
                 else {
                     ++$freq{$_} if $_ eq $collect;
                 }
-
                 ++$count;
             };
         };
 
-        my $list_data = [];
-        foreach my $key (sort keys %freq) {
-            my $temp_hash = {
-                alphabet => "$key",
-                frequency => "$freq{$key}"
-            };
-            push( @$list_data, $temp_hash );
-        }
 
+        my @temp_keys;
         my @out;
-        foreach my $key (keys %freq) {
+        foreach my $key (sort keys %freq) {
+            push( @temp_keys, $key );
             push @out, join ":", $key, $freq{$key} . "/" . $count;
         };
 
-        my $plaintext = join ' ', @out if @out;
-    	
+        @record_data = \%freq;
+        @record_keys = \@temp_keys;
+
+        my $plaintext = join ' ', @out;
+
         return $plaintext,
         structured_answer => {
             data => {
                 title => "Frequency",
-                list => $list_data
+                record_data => @record_data,
+                record_keys => @record_keys
             },
             meta => {},
             templates => {
                 group => 'list',
                 options => {
-                    list_content => "DDH.frequency.list_content"
+                    content => "record"
                 },
             },
-        };
+        } if @out;
     };
 
     return;
