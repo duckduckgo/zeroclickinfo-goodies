ddc566cf5922dad13f483e5ac82bf3f4f6691bf6
Ben Moon <guiltydolphin@gmail.com>
2016-06-28 15:40:03 +0100

    Remove redundant 'require's
    We only need one call that requires both libs.

--- share/goodie/date_math/date_math.js
+++ share/goodie/date_math/date_math.js
@@ -40,7 +40,7 @@ DDH.date_math = DDH.date_math || {};
         var saData = ops.data;
         var isInitialized = false;
 
-        DDG.require('moment.js', function() {
+        DDG.require(['moment.js', 'pikaday'], function() {
             DDH_build_async({
                 templates: {
                     group: 'base',
@@ -92,22 +92,17 @@ DDH.date_math = DDH.date_math || {};
                         }
                     });
 
-                    DDG.require('moment.js', function() {
-                        var startDate = moment.unix(saData.start_date);
-                        $hour.val(startDate.format('HH'));
-                        $minute.val(startDate.format('mm'));
-                        $second.val(startDate.format('ss'));
-                        DDG.require('pikaday', function() {
-                            var picker = new Pikaday({
-                                field: $('.date--start')[0],
-                                format: dateFormat,
-                                firstDay: 1,
-                                yearRange: [1, 9999],
-                            });
-                            picker.setMoment(moment.unix(saData.start_date));
-                        });
+                    var startDate = moment.unix(saData.start_date);
+                    $hour.val(startDate.format('HH'));
+                    $minute.val(startDate.format('mm'));
+                    $second.val(startDate.format('ss'));
+                    var picker = new Pikaday({
+                        field: $('.date--start')[0],
+                        format: dateFormat,
+                        firstDay: 1,
+                        yearRange: [1, 9999],
                     });
-
+                    picker.setMoment(moment.unix(saData.start_date));
                 },
                 onItemShown: function(item) {
                     var $dom = $(".zci--date_math"),
@@ -119,83 +114,80 @@ DDH.date_math = DDH.date_math || {};
                         $minute = $dom.find('.input--minute'),
                         $second = $dom.find('.input--second');
 
-                    DDG.require('moment.js', function() {
-                        function getDate() {
-                            var hour = $hour.val();
-                            var minute = $minute.val();
-                            var second = $second.val();
-                            var date = moment(
-                                $startDate.val(), dateFormat
-                            );
-                            date.set({
-                                'hour': hour,
-                                'minute': minute,
-                                'second': second
-                            });
-                            return date;
-                        }
-
-                        function setAmountInvalid() {
-                            $amount[0].setCustomValidity('Date out of range');
-                        }
-
-                        function setAmountValid() {
-                            $amount[0].setCustomValidity('');
-                        }
-
-                        function getSign() {
-                            return $sign.hasClass('ddgsi-plus') ? '+' : '-';
-                        }
-
-                        function calculateResult(date) {
-                            var resultDate = moment(date);
-                            var amount = Number($amount.val());
-                            amount = getSign() === '+' ?
-                                amount : -amount;
-                            var type = $type.val();
-                            resultDate.add(amount, type);
-                            if (!isNaN(resultDate)) {
-                                setAmountValid();
-                            } else {
-                                setAmountInvalid();
-                                return;
-                            }
-                            return resultDate;
-                        }
-
-                        function formatDate(date) {
-                            return date.format('dddd ' + dayFormat + ' ' + monthFormat + ' YYYY HH:mm:ss');
-                        }
-
-                        function dateFieldsValid() {
-                            if ($dom.find('.input--date').find(':invalid').length > 0) {
-                                return false;
-                            }
-                            return true;
-                        }
-
-                        function performCalculation() {
-                            if (dateFieldsValid() !== true) {
-                                return;
-                            }
-                            var date = getDate();
-                            if (date === undefined || !date.isValid()) {
-                                return;
-                            }
-                            var result = calculateResult(date);
-                            if (result === undefined || !result.isValid()) {
-                                return;
-                            }
-                            item.set({result: formatDate(result)});
-                        }
-
-                        $sign.click(function() {
-                            performCalculation();
+                    function getDate() {
+                        var hour = $hour.val();
+                        var minute = $minute.val();
+                        var second = $second.val();
+                        var date = moment(
+                            $startDate.val(), dateFormat
+                        );
+                        date.set({
+                            'hour': hour,
+                            'minute': minute,
+                            'second': second
                         });
-                        $('.input--date *, .input--op *').change(function() {
-                            performCalculation();
-                        });
+                        return date;
+                    }
 
+                    function setAmountInvalid() {
+                        $amount[0].setCustomValidity('Date out of range');
+                    }
+
+                    function setAmountValid() {
+                        $amount[0].setCustomValidity('');
+                    }
+
+                    function getSign() {
+                        return $sign.hasClass('ddgsi-plus') ? '+' : '-';
+                    }
+
+                    function calculateResult(date) {
+                        var resultDate = moment(date);
+                        var amount = Number($amount.val());
+                        amount = getSign() === '+' ?
+                            amount : -amount;
+                        var type = $type.val();
+                        resultDate.add(amount, type);
+                        if (!isNaN(resultDate)) {
+                            setAmountValid();
+                        } else {
+                            setAmountInvalid();
+                            return;
+                        }
+                        return resultDate;
+                    }
+
+                    function formatDate(date) {
+                        return date.format('dddd ' + dayFormat + ' ' + monthFormat + ' YYYY HH:mm:ss');
+                    }
+
+                    function dateFieldsValid() {
+                        if ($dom.find('.input--date').find(':invalid').length > 0) {
+                            return false;
+                        }
+                        return true;
+                    }
+
+                    function performCalculation() {
+                        if (dateFieldsValid() !== true) {
+                            return;
+                        }
+                        var date = getDate();
+                        if (date === undefined || !date.isValid()) {
+                            return;
+                        }
+                        var result = calculateResult(date);
+                        if (result === undefined || !result.isValid()) {
+                            return;
+                        }
+                        item.set({result: formatDate(result)});
+                    }
+
+                    $sign.click(function() {
+                        performCalculation();
+                    });
+                    $('.input--date *, .input--op *').change(function() {
+                        performCalculation();
                     });
                 }
             });
