f1d6f582f54d39bd2f9c282668d728efbc769ce9
sdua <sahildua2305@gmail.com>
2016-07-02 18:31:32 +0000

    AjaxFrameworks: Fix fetching language from query remainder

--- lib/DDG/Goodie/AjaxFrameworks.pm
+++ lib/DDG/Goodie/AjaxFrameworks.pm
@@ -2,9 +2,10 @@ package DDG::Goodie::AjaxFrameworks;
 # ABSTRACT: Return a list of Ajax frameworks for a given language
 
 use DDG::Goodie;
-use strict;
-
 use YAML::XS 'LoadFile';
+use Text::Trim;
+
+use strict;
 
 zci answer_type => 'ajax_frameworks';
 zci is_cached => 1;
@@ -16,12 +17,19 @@ my $DATA = LoadFile(share('frameworks.yml'));
 # Handle statement
 handle remainder => sub {
 
-    my $remainder = $_;
-    print $remainder;
+    # Remove keywords 'framework' and 'frameworks' from the remainder
+    #
+    # Useful for queries like 'ajax javascript frameworks'
+    # Remainder in such cases is 'javascript frameworks'
+    $_ =~ s/(frameworks|framework)//g;
+    trim($_);  # Trim
+    print $_;
 
-    return unless $remainder;
+    # Return if the remainder is empty after above operations
+    return unless $_;
 
-    return if $remainder !~ /^(javascript|js|perl|php|python|java|ruby)$/i;
+    # Return if the remainder doesn't match supported languages
+    return if $_ !~ /^(javascript|js|perl|php|python|java|ruby)$/i;
 
     return "plain text response",
         structured_answer => {
