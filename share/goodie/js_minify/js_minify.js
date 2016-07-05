a76abb9badc7eeb61af4ea09fcfea411940bceef
sdua <sahildua2305@gmail.com>
2016-07-05 22:41:51 +0000

    JsMinify: Add basic template and js for minifier

--- share/goodie/js_minify/js_minify.js
+++ share/goodie/js_minify/js_minify.js
@@ -1,14 +1,27 @@
 DDH.js_minify = DDH.js_minify || {};
 
-(function(DDH) {
-    "use strict";
+"use strict";
 
-    DDH.js_minify.build = function(ops) {
+DDH.js_minify.build = function(ops) {
 
-        return {
-            onShow: function() {
+    var shown = false;
 
-            }
-        };
-    }
-});
+    return {
+        onShow: function() {
+            console.log("onShow called");
+            // make sure this function is run only once, the first time
+            // the IA is shown otherwise things will get initialized more than once
+            if (shown)
+                return;
+
+            // set the flag to true so it doesn't get run again
+            shown = true;
+
+            var $dom = $('#zci-js_minify'),
+                $main = $dom.find('.zci__main');
+
+            $main.toggleClass('c-base');
+
+        }
+    };
+};
