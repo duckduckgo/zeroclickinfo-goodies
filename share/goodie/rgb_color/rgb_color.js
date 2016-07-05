98b3d4cdd4cdc5dd196a8d6ba6bab3d8b62fd563
Ben Moon <guiltydolphin@gmail.com>
2016-07-05 13:33:14 +0100

    Add links to the ColorPicker in results
    Clicking on a result (or input) color will link to the color picker for
    the appropriate color.

--- share/goodie/rgb_color/rgb_color.js
+++ share/goodie/rgb_color/rgb_color.js
@@ -1,8 +1,16 @@
 Handlebars.registerHelper('color', function(color) {
     var nameText = '';
+    var hex = color.hex;
+    var colorPickerLink = 'https://duckduckgo.com/?q=color+picker+' +
+        encodeURIComponent(hex);
     if (color.name !== '') {
         nameText = ' (' + color.name + ')';
     }
-    return color.hex + nameText + ' <span class="rgb_color--color-box" style="color:' +
-        color.hex + '">&#9632</span>';
+    var href = 'href="' + colorPickerLink + '"';
+    var hexBody = '<a class="rgb_color--link" ' + href + '>' + hex +
+        nameText + '</a>';
+    var boxBody = '<a class="rgb_color--link rgb_color--color-box" ' +
+        href + ' style="color:' + hex + '">&#9632</a>';
+    var body = '<span>' + hexBody + ' ' + boxBody + '</span>';
+    return body;
 });
