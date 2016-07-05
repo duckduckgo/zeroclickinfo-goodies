718820888fc4564b3843cfb9e3e808283d485436
Ben Moon <guiltydolphin@gmail.com>
2016-07-05 09:29:42 +0100

    Add support for basic random color queries

--- lib/DDG/Goodie/RgbColor.pm
+++ lib/DDG/Goodie/RgbColor.pm
@@ -4,23 +4,50 @@ use DDG::Goodie;
 use strict;
 use warnings;
 
+use Color::RGB::Util qw(
+    rand_rgb_color
+);
+
 zci answer_type => 'rgb_color';
 
-zci is_cached => 1;
+zci is_cached => 0;
 
-triggers any => 'triggerword', 'trigger phrase';
+triggers any => 'color';
+
+my $color_re = 'color';
+
+my %query_forms = (
+    "random $color_re" => \&random_color,
+);
+
+sub random_color {
+    srand;
+    my %data = (
+        subtitle => 'Random color',
+    );
+    my %result;
+    my $color = rand_rgb_color();
+    $data{title} = "$color";
+    $result{text_answer}  = "$color";
+    $result{data} = \%data;
+    return %result;
+}
 
 handle query_lc => sub {
+    my $query = $_;
 
-    my $query_lc = $_;
+    my %result;
+    while (my ($form, $action) = each %query_forms) {
+        if ($query =~ qr/$form/) {
+            %result = $action->();
+            last;
+        }
+    }
 
-    return "plain text response",
+    return $result{text_answer},
         structured_answer => {
 
-            data => {
-                title    => "My Instant Answer Title",
-                subtitle => "My Subtitle",
-            },
+            data => $result{data},
 
             templates => {
                 group => "text",
--- t/RgbColor.t
+++ t/RgbColor.t
@@ -7,18 +7,17 @@ use Test::Deep;
 use DDG::Test::Goodie;
 
 zci answer_type => "rgb_color";
-zci is_cached   => 1;
+zci is_cached   => 0;
 
 sub build_structured_answer {
     my @test_params = @_;
 
-    return "plain text response",
+    my %answer = build_answer_random();
+
+    return $answer{text_answer},
         structured_answer => {
 
-            data => {
-                title    => "My Instant Answer Title",
-                subtitle => "My Subtitle",
-            },
+            data => $answer{data},
 
             templates => {
                 group => "text",
@@ -26,12 +25,24 @@ sub build_structured_answer {
         };
 }
 
+my $color_re = qr/\p{XDigit}{6}/i;
+
+sub build_answer_random {
+    return (
+        text_answer => re($color_re),
+        data => {
+            title    => re($color_re),
+            subtitle => 'Random color',
+        },
+    );
+}
+
 sub build_test { test_zci(build_structured_answer(@_)) }
 
 ddg_goodie_test(
     [qw( DDG::Goodie::RgbColor )],
-    'example query' => build_test('query'),
-    'bad example query' => undef,
+    # Random colors
+    'random color' => build_test(),
 );
 
 done_testing;
