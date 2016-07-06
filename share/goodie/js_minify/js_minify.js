33834205df878f095ecabe77159b576d0f2b302d
sdua <sahildua2305@gmail.com>
2016-07-06 11:33:55 +0000

    JsMinify: Complete minify operation with basic UI

--- share/goodie/js_minify/js_minify.js
+++ share/goodie/js_minify/js_minify.js
@@ -18,13 +18,27 @@ DDH.js_minify.build = function(ops) {
 
             var $dom = $('#zci-js_minify'),
                 $main = $dom.find('.zci__main'),
-                $minifyButton = $dom.find('.js_minify__action');
+                $minifyButton = $dom.find('.js_minify__action'),
+                $input = $dom.find('#js_minify__input'),
+                $output = $dom.find('#js_minify__output');
 
             $main.toggleClass('c-base');
 
-            $.getScript('https://raw.githubusercontent.com/prettydiff/prettydiff/master/prettydiff.js', function(lib) {
+            $.getScript('http://sahildua.com/js/prettydiff.min.js', function() {
+                // Add click handler for the minify button
                 $minifyButton.click(function() {
-                    console.log(lib);
+                    // Set config options for minify operation
+                    var args = {
+                        mode: "minify",
+                        lang: "javascript",
+                        source: $input.val()
+                    };
+
+                    // Operate using the prettydiff function provided by the library
+                    var output = prettydiff(args);
+
+                    // Add the output to output textarea field
+                    $output.val(output);
                 });
             })
         }
