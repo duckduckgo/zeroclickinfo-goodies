147150ad8fe27bf598bdd306e8a69553dda14629
sdua <sahildua2305@gmail.com>
2016-07-15 21:53:21 +0000

    JsMinify: Load library on input change

--- share/goodie/js_minify/js_minify.js
+++ share/goodie/js_minify/js_minify.js
@@ -3,16 +3,21 @@ DDH.js_minify = DDH.js_minify || {};
 DDH.js_minify.build = function(ops) {
     "use strict";
 
+    // Flag to make denote if IA has been shown or not
     var shown = false;
+
+    // Flag to denote if library 'prettydiff' has been loaded or not
+    var libLoaded = false;
 
     return {
         onShow: function() {
-            // make sure this function is run only once, the first time
-            // the IA is shown otherwise things will get initialized more than once
+            // Make sure this function is run only once, the first time
+            // the IA is shown otherwise things will get initialized
+            // more than once
             if (shown)
                 return;
 
-            // set the flag to true so it doesn't get run again
+            // Set the flag to true so it doesn't get run again
             shown = true;
 
             var $dom = $('#zci-js_minify'),
@@ -23,28 +28,42 @@ DDH.js_minify.build = function(ops) {
 
             $main.toggleClass('c-base');
 
-            // hide output textarea by default
+            // Hide output textarea by default
             $output.css('display', 'none');
 
-            DDG.require('prettydiff', function() {
-                // Add click handler for the minify button
-                $minifyButton.click(function() {
-                    // Set config options for minify operation
-                    var args = {
-                        mode: "minify",
-                        lang: "javascript",
-                        source: $input.val()
-                    };
+            // Disable 'Minify' button by default and change cursor to default
+            $minifyButton.prop('disabled', 'true');
+            $minifyButton.css('cursor', 'default');
 
-                    // Operate using the prettydiff function provided by the library
-                    var output = prettydiff(args);
+            // Add event handler for change in input of textarea
+            $input.on('input', function() {
+                if (!libLoaded) {
+                    // Set the flag to make sure the library isn't loaded
+                    // again and again
+                    libLoaded = true;
 
-                    // hide output textarea by default
-                    $output.css('display', 'inline');
+                    // load the library
+                    DDG.require('prettydiff', function() {});
+                }
+            });
 
-                    // Add the output to output textarea field
-                    $output.val(output);
-                });
+            // Add click handler for the minify button
+            $minifyButton.click(function() {
+                // Set config options for minify operation
+                var args = {
+                    mode: "minify",
+                    lang: "javascript",
+                    source: $input.val()
+                };
+
+                // Operate using the prettydiff function provided by the library
+                var output = prettydiff(args);
+
+                // hide output textarea by default
+                $output.css('display', 'inline');
+
+                // Add the output to output textarea field
+                $output.val(output);
             });
         }
     };
