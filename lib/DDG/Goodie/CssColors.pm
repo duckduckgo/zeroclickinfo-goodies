b0fcfd1b3afa6c70361b25fb0b943e7977e58353
sdua <sahildua2305@gmail.com>
2016-07-16 21:36:26 +0000

    CssColors: Init

---
+++ lib/DDG/Goodie/CssColors.pm
@@ -0,0 +1,36 @@
+package DDG::Goodie::CssColors;
+# ABSTRACT: List of all the named CSS colors
+
+use DDG::Goodie;
+
+use strict;
+use warnings;
+
+zci answer_type => 'css_colors';
+
+zci is_cached => 1;
+
+triggers any => 'css colors', 'css3 colors', 'css named colors', 'css3 named colors', 'named css colors', 'named css3 colors', 'css colours', 'css3 colours', 'css named colours', 'css3 named colours', 'named css colours', 'named css3 colours';
+
+handle query_lc => sub {
+
+    my $query_lc = $_;
+
+    return 'CSS Colors',
+        structured_answer => {
+
+            data => {
+                title    => 'CSS Colors',
+                list => ''
+            },
+
+            templates => {
+                group => 'list',
+                options => {
+                    list_content => 'DDH.css_colors.list_content'
+                }
+            }
+        };
+};
+
+1;
---
+++ t/CssColors.t
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use Test::Deep;
+use DDG::Test::Goodie;
+
+zci answer_type => 'css_colors';
+zci is_cached   => 1;
+
+sub build_structured_answer {
+
+    return 'CSS Colors',
+        structured_answer => {
+
+            data => {
+                title    => 'CSS Colors',
+                list => '',
+            },
+
+            templates => {
+                group => 'list',
+                options => {
+                    list_content => 'DDH.css_colors.list_content'
+                }
+            }
+        };
+}
+
+# Use this to build expected results for your tests.
+sub build_test { test_zci(build_structured_answer(@_)) }
+
+ddg_goodie_test(
+    [qw( DDG::Goodie::CssColors )],
+
+    'css colors' => build_test(),
+
+    'color picker' => undef,
+);
+
+done_testing;
