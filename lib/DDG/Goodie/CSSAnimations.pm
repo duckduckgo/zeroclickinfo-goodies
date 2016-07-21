fff7b525743f04dbd5318884e39dd03dfc8f2c5b
msingh2 <manrajsinghgrover@gmail.com>
2016-07-21 07:32:24 +0000

    Codepen logo added

--- lib/DDG/Goodie/CSSAnimations.pm
+++ lib/DDG/Goodie/CSSAnimations.pm
@@ -2,6 +2,8 @@ package DDG::Goodie::CSSAnimations;
 # ABSTRACT: Shows examples of CSS Animations from various references
 
 use DDG::Goodie;
+use YAML::XS 'LoadFile';
+use Data::Dumper;
 use strict;
 use warnings;
 
@@ -11,23 +13,18 @@ zci is_cached => 1;
 
 triggers start => 'css animations', 'css animations example', 'css animation', 'css animation examples', 
                   'css animation demo', 'css animations demos', 'css animations demos', 'css animation demos';
+                  
+my $animations = LoadFile(share('css-animations.yml'));
 
 handle remainder => sub {
-    
+    print Dumper($animations);
     return 'CSS Animations',
         structured_answer => {
             id => 'CSSAnimations',
             name => 'CSS Animations',
-            data => {
-                title    => 'CSS Animations',
-                subtitle => 'My Subtitle'
-            },
+            data => $animations,
             templates => {
-                group => 'text',
-                variants => {
-                    tileTitle => "3line-small",
-                    tileFooter => "2line"
-                },
+                group => 'icon',
                 detail => false,
                 item_detail => false
             }
