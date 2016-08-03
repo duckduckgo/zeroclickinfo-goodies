16e952946d29dccf883001fcc873f17ef30f86cb
bsinger <brian@convolve.com>
2016-08-03 15:45:56 +0000

    Linked hover effect for dot and SVG

--- share/goodie/geometry/geometry.js
+++ share/goodie/geometry/geometry.js
@@ -1,22 +1,34 @@
-DDH.geometry = DDH.geometry || {};
+DDH.geometry_goodie = DDH.geometry_goodie || {};
+
 (function(DDH) {
     "use strict";
 
-    DDH.geometry.build = function(ops) {
-
+    DDH.geometry_goodie.build = function(ops) {
         return {
-
-            // Function that executes after template content is displayed
-            onShow: function() {
+            onShow: function() { 
                 var formulaSelector = '#zci--geometry-formulas .formula .dot';
                 var svgSelector = '#zci--geometry-svg *';
-                $(formulaSelector).hover(
-                  function() {
-                    console.log('test');
-                    $( this ).addClass( "hover" );
-                  }, function() {
-                    $( this ).removeClass( "hover" );
-                  }
+                    
+                $(svgSelector + ',' + formulaSelector).hover(
+                    function() {
+                        // Get Current geoemetry type
+                        var datatype = $( this ).data('type');
+                        // Add Hover to Dot
+                        $('#zci--geometry .'+ datatype).addClass( "hover" );
+                        // Add Hover to SVG
+                        // JQuery 1.1 cant select SVGs by normal selectors
+                        var svg = document.querySelector("#zci--geometry-svg ." + datatype);
+                        svg.classList.add("hover");
+                        
+                        
+                    }, function() {
+                        // Remove hovers when not hovering
+                        var datatype = $( this ).data('type');
+                        $('#zci--geometry .' + datatype).removeClass( "hover" );
+                        
+                        var svg = document.querySelector("#zci--geometry-svg ." + datatype);
+                        svg.classList.remove("hover");
+                    }
                 );
             }
         };
