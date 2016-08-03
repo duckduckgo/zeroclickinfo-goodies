ded8c1a14b6fd4849b9cfa65db3fa6804fcc1786
bsinger <brian@convolve.com>
2016-08-03 15:55:57 +0000

    updated css

--- share/goodie/geometry/geometry.js
+++ share/goodie/geometry/geometry.js
@@ -6,27 +6,27 @@ DDH.geometry_goodie = DDH.geometry_goodie || {};
     DDH.geometry_goodie.build = function(ops) {
         return {
             onShow: function() { 
-                var formulaSelector = '#zci--geometry-formulas .formula .dot';
-                var svgSelector = '#zci--geometry-svg *';
+                var formulaSelector = '.geometry-formulas .formula .dot';
+                var svgSelector = '.geometry-svg *';
                     
                 $(svgSelector + ',' + formulaSelector).hover(
                     function() {
                         // Get Current geoemetry type
                         var datatype = $( this ).data('type');
                         // Add Hover to Dot
-                        $('#zci--geometry .'+ datatype).addClass( "hover" );
+                        $('#geometry--goodie .'+ datatype).addClass( "hover" );
                         // Add Hover to SVG
                         // JQuery 1.1 cant select SVGs by normal selectors
-                        var svg = document.querySelector("#zci--geometry-svg ." + datatype);
+                        var svg = document.querySelector(".geometry-svg ." + datatype);
                         svg.classList.add("hover");
                         
                         
                     }, function() {
                         // Remove hovers when not hovering
                         var datatype = $( this ).data('type');
-                        $('#zci--geometry .' + datatype).removeClass( "hover" );
+                        $('#geometry--goodie .' + datatype).removeClass( "hover" );
                         
-                        var svg = document.querySelector("#zci--geometry-svg ." + datatype);
+                        var svg = document.querySelector(".geometry-svg ." + datatype);
                         svg.classList.remove("hover");
                     }
                 );
