1624bae6085773f552cb33a9df1c43d79f9b1fe8
jnavi <jshnavi@gmai.com>
2016-08-19 02:37:21 +0000

    Working prime checker

---
+++ share/goodie/prime/prime.js
@@ -0,0 +1,66 @@
+DDH.prime = DDH.prime || {};
+
+(function(DDH) {
+    "use strict";
+
+    console.log('DDH.prime.build'); // remove this before submitting pull request
+
+    // define private variables and functions here
+    //
+    // function helper () { ... }
+    //
+    // var a = '',
+    //     b = '',
+    //     c = '';
+
+    DDH.prime.build = function(ops) {
+
+        return {
+
+            meta: {
+                sourceName: 'Source Domain',
+                sourceUrl: 'https://source.website.com'
+            },
+
+            // data: {
+            //     already defined in Perl Package
+            //     you can re-define it here
+            //     or access/modify 'ops.data'
+            // },
+
+            // normalize: function(item){
+            //     use this to map your 'data'
+            //     to the properties required for your chosen template
+            //
+            //     return {
+            //         title: item.myTitle,
+            //         subtitle: item.foo.subtitle
+            //     };
+            // },
+
+            // templates: {
+            //     group: 'text',
+            //
+            //     options: {
+            //
+            //     },
+            //
+            //     variants: {
+            //
+            //     }
+            // },
+
+            // Function that executes after template content is displayed
+            onShow: function() {
+
+                // define any callbacks or event handlers here
+                //
+                // var $dom = $(".zci--prime");
+                // $dom.find(".my-special-class").click(function(){
+                //
+                // });
+
+            }
+        };
+    };
+})(DDH);
