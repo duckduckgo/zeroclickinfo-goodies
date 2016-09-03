42d1b117918a69d5612b4ec9de47b34083cf12f5
hchienjo <hhathersage@gmail.com>
2016-09-03 13:19:15 +0000

    Make '10 to roman numerals' and 'D to arabic' trigger.

--- lib/DDG/Goodie/Roman.pm
+++ lib/DDG/Goodie/Roman.pm
@@ -14,7 +14,7 @@ zci answer_type => "roman_numeral_conversion";
 
 handle remainder => sub {
     my $in = uc shift;
-    $in =~ s/(?:\s*|in|numerals?|number|\s*)//gi;
+    $in =~ s/(?:\s*|in|to|numerals?|number|\s*)//gi;
 
     return unless $in;
 
