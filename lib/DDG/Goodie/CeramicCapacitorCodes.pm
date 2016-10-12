23a954c8826f50a2a9ca01cf9bf2c4af62a5b1e6
Rob <rob@mintsoft.net>
2016-10-12 23:49:14 +0100

    CeramicCapacitorCodes: Answer is now correct; but formatting is poor

--- lib/DDG/Goodie/CeramicCapacitorCodes.pm
+++ lib/DDG/Goodie/CeramicCapacitorCodes.pm
@@ -4,6 +4,8 @@ package DDG::Goodie::CeramicCapacitorCodes;
 use strict;
 use DDG::Goodie;
 use Text::Trim;
+use POSIX;
+use utf8;
 
 triggers startend =>
     'ceramic capacitor',
@@ -14,17 +16,31 @@ triggers startend =>
 zci answer_type => "ceramic_capacitor_codes";
 zci is_cached   => 1;
 
+my %prefixes = (
+    '0' => 'pF',
+    '3' => 'nF',
+    '6' => 'μF',
+    '9' => 'mF'
+);
+
 handle remainder_lc => sub {
     
-    return unless my ($digits, $multiplier, $tolerance) = /^([0-9]{2})([0-9])([cjkmdz]?)$/;
+    return unless my ($digits, $multiplier, $tolerance) = /^([1-9][0-9])([0-9])([cjkmdz]?)$/;
 
     my $pico_farads = $digits * 10**$multiplier;
-    my $answer = "$pico_farads pF";
+    my $order_of_magnitude = 1+$multiplier;
+
+    my $engineering_order_of_magnitude = $order_of_magnitude - ($order_of_magnitude % 3);
+    my $capacitance_in_engineering_units = $pico_farads / 10**$engineering_order_of_magnitude;
+
+    my $unit = $prefixes{$engineering_order_of_magnitude};
+
+    my $answer = "$capacitance_in_engineering_units $unit";
     return $answer,
         structured_answer => {
             data => {
-                title    => "$pico_farads",
-                subtitle => "$pico_farads"
+                title    => $answer,
+                subtitle => $answer
             },
             templates => {
                 group => 'text'
