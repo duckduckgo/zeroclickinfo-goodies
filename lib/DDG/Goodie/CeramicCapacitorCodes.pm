df496382e3b68a30d76fc9ab466fd7770ecfcc13
Rob <rob@mintsoft.net>
2016-10-12 22:47:08 +0100

    CeramicCapacitorCodes: Partial implementation for PR to force metadata into "in development"

---
+++ lib/DDG/Goodie/CeramicCapacitorCodes.pm
@@ -0,0 +1,35 @@
+package DDG::Goodie::Chars;
+# ABSTRACT: Give the number of characters (length) of the query.
+
+use strict;
+use DDG::Goodie;
+use Text::Trim;
+
+triggers startend =>
+    'ceramic capacitor',
+    'ceramic capacitors',
+    'decode ceramic capacitor',
+    'decode ceramic capacitors';
+
+zci answer_type => "ceramic_capacitor_codes";
+zci is_cached   => 1;
+
+handle remainder_lc => sub {
+    
+    return unless my ($digits, $multiplier, $tolerance) = /^([0-9]{2})([0-9])([cjkmdz]?)$/;
+
+    $pico_farads = $digits * 10**$multiplier;
+
+    return $answer,
+        structured_answer => {
+            data => {
+                title    => $len,
+                subtitle => "Character count: $str"
+            },
+            templates => {
+                group => 'text'
+            }
+    };
+};
+
+1;
