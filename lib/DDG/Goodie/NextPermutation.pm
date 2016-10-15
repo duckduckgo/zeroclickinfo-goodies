f13f7158e00ec893fe3e7db3db9d855a0d72b526
Hasil-Sharma <hasilsharma7@gmail.com>
2016-10-15 12:48:49 +0000

    Added Abstract

--- lib/DDG/Goodie/NextPermutation.pm
+++ lib/DDG/Goodie/NextPermutation.pm
@@ -1,5 +1,5 @@
 package DDG::Goodie::NextPermutation;
-
+#ABSTRACT : For a given sequence of numbers or alphabets (delimited by comma or space), generating the next permutation which will be lexicographically smallest
 use DDG::Goodie;
 use strict;
 use warnings;
