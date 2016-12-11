addd79326e66cf4273e567660d5c4d5d964bd744
Marcel Robitaille <marcelrobitaille11@gmail.com>
2016-12-11 14:50:11 +0000

    Got it to work with case insensitivity (is that a word?). Use underscores to separate elements as needed.

--- lib/DDG/Goodie/MolarMass.pm
+++ lib/DDG/Goodie/MolarMass.pm
@@ -2,24 +2,21 @@ package DDG::Goodie::MolarMass;
 
 # ABSTRACT: Write an abstract here
 
-# Start at http://docs.duckduckhack.com/walkthroughs/calculation.html if you are new
-# to instant answer development
-
 use strict;
 use DDG::Goodie;
 use List::Util qw(first);
 use YAML::XS 'LoadFile';
 
 zci answer_type => 'molar_mass';
-
-# Caching - http://docs.duckduckhack.com/backend-reference/api-reference.html#caching`
 zci is_cached => 1;
 
 my @elements = @{ LoadFile(share('elements.yml')) };
+my @symbols = map { lc @$_[3] } @elements;
+use Data::Dumper;
+print "\n";
 
 # Triggers - http://docs.duckduckhack.com/walkthroughs/calculation.html#triggers
 triggers startend => 'molar mass of', 'molar mass';
-
 
 # Handle statement
 handle remainder => sub {
@@ -37,108 +34,83 @@ handle remainder => sub {
     #
     # return unless qr/^\w+|\d{5}$/;
     # 
-    # 
-    # 
 
-my $compoundString = get_mass_from_compound($compound, "1");
-my $molarMass = parse_compound_string($compoundString);
-
+    my $mass = parseParens($compound, 1);
+#     my $compoundString = get_mass_from_compound($compound, 1);
+#     my $molarMass = parse_compound_string($compoundString);
 
 
-       return "Molar Mass of $compound is $molarMass.",
+    return "Molar Mass of $compound is $mass.",
         structured_answer => {
             input     => [$compound],
             operation => 'Molar Mass of Compound',
-            result    => $molarMass
+            result    => $mass
         };
 };
 
-
-
-sub get_mass_from_compound
-{
-    my($compound, $asdf) = @_;
-    my @matches = ($compound =~ m/[A-Z][a-z]?\d*|\((?:[^()]*(?:\(.*\))?[^()]*)+\)\d+/g);
-    my $molarString = "";
-    my $trailingNum = $asdf;
-
-    foreach my $x (@matches)
-    {
-        my ($one, $two) = get_trailing_num($x);
-
-        if ($x !~ /\(|\)/g)
-        {
-            $molarString .= $two * $trailingNum;
-            $molarString .= $one .= ',';
-        }
-        else
-        {
-            #if there are parantheses we need to unpack them.
-            if (substr($x, 0, 1) eq '(')
-            {
-                #deletes parentheses and grabs the substring within those parantheses
-                $x = substr($x, 1, rindex($x, ')'));
-            }
-
-            $molarString .= get_mass_from_compound($x, $two);
-        }
-    }
-
-    return $molarString;
+sub getMassOfElement{
+    my($symbol) = @_;
+    my $element = first { lc $symbol eq lc $_->[3] } @elements;
+    return $element->[1];
 }
 
-
-sub get_trailing_num
-{
-    my $compound = shift;
-    #this regex checks the string for the format of 'asdf1234' and divides the
-    #characters from the digits.
-    if ($compound =~ /(\D+)(\d+)$/g)
-    {
-        #$1 holds the compound name, #2 holds the number trailing.
-        return ($1, $2);
+sub getElement{
+    my($element, $multiplier) = @_;
+    
+    if($element ~~ @symbols){
+        return getMassOfElement($element) * $multiplier;
     }
-    else
-    {
-        #since there is no trailing number, there is an understood 1, this
-        #returns as such.
-        return ($compound, 1);
+    
+    my $length = length($element);
+    
+    if($length gt 3 && substr($element, 0, 3) ~~ @symbols){
+        return getMassOfElement(substr($element, 0, 3)) + getElement(substr($element, 3), $multiplier);
     }
+    
+    if($length gt 2 && substr($element, 0, 2) ~~ @symbols){
+        return getMassOfElement(substr($element, 0, 2)) + getElement(substr($element, 2), $multiplier);
+    }
+    
+    if($length gt 1 && substr($element, 0, 1) ~~ @symbols){
+        return getMassOfElement(substr($element, 0, 1)) + getElement(substr($element, 1), $multiplier);
+    }
+    
+    if($length eq 1){
+        print "Could not find $element\n";
+    }
+    
 }
 
-
-sub parse_compound_string
-{
-    my $compoundString = shift;
-    my $weight;
-    my $compoundWeight = 0;
-    my @values = split(',', $compoundString);
-
-    foreach my $x (@values)
-    {
-        if ($x =~ /(\d+)(\D+)$/g)
-        {
-            #$1 holds the coefficient, #2 holds the compound.
-            #return ($1, $2);
-            #print "$1\n$2\n";
-            
-            my $match = first { lc $_->[3] eq lc $2 } @elements or return;
-
-            my ( $atomic_number, $atomic_mass, $element_name, $element_symbol, $element_type ) = @{$match};
-            
-#                 $weight = $chemical_elements{$2}{weight};
-                $compoundWeight += $atomic_mass * $1;
-                # print "$weight\n";
-            
-        }
-
-        # print "$x\n";
+sub calculateMass{
+    my($compound) = @_;
+    my $sum = 0;
+    my @matches;
+    push @matches, [$1, $2] while $compound =~ /([a-z]+)(\d*)/gi;
+    
+    foreach my $match (@matches){
+        my $element = @$match[0];
+        my $multiplier = @$match[1] ? @$match[1] : 1;
+        $sum += getElement($element, $multiplier);
     }
-
-    return $compoundWeight;
+    
+    return $sum;
 }
 
-
-
+sub parseParens{
+    my($compound, $multiplier) = @_;
+    my $sum = 0;
+    my @matches;
+    push @matches, [$1, $2] while $compound =~ /\(([a-z0-9]*\(.*\)[a-z0-9]*|[a-z0-9]+?)\)(\d+)/gi;
+    
+    $compound =~ s/\(([a-z0-9]*\(.*\)[a-z0-9]*|[a-z0-9]+?)\)(\d+)/_/g;
+    
+    foreach my $match (@matches){
+        $sum += parseParens(@$match[0], @$match[1]);
+    }
+    
+    $sum += calculateMass($_) for split('_', $compound);
+    
+    return $sum * $multiplier;
+}
 
 1;
