190fd4444c5efd4b91d49eeb594174799af33622
Manraj Singh <manrajsinghgrover@gmail.com>
2016-12-16 01:39:55 +0530

    CSS Animations Goodie (#3424)
    * Base created
    
    * YML File Updated
    
    * Codepen logo added
    
    * Logo renamed | urls added
    
    * Initial version added
    
    * Removes unwanted CSS
    
    * Remove codepen logo
    
    * Removes cssanimations.js
    
    * Restructuring things
    
    * Fixes bareword test file issue
    
    * Test File fixed
    
    * CSS Tricks Demos added
    
    * Changes to tile and data
    
    * Title added and styled
    
    * Adds 2 demos to Goodie
    
    * Fixes margin between title and demo
    
    * Modularizing demos
    
    * CSS Animations: Makes code more modular
    
    * Data YML updated
    
    * Fixes no file exist issue
    
    * Minor changes in test file
    
    * Fix: Reduces width of tile, fixes animation overflows
    
    * Feature: CodePen AutoPen Generation added
    
    * Fix: Test File fixed
    
    * Add: Proportional Animations
    
    * Add: Changing transform-origin mid-animation
    
    * Add: Negative transform-origins
    
    * Add: Box shadow magic
    
    * Fix: Fix conflict in CSS
    
    * Update: Removes unnecessary keys
    
    * Fix: Warnings and errors for https
    
    * Fix: Fixes text in demo 1
    
    * Formats: Formats all CSS Stylesheets
    
    * Fix: Handles unnecessary triggering
    
    * Fix: Handles if variables are undefined
    
    * Adds: Adds more test cases
    
    * Fix: Moves logic to a separate sub routine
    
    * Fix: Removes external resource usage

---
+++ lib/DDG/Goodie/CssAnimations.pm
@@ -0,0 +1,65 @@
+package DDG::Goodie::CssAnimations;
+# ABSTRACT: Shows examples of CSS Animations from various references
+
+use DDG::Goodie;
+use YAML::XS 'LoadFile';
+use strict;
+use warnings;
+use JSON;
+
+zci answer_type => 'css_animations';
+
+zci is_cached => 1;
+
+triggers start => 'css animations', 'css animations example', 'css animation', 'css animation examples', 
+                  'css animation demo', 'css animations demos', 'css animations demo', 'css animation demos';
+
+my $animations = LoadFile(share('data.yml'));
+
+sub build_response() {
+
+    my $demo_count = keys $animations;
+    
+    my @result = ();
+    
+    for (my $i=0; $i < $demo_count; $i++) {
+        my $demo = "demo_$i";
+        my $title = $animations->{$demo}->{'title'};
+        my $html = share("$demo/demo.html")->slurp if -e share("$demo/demo.html");
+        my $css = share("$demo/style.css")->slurp if -e share("$demo/style.css");
+        my %value = ('title' => $title, 'html' => $html, 'css' => $css);
+        
+        $animations->{$demo}->{'html'} = $html || '';
+        $animations->{$demo}->{'css'} = $css || '';
+        $animations->{$demo}->{'value'} = encode_json \%value || '';
+        push(@result, $animations->{$demo});
+    }
+    
+    return @result;
+}
+
+my @result = build_response();
+
+handle remainder => sub {
+    return unless $_ eq '';
+    
+    return 'CSS Animations',
+        structured_answer => {
+            id => 'css_animations',
+            name => 'CSS Animations',
+            data => \@result,
+            templates => {
+                group => 'base',
+                detail => 0,
+                item_detail => 0,
+                options => {
+                    content => 'DDH.css_animations.content'
+                },
+                variants => {
+                    tile => 'xwide'
+                }
+            }
+        };
+};
+
+1;
