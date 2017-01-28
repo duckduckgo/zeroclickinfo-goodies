3a0c00102c565e06ae24ee12a7f154d4710b2a15
Zach Thompson <zach@duckduckgo.com>
2017-01-28 10:06:21 -0700

    No @, or $ not followed by a number or .

--- lib/DDG/Goodie/Calculator.pm
+++ lib/DDG/Goodie/Calculator.pm
@@ -88,7 +88,9 @@ handle query_nowhitespace => sub {
     return if ($query =~ /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/); # Probably are searching for a phone number, not making a calculation
     return if $query =~ /[":]/;
     return if $query =~ m{[x X × ∙ ⋅ * % + \- ÷ / \^ \$ \. ,]{3,}};
-    return if $query =~ /\((?:[\$\@\%])?\)/;
+    return if $query =~ /\@/;
+    return if $query =~ /\$[^\d\.]/;
+    return if $query =~ /\(\)/;
 
     $query =~ s/^(?:whatis|calculate|solve|math)//;
     $query =~ s/factorial/fact/;     #replace factorial with fact
