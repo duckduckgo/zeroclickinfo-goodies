02cb7b2f5b752ac473ee09414f86340c44d24991
Zaahir Moolla <moollaza@duckduckgo.com>
2017-03-23 14:34:28 -0400

    Pass Goodie Bundle version from Perl to JS for asset loading

--- share/goodie/timer/timer.js
+++ share/goodie/timer/timer.js
@@ -10,7 +10,7 @@ DDH.timer.build = function(ops) {
     'use strict';
 
     var SOUND_NAME = "alarm-sound",
-        soundUrl = '/share/goodie/timer/alarm.mp3',
+        soundUrl = '/share/goodie/timer/' + ops.data[0].goodie_version + '/alarm.mp3',
         soundIsPlaying = false,
         hasShown = false,
         $lastTimerToFinish,
