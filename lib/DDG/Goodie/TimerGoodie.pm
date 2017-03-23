de98ce988bda4c62b38912909459ca7c2f45eb54
Ben Moon <software@guiltydolphin.com>
2017-03-23 16:51:39 +0000

    Port Timer to Goodies (#2253)
    * Add Timer files
    
    * Rename Spice -> Goodie
    
    * Move structured answer into Perl
    
    * Update display
    
    Now working.
    
    I removed the 'isMobile' flag, as 'DDG.device.isMobile' was causing it
    to crash, and the mobile display wasn't pretty anyway ("Add Timer" was
    wrapping). Instead it will use '+' for both.
    
    * Move time parsing into Perl
    
    This way we can check that parsed values are correct in the tests
    easily; it also gives us something sensible to return (the start time in
    seconds).
    
    * Fix issue with queries like '5s'
    
    * Fix alarm not being located correctly
    
    It was pointing at 'share/spice'.
    
    * Re-enable mobile text
    
    Fixed the issue with mobile display.
    
    * Integrate changes from Spice #3134
    
    Adds the changes from duckduckgo/zeroclickinfo-spice#3134.
    
    * Integrate changes from Spice #2656
    
    Adds the changes from duckduckgo/zeroclickinfo-spice#2656
    
    * Update ID to 'timer_goodie'
    
    * Ensure time from query is used as default

---
+++ lib/DDG/Goodie/TimerGoodie.pm
@@ -0,0 +1,147 @@
+package DDG::Goodie::TimerGoodie;
+# ABSTRACT: Shows a countdown timer
+
+use strict;
+use DDG::Goodie;
+
+zci answer_type => 'timer_goodie';
+zci is_cached   => 1;
+
+my @triggers = qw(timer countdown alarm);
+# Triggers that are valid in start only
+my @startTringgers = qw(start begin set run);
+# Beautifies the trigger can be appended in front/back of trigger
+my @beautifierTringgers = qw(online);
+#Joins the Timer Value
+my @joiners = qw(for on at with);
+# StartEndTriggers to trigger on startTringgers, beautifierTringgers and triggers
+my @triggersStartEnd = (@triggers, @startTringgers, @beautifierTringgers);
+
+triggers startend => @triggersStartEnd;
+
+my $MAX_TIME = 359999; # 99 hrs 59 mins 59 secs
+
+sub normalize_time_format {
+    my $time = shift;
+    $time =~ /(?:(?<h>\d{1,2}):)?(?<min>\d{1,2}):(?<sec>\d{2})/;
+    my $min_sec = $+{min} . 'min ' . $+{sec} . 'sec';
+    return (defined $+{h} ? $+{h} . 'h ' : '') . $min_sec;
+}
+
+sub parse_query_for_time {
+    my $query = shift;
+    $query =~ s/^\s*//;
+    $query =~ s/(timer|online)\s*//gi;
+    $query =~ s/(?!\a)s/sec/i;
+    $query =~ s/(?!\a)m/min/i;
+    my $timer_re = qr/(?<val>[\d]+\.?[\d]*) ?(?<unit>min|sec|h)/;
+    my $time = 0;
+    my ($match, $val, $unit);
+    $query = normalize_time_format $query if $query =~ /:/;
+    while ($query =~ $timer_re) {
+        $val = $+{val};
+        $unit = $+{unit};
+        if ($unit eq 'h') {
+            $time += $val * 3600;
+        } elsif ($unit eq 'min') {
+            $time += $val * 60;
+        } elsif ($unit eq 'sec') {
+            $time += $val;
+        }
+        $query =~ s/$timer_re//;
+    }
+    return ($time <= $MAX_TIME) ? $time : $MAX_TIME;
+}
+
+sub build_result {
+    my $req = shift;
+    my $time = parse_query_for_time($req->query_lc);
+    return "$time",
+        structured_answer => {
+            id     =>  'timer_goodie',
+            name   => 'Timer',
+            signal => 'high',
+            meta => {
+                sourceName => 'Timer',
+                itemType   => 'timer',
+            },
+            data => {
+                time => "$time",
+            },
+            templates => {
+                group       => 'base',
+                detail      => 'DDH.timer_goodie.timer_wrapper',
+                wrap_detail => 'base_detail',
+            },
+        };
+}
+
+handle remainder => sub {
+    my $qry = $_;
+    my $raw = lc($req->query_raw);
+    my $trgx = join('|', @triggers);
+    my $stTrgx = join('|', @startTringgers);
+    my $stTrgxSize = @startTringgers;
+    my $btfrTrgx = join('|', @beautifierTringgers);
+    my $btfrTrgxSize = @beautifierTringgers;
+    my $joinTrgx = join('|', @joiners);
+    my $btfrTrigStart = $btfrTrgx.'|'.$stTrgx;
+    my $btfrTrigSize = $stTrgxSize+$btfrTrgxSize;
+
+    # Verify that the trigger words are wrapped with whitespace or that
+    # the trigger is at the start or end of the string with white space
+    # on either side of it. This prevents triggering on queries such as
+    # "countdown.js", "timer.x", "five-alarm", etc
+    if($raw !~ /(^|\s)($trgx)(\s|$)/i) {
+        return;
+    }
+
+    # When the query is empty and we know that the trigger word matches
+    # the trigger exactly (whitespace check) we can return a valid result
+    if($qry eq '') {
+        return build_result($req);
+    }
+
+    # Trim both sides of the raw query to have the raw regex work
+    # properly. Since we need to make sure /^<trigger> for $/ doesn't
+    # trigger. Additionally trims down the start triggers
+    # <startTringgers> <specific time> <beautifierTringgers> <trigger> ------------- start 10 minute online timer
+    # <startTringgers> <trigger> <beautifierTringgers> <specific time> ------------- begin timer online 10 minutes
+    # <startTringgers> <trigger> <beautifierTringgers> <joiners> <specific time> --- set timer online for 10 min
+    $raw =~ s/^\s*(\b(\s*($btfrTrigStart)\s*)\b){1,$btfrTrigSize}\s*//;
+    $raw =~ s/\s*($btfrTrgx)\s*$//;
+
+    # Parse the raw query to remove common terms. This allows us to
+    # catch the more specific queries and handle them. We also strip
+    # the extra bounding whitespace. The trigger is
+    # set to startend, causing "online timer for ..." to trigger.
+    #
+    #
+    # Note: Beautifiers surrounds the triggers and joiners connect it to the value
+    #
+    # Matches on...
+    # <specific time> <beautifierTringgers> <trigger> ------------------------------ 10 minute online timer
+    # <trigger> <beautifierTringgers> <specific time> ------------------------------ timer online 10 minutes
+    # <trigger> <beautifierTringgers> <joiners> <specific time> -------------------- timer online for 10 min
+    # <specific time> <trigger> <beautifierTringgers> ------------------------------ 10 minute timer online
+    # <startTringgers> <beautifierTringgers> <trigger> <specific time> ------------- online timer 10 minutes
+    # <beautifierTringgers> <trigger> <joiners> <specific time> -------------------- online timer for 10 min
+    # <specific time> <beautifierTringgers> <trigger> ------------------------------ 10 minute online countdown timer
+    # <trigger> <beautifierTringgers> <specific time> ------------------------------ timer alarm online 10 minutes
+    # <trigger> <beautifierTringgers> <joiners> <specific time> -------------------- alarm timer online at 10 min
+    # <specific time> <trigger> <beautifierTringgers> ------------------------------ 10 minute countdown timer online
+    # <startTringgers> <beautifierTringgers> <trigger> <specific time> ------------- online countdown alarm 10 minutes
+    # <beautifierTringgers> <trigger> <joiners> <specific time> -------------------- online timer with 10 min
+    $raw =~ s/\s*($btfrTrgx\s*)?(\b(\s*($trgx)\s*)\b)($btfrTrgx)?\s*($joinTrgx)?\s*//ig;
+
+    if($raw eq '') {
+        return build_result($req);
+    }elsif($raw =~ /^(\s?([\d.]+ ?(m(in((ute)?s?)?)?|s(ec((ond)?s?)?)?|h(ours?)?|hr))\s?)+$/) {
+        return build_result($req);
+    }elsif($raw =~ /^( ?((\d{1,2}:)?\d{1,2}:\d{2}) ?)/) {
+        return build_result($req);
+    }
+    return;
+};
+
+1;
---
+++ t/TimerGoodie.t
@@ -0,0 +1,113 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use DDG::Test::Goodie;
+
+zci answer_type => 'timer_goodie';
+zci is_cached   => 1;
+
+sub build_structured_answer {
+    my $time = shift;
+    $time = $time || 0;
+    return "$time",
+        structured_answer => {
+            id     =>  'timer_goodie',
+            name   => 'Timer',
+            signal => 'high',
+            meta => {
+                sourceName => 'Timer',
+                itemType   => 'timer',
+            },
+            data => {
+                time => "$time",
+            },
+            templates => {
+                group       => 'base',
+                detail      => 'DDH.timer_goodie.timer_wrapper',
+                wrap_detail => 'base_detail',
+            },
+        };
+}
+
+sub build_test { test_zci(build_structured_answer(@_)) }
+
+ddg_goodie_test(
+    [
+        'DDG::Goodie::TimerGoodie'
+    ],
+    # With no initial time
+    'timer'                  => build_test(),
+    'online timer'           => build_test(),
+    'timer online'           => build_test(),
+    'online alarm'           => build_test(),
+    'countdown online'       => build_test(),
+    'online timer'           => build_test(),
+    'online alarm'           => build_test(),
+    'countdown online'       => build_test(),
+    'timer online for '      => build_test(),
+    'Countdown timer'        => build_test(),
+    'Online Countdown timer' => build_test(),
+
+    # With initial time
+    'timer 15 mins'                                  => build_test('900'),
+    'timer 77 mins 13 secs'                          => build_test('4633'),
+    '1 minute timer'                                 => build_test('60'),
+    '2min30sec online timer'                         => build_test('150'),
+    '3 hr 5 min timer'                               => build_test('11100'),
+    'timer 30 seconds'                               => build_test('30'),
+    '3.5 minute timer'                               => build_test('210'),
+    'alarm 30 minutes'                               => build_test('1800'),
+    'timer for 15 mins'                              => build_test('900'),
+    'timer for 77 mins 13 secs'                      => build_test('4633'),
+    'timer 2:30'                                     => build_test('150'),
+    'begin timer for 15 mins'                        => build_test('900'),
+    'run timer for 77 mins 13 secs'                  => build_test('4633'),
+    'start timer 2:30'                               => build_test('150'),
+    'begin timer with 15 mins'                       => build_test('900'),
+    'run timer at 77 mins 13 secs'                   => build_test('4633'),
+    'start timer alarm 2:30'                         => build_test('150'),
+    'begin countdown timer with 15 mins'             => build_test('900'),
+    'run timer alarm online at 77 mins 13 secs'      => build_test('4633'),
+    'start timer alarm for 2:30 online'              => build_test('150'),
+    'online start timer alarm for 2:30 online'       => build_test('150'),
+    'timer 1:20:30'                                  => build_test('4830'),
+    'timer 5s'                                       => build_test('5'),
+    'timer 5m'                                       => build_test('300'),
+    'timer 5h'                                       => build_test('18000'),
+    'countdown for 10 minutes'                       => build_test('600'),
+    'Countdown timer 15 mins'                        => build_test('900'),
+    'Online Countdown Alarm timer 15 mins'           => build_test('900'),
+    'Countdown Alarm timer 15 mins Online'           => build_test('900'),
+    'online start alarm Timer for 10 minutes online' => build_test('600'),
+    'start online CountDown alarm for 10 minutes'    => build_test('600'),
+    'CountDown Timer with 10 minutes'                => build_test('600'),
+    'CountDown Timer at 10 minutes'                  => build_test('600'),
+    '3.5 minute countdown timer'                     => build_test('210'),
+    'begin alarm 30 minutes'                         => build_test('1800'),
+    'set alarm 30 minutes'                           => build_test('1800'),
+    'begin alarm at 30 minutes'                      => build_test('1800'),
+    'set alarm with 30 minutes'                      => build_test('1800'),
+    'set alarm with30 minutes'                       => build_test('1800'),
+    'countdown timer for 77 mins 13 secs'            => build_test('4633'),
+    'start countdown timer for 77 mins 13 secs'      => build_test('4633'),
+    # Should not trigger
+    'start 30 minutes'           => undef,
+    'start 30 minutes for timer' => undef,
+    'run with timer'             => undef,
+    'begin for 30 seconds'       => undef,
+    'blahblah timer'             => undef,
+    'wwdc 2015 countdown'        => undef,
+    'timer 5 hellos'             => undef,
+    'time'                       => undef,
+    'Time::Piece'                => undef,
+    'timer.x'                    => undef,
+    'countdown.x'                => undef,
+    'alarm.x'                    => undef,
+    'countdown.js 10 minutes'    => undef,
+    'five-alarm'                 => undef # issue 1937
+);
+
+done_testing;
