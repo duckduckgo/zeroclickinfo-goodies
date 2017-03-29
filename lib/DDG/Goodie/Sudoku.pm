2f440dee20aff550fbe292b4602bd729a323806e
Philip Kirkbride <kirkins@gmail.com>
2017-03-29 20:04:07 +0000

    convert spaces to tabs

--- lib/DDG/Goodie/Sudoku.pm
+++ lib/DDG/Goodie/Sudoku.pm
@@ -15,37 +15,37 @@ zci answer_type => "sudoku";
 handle remainder => sub
 {
   
-    return unless /^(easy|average|medium|hard|random|generate|play|)$/;
-    
-    my($difficulty) = m/^(average|medium|hard)?$/;
-    $difficulty = "easy" unless ($difficulty);
-    
-    my $sudoku = Games::Sudoku::Component->new(size => 9);
-    
-    # proportion of the grid to be blank
-    my $blanks = 0.25;
-    $blanks = 0.75 if ($difficulty eq "hard");
-    $blanks = 0.5 if ($difficulty eq "medium" || $difficulty eq "average");
-    
-    $sudoku->generate(blanks => (9 ** 2) * $blanks);
-    my $str_output = $sudoku->as_string();
-    
-    $str_output =~ s/\s/,/g;
-    my @numbers = split /,/, $str_output;
-    
-    return $sudoku->as_string(), 
-    structured_answer => {
-        data => {
-            title => 'Sudoku',
-            sudoku_values => \@numbers
-        },
-        templates => {
-            group => 'media',
-            options => {
-                content => 'DDH.sudoku.content'
-            }
-        }
-    };
+	return unless /^(easy|average|medium|hard|random|generate|play|)$/;
+	
+	my($difficulty) = m/^(average|medium|hard)?$/;
+	$difficulty = "easy" unless ($difficulty);
+	
+	my $sudoku = Games::Sudoku::Component->new(size => 9);
+	
+	# proportion of the grid to be blank
+	my $blanks = 0.25;
+	$blanks = 0.75 if ($difficulty eq "hard");
+	$blanks = 0.5 if ($difficulty eq "medium" || $difficulty eq "average");
+	
+	$sudoku->generate(blanks => (9 ** 2) * $blanks);
+	my $str_output = $sudoku->as_string();
+	
+	$str_output =~ s/\s/,/g;
+	my @numbers = split /,/, $str_output;
+	
+	return $sudoku->as_string(), 
+	structured_answer => {
+		data => {
+			title => 'Sudoku',
+			sudoku_values => \@numbers
+		},
+		templates => {
+			group => 'media',
+			options => {
+				content => 'DDH.sudoku.content'
+			}
+		}
+	};
 };
 
 1;
