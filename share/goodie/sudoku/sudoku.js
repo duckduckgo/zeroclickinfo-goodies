9fe4c87d7dfaf0ae0608963574525774a9f87e26
Philip Kirkbride <kirkins@gmail.com>
2017-03-29 19:57:13 +0000

    use duckduckgo style guide

--- share/goodie/sudoku/sudoku.js
+++ share/goodie/sudoku/sudoku.js
@@ -1,17 +1,20 @@
 DDH.sudoku = DDH.sudoku || {};
 var sudoku_values;
-DDH.sudoku.build = function(ops) {
-  sudoku_values = ops.data.sudoku_values;
+DDH.sudoku.build = function(ops)
+{
+	sudoku_values = ops.data.sudoku_values;
 };
 
-Handlebars.registerHelper("moduloIf", function(index_count,mod,block) {
-  if(parseInt(index_count)%(mod)=== 0)
-    return block.fn(this);
+Handlebars.registerHelper("moduloIf", function(index_count,mod,block)
+{
+	if(parseInt(index_count)%(mod)=== 0)
+		return block.fn(this);
 });
 
-Handlebars.registerHelper('ifBlank', function(value) {
-  if(value==0)
-    return new Handlebars.SafeString("<input maxlength='1'/>")
-  else
-    return this;
+Handlebars.registerHelper('ifBlank', function(value)
+{
+	if(value==0)
+		return new Handlebars.SafeString("<input maxlength='1'/>")
+	else
+		return this;
 });
