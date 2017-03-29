5556fb1263b5081399fa090fd3f5bc5d2f884346
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2017-03-29 19:23:05 +0000

    Updated js.

--- share/goodie/calculator/calculator.js
+++ share/goodie/calculator/calculator.js
@@ -4,178 +4,236 @@ DDH.calculator = DDH.calculator || {};
     "use strict";
 
     DDH.calculator.build = function(ops) {
-        return {
+           
+        var usingState = false;
+        var buttons;
+        var currentDisplay; // pjh: refactor this out, it's not dry
+        var operators = ["+", "-", "×", "÷"];
+        var cButton;
+        var evaluatedExpression;
+        
+        
+        function normalizeExpression( expression ) {
+            var expression = expression;
 
+            return expression
+                .replace(/x/g, '*')
+                .replace(/×/g, '*')
+                .replace(/%/g,'/ 100')
+                .replace(/[÷]/g,'/')
+                .replace(/[,]/g,'')
+        }        
+        
+
+        function setExpression( expression = "" ){
+            evaluatedExpression.innerHTML = expression;
+        } // setExpression()       
+        
+        
+        function setCButtonState( state ) {
+            if(state === "C") {
+                cButton.innerHTML = "C";
+                cButton.value = "C";
+                display.innerHTML = "0";
+            } else if(state === "CE") {
+                cButton.innerHTML = "CE";
+                cButton.value = "CE";
+            }
+        } // setCButtonState()
+        
+        
+        function calcUpdate( element ){
+            usingState = true;
+            currentDisplay = display.value;
+
+            if(element === "C_OPT" || element === "C" || element === "CE") {
+
+                if(element === "C" || display.value.length < 1 || usingState === false) {
+                    display.value = "";
+                    usingState = false;
+                    setExpression();
+                    setCButtonState("C");
+                } else if(element === "CE" ) {
+
+                    if (display.value.length > 1) {
+                        display.value = display.value.substring(0, display.value.length - 1);
+                    } else if (display.value.length === 1) {
+                        display.innerHTML = "0";
+                        setCButtonState("C");
+                    } else {
+                        setCButtonState("C");
+                        usingState = true;
+                    } // if
+
+                } else {
+
+                    display.value = display.value.substring(0, display.value.length - 1);
+
+                }
+
+            } else if(element === "=") {
+
+                try {
+                    var total = math.eval(
+                        normalizeExpression(currentDisplay)
+                    );
+                } catch(err) {
+                    console.log(err);
+                    display.innerHTML = "Error";
+                    display.value = "0";
+                    return;
+                } // try / catch
+
+                if(total === Infinity) {
+                    display.innerHTML = "Infinity";
+                    display.value = "0";
+                    return false;
+                }
+
+                setExpression(display.value);
+                // TODO: replace with DDG.commifyNumber()
+                display.value = DDG.commifyNumber(total);
+                // display.value = total;
+
+                setCButtonState("C");
+
+            } else if(element !== undefined) {
+
+                if(display.value === "0" && usingState === true && element === "0") {
+                    display.value = "";
+                } else if (display.value === "-0"){
+                    display.value = "-";
+                    currentDisplay = display.value;
+                } // else if
+
+                // adds spaces into the console
+                ($.inArray(element, operators) >= 0) 
+                    ? display.value = currentDisplay + " " + element + " " : 
+                display.value = currentDisplay + element;
+
+                if (display.value.length > 1) {
+                    setCButtonState("CE");
+                }
+            }// if / else block
+
+            // sets the display
+            (usingState 
+             ? display.innerHTML = display.value : 
+             display.innerHTML = "0");
+
+        } // calcUpdate()
+
+        return {
             onShow: function() {
 
                 DDG.require('math.js', function() {
 
-                    var display;
-                    var buttons;
-                    var currentDisplay;
-                    var operators = ["+", "-", "%", "x", "×"];
-
-                    function commatize(expression) {
-                      expression += ''; // the expression that has been evaluated
-                      var x = expression.split('.');
-
-                      // temp vars
-                      var x1 = x[0];
-                      var x2 = x.length > 1 ? '.' + x[1] : '';
-                      var rgx = /(\d+)(\d{3})/;
-
-                      while (rgx.test(x1)) {
-                        x1 = x1.replace(rgx, '$1' + ',' + '$2');
-                      }
-
-                      return x1 + x2;
-                    }
-
-                    function normalizeExpression(expression) {
-                        var normalized;
-                        var expression = expression;
-
-                        normalized = expression
-                                        .replace(/x/g, '*')
-                                        .replace(/×/g, '*')
-                                        .replace(/%/g,'/ 100')
-                                        .replace(/[÷]/g,'/')
-                                        .replace(/[,]/g,'')
-                                        // scientific funcs, operators, constants
-                                        // this is currently on hold
-                                        .replace(/[π]/g,'(3.14159265359)')
-                                        .replace(/(√)(\(\d+\))/g, 'sqrt($2)')
-                                        .replace(/(log)(\(\d+\))/g, 'log($2, 10)')
-                                        .replace(/(ln)(\(\d+\))/g, 'log($2)')
-                                        .replace(/[e]/g, '(2.71828182846)')
-                                        .replace(/<sup>2<\/sup>/gi, '^2')
-                                        .replace(/<sup>3<\/sup>/gi, '^3');
-                        return normalized;
-                    }
-
-                    function calcUpdate(element){
-                        var currentClassName;
-                        var elementValue = element;
-
-                        var inputs = document.getElementsByTagName("button");
-                        currentDisplay = display.value;
-
-
-                        if(element === "C") {
-                            display.value = "0";
-                        // pjh: will implement later. this is tricky
-                        // } else if(element === "inv") {
-                        //     var temp = parseInt(display.input);
-                        //     console.log(temp);
-                        } else if(element === "CE") {
-                            display.value = display.value.substring(0, display.value.length - 1);
-                        } else if(element === "=") {
-
-                            try {
-                                var total = math.eval(
-                                    normalizeExpression(currentDisplay)
-                                );        
-                            } catch(err) {
-                                console.log(err);
-                                display.innerHTML = "Error";
-                                display.value = "0";
-                                return;
-                            } // try / catch
-
-                            if(total === Infinity) {
-                                display.innerHTML = "Infinity";
-                                display.value = "0";
-                                return false;
-                            }
-
-                            display.value = commatize(total);
-
-                            } else if(element !== undefined) {
-                                if(display.value === "0"){
-                                display.value = "";
-                                currentDisplay = display.value;
-                            } else if (display.value === "-0"){
-                                display.value = "-";
-                                currentDisplay = display.value;
-                            } // else if
-
-                            // adds spaces into the console
-                            // pjh: BUG: '%' not being captured for some reason. to come back
-                            ($.inArray(element, operators) >= 0) 
-                                ? display.value = currentDisplay + " " + element + " " : 
-                                  display.value = currentDisplay + element;
-
-                        }
-
-                        console.log(display.value + " = display value");
-                        display.innerHTML = display.value;
-
-                    }
-
-                    display = document.getElementById('display');
-                    buttons = document.getElementsByTagName("button");
+                    var $calc = $(".zci--calculator");
+                    buttons = $("button");
+                    var display = $('#display')[0];
                     display.value = "0";
-
+                    evaluatedExpression = $('#expression')[0];
+                    cButton = $('#clear_button')[0];
+                    
                     for (var i = 0; i < buttons.length; i ++){
                         buttons[i].addEventListener("click", function(){ 
                             calcUpdate(this.value); 
                         });
                     }
 
-                    window.onkeydown = function(e){
-                      var keyID = e.which;
-                      var keyValue;
-                      
-                      switch(keyID) {
-                        case 49:
-                          keyValue = 1;
-                          break;
-                        case 50:
-                          keyValue = 2;
-                          break;
-                        case 51:
-                          keyValue = 3;
-                          break;
-                        case 52:
-                          keyValue = 4;
-                          break;
-                        case 53:
-                          keyValue = 5;
-                          break;
-                        case 54:
-                          keyValue = 6;
-                          break;
-                        case 55:
-                          keyValue = 7;
-                          break;
-                        case 56:
-                          keyValue = 8;
-                          break;
-                        case 57:
-                          keyValue = 9;
-                          break;
-                        case 48:
-                          keyValue = 0;
-                          break;
-                        case 13:
-                          keyValue = "=";
-                          break;
-                        case 187:
-                          keyValue = "+";
-                          break;
-                        case 189:
-                          keyValue = "-";
-                          break;
-                        case 88:
-                          keyValue = "×";
-                          break;
-                        case 191:
-                          keyValue = "÷";
-                          break;
-                      }
-                      calcUpdate(keyValue);
+                    $(document).on('keydown', function(e){
 
-                      };
+                        var keyID = e.which;
+                        var keyValue;
+
+                        if(e.shiftKey) {
+                            switch(keyID)
+                            {
+                                case 187:
+                                    keyValue = "+";
+                                    break;
+                                case 56:
+                                    keyValue = "×";
+                                    break;
+                                case 53:
+                                    keyValue = "%";
+                                    break;
+                                case 57:
+                                    keyValue = "(";
+                                    break;
+                                case 48:
+                                    keyValue = ")";
+                                    break;
+                            }
+                        } else {
+                            switch(keyID)
+                            {
+                                case 49:
+                                    keyValue = 1;
+                                    break;
+                                case 50:
+                                    keyValue = 2;
+                                    break;
+                                case 51:
+                                    keyValue = 3;
+                                    break;
+                                case 52:
+                                    keyValue = 4;
+                                    break;
+                                case 53:
+                                    keyValue = 5;
+                                    break;
+                                case 54:
+                                    keyValue = 6;
+                                    break;
+                                case 55:
+                                    keyValue = 7;
+                                    break;
+                                case 56:
+                                    keyValue = 8;
+                                    break;
+                                case 57:
+                                    keyValue = 9;
+                                    break;
+                                case 48:
+                                    keyValue = 0;
+                                    break;
+                                case 13:
+                                    keyValue = "=";
+                                    break;
+                                case 187:
+                                    keyValue = "=";
+                                    break;
+                                case 88:
+                                    keyValue = "×";
+                                    break;
+                                case 191:
+                                    keyValue = "÷";
+                                    break;
+                                case 188:
+                                    keyValue = ",";
+                                    break;
+                                case 190:
+                                    keyValue = ".";
+                                    break;
+                                case 189:
+                                    keyValue = "-";
+                                    break;
+                                case 8:
+                                    keyValue = "C_OPT";
+                                    break;
+                            }
+                        }
+
+                        calcUpdate(keyValue);
+
+                    });
+
+                    $calc.on("click", function(e) {
+                        e.preventDefault();
+                        $calc.focus();
+                    });
+
 
                 }); // DDG.require('math.js')
             }
