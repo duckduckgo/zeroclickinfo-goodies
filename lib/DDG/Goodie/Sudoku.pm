71158dc095e7b04a3d3d5ba1b1cd7e6c8c9e816b
Philip Kirkbride <kirkins@gmail.com>
2017-03-29 19:25:05 +0000

    convert sudoku to use structured answer

--- lib/DDG/Goodie/Sudoku.pm
+++ lib/DDG/Goodie/Sudoku.pm
@@ -12,47 +12,39 @@ triggers startend => 'sudoku';
 zci is_cached => 0;
 zci answer_type => "sudoku";
 
-sub parse_to_html_table(@)
-{
-	my @sudoku_lines = @_;
-	my $html_table = "<table class='sudoku'>\n";
-	for my $line (@sudoku_lines)
-	{
-		my @chars = split(/ /, $line);
-		for my $char (@chars)
-		{
-			$char = "<input maxlength='1'/>" if $char eq "_";
-		}
-		$html_table .= "<tr><td>" . join("</td><td>", @chars) . "</td></tr>\n";
-	}
-	$html_table .= "</table>";
-	return $html_table;
-}
-
 handle remainder => sub {
+  
+  return unless /^(easy|average|medium|hard|random|generate|play|)$/;
+  
+  my($difficulty) = m/^(average|medium|hard)?$/;
+  $difficulty = "easy" unless ($difficulty);
+  
+  my $sudoku = Games::Sudoku::Component->new(size => 9);
+  
+  # proportion of the grid to be blank
+  my $blanks = 0.25;
+  $blanks = 0.75 if ($difficulty eq "hard");
+  $blanks = 0.5 if ($difficulty eq "medium" || $difficulty eq "average");
+  
+  $sudoku->generate(blanks => (9 ** 2) * $blanks);
+  my $str_output = $sudoku->as_string();
 
-	return unless /^(easy|average|medium|hard|random|generate|play|)$/;
+  $str_output =~ s/\s/,/g;
+  my @numbers = split /,/, $str_output;
 
-	my($difficulty) = m/^(average|medium|hard)?$/;
-	$difficulty = "easy" unless ($difficulty);
-
-	my $sudoku = Games::Sudoku::Component->new(size => 9);
-
-	#proportion of the grid to be blank
-	my $blanks = 0.25;
-	$blanks = 0.75 if ($difficulty eq "hard");
-	$blanks = 0.5 if ($difficulty eq "medium" || $difficulty eq "average");
-
-	$sudoku->generate(blanks => (9 ** 2) * $blanks);
-	my $str_output = $sudoku->as_string();
-
-	#switch 0 to more sensible placeholders
-	$str_output =~ s/0/_/g;
-
-	my @sudoku_lines = split(/\n/, $str_output);
-	my $html_table = parse_to_html_table(@sudoku_lines);
-
-	return $str_output, html => $html_table;
+  return $sudoku->as_string(), 
+  structured_answer => {
+    data => {
+      title => 'Sudoku',
+      sudoku_values => \@numbers # works if I return $sudoku->as_string();
+    },
+    templates => {
+      group => 'media',
+      options => {
+        content => 'DDH.sudoku.content'
+      }
+    }
+  };
 };
 
 1;
