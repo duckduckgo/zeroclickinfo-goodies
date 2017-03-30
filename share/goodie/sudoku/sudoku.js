ae57ae794b186de944aca18a13b20d128337b949
Philip Kirkbride <kirkins@gmail.com>
2017-03-30 16:30:39 -0400

    Removing JavaScript not relevant to commit
    I'm removing some javascript which I wrote for another function I was going to add as it's not relevant to this commit.

--- share/goodie/sudoku/sudoku.js
+++ share/goodie/sudoku/sudoku.js
@@ -1,10 +1,3 @@
-DDH.sudoku = DDH.sudoku || {};
-var sudoku_values;
-DDH.sudoku.build = function(ops)
-{
-	sudoku_values = ops.data.sudoku_values;
-};
-
 Handlebars.registerHelper("moduloIf", function(index_count,mod,block)
 {
 	if(parseInt(index_count)%(mod)=== 0)
