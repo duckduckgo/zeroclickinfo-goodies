5c4dacfd5bd26d4f0f12af37e38513931109cdec
edcallaghan <ejc87@drexel.edu>
2017-03-31 10:21:54 -0400

    allow terminating equal-sign + tests

--- lib/DDG/Goodie/PercentOf.pm
+++ lib/DDG/Goodie/PercentOf.pm
@@ -9,11 +9,11 @@ zci is_cached   => 1;
 
 my $result;
 
-triggers query_nowhitespace => qr/\d{1,3}\%$/;
+triggers query_nowhitespace => qr/\d{1,3}\%=?$/;
 
 handle query_nowhitespace => sub {
 
-    return unless $_ =~ qr/^(?:\p{Currency_Symbol})*\s*(\d+\.?\d*)\s*(\+|\*|\/|\-)\s*(\d+\.?\d*)\%$/;
+    return unless $_ =~ qr/^(?:\p{Currency_Symbol})*\s*(\d+\.?\d*)\s*(\+|\*|\/|\-)\s*(\d+\.?\d*)\%\s*=?$/;
 
     my $partRes = ($1 * $3) / 100;
 
