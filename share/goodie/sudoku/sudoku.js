164a1537945d94155b5c77cd3733d316563c762e
Mailkov <miticomilko@hotmail.com>
2017-04-04 19:02:05 +0000

    write sudoku scheme

--- share/goodie/sudoku/sudoku.js
+++ share/goodie/sudoku/sudoku.js
@@ -1,21 +1,39 @@
 DDH.sudoku = DDH.sudoku || {}; // create the namespace in case it doesn't exist
 
 DDH.sudoku.build = function(ops) {
-
+    var started;
+    var levels = { "easy" : 2, "average" : 3, "medium" : 3, "hard" : 4 }; 
+    var level = levels[ops.data.level];
     var cells =[];
-    $.getScript('https://github.com/Mailkov/SudokuJSMA/blob/master/sudokujsma2.js')
-    .done(function(){
-        cells = getSudoku(1);
-        var t=0;
-    })  
-    .fail(function() {
-        var p=0;
-    });
     
+    function start() {
+        var nsquare = 0;
+        $.getScript('https://raw.githubusercontent.com/Mailkov/SudokuJSMA/master/sudokujsma2.js')
+        .done(function(){
+            cells = getSudoku(level);
+            squares.each(function(index) {
+                $(this).attr("id", "C"+nsquare);
+                if (cells[nsquare] != 0) {
+                    $("#C"+nsquare+" span").html(cells[nsquare++]);
+                } else {
+                    $("#C"+(nsquare++)+" span").html("<input maxlength='1'/>");
+                }
+            });
+        })  
+        .fail(function() {
+            var p=0;
+        });       
+     }
+     
     return {
-        // Specify any frontend display properties here
+        onShow: function() {
+                if (!started) {
+                    started = true;
+                    squares = $('td.square');               
+                    start();
+                }
+            }
     };
-
 }; 
     
     
