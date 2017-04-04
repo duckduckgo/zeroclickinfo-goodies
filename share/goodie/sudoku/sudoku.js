60536d7c2745c816a6484c16cb1a7be3e726db22
Mailkov <miticomilko@hotmail.com>
2017-04-04 07:55:25 +0000

    Issue #1163 - Create file sudoku.js

---
+++ share/goodie/sudoku/sudoku.js
@@ -0,0 +1,9 @@
+DDH.sudoku = DDH.sudoku || {}; // create the namespace in case it doesn't exist
+
+DDH.sudoku.build = function(ops) {
+
+    return {
+        // Specify any frontend display properties here
+    };
+
+};
