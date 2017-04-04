6fa778144c1326508f37766bf526073f8fe41820
Mailkov <miticomilko@hotmail.com>
2017-04-04 17:18:48 +0000

    test for sudokujsma library

--- share/goodie/sudoku/sudoku.js
+++ share/goodie/sudoku/sudoku.js
@@ -2,8 +2,20 @@ DDH.sudoku = DDH.sudoku || {}; // create the namespace in case it doesn't exist
 
 DDH.sudoku.build = function(ops) {
 
+    var cells =[];
+    $.getScript('https://github.com/Mailkov/SudokuJSMA/blob/master/sudokujsma2.js')
+    .done(function(){
+        cells = getSudoku(1);
+        var t=0;
+    })  
+    .fail(function() {
+        var p=0;
+    });
+    
     return {
         // Specify any frontend display properties here
     };
 
-};
+}; 
+    
+    
