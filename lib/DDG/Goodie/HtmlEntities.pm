a67bc00ef75168cbe04df607e36348637987fd41
Robert Badea <owlree128@gmail.com>
2017-04-12 00:41:17 +0300

    HTML Entities: New HTML Entities Goodie (#4056)
    * added html entities goodie
    
    * fixed trigger
    
    * added tests
    
    * fixed review issues
    
    fixed CSS classes, fixed file names, switched from JSON to YML
    
    * fixed tests and file loading
    
    * renamed to camel case
    
    * deleted AsciiTable.pm
    
    * changed to camel case
    
    * Remove extra newlines
    
    * Fix indentation for Beta server
    
    * Use chompContent
    
    * Simplify CSS
    
    * Remove scrolling div
    
    * Fix test
    
    * Switch overflow control to .c-base__content div

---
+++ lib/DDG/Goodie/HtmlEntities.pm
@@ -0,0 +1,42 @@
+package DDG::Goodie::HtmlEntities;
+
+# ABSTRACT:  This goodie loads a static table of HTML entities.
+
+use DDG::Goodie;
+use strict;
+use warnings;
+use YAML::XS 'LoadFile';
+
+zci answer_type => 'html_entities';
+zci is_cached => 1;
+
+triggers startend => 'html entities';
+
+my $table = LoadFile(share('entities.yml'));
+
+handle query_lc => sub {
+
+    return unless m/^(list of )?html entities( table| list)?$/;
+
+    return 'HTML Entities', structured_answer => {
+        data => {
+            title => "HTML Entities",
+            table => $table
+        },
+        meta => {
+            sourceName => "W3.org",
+            sourceUrl => "https://dev.w3.org/html5/html-author/charref"
+        },
+        templates => {
+            group => 'text',
+            item => 0,
+            options => {
+                content => 'DDH.html_entities.content',
+                chompContent => 1,
+                moreAt => 1
+            }
+        }
+    };
+};
+
+1;
