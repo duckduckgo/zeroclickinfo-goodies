79a4e7b6b6f0a2418b54fd483e93c4456b321621
Asli Gunay Yildirim <asligunay@me.com>
2017-04-12 20:46:40 +0000

    Converted some of the JavaScript selectors to jQuery

--- share/goodie/css_gradient_generator/css_gradient_generator.js
+++ share/goodie/css_gradient_generator/css_gradient_generator.js
@@ -58,77 +58,80 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
     // Initializes local domain and handles user events
     function initializeLocalDom() {
         localDom = {           
-            $linear_type_button: document.getElementById('linear-type-btn'),
-            $radial_type_button: document.getElementById('radial-type-btn'),
+            $linear_type_button: $("#linear-type-btn"),
+            $radial_type_button: $("#radial-type-btn"),
             
-            $direction_input: document.getElementById('direction-input'),
-            $direction_output: document.getElementById('direction-output'),
+            $direction_input: $("#direction-input"),
+            $direction_output: $("#direction-output"),
             
-            $radial_size_dropdown: document.getElementById('radial-size-dropdown'),
-            $radial_x_position_input: document.getElementById('radial-x-position-input'),
-            $radial_y_position_input: document.getElementById('radial-y-position-input'),
+            $radial_size_dropdown: $("#radial-size-dropdown"),
+            $radial_x_position_input: $("#radial-x-position-input"),
+            $radial_y_position_input: $("#radial-y-position-input"),
             
             $color_box_arr: document.getElementsByClassName('color-box'),
             
-            $new_color_point: document.getElementById('new-color-point'),
-            $new_color_r: document.getElementById('new-color-r'),
-            $new_color_g: document.getElementById('new-color-g'),
-            $new_color_b: document.getElementById('new-color-b'),
-            $new_color_add_button: document.getElementById('new-color-add-btn'),
+            $new_color_point: $("#new-color-point"),
+            $new_color_r: $("#new-color-r"),
+            $new_color_g: $("#new-color-g"),
+            $new_color_b: $("#new-color-b"),
+            $new_color_add_button: $("#new-color-add-btn"),
             
-            $selected_color_box: document.getElementById('selected-color-box'),
-            $selected_color_text: document.getElementById('selected-color-text'),
-            $selected_point_input: document.getElementById('selected-point-input'),
-            $selected_r_picker: document.getElementById('selected-r-picker'),
-            $selected_g_picker: document.getElementById('selected-g-picker'),
-            $selected_b_picker: document.getElementById('selected-b-picker'),
-            $selected_delete_button: document.getElementById('selected-delete-btn'),
+            $selected_color_text: $("#selected-color-text"),
+            $selected_point_input: $("#selected-point-input"),
+            $selected_r_picker: $("#selected-r-picker"),
+            $selected_g_picker: $("#selected-g-picker"),
+            $selected_b_picker: $("#selected-b-picker"),
+            $selected_delete_button: $("#selected-delete-btn"),
             
-            $preview_panel: document.getElementById('preview-panel'),
-            $output_panel_text: document.getElementById('output-panel-text'),
+            $preview_panel: $("#preview-panel"),
+            $output_panel_text: $("#output-panel-text"),
             
-            $output_modal: document.getElementById('output-modal'),
-            $output_modal_close: document.getElementById('output-modal-close'),
+            $output_modal: $("#output-modal"),
+            $output_modal_close: $("#output-modal-close"),
 
             activeColorIndex: 0,
             initialized: true
         } 
 
         // Gradient type selectors
-        localDom.$linear_type_button.onclick = function() { typeChange("linear") };
-        localDom.$radial_type_button.onclick = function() { typeChange("radial") };
+        localDom.$linear_type_button.on("click", function() { typeChange("linear"); });
+        localDom.$radial_type_button.on("click", function() { typeChange("radial"); });
         
         // Linear gradient settings change
-        localDom.$direction_input.oninput = function() { linearSettingsChange() };
+        localDom.$direction_input.on("input", function() { linearSettingsChange(); });
         
         // Radial gradient settings change
-        localDom.$radial_size_dropdown.onchange = function() { radialSettingsChange() };
-        localDom.$radial_x_position_input.oninput = function() { radialSettingsChange() };
-        localDom.$radial_y_position_input.oninput = function() { radialSettingsChange() };
+        localDom.$radial_size_dropdown.on("change", function() { radialSettingsChange(); });
+        localDom.$radial_x_position_input.on("input", function() { radialSettingsChange(); });
+        localDom.$radial_y_position_input.on("input", function() { radialSettingsChange(); });
         
         // Handle form input change
-        localDom.$new_color_point.oninput = function() { toggleAddButton() };
-        localDom.$new_color_r.oninput = function() { toggleAddButton() };
-        localDom.$new_color_g.oninput = function() { toggleAddButton() };
-        localDom.$new_color_b.oninput = function() { toggleAddButton() };
+        localDom.$new_color_point.on("input", function() { toggleAddButton() });
+        localDom.$new_color_r.on("input", function() { toggleAddButton(); });
+        localDom.$new_color_g.on("input", function() { toggleAddButton(); });
+        localDom.$new_color_b.on("input", function() { toggleAddButton(); });
         
         // Add new stop color
-        localDom.$new_color_add_button.onclick = function() { addColor() };
+        localDom.$new_color_add_button.on("click", function() { addColor(); });
         
         // Remove a stop color
-        localDom.$selected_delete_button.onclick = function() { deleteColor() };
+        localDom.$selected_delete_button.on("click", function() { deleteColor(); });
         
         // Point picker change
-        localDom.$selected_point_input.oninput = function() { pointChange() };
+        localDom.$selected_point_input.on("input", function() { pointChange(); });
         
         // RGB picker change
-        localDom.$selected_r_picker.oninput = function() { rgbChange() };
-        localDom.$selected_g_picker.oninput = function() { rgbChange() };
-        localDom.$selected_b_picker.oninput = function() { rgbChange() };
+        localDom.$selected_r_picker.on("input", function() { rgbChange(); });
+        localDom.$selected_g_picker.on("input", function() { rgbChange(); });
+        localDom.$selected_b_picker.on("input", function() { rgbChange(); });
         
         // Modal handlers        
-        localDom.$preview_panel.onclick = function() { toggleClass(localDom.$output_modal, 'is-showing') };
-        localDom.$output_modal_close.onclick = function() { toggleClass(localDom.$output_modal, 'is-showing') };
+        localDom.$preview_panel.on("click", function() { 
+            localDom.$output_modal.addClass("is-showing"); 
+        });
+        localDom.$output_modal_close.on("click", function() { 
+            localDom.$output_modal.removeClass("is-showing"); 
+        });
         
         drawColorPanel();
         update();
@@ -165,22 +168,24 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
         localDom.activeColorIndex = index;
 
         // Update domain elements
-        localDom.$selected_point_input.value = selectedColor.point;
-        localDom.$selected_r_picker.value = selectedColor.r;
-        localDom.$selected_g_picker.value = selectedColor.g;
-        localDom.$selected_b_picker.value = selectedColor.b;
+        localDom.$selected_point_input.val(selectedColor.point);
+        localDom.$selected_r_picker.val(selectedColor.r);
+        localDom.$selected_g_picker.val(selectedColor.g);
+        localDom.$selected_b_picker.val(selectedColor.b);
                         
-        localDom.$selected_color_text.innerHTML = selectedColorStr;
-        localDom.$selected_color_text.style.color = selectedColorStr;
-        localDom.$selected_color_text.style.textDecorationColor = selectedColorStr;
+        localDom.$selected_color_text.text(selectedColorStr);
+        localDom.$selected_color_text.css({
+            'color': selectedColorStr, 
+            'text-decoration-color': selectedColorStr
+        });
     }
     
     
     // Adds a new color object to the global params object based on user input
     function addColor() { 
         // Return if any of the form input is left empty
-        if (localDom.$new_color_point.value === "" || localDom.$new_color_r.value.value === "" || 
-            localDom.$new_color_g.value === "" || localDom.$new_color_b.value === "")
+        if (localDom.$new_color_point.val() === "" || localDom.$new_color_r.val() === "" || 
+            localDom.$new_color_g.val() === "" || localDom.$new_color_b.val() === "")
             return;
         
         // New color object takes point and rgb info from the user-filled form
@@ -188,10 +193,10 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
         var newColorObj = {
             id: "",
             index: 0,
-            point: parseInt(localDom.$new_color_point.value),
-            r: parseInt(localDom.$new_color_r.value),
-            g: parseInt(localDom.$new_color_g.value),
-            b: parseInt(localDom.$new_color_b.value)
+            point: parseInt(localDom.$new_color_point.val()),
+            r: parseInt(localDom.$new_color_r.val()),
+            g: parseInt(localDom.$new_color_g.val()),
+            b: parseInt(localDom.$new_color_b.val())
         }
         var temp = params.colors;
         
@@ -205,10 +210,10 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
         sortColors(temp); 
         
         // Reset form inputs
-        localDom.$new_color_point.value = "";
-        localDom.$new_color_r.value = "";
-        localDom.$new_color_g.value = "";
-        localDom.$new_color_b.value = "";
+        localDom.$new_color_point.val("");
+        localDom.$new_color_r.val("");
+        localDom.$new_color_g.val("");
+        localDom.$new_color_b.val("");
         toggleAddButton();
         toggleDeleteButton();
     }
@@ -318,15 +323,15 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
         // And update preview panel
         if (params.type === "linear") {
             generatedCSS += 'background: ' + params.type + '-gradient(' + gradientParams + colorParams  + ');\n';
-            localDom.$preview_panel.style.backgroundImage = params.prefix + 'linear-gradient(' + gradientParams + colorParams + ')';
+            localDom.$preview_panel.css("background-image", params.prefix + 'linear-gradient(' + gradientParams + colorParams + ')');
         }
         
         else if (params.type === "radial") {
             generatedCSS += 'background: ' + params.type + '-gradient(' + gradientParams2 + colorParams  + ');\n';
-            localDom.$preview_panel.style.backgroundImage = params.prefix + 'radial-gradient(' + gradientParams + colorParams + ')';
+            localDom.$preview_panel.css("background-image", params.prefix + 'radial-gradient(' + gradientParams + colorParams + ')');
         }
         
-        localDom.$output_panel_text.innerHTML = generatedCSS;
+        localDom.$output_panel_text.html(generatedCSS);
     }
     
     
@@ -335,15 +340,15 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
         params.type = type;
 
         if (type === "linear") {
-            localDom.$linear_type_button.className += " btn--alt";
-            localDom.$radial_type_button.className = localDom.$radial_type_button.className.replace(/ btn--alt/g , '' );
-            
+            localDom.$linear_type_button.addClass("btn--alt");
+            localDom.$radial_type_button.removeClass("btn--alt");
+
             document.getElementById('radial-type-settings').style.display = "none";
             document.getElementById('linear-type-settings').style.display = "block";
         }
         else if (type === "radial") {
-            localDom.$radial_type_button.className += " btn--alt";
-            localDom.$linear_type_button.className = localDom.$linear_type_button.className.replace(/ btn--alt/g , '' );
+            localDom.$radial_type_button.addClass("btn--alt");
+            localDom.$linear_type_button.removeClass("btn--alt");
             
             document.getElementById('linear-type-settings').style.display = "none";
             document.getElementById('radial-type-settings').style.display = "block";
@@ -355,17 +360,17 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
     
     // Handles changes for linear type gradients (direction)
     function linearSettingsChange() {
-        params.direction = localDom.$direction_input.value;
-        localDom.$direction_output.value = params.direction;
+        params.direction = localDom.$direction_input.val();
+        localDom.$direction_output.val(params.direction);
         update();
     }
     
     
     // Handles changes for radial type gradients (size and position)
     function radialSettingsChange() {
-        params.radial_size = localDom.$radial_size_dropdown.value;
-        params.radial_x = localDom.$radial_x_position_input.value;
-        params.radial_y = localDom.$radial_y_position_input.value;
+        params.radial_size = localDom.$radial_size_dropdown.val();
+        params.radial_x = localDom.$radial_x_position_input.val();
+        params.radial_y = localDom.$radial_y_position_input.val();
         
         update();
     }
@@ -374,13 +379,14 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
     // Toggles 'Add Color' button based on the validity of the new color form
     function toggleAddButton() {
         var addButton = localDom.$new_color_add_button;
-        if (localDom.$new_color_point.value !== "" && localDom.$new_color_r.value.value !== "" && 
-            localDom.$new_color_g.value !== "" && localDom.$new_color_b.value !== "") {
-            addButton.removeAttribute("disabled");
+        
+        if (localDom.$new_color_point.val() !== "" && localDom.$new_color_r.val() !== "" && 
+            localDom.$new_color_g.val() !== "" && localDom.$new_color_b.val() !== "") {
+            addButton.attr("disabled", false);
         }
-        else if (localDom.$new_color_point.value === "" || localDom.$new_color_r.value.value === "" || 
-                 localDom.$new_color_g.value === "" || localDom.$new_color_b.value === "") {
-            addButton.setAttribute("disabled", true);
+        else if (localDom.$new_color_point.val() === "" || localDom.$new_color_r.val() === "" || 
+                 localDom.$new_color_g.val()  === "" || localDom.$new_color_b.val() === "") {
+            addButton.attr("disabled", true);
         }
     }
     
@@ -388,18 +394,19 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
     // Toggles 'Add Color' button based on the number of colors available on color panel
     function toggleDeleteButton() {
         var deleteButton = localDom.$selected_delete_button;
-        if (params.colors.length > 2 && deleteButton.hasAttribute("disabled")) {
-            deleteButton.removeAttribute("disabled");
+        
+        if (params.colors.length > 2) {
+            deleteButton.attr("disabled", false);
         }
-        else if (params.colors.length <= 2 && !deleteButton.hasAttribute("disabled")) {
-            deleteButton.setAttribute("disabled", true);
+        else if (params.colors.length <= 2) {
+            deleteButton.attr("disabled", true);
         }
     }
     
     
     // Handles changes done on point picker for the selected color
     function pointChange() {
-        var point = localDom.$selected_point_input.value;
+        var point = localDom.$selected_point_input.val();
         params.colors[localDom.activeColorIndex].point = point;
         
         update();
@@ -408,9 +415,9 @@ DDH.css_gradient_generator = DDH.css_gradient_generator || {};
     
     // Handles changes done on RGB pickers for the selected color
     function rgbChange() {
-        var r = localDom.$selected_r_picker.value,
-            g = localDom.$selected_g_picker.value,
-            b = localDom.$selected_b_picker.value;
+        var r = localDom.$selected_r_picker.val(),
+            g = localDom.$selected_g_picker.val(),
+            b = localDom.$selected_b_picker.val();
 
         params.colors[localDom.activeColorIndex].r = r;
         params.colors[localDom.activeColorIndex].g = g;
