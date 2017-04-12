60c1ac3f36a8cf1570b1ad75763ee3f316ce8a04
Zaahir Moolla <moollaza@duckduckgo.com>
2017-04-12 13:47:05 -0400

    Strip trailing spaces

--- share/goodie/calculator/calculator.js
+++ share/goodie/calculator/calculator.js
@@ -398,23 +398,23 @@ DDH.calculator = DDH.calculator || {};
 
     /**
      * Ledger
-     * 
+     *
      * The Ledger is the object responsible for persisting information, adding information into
      * the ledger / history section of the calculators UI and reloading it back into the calculator
      * if a user wants to work with the result.
      */
     var Ledger = {
-        
+
         result: null,
         expression: null,
-        
+
         // addToHistory: adds expression and result to history and appends to list
         addToHistory: function( expression, result ) {
             // DDH.calculator.ledger_item is a ref to the ledger_item.handlebars template
             var ledger_item = DDH.calculator.ledger_item({expression: expression, result: result});
             $(".tile__history").prepend(ledger_item);
         },
-        
+
         // reloadIntoCalc: reloads the clicked <li> into the calculator
         reloadIntoCalc: function( expression, result ) {
             ExpressionParser.setExpression(expression);
@@ -480,7 +480,7 @@ DDH.calculator = DDH.calculator || {};
 
         ExpressionParser.setExpression(display.value);
         Ledger.addToHistory(display.value, DDG.commifyNumber(total));
-        
+
         display.value = total;
         evaluated = true;
         setCButtonState("C");
@@ -538,7 +538,7 @@ DDH.calculator = DDH.calculator || {};
             } else if(/<sup>\d+<\/sup>$/.test(display.value)) {
                 ExpressionParser.backspace(7);
                 display.value = display.value + "</sup>";
-                
+
             } else if(ExpressionParser.getExpressionLength() > 1 && (display.value[display.value.length-1] === " " && Utils.isNumber(display.value[display.value.length-2]))) {
                 ExpressionParser.backspace(2);
             } else if (ExpressionParser.getExpressionLength() > 1 && display.value[display.value.length-2] !== " ") {
@@ -841,13 +841,13 @@ DDH.calculator = DDH.calculator || {};
                         setFocus();
                         e.stopImmediatePropagation();
                     });
-                    
+
                     $(".tile__history").on('click', '.tile__past-calc', function() {
                         var expression = $(this).find("span.tile__past-formula").html();
                         var result = $(this).find("span.tile__past-result").text();
                         Ledger.reloadIntoCalc(expression, result);
                     });
-                    
+
                 }); // DDG.require('math.js')
             }
         };
