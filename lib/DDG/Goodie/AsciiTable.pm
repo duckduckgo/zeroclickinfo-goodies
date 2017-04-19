adf25c7aee6e35b6685e56c8e5fbe5a39d6ab2c9
Aditya Tandon <adityatandon007@gmail.com>
2017-04-19 10:36:13 +0000

    Fixes #4107 Added triggers

--- lib/DDG/Goodie/AsciiTable.pm
+++ lib/DDG/Goodie/AsciiTable.pm
@@ -11,7 +11,9 @@ zci answer_type => 'ascii_table';
 zci is_cached => 1;
 
 triggers start => 'ascii table', 'ascii tables', 'ascii reference table',
-                  'ascii reference';
+                  'ascii reference', 'ascii codes', 'char codes', 'character codes', 
+                  'ascii character codes', 'ascii characters', 'ascii character table', 'ascii characters table',
+                  'ascii chart', 'ascii codes chart', 'ascii characters chart';
 
 my $ascii = LoadFile(share('data.yml'));
 
