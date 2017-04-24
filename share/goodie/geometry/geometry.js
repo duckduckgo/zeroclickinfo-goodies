07b96612813a7d0049f92eb5213fc9d1111e4bb3
Zaahir Moolla <moollaza@duckduckgo.com>
2017-04-24 17:34:30 -0400

    Fix Goodie ID in JS

--- share/goodie/geometry/geometry.js
+++ share/goodie/geometry/geometry.js
@@ -1,14 +1,14 @@
-DDH.geometry_goodie = DDH.geometry_goodie || {};
+DDH.geometry = DDH.geometry || {};
 
 (function(DDH) {
     "use strict";
 
-    DDH.geometry_goodie.build = function(ops) {
+    DDH.geometry.build = function(ops) {
         return {
-            onShow: function() { 
+            onShow: function() {
                 var formulaSelector = '.geometry-formulas .formula .dot';
                 var svgSelector = '.geometry-svg *';
-                    
+
                 $(svgSelector + ',' + formulaSelector).hover(
                     function() {
                         // Get Current geoemetry type
@@ -19,13 +19,13 @@ DDH.geometry_goodie = DDH.geometry_goodie || {};
                         // JQuery 1.1 cant select SVGs by normal selectors
                         var svg = document.querySelector(".geometry-svg ." + datatype);
                         svg.classList.add("hover");
-                        
-                        
+
+
                     }, function() {
                         // Remove hovers when not hovering
                         var datatype = $( this ).data('type');
                         $('#geometry--goodie .' + datatype).removeClass( "hover" );
-                        
+
                         var svg = document.querySelector(".geometry-svg ." + datatype);
                         svg.classList.remove("hover");
                     }
