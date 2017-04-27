a2433682be91f81b203a640c3937ffa72bad8d63
Zaahir Moolla <moollaza@duckduckgo.com>
2017-04-27 16:36:53 -0400

    Call DDH.failed for mobile devices

--- share/goodie/game2048/game2048.js
+++ share/goodie/game2048/game2048.js
@@ -2,7 +2,12 @@ DDH.game2048 = DDH.game2048 || {};
 
 DDH.game2048.build = function(ops) {
     "use strict";
-    
+
+    //Hide this goodie on mobile devices for now
+    if (DDG.device.isMobile || DDG.device.isMobileDevice) {
+        return DDH.failed('game2048');
+    }
+
     // Global Variables Declaration
     var WINNUM = 2048,
         SIZE = 4,
