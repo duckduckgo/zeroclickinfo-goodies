64bda99e02a0ae9a205941eb47be865e1073b47f
Melchiorre Alastra <miticomilko@hotmail.com>
2017-04-28 13:17:24 +0000

    verify value and change color

--- share/goodie/sudoku/sudoku.js
+++ share/goodie/sudoku/sudoku.js
@@ -4,11 +4,12 @@ DDH.sudoku.build = function(ops) {
     var started;
     var levels = { "simple" : 1, "easy" : 2, "average" : 3, "medium" : 3, "hard" : 4 }; 
     var level = levels[ops.data.level];
-    var cells =[];
+    var cells = [];
+    var controlcells= [];
     
     function start() {
         var nsquare = 0;
-        $.getScript('https://raw.githubusercontent.com/Mailkov/SudokuJSMA/master/sudokujsma2.js')
+        $.getScript('https://rawgit.com/Mailkov/SudokuJSMA/master/sudokujsma2.js')
         .done(function(){
             cells = getSudoku(level);
             squares.each(function(index) {
@@ -16,23 +17,44 @@ DDH.sudoku.build = function(ops) {
                 if (cells[nsquare] != 0) {
                     $("#C"+nsquare+" span").html(cells[nsquare++]);
                 } else {
-                    $("#C"+(nsquare++)+" span").html("<input maxlength='1'/>");
+                    $("#C"+(nsquare)+" span").html("<input class='inputsquare' maxlength='1' id='I"+(nsquare++)+"'/>");//"' onblur='verifyValue("+(nsquare++)+")'/>");
                 }
             });
+            
+            inputs = $('input.inputsquare');
+            inputs.each(function(index) {
+                $(this).on("blur", function () {
+                    verifyValue($(this));
+                });
+            });
+            
+            controlcells = initialcells;
         })  
         .fail(function() {
             var p=0;
         });       
      }
+    
+     function verifyValue(input){
+         var value = input.val();
+         var numcell = input.attr("id").substring(1);
+         if (value != "" && value != controlcells[numcell]) {
+             input.css("background-color", "red");
+             input.css("color", "white");
+         } else {
+             input.css("background-color", "white");
+             input.css("color", "black");
+         }
+     }
      
     return {
         onShow: function() {
-                if (!started) {
-                    started = true;
-                    squares = $('td.square');               
-                    start();
-                }
+            if (!started) {
+                started = true;
+                squares = $('td.square');               
+                start();
             }
+        }
     };
 }; 
     
