4bd79a4c1369518b9f654b19092db9583118e898
PJ Hampton <hampton-p1@email.ulster.ac.uk>
2017-04-28 11:43:25 +0000

    Add jasmine setup.

---
+++ spec/conversions_spec.js
@@ -0,0 +1,13 @@
+GLOBAL.DDH = {};
+
+describe("Conversions", function() {
+
+  it("should be able to add two numbers", function() {
+    expect(true).toEqual(false);
+  });
+
+  it("should be able to subtract two numbers", function() {
+    expect(true).toEqual(true);
+  });
+
+});
