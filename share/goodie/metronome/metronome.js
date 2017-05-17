997ff5cb2e9f0c0d4442f736a54facad3bdae460
M K <michaelkaufman@protonmail.ch>
2017-05-17 16:31:44 +0000

    Proof of concept.  Note: At this commit play/pause functionality is not robust and click frequency is not responding correctly to user input.

--- share/goodie/metronome/metronome.js
+++ share/goodie/metronome/metronome.js
@@ -3,24 +3,50 @@ DDH.metronome = DDH.metronome || {};
 (function(DDH) {
     "use strict";
 
-    console.log('DDH.metronome.build'); // remove this before submitting pull request
-
-    // define private variables and functions here
-    //
-    // function helper () { ... }
-    //
-    // var a = '',
-    //     b = '',
-    //     c = '';
+    DDH.metronome = DDH.metronome || {};
 
     DDH.metronome.build = function(ops) {
         console.log("ops: " + JSON.stringify(ops));
         console.log("ops.data: " + JSON.stringify(ops.data));
+
+        var metronomeIsOn,
+            clickTrack;
+
+        function bpmToMs(bpm) {
+           return 60000/bpm;
+        }
+
+        function playBeat() {
+            var clickSound = document.getElementById("audioBeat");
+
+            // Get the user specified bpm and convert to ms
+            var bpm = document.getElementById("slider").value;
+            console.log("user specified bpm is " + bpm);
+            var bpmInMs = bpmToMs(bpm);
+
+            //Play the click sound at the specified bpms
+            clickTrack = setInterval(function() {
+                clickSound.play();
+                metronomeIsOn = true;
+                console.log("every second" + bpmInMs);
+            }, bpmInMs);
+        }
+
+        function stopBeat() {
+            clearInterval(clickTrack);
+            metronomeIsOn = false;
+        }
+
+        function playOrStopBeat() {
+            metronomeIsOn ? stopBeat(): playBeat();
+        }
+
         return {
 
             // Function that executes after template content is displayed
             onShow: function() {
 
+                $( '#playButton' ).click(playOrStopBeat);
                 // define any callbacks or event handlers here
                 //
                 // var $dom = $(".zci--metronome");
