0e71ebb47517443e892e4fbf51631a7afc7dd4a9
Parth Mehta <parth@iamdifferent.io>
2017-06-02 23:11:51 +0000

    removed duplicate e+12 from test spec

--- spec/conversions_spec.js
+++ spec/conversions_spec.js
@@ -183,27 +183,27 @@ describe("Conversion of Custom Units", function() {
      */
     it("should convert radians to microarcseconds", function() {
         var conversion = stripUnit(math.eval("5rad to microarcsec"));
-        expect(conversion).toEqual("1.031e+12");
+        expect(conversion).toEqual("1.0313531353135312e+12");
     });
     
     it("should convert microarcseconds to radians", function() {
         var conversion = stripUnit(math.eval("8microarcsec to rad"));
-        expect(conversion).toEqual("3.878e-11");
+        expect(conversion).toEqual("3.8784e-11");
     });
     
     it("should convert radians to milliarcseconds", function() {
         var conversion = stripUnit(math.eval("12rad to milliarcsec"));
-        expect(conversion).toEqual("2.475e+9");
+        expect(conversion).toEqual("2.4752475247524753e+9");
     });
     
     it("should convert milliarcseconds to radians", function() {
         var conversion = stripUnit(math.eval("9milliarcsec to rad"));
-        expect(conversion).toEqual("4.363e-9");
+        expect(conversion).toEqual("4.3631999999999997e-8");
     });
     
     it("should convert radians to microradians", function() {
         var conversion = stripUnit(math.eval("4rad to microrad"));
-        expect(conversion).toEqual("4e-6");
+        expect(conversion).toEqual("4e+6");
     });
     
     it("should convert microradians to radians", function() {
@@ -213,12 +213,12 @@ describe("Conversion of Custom Units", function() {
     
     it("should convert radians to revolutions", function() {
         var conversion = stripUnit(math.eval("16rad to rev"));
-        expect(conversion).toEqual("2.547");
+        expect(conversion).toEqual("2.546473134708429");
     });
     
     it("should convert revolutions to radians", function() {
         var conversion = stripUnit(math.eval("7rev to rad"));
-        expect(conversion).toEqual("43.982");
+        expect(conversion).toEqual("43.9824");
     });
   
     /**
