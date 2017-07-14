1713e5f35999b91d61d3aa06090d8448e9da9a3a
Philip Kirkbride <kirkins@gmail.com>
2017-07-14 01:18:20 +0000

    Convert palindrome to structured, tabs, negative tests

--- lib/DDG/Goodie/Palindrome.pm
+++ lib/DDG/Goodie/Palindrome.pm
@@ -10,21 +10,34 @@ triggers any => 'palindrome';
 zci is_cached => 1;
 
 handle query => sub {
-	#Remove the trigger text from the query.
-	return unless /^(?:is\s+|)(.*?)\s+an?\s*palindrome\??$/i;
+    #Remove the trigger text from the query.
+    return unless /^(?:is\s+|)(.*?)\s+an?\s*palindrome\??$/i;
+    
+    #Return if starts with 'what is'
+    return if /^(what is*)/;
 
-	my $palindrome = $1;
+    my $palindrome = $1;
 
-	my $is_palindrome = 0;
+    my $is_palindrome = 0;
 
-	# Clean up.
-	my $clean_palindrome = lc $palindrome;
-	$clean_palindrome =~ s/[^a-z0-9]+//g;
+    # Clean up.
+    my $clean_palindrome = lc $palindrome;
+    $clean_palindrome =~ s/[^a-z0-9]+//g;
 
-	$is_palindrome = 1 if $clean_palindrome eq scalar reverse $clean_palindrome;
+    $is_palindrome = 1 if $clean_palindrome eq scalar reverse $clean_palindrome;
 
-	#Check to see if it is a palindrome.
-	return $is_palindrome ? qq("$palindrome" is a palindrome.) : qq("$palindrome" is not a palindrome.);
+    #Check to see if it is a palindrome.
+    my $title = $is_palindrome ? "Yes" : "No";
+    my $subtitle = $is_palindrome ? qq("$palindrome" is a palindrome.) : qq("$palindrome" is not a palindrome.);
+    return $subtitle, structured_answer => {
+        data => {
+            title => $title,
+            subtitle => $subtitle,
+        },
+        templates => {
+            group => 'text'
+        }
+    }
 };
 
 1;
