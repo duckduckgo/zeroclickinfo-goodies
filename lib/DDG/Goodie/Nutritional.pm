d7feda415990ddeb197c18df8b6d28157b495432
PJ Hampton <pjhampton@protonmail.com>
2017-08-25 21:57:46 +0100

    Check in work.

--- lib/DDG/Goodie/Nutritional.pm
+++ lib/DDG/Goodie/Nutritional.pm
@@ -10,16 +10,41 @@ zci is_cached => 1;
 
 triggers any => 'nutrition';
 
+my %info;
+my $data = share('data/apple.csv')->slurp;
+my @lines = split("\n", $data);
+
+my $i = 0;
+foreach(@lines) {
+  
+  if ($i == 0) {
+      my @line = split(",", $_);
+      shift @line;
+      $info{"headings"} = \@line;
+  }
+
+  if ($_ && scalar(split(",", $_)) > 1) {
+      my @entry = split(",", $_);
+      $entry[0] =~ s/^\s+|\s+$|"//g;
+      my $type = $entry[0];
+      shift @entry;
+      $info{$type} = \@entry;
+  }
+  
+  $i++;
+}
+
 handle query => sub {
 
     my $query = $_;
 
-    return 'plain text response',
+    return '',
         structured_answer => {
 
             data => {
                 title    => 'Nutritional',
                 subtitle => 'The new nutritional IA',
+                information => \%info,
             },
             templates => {
                 group => 'text',
--- share/goodie/nutritional/nutritional.js
+++ share/goodie/nutritional/nutritional.js
@@ -3,39 +3,32 @@ DDH.nutritional = DDH.nutritional || {};
 (function(DDH) {
     "use strict";
 
-    console.log('DDH.nutritional.build'); // remove this before submitting pull request
-
-    // define private variables and functions here
-    //
-    // function helper () { ... }
-    //
-    // var a = '',
-    //     b = '',
-    //     c = '';
-
     DDH.nutritional.build = function(ops) {
+
+        var headings = ops.data.information["headings"];
+        var protein = ops.data.information["Protein"];
+        console.log(ops.data.information);
 
         return {
 
             meta: {
                 sourceName: 'United States Department of Agriculture',
-                sourceUrl: 'https://source.website.com'
+                sourceUrl: 'https://ndb.nal.usda.gov/ndb/search/list?qlookup=09040'
             },
 
-            // normalize: function(item){
-            //     use this to map your 'data'
-            //     to the properties required for your chosen template
-            //
-            //     return {
-            //         title: item.myTitle,
-            //         subtitle: item.foo.subtitle
-            //     };
-            // },
+            normalize: function(item){
+            
+                return {
+                    title: "Banana",
+                    subtitle: "Fruit",
+                    headings: headings,
+                    protein: "1.1g",
+                    calories: 89,
+                };
+            },
             
             onShow: function() {
 
-                // define any callbacks or event handlers here
-                //
                 // var $dom = $(".zci--nutritional");
                 // $dom.find(".my-special-class").click(function(){
                 //
