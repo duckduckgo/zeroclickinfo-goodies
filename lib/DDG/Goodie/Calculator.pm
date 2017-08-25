750dc498132f96276b4f69ea308354bdd156b981
PJ Hampton <pjhampton@protonmail.com>
2017-08-25 17:47:24 +0100

    Fix overtriggering.

--- lib/DDG/Goodie/Calculator.pm
+++ lib/DDG/Goodie/Calculator.pm
@@ -195,6 +195,7 @@ handle query => sub {
     return if $query =~ m/^\)|\($/; # shouldn't open with a closing brace or finish with an opening brace
     return if $query =~ m/(a?cosh?|tau|a?sin|a?tan|log|ln|exp|tanh|cbrt|cubed?)e?$/i; # stops empty functions at end or with <func>e
     return if $query =~ m#(?:x(\^|/)|(\^|/)x)#; # stops triggering on what is most likely algebra
+    return if $query =~ m#^0\d+/\d+$#;
 
     # some shallow preprocessing of the query
     $query =~ s/^(?:what is|calculat(e|or)|solve|math)//i;
