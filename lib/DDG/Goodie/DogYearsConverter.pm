f3229da7648c351f12760d0ed3ce2fb38a53d7ea
Mitchell Kossoris <mkossoris@gmail.com>
2017-08-26 07:04:31 +0000

    Updated tests so they now pass and fixed previous issue with incorrect arguments

--- lib/DDG/Goodie/DogYearsConverter.pm
+++ lib/DDG/Goodie/DogYearsConverter.pm
@@ -12,24 +12,24 @@ zci is_cached => 1;
 triggers end => 'dog years';
 
 sub dog2human {
-    return $_/7;
+    return (shift)/7;
 }
 
 sub human2dog {
-    return $_*7;
+    return (shift)*7;
 }
 
 handle remainder => sub {
     my ($input, $from, $to, $result);
 
     #From dog years to human years
-    if (/^([\d]+)(((\s)|(\s+(years|year))|(\s+human+\s+(years|year)+\s))+from)?$/) {
+    if (/^(\d+)(((\s)|(\s+(years|year))|(\s+human+\s+(years|year)+\s))+from)?$/) {
         $input = $1;
         $from = "Dog Years";
         $to = "Human Years";
         $result = dog2human($1);
     #From human years to dog years
-    } elsif (/^([\d]+)(((\s)|(\s+(years|year)+\s)|(\s+human+\s+(years|year)+\s))+(in|to|into|as))?$/) {
+    } elsif (/^(\d+)(((\s)|(\s+(years|year)+\s)|(\s+human+\s+(years|year)+\s))+(in|to|into|as))?$/) {
         $input = $1;
         $from = "Human Years";
         $to = "Dog Years";
