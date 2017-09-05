781856b4e01f11b355c7267a86488b2b9a197f4d
Zach Thompson <zachthompson@users.noreply.github.com>
2017-09-05 15:43:43 -0600

    SolarSystem: Use unicode superscripts, multiplication symbol (#4050)
    * Use unicode superscripts, multiplication symbol
    
    * Update tests with new unicode
    
    * Put trans in sub

--- lib/DDG/Goodie/SolarSystem.pm
+++ lib/DDG/Goodie/SolarSystem.pm
@@ -5,6 +5,7 @@ use DDG::Goodie;
 use YAML::XS 'LoadFile';
 use POSIX;
 use Text::Trim;
+use utf8;
 use strict;
 
 zci answer_type => "solarsystem";
@@ -75,15 +76,17 @@ handle query_lc => sub {
     # Superscript for km3, mi3, km2 or mi2 
     if($result =~ m/(km|mi)(\d)/) {
         my ($symbol, $superscript) = ($1, $2);
-        $result =~ s/$symbol$superscript/$symbol<sup>$superscript<\/sup>/;
+        my $unisuper = unicode_superscript($superscript);
+        $result =~ s/$symbol$superscript/$symbol$unisuper/;
     }
     
     # Superscript for scientific notation
-    # Convert x to HTML entity &times;
+    # Convert x to unicode
     if($result =~ m/x\s(10)(\d\d)/) {
         my ($number, $exponent) = ($1, $2);
-        $result =~ s/$number$exponent/$number<sup>$exponent<\/sup>/;
-        $result =~ s/x/&times;/;
+        my $uniexp = unicode_superscript($exponent);
+        $result =~ s/$number$exponent/$number$uniexp/;
+        $result =~ s/x/×/;
     }
 
     #$saturn var is provided to handlebars template to set size of image
@@ -112,4 +115,6 @@ handle query_lc => sub {
         }
     };
 };
+
+sub unicode_superscript { return $_[0] =~ tr/0123456789/⁰ⁱ²³⁴⁵⁶⁷⁸⁹/r }
 1;
